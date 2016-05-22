2a9e69bd9550d8647614b075d704652bd0d2a5ed
Lukas Stabe <lukas@stabe.de>
2016-05-23 00:06:53 +0200

    add captureError

---
+++ Sources/Sentry+NSError.swift
@@ -0,0 +1,58 @@
+//
+//  Sentry+NSError.swift
+//  SentrySwift
+//
+//  Created by Lukas Stabe on 22.05.16.
+//
+//
+
+import Foundation
+
+
+private func cleanValue(v: AnyObject) -> AnyObject? {
+    switch v {
+    case is NSNumber: fallthrough
+    case is NSString: fallthrough
+    case is NSNull:
+        return v
+
+    case let v as [String: AnyObject]:
+        return cleanDict(v)
+
+    case let v as [AnyObject]:
+        return v.flatMap(cleanValue)
+
+    case let v as NSURL:
+        return v.absoluteString
+
+    default:
+        return nil
+    }
+}
+
+private func cleanDict(d: [String: AnyObject]) -> [String: AnyObject] {
+    var ret = [String: AnyObject]()
+
+    for (k, v) in d {
+        guard let c = cleanValue(v) else { continue }
+        ret[k] = c
+    }
+
+    return ret
+}
+
+extension SentryClient {
+
+    public func captureError(error: NSError, function: String = #function, file: String = #file, line: Int = #line) {
+        let culprit = "\((file as NSString).lastPathComponent):\(line) \(function)"
+        let message = "\(error.domain).\(error.code) in \(culprit)"
+
+        let event = Event.build(message) {
+            $0.level = .Error
+            $0.culprit = culprit
+            $0.extra = ["user_info": cleanValue(error.userInfo as! [String: AnyObject])!]
+        }
+
+        captureEvent(event)
+    }
+}
