5492833a9487d7efda26f8dd69491c55db17216e
Benjamin Horsleben <fizker@users.noreply.github.com>
2016-06-16 03:25:58 +0200

    Pass release on to the sentry-server (#37)
    * Added support for passing the release-version along to the server
    
    * Added some basic tests regarding setting the releaseVersion on the SentryClient

---
+++ SentrySwiftTests/SentryClientTests.swift
@@ -0,0 +1,65 @@
+//
+//  SentryClientTests.swift
+//  SentrySwift
+//
+//  Created by Benjamin Horsleben on 16/06/16.
+//
+//
+
+import Foundation
+import XCTest
+@testable import SentrySwift
+
+class TestCrashHandler:CrashHandler {
+	required init(client: SentryClient) {
+	}
+
+	var crashReportingHasStarted = false
+	func startCrashReporting() {
+		crashReportingHasStarted = true
+	}
+
+	var breadcrumbsSerialized: BreadcrumbStore.SerializedType?
+	var releaseVersion: String?
+	var tags: EventTags = [:]
+	var extra: EventExtra = [:]
+	var user: User?
+}
+
+class TestableSentryClient: SentryClient {
+	var dataToSend:[(data:NSData, callback:EventFinishedSending?)] = []
+	override func sendData(data: NSData, finished: EventFinishedSending?) {
+		// Do nothing. The original sends data to the server
+
+		dataToSend.append((data, finished))
+	}
+}
+
+class SentryClientTests: XCTestCase {
+	var client:SentryClient!
+	override func setUp() {
+		super.setUp()
+
+		let dsn = DSN(dsn: NSURL(), serverURL: NSURL(), publicKey: nil, secretKey: nil, projectID: "some project")
+		client = TestableSentryClient(dsn: dsn)
+	}
+
+	func test_setReleaseVersion_crashHandlerIsPresent_releaseVersionIsPassedOnToCrashHandler() {
+		let crashHandler = TestCrashHandler(client: client)
+		client.crashHandler = crashHandler
+
+		let releaseVersion = "1.2.3"
+		client.releaseVersion = releaseVersion
+
+		XCTAssertEqual(releaseVersion, client.releaseVersion)
+		XCTAssertEqual(releaseVersion, crashHandler.releaseVersion)
+	}
+
+	func test_setReleaseVersion_crashHandlerIsNotSet_releaseVersionIsOnlySetOnClient() {
+		let releaseVersion = "1.2.3"
+		client.releaseVersion = releaseVersion
+
+		XCTAssertEqual(releaseVersion, client.releaseVersion)
+		XCTAssertNil(client.crashHandler)
+	}
+}
