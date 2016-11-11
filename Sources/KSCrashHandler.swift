ebd247475ce049f606d96ed1f2abd904f4182af5
Daniel Griesser <daniel.griesser.86@gmail.com>
2016-11-11 13:33:08 +0100

    Fix indentation

--- Sources/KSCrashHandler.swift
+++ Sources/KSCrashHandler.swift
@@ -10,9 +10,9 @@ import KSCrash
 import Foundation
 
 extension SentryClient {
-	public func startCrashHandler() {
-		crashHandler = KSCrashHandler(client: self)
-	}
+    public func startCrashHandler() {
+        crashHandler = KSCrashHandler(client: self)
+    }
 }
 
 internal typealias CrashDictionary = [String: AnyType]
@@ -25,118 +25,118 @@ internal let keyReleaseVersion = "releaseVersion_serialized"
 
 /// A class to report crashes to Sentry built upon KSCrash
 internal class KSCrashHandler: CrashHandler {
-
-	// MARK: - Attributes
-
-	private var installation: KSCrashSentryInstallation
-	
-	private var lock = NSObject()
-	private var isInstalled = false
-
-	// MARK: - EventProperties
-
-	internal var releaseVersion: String? {
-		didSet { updateUserInfo() }
-	}
-	internal var tags: EventTags = [:] {
-		didSet { updateUserInfo() }
-	}
-	internal var extra: EventExtra = [:] {
-		didSet { updateUserInfo() }
-	}
-	internal var user: User? {
-		didSet { updateUserInfo() }
-	}
-
-	required init(client: SentryClient) {
+    
+    // MARK: - Attributes
+    
+    private var installation: KSCrashSentryInstallation
+    
+    private var lock = NSObject()
+    private var isInstalled = false
+    
+    // MARK: - EventProperties
+    
+    internal var releaseVersion: String? {
+        didSet { updateUserInfo() }
+    }
+    internal var tags: EventTags = [:] {
+        didSet { updateUserInfo() }
+    }
+    internal var extra: EventExtra = [:] {
+        didSet { updateUserInfo() }
+    }
+    internal var user: User? {
+        didSet { updateUserInfo() }
+    }
+    
+    required init(client: SentryClient) {
         installation = KSCrashSentryInstallation(client: client)
-	}
-
-	// MARK: - CrashHandler
-
-	internal var breadcrumbsSerialized: BreadcrumbStore.SerializedType? {
-		didSet { updateUserInfo() }
-	}
-
-	/*
-	Starts the crash reporting and sends any previously saved crash reports
-	- Parameter createdEvent: A closure that passes in a created event
-	*/
-	internal func startCrashReporting() {
-		// Sychrnoizes this function
-		objc_sync_enter(lock)
-		defer { objc_sync_exit(lock) }
-
-		// Return out if already installed
-		if isInstalled { return }
-		isInstalled = true
-		
-		// Install
-		installation.install()
+    }
+    
+    // MARK: - CrashHandler
+    
+    internal var breadcrumbsSerialized: BreadcrumbStore.SerializedType? {
+        didSet { updateUserInfo() }
+    }
+    
+    /*
+     Starts the crash reporting and sends any previously saved crash reports
+     - Parameter createdEvent: A closure that passes in a created event
+     */
+    internal func startCrashReporting() {
+        // Sychrnoizes this function
+        objc_sync_enter(lock)
+        defer { objc_sync_exit(lock) }
         
-    SentryLog.Debug.log("Started Sentry Client \(SentryClient.versionString)")
+        // Return out if already installed
+        if isInstalled { return }
+        isInstalled = true
         
-		// Maps KSCrash reports in `Events`
-		#if swift(>=3.0)
-			installation.sendAllReports() { (filteredReports, completed, error) -> Void in
-				SentryLog.Debug.log("Sent \(filteredReports?.count) report(s)")
-			}
-		#else
-			installation.sendAllReportsWithCompletion() { (filteredReports, completed, error) -> Void in
-				SentryLog.Debug.log("Sent \(filteredReports.count) report(s)")
-			}
-		#endif
-	}
-
-
-	// MARK: - Private Helpers
-
-	private func updateUserInfo() {
-		var userInfo = CrashDictionary()
-		userInfo[keyEventTags] = tags
-		userInfo[keyEventExtra] = extra
-		userInfo[keyReleaseVersion] = releaseVersion
-
-		if let user = user?.serialized {
-			userInfo[keyUser] = user
-		}
-
-		if let breadcrumbsSerialized = breadcrumbsSerialized {
-			userInfo[keyBreadcrumbsSerialized] = breadcrumbsSerialized
-		}
-
-		KSCrash.sharedInstance().userInfo = userInfo
-	}
-
+        // Install
+        installation.install()
+        
+        SentryLog.Debug.log("Started Sentry Client \(SentryClient.versionString)")
+        
+        // Maps KSCrash reports in `Events`
+        #if swift(>=3.0)
+            installation.sendAllReports() { (filteredReports, completed, error) -> Void in
+            SentryLog.Debug.log("Sent \(filteredReports?.count) report(s)")
+            }
+        #else
+            installation.sendAllReportsWithCompletion() { (filteredReports, completed, error) -> Void in
+                SentryLog.Debug.log("Sent \(filteredReports.count) report(s)")
+            }
+        #endif
+    }
+    
+    
+    // MARK: - Private Helpers
+    
+    private func updateUserInfo() {
+        var userInfo = CrashDictionary()
+        userInfo[keyEventTags] = tags
+        userInfo[keyEventExtra] = extra
+        userInfo[keyReleaseVersion] = releaseVersion
+        
+        if let user = user?.serialized {
+            userInfo[keyUser] = user
+        }
+        
+        if let breadcrumbsSerialized = breadcrumbsSerialized {
+            userInfo[keyBreadcrumbsSerialized] = breadcrumbsSerialized
+        }
+        
+        KSCrash.sharedInstance().userInfo = userInfo
+    }
+    
 }
 
 private class KSCrashSentryInstallation: KSCrashInstallation {
-	
-	private let client: SentryClient
+    
+    private let client: SentryClient
     
     init(client: SentryClient) {
         self.client = client
-		super.init(requiredProperties: [])
-	}
-	
-	override func sink() -> KSCrashReportFilter! {
+        super.init(requiredProperties: [])
+    }
+    
+    override func sink() -> KSCrashReportFilter! {
         return KSCrashReportSinkSentry(client: client)
-	}
-	
+    }
+    
 }
 
 private class KSCrashReportSinkSentry: NSObject, KSCrashReportFilter {
-	
-	private let client: SentryClient
-	
-	init(client: SentryClient) {
-		self.client = client
-		super.init()
-	}
-	
-	@objc func filterReports(_ reports: [AnyType]!, onCompletion: KSCrashReportFilterCompletion!) {
+    
+    private let client: SentryClient
+    
+    init(client: SentryClient) {
+        self.client = client
+        super.init()
+    }
+    
+    @objc func filterReports(_ reports: [AnyType]!, onCompletion: KSCrashReportFilterCompletion!) {
         #if swift(>=3.0)
-        DispatchQueue.global(qos: .background).async {
+            DispatchQueue.global(qos: .background).async {
             // Mapping reports
             let events: [Event] = reports?
             .flatMap({$0 as? CrashDictionary})
@@ -144,7 +144,7 @@ private class KSCrashReportSinkSentry: NSObject, KSCrashReportFilter {
             
             // Sends events recursively
             self.sendEvent(reports, events: events, success: true, onCompletion: onCompletion)
-        }
+            }
         #else
             let qualityOfServiceClass = QOS_CLASS_BACKGROUND
             let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
@@ -158,22 +158,22 @@ private class KSCrashReportSinkSentry: NSObject, KSCrashReportFilter {
                 self.sendEvent(reports, events: events, success: true, onCompletion: onCompletion)
             })
         #endif
-	}
-	
+    }
+    
     // TODO: move to seperate class
-	private func sendEvent(_ reports: [AnyType]!, events allEvents: [Event], success: Bool, onCompletion: KSCrashReportFilterCompletion!) {
-		var events = allEvents
-		
-		// Complete when no more
-		guard let event = events.popLast() else {
-			onCompletion(reports, success, nil)
-			return
-		}
-		
-		// Send event
-		client.captureEvent(event, useClientProperties: true) { [weak self] eventSuccess in
-			self?.sendEvent(reports, events: events, success: success && eventSuccess, onCompletion: onCompletion)
-		}
-	}
-	
+    private func sendEvent(_ reports: [AnyType]!, events allEvents: [Event], success: Bool, onCompletion: KSCrashReportFilterCompletion!) {
+        var events = allEvents
+        
+        // Complete when no more
+        guard let event = events.popLast() else {
+            onCompletion(reports, success, nil)
+            return
+        }
+        
+        // Send event
+        client.captureEvent(event, useClientProperties: true) { [weak self] eventSuccess in
+            self?.sendEvent(reports, events: events, success: success && eventSuccess, onCompletion: onCompletion)
+        }
+    }
+    
 }
