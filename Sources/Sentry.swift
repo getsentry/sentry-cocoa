856fb6634388f230e77bf51d1c7325a2597ec85e
Daniel Griesser <daniel.griesser.86@gmail.com>
2016-12-12 14:57:22 +0100

    Release 1.3.0

--- Sources/Sentry.swift
+++ Sources/Sentry.swift
@@ -14,84 +14,84 @@ import KSCrash
 
 // This is declared here to keep namespace compatibility with objc
 @objc public enum SentryLog: Int, CustomStringConvertible {
-	case None, Error, Debug
-
-	public var description: String {
-		switch self {
-		case .None: return ""
-		case .Error: return "Error"
-		case .Debug: return "Debug"
-		}
-	}
-
-	internal func log(_ message: String) {
-		guard rawValue <= SentryClient.logLevel.rawValue else { return }
-		print("SentrySwift - \(description):: \(message)")
-	}
+    case None, Error, Debug
+    
+    public var description: String {
+        switch self {
+        case .None: return ""
+        case .Error: return "Error"
+        case .Debug: return "Debug"
+        }
+    }
+    
+    internal func log(_ message: String) {
+        guard rawValue <= SentryClient.logLevel.rawValue else { return }
+        print("SentrySwift - \(description):: \(message)")
+    }
 }
 
 #if swift(>=3.0)
-	public typealias AnyType = Any
+    public typealias AnyType = Any
 #else
-	public typealias AnyType = AnyObject
-	internal typealias Error = ErrorType
-	internal typealias ProcessInfo = NSProcessInfo
-	internal typealias JSONSerialization = NSJSONSerialization
-	internal typealias Bundle = NSBundle
+    public typealias AnyType = AnyObject
+    internal typealias Error = ErrorType
+    internal typealias ProcessInfo = NSProcessInfo
+    internal typealias JSONSerialization = NSJSONSerialization
+    internal typealias Bundle = NSBundle
     internal typealias URLQueryItem = NSURLQueryItem
 #endif
 
 internal enum SentryError: Error {
-	case InvalidDSN
+    case InvalidDSN
 }
 
 #if os(iOS)
-@objc public protocol SentryClientUserFeedbackDelegate {
-    func userFeedbackReady()
-    func userFeedbackSent()
-}
+    @objc public protocol SentryClientUserFeedbackDelegate {
+        func userFeedbackReady()
+        func userFeedbackSent()
+    }
 #endif
 
 @objc public class SentryClient: NSObject, EventProperties {
-
-	// MARK: - Static Attributes
-	
-	public static var shared: SentryClient?
-	public static var logLevel: SentryLog = .None
+    
+    // MARK: - Static Attributes
+    
+    public static var shared: SentryClient?
+    public static var logLevel: SentryLog = .None
     
     public static var versionString: String {
         return "\(Info.version) (\(Info.sentryVersion))"
     }
     
     internal static let queueName = "io.sentry.event.queue"
-
-	// MARK: - Enums
-
-	internal struct Info {
-		static let version: String = "1.2.0"
-		static let sentryVersion: Int = 7
-	}
     
-	// MARK: - Attributes
-	
-	internal let dsn: DSN
-	internal(set) var crashHandler: CrashHandler? {
-		didSet {
-			crashHandler?.startCrashReporting()
-			crashHandler?.releaseVersion = releaseVersion
-			crashHandler?.tags = tags
-			crashHandler?.extra = extra
-			crashHandler?.user = user
-		}
-	}
-	
-	public lazy var breadcrumbs: BreadcrumbStore = {
-		let store = BreadcrumbStore()
-		store.storeUpdated = {
-			self.crashHandler?.breadcrumbsSerialized = $0.serialized
-		}
-		return store
-	}()
+    // MARK: - Enums
+    
+    internal struct Info {
+        static let version: String = "1.3.0"
+        static let sentryVersion: Int = 7
+    }
+    
+    // MARK: - Attributes
+    
+    internal let dsn: DSN
+    internal(set) var crashHandler: CrashHandler? {
+        didSet {
+            crashHandler?.startCrashReporting()
+            crashHandler?.releaseVersion = releaseVersion
+            crashHandler?.tags = tags
+            crashHandler?.extra = extra
+            crashHandler?.user = user
+        }
+    }
+    
+    public lazy var breadcrumbs: BreadcrumbStore = {
+        let store = BreadcrumbStore()
+        store.storeUpdated = {
+            self.crashHandler?.breadcrumbsSerialized = $0.serialized
+        }
+        return store
+    }()
     
     internal var stacktraceSnapshot: Event.StacktraceSnapshot?
     
@@ -113,72 +113,72 @@ internal enum SentryError: Error {
                 }
             #else
                 dispatch_async(dispatch_get_main_queue(), {
-                    self.delegate?.userFeedbackReady()
+                self.delegate?.userFeedbackReady()
                 })
             #endif
         }
     }
     #endif
-
-	// MARK: EventProperties
-
-	public var releaseVersion: String? {
-		didSet { crashHandler?.releaseVersion = releaseVersion }
-	}
-	public var tags: EventTags = [:] {
-		didSet { crashHandler?.tags = tags }
-	}
-	public var extra: EventExtra = [:] {
-		didSet { crashHandler?.extra = extra }
-	}
-	public var user: User? = nil {
-		didSet { crashHandler?.user = user }
-	}
-
+    
+    // MARK: EventProperties
+    
+    public var releaseVersion: String? {
+        didSet { crashHandler?.releaseVersion = releaseVersion }
+    }
+    public var tags: EventTags = [:] {
+        didSet { crashHandler?.tags = tags }
+    }
+    public var extra: EventExtra = [:] {
+        didSet { crashHandler?.extra = extra }
+    }
+    public var user: User? = nil {
+        didSet { crashHandler?.user = user }
+    }
+    
     public typealias EventBeforeSend = (inout Event) -> ()
     /// Use this block to get the event that will be send with the next
     public var beforeSendEventBlock: EventBeforeSend?
-
-	/// Creates a Sentry object to use for reporting
-	internal init(dsn: DSN) {
-		self.dsn = dsn
-		
-		#if swift(>=3.0)
-			self.releaseVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-		#else
-			self.releaseVersion = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as? String
-		#endif
-		
-		super.init()
+    
+    /// Creates a Sentry object to use for reporting
+    internal init(dsn: DSN) {
+        self.dsn = dsn
+        
+        #if swift(>=3.0)
+            self.releaseVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        #else
+            self.releaseVersion = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as? String
+        #endif
+        
+        super.init()
         sendEventsOnDiskInBackground()
-	}
-	
-	/// Creates a Sentry object iff a valid DSN is provided
-	@objc public convenience init?(dsnString: String) {
-		// Silently not creating a client if dsnString is empty string
-		if dsnString.isEmpty {
-			SentryLog.Debug.log("DSN provided was empty - not creating a SentryClient object")
-			return nil
-		}
-		
-		// Try to create a client with a DSN string
-		// Log error if cannot make one
-		do {
-			let dsn = try DSN(dsnString)
-			self.init(dsn: dsn)
-		} catch SentryError.InvalidDSN {
-			SentryLog.Error.log("DSN is invalid")
-			return nil
-		} catch {
-			SentryLog.Error.log("DSN is invalid")
-			return nil
-		}
-	}
-	
+    }
+    
+    /// Creates a Sentry object iff a valid DSN is provided
+    @objc public convenience init?(dsnString: String) {
+        // Silently not creating a client if dsnString is empty string
+        if dsnString.isEmpty {
+            SentryLog.Debug.log("DSN provided was empty - not creating a SentryClient object")
+            return nil
+        }
+        
+        // Try to create a client with a DSN string
+        // Log error if cannot make one
+        do {
+            let dsn = try DSN(dsnString)
+            self.init(dsn: dsn)
+        } catch SentryError.InvalidDSN {
+            SentryLog.Error.log("DSN is invalid")
+            return nil
+        } catch {
+            SentryLog.Error.log("DSN is invalid")
+            return nil
+        }
+    }
+    
     /*
      Captures current stracktrace of the thread and stores it in internal var stacktraceSnapshot
      Use event.fetchStacktrace() to fill your event with this stacktrace
-    */
+     */
     @objc public func snapshotStacktrace() {
         guard let crashHandler = crashHandler else {
             SentryLog.Error.log("crashHandler not yet initialized")
@@ -188,17 +188,17 @@ internal enum SentryError: Error {
         crashHandler.sendAllReports()
     }
     
-	/*
-	Reports message to Sentry with the given level
-	- Parameter message: The message to send to Sentry
-	- Parameter level: The severity of the message
-	*/
-	@objc public func captureMessage(_ message: String, level: SentrySeverity = .Info) {
+    /*
+     Reports message to Sentry with the given level
+     - Parameter message: The message to send to Sentry
+     - Parameter level: The severity of the message
+     */
+    @objc public func captureMessage(_ message: String, level: SentrySeverity = .Info) {
         self.captureEvent(Event(message, level: level))
-	}
-
-	/// Reports given event to Sentry
-	@objc public func captureEvent(_ event: Event) {
+    }
+    
+    /// Reports given event to Sentry
+    @objc public func captureEvent(_ event: Event) {
         #if swift(>=3.0)
             DispatchQueue(label: SentryClient.queueName).sync {
                 self.captureEvent(event, useClientProperties: true)
@@ -208,7 +208,7 @@ internal enum SentryError: Error {
                 self.captureEvent(event, useClientProperties: true)
             })
         #endif
-	}
+    }
     
     #if os(iOS)
     @objc public func enableAutomaticBreadcrumbTracking() {
@@ -239,7 +239,7 @@ internal enum SentryError: Error {
             let frameworkBundle = NSBundle(forClass: self.dynamicType)
             bundle = frameworkBundle
             if let bundleURL = frameworkBundle.URLForResource("storyboards", withExtension: "bundle") {
-                bundle = NSBundle(URL: bundleURL) 
+                bundle = NSBundle(URL: bundleURL)
             }
         #endif
         
@@ -282,47 +282,47 @@ internal enum SentryError: Error {
     
     #endif
     
-	/*
-	Reports given event to Sentry
-	- Parameter event: An event struct
-	- Parameter useClientProperties: Should the client's user, tags and extras also be reported (default is `true`)
-	*/
-	internal func captureEvent(_ event: Event, useClientProperties: Bool = true, completed: SentryEndpointRequestFinished? = nil) {
-		// Don't allow client attributes to be used when reporting an `Exception`
-		if useClientProperties {
-			event.user = event.user ?? user
-			event.releaseVersion = event.releaseVersion ?? releaseVersion
-
-			if JSONSerialization.isValidJSONObject(tags) {
-				event.tags.unionInPlace(tags)
-			}
-
-			if JSONSerialization.isValidJSONObject(extra) {
-				event.extra.unionInPlace(extra)
-			}
-		}
-
+    /*
+     Reports given event to Sentry
+     - Parameter event: An event struct
+     - Parameter useClientProperties: Should the client's user, tags and extras also be reported (default is `true`)
+     */
+    internal func captureEvent(_ event: Event, useClientProperties: Bool = true, completed: SentryEndpointRequestFinished? = nil) {
+        // Don't allow client attributes to be used when reporting an `Exception`
+        if useClientProperties {
+            event.user = event.user ?? user
+            event.releaseVersion = event.releaseVersion ?? releaseVersion
+            
+            if JSONSerialization.isValidJSONObject(tags) {
+                event.tags.unionInPlace(tags)
+            }
+            
+            if JSONSerialization.isValidJSONObject(extra) {
+                event.extra.unionInPlace(extra)
+            }
+        }
+        
         if nil == event.breadcrumbsSerialized { // we only want to set the breadcrumbs if there are non in the event
             event.breadcrumbsSerialized = breadcrumbs.serialized
         }
         breadcrumbs.clear()
         
-		sendEvent(event) { [weak self] success in
-			completed?(success)
-			guard !success else {
+        sendEvent(event) { [weak self] success in
+            completed?(success)
+            guard !success else {
                 #if os(iOS)
-                if event.level == .Fatal {
-                    self?.lastSuccessfullySentEvent = event
-                }
+                    if event.level == .Fatal {
+                        self?.lastSuccessfullySentEvent = event
+                    }
                 #endif
                 return
             }
-			self?.saveEvent(event)
-		}
+            self?.saveEvent(event)
+        }
         
         // In the end we check if there are any events still stored on disk and send them
         sendEventsOnDiskInBackground()
-	}
+    }
     
     /// Sends events that are stored on disk to the server
     private func sendEventsOnDiskInBackground() {
@@ -336,16 +336,16 @@ internal enum SentryError: Error {
             })
         #endif
     }
-
-	/// Attempts to send all events that are saved on disk
-	private func sendEventsOnDisk() {
-		let events = savedEvents()
-		
-		for savedEvent in events {
+    
+    /// Attempts to send all events that are saved on disk
+    private func sendEventsOnDisk() {
+        let events = savedEvents()
+        
+        for savedEvent in events {
             sendEvent(savedEvent) { success in
-				guard success else { return }
-				savedEvent.deleteEvent()
-			}
-		}
-	}
+                guard success else { return }
+                savedEvent.deleteEvent()
+            }
+        }
+    }
 }
--- release-checklist.md
+++ release-checklist.md
@@ -3,9 +3,10 @@
   - [ ] Bump version in podspec file
   - [ ] Bump version in framework xcode targets
   - [ ] Bump version in `Sentry.swift`
-  - [ ] Ensure all needed changes are checked in the master branch
+  - [ ] Bump version in `docs/sentry-doc-config.json`
   - [ ] Run `make release`
   - [ ] `pod spec lint` and add new version to cocoapods repo
+  - [ ] Ensure all needed changes are checked in the master branch
   - [ ] Push new podspec version to master repo `pod trunk push SentrySwift.podspec --allow-warnings`
   - [ ] Create a version tag
   - [ ] Update sentry-docs
