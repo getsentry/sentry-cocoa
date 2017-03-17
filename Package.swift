8dbd4228953279400baa18096e72e9a54d5d6cd9
Josh Holtz <josh@rokkincat.com>
2017-03-17 15:49:36 -0500

    Vapor support added to its own module

--- Package.swift
+++ Package.swift
@@ -1,8 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "sentry",
-    dependencies : [],
+    name: "Sentry",
+    dependencies : [
+      .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+    ],
     exclude: [
         "Sources/Sentry/KSCrash",
         "Sources/Sentry/ios"
