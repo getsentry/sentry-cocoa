52a7cf63df9f633df4fb6fbe0658511d9aa0aa36
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-05-04 15:14:04 +0200

    Remove xcode build because fastline does it already

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 if [ "$LANE" = "test" ];
 then
-    xcodebuild -project Sentry.xcodeproj -scheme SentryTests build test -sdk iphonesimulator GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
     bash <(curl -s https://codecov.io/bash) -J '^Swift$'
-elif [ "$LANE" = "do_cocoapods" ];
+elif [ "$LANE" = "test_swift" ];
 then
-    xcodebuild -project Sentry.xcodeproj -scheme SentrySwiftTests build test -sdk iphonesimulator GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
     bash <(curl -s https://codecov.io/bash) -J '^SentrySwift$'
 fi
