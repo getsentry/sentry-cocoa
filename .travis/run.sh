f50548bd6f65aa21a0858bbeafa692c69bf70d98
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-05-18 14:03:21 +0200

    Add reset simulators

--- .travis/run.sh
+++ .travis/run.sh
@@ -13,10 +13,10 @@ then
 elif [ "$LANE" = "test" ];
 then
     gem install slather
-    fastlane reset_simulators
+    fastlane snapshot reset_simulators
 elif [ "$LANE" = "test_swift" ];
 then
     gem install slather
-    fastlane reset_simulators
+    fastlane snapshot reset_simulators
 fi
 fastlane $LANE;
