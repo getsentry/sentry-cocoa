1a3b6ae433adff6839dad97c0fdaa5557a2d7588
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-06-05 19:07:01 +0200

    Bump version to 3.0.3

--- Sources/Sentry/SentryClient.m
+++ Sources/Sentry/SentryClient.m
@@ -46,7 +46,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-NSString *const SentryClientVersionString = @"3.0.2";
+NSString *const SentryClientVersionString = @"3.0.3";
 NSString *const SentryClientSdkName = @"sentry-cocoa";
 
 static SentryClient *sharedClient = nil;
