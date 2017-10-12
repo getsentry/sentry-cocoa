a75f5734f21f92a71f23a432b92528e5c94e2d6e
Jan Michael Auer <jan.auer@sentry.io>
2017-10-12 10:52:41 +0200

    meta(travis): Move coverage reports to run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,23 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+
 gem install fastlane
 
-if [ "$LANE" = "lint" ];
-then
-    if [ "$TRAVIS_PULL_REQUEST" != "false" ];
-    then
+if [ "$LANE" = "lint" ]; then
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
         echo "We don't run linter for PRs, because Danger!"
         exit 0;
     fi
+
     gem install danger
     gem install danger-swiftlint
-    brew update
+    brew update > /dev/null
     brew outdated swiftlint || brew upgrade swiftlint
-elif [ "$LANE" = "pod" ];
-then
+elif [ "$LANE" = "pod" ]; then
     gem install cocoapods
     pod repo update
-elif [ "$LANE" = "test" ];
-then
-    gem install slather
 fi
+
 fastlane $LANE
+
+if [ "$LANE" = "test" ]; then
+    gem install slather
+    slather coverage --scheme Sentry && bash <(curl -s https://codecov.io/bash) -f cobertura.xml;
+fi
