55de5de24f9fbbbc282f23ec646fe0a974ce39f7
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-10-17 15:40:18 +0200

    fix: travis.yml for probot-release

--- .travis/stage-carthage.sh
+++ .travis/stage-carthage.sh
@@ -6,3 +6,5 @@ brew outdated carthage || brew upgrade carthage
 cd KSCrash
 carthage build --no-skip-current
 carthage archive --output Sentry.framework.zip
+mkdir ../build
+mv Sentry.framework.zip ../build
