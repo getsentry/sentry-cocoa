67770fc6d1ff3cc1013832ef203680d2a5439caa
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-10-23 17:34:44 +0200

    fix: Add bridge to static lib

--- Tests/SentryTests/SentryJavaScriptBridgeHelperTests.m
+++ Tests/SentryTests/SentryJavaScriptBridgeHelperTests.m
@@ -53,13 +53,4 @@
     XCTAssertEqual([SentryJavaScriptBridgeHelper sentrySeverityFromLevel:@"warning"], kSentrySeverityWarning);
 }
 
-- (void)
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
