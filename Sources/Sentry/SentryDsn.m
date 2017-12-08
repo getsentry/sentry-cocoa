5394808b9db0c343569a41f186e0ff7062c4316e
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-12-08 23:14:31 +0100

    ci: Update to new xcode9.2 image

--- Sources/Sentry/SentryDsn.m
+++ Sources/Sentry/SentryDsn.m
@@ -42,7 +42,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (NSString *)getHash {
     NSData *data = [[self.url absoluteString] dataUsingEncoding:NSUTF8StringEncoding];
     uint8_t digest[CC_SHA1_DIGEST_LENGTH];
-    CC_SHA1(data.bytes, data.length, digest);
+    CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
     NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
     for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
         [output appendFormat:@"%02x", digest[i]];
