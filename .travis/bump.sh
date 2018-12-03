c0094870d93a3ef3ef06bc274fcca3c1dbfe11b0
Daniel Griesser <daniel.griesser.86@gmail.com>
2018-12-03 13:43:47 +0100

    fix: Scritp

--- .travis/bump.sh
+++ .travis/bump.sh
@@ -3,10 +3,11 @@ set -eux
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 
-NEW_VERSION="${1}"
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
 
 echo "--> Clean VersionBump"
 cd Utils/VersionBump && swift build
 cd $SCRIPT_DIR/..
-echo "--> Bumping version to ${NEW_VERSION}"
+echo "--> Bumping version to ${OLD_VERSION} ${NEW_VERSION}"
 ./Utils/VersionBump/.build/debug/VersionBump ${NEW_VERSION}
