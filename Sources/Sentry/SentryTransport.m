a5439d3e5ba763a6e3aac24ba49e671b715553f9
Klemens Mantzos <klemens@fetzig.at>
2019-11-27 13:45:42 +0100

    wip: adds SentryTransport

---
+++ Sources/Sentry/SentryTransport.m
@@ -0,0 +1,13 @@
+//
+//  SentryTransport.m
+//  Sentry
+//
+//  Created by Klemens Mantzos on 27.11.19.
+//  Copyright © 2019 Sentry. All rights reserved.
+//
+
+#import "SentryTransport.h"
+
+@implementation SentryTransport
+
+@end
