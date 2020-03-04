95694fe22ec9935df332c49301f9953d831fab1f
Bruno Garcia <bruno@brunogarcia.com>
2020-03-03 23:14:16 -0500

    feat: Serialize envelope

--- Sources/Sentry/SentrySerialization.m
+++ Sources/Sentry/SentrySerialization.m
@@ -31,6 +31,40 @@ NS_ASSUME_NONNULL_BEGIN
     return data;
 }
 
++ (NSData *_Nullable)dataWithEnvelope:(SentryEnvelope *)envelope
+                              options:(NSJSONWritingOptions)opt
+                                error:(NSError *_Nullable *_Nullable)error {
+
+    NSMutableData *envelopeData = [[NSMutableData alloc] init];
+    NSData *header = [SentrySerialization dataWithJSONObject:envelope.header options:opt error:error];
+    if (nil == header) {
+        [SentryLog logWithMessage:[NSString stringWithFormat:@"Envelope header cannot be converted to JSON."] andLevel:kSentryLogLevelError];
+        if (error) {
+            *error = NSErrorFromSentryError(kSentryErrorJsonConversionError, @"Envelope header cannot be converted to JSON");
+        }
+        return nil;
+    }
+    [envelopeData appendData:header];
+
+    for (int i = 0; i < envelope.items.count; ++i) {
+        [envelopeData appendData:[@"\n" dataUsingEncoding:NSUTF8StringEncoding]];
+        NSData *itemHeader = [SentrySerialization dataWithJSONObject:envelope.items[i].header options:opt error:error];
+        if (nil == itemHeader) {
+            [SentryLog logWithMessage:[NSString stringWithFormat:@"Envelope item header cannot be converted to JSON."] andLevel:kSentryLogLevelError];
+            if (error) {
+                *error = NSErrorFromSentryError(kSentryErrorJsonConversionError, @"Envelope item header cannot be converted to JSON");
+            }
+            return nil;
+        }
+        [envelopeData appendData:itemHeader];
+        [envelopeData appendData:[@"\n" dataUsingEncoding:NSUTF8StringEncoding]];
+        [envelopeData appendData:envelope.items[i].data];
+    }
+
+    return envelopeData;
+}
+
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Sources/Sentry/include/SentrySerialization.h
+++ Sources/Sentry/include/SentrySerialization.h
@@ -2,8 +2,10 @@
 
 #if __has_include(<Sentry/Sentry.h>)
 #import <Sentry/SentryDefines.h>
+#import <Sentry/SentryEnvelope.h>
 #else
 #import "SentryDefines.h"
+#import "SentryEnvelope.h"
 #endif
 
 NS_ASSUME_NONNULL_BEGIN
@@ -11,6 +13,10 @@ NS_ASSUME_NONNULL_BEGIN
 @interface SentrySerialization : NSObject
 
 + (NSData *_Nullable)dataWithJSONObject:(NSDictionary *)dictionary
+                                options:(NSJSONWritingOptions)opt
+                                  error:(NSError *_Nullable *_Nullable)error;
+
++ (NSData *_Nullable)dataWithEnvelope:(SentryEnvelope *)envelope
                                 options:(NSJSONWritingOptions)opt
                                   error:(NSError *_Nullable *_Nullable)error;
 
