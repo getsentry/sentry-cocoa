fb4b4be84ae61f9e306348a65600c9dd93977e6f
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-03-09 09:10:20 +0100

    release: 4.5.0

--- Sources/Sentry/SentryClient.m
+++ Sources/Sentry/SentryClient.m
@@ -47,7 +47,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-NSString *const SentryClientVersionString = @"4.4.3";
+NSString *const SentryClientVersionString = @"4.5.0";
 NSString *const SentryClientSdkName = @"sentry-cocoa";
 
 static SentryClient *sharedClient = nil;
