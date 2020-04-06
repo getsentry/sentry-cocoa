cdfbb176553626785c3c85d2590225a8be81b9b1
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-04-06 14:27:46 +0200

    fix: Embed frameworks

--- Sources/Sentry/SentryBinaryImages.m
+++ Sources/Sentry/SentryBinaryImages.m
@@ -15,6 +15,17 @@
 
 + (NSArray *)getDebugMeta {
     // TODO implement
+//    NSMutableArray<SentryDebugMeta *> *result = [NSMutableArray new];
+//    for (NSDictionary *sourceImage in self.report[@"binary_images"]) {
+//        SentryDebugMeta *debugMeta = [[SentryDebugMeta alloc] init];
+//        debugMeta.uuid = sourceImage[@"uuid"];
+//        debugMeta.type = @"apple";
+//        debugMeta.imageAddress = hexAddress(sourceImage[@"image_addr"]);
+//        debugMeta.imageSize = sourceImage[@"image_size"];
+//        debugMeta.name = sourceImage[@"name"];
+//        [result addObject:debugMeta];
+//    }
+//    return result;
     return @[]; // Returns an array SentryDebugMeta containing all binary images
 }
 
