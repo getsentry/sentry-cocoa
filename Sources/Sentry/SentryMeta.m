efddd090f3cf6d7fdfc27de21ebac2b4ee7eac9f
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-04-27 10:26:59 +0200

    release: 5.0.0-beta.6

--- Sources/Sentry/SentryMeta.m
+++ Sources/Sentry/SentryMeta.m
@@ -2,7 +2,7 @@
 
 @implementation SentryMeta
 
-NSString *const versionString = @"5.0.0-beta.5";
+NSString *const versionString = @"5.0.0-beta.6";
 NSString *const sdkName = @"sentry.cocoa";
 
 + (NSString *)versionString {
