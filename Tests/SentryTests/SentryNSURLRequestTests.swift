6214bad058b1f133d0bde86dc5a76f9dcb467f3b
Bruno Garcia <bruno@brunogarcia.com>
2020-04-29 14:33:09 -0300

    test: SentryNSURLRequest endpoints

---
+++ Tests/SentryTests/SentryNSURLRequestTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import Sentry
+
+class SentryNSURLRequestTests: XCTestCase {
+    func testRequestWithEnvelopeEndpoint()  {
+        let request = try! SentryNSURLRequest(envelopeRequestWith: TestConstants.dsn, andData: Data());
+        XCTAssertTrue(request.url!.absoluteString.hasSuffix("/envelope/"));
+    }
+    func testRequestWithStoreEndpoint()  {
+        let request = try! SentryNSURLRequest(storeRequestWith: TestConstants.dsn, andData: Data());
+        XCTAssertTrue(request.url!.absoluteString.hasSuffix("/store/"));
+    }
+}
