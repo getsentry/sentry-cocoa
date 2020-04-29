6ed6ef6c8324bce5dfeab35717657ae39e04f7d0
Bruno Garcia <bruno@brunogarcia.com>
2020-04-29 11:35:05 -0300

    cache store URL

--- Sources/Sentry/SentryDsn.m
+++ Sources/Sentry/SentryDsn.m
@@ -9,12 +9,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
-@implementation SentryDsn
+@implementation SentryDsn {
+    NSURL *_storeEndpoint;
+}
 
 - (_Nullable instancetype)initWithString:(NSString *)dsnString didFailWithError:(NSError *_Nullable *_Nullable)error {
     self = [super init];
     if (self) {
-        self.url = [self convertDsnString:dsnString didFailWithError:error];
+        _url = [self convertDsnString:dsnString didFailWithError:error];
         if (nil != error && nil != *error) {
             return nil;
         }
@@ -34,25 +36,32 @@ NS_ASSUME_NONNULL_BEGIN
 }
 
 - (NSURL *)getStoreEndpoint {
-    NSURL *url = self.url;
-    NSString *projectId = url.lastPathComponent;
-    NSMutableArray *paths = [url.pathComponents mutableCopy];
-    // [0] = /
-    // [1] = projectId
-    // If there are more than two, that means someone wants to have an additional path
-    // ref: https://github.com/getsentry/sentry-cocoa/issues/236
-    NSString *path = @"";
-    if ([paths count] > 2) {
-        [paths removeObjectAtIndex:0]; // We remove the leading /
-        [paths removeLastObject]; // We remove projectId since we add it later
-        path = [NSString stringWithFormat:@"/%@", [paths componentsJoinedByString:@"/"]]; // We put together the path
+    if (nil == _storeEndpoint) {
+        @synchronized (self) {
+            if (nil == _storeEndpoint) {
+                NSURL *url = self.url;
+                NSString *projectId = url.lastPathComponent;
+                NSMutableArray *paths = [url.pathComponents mutableCopy];
+                // [0] = /
+                // [1] = projectId
+                // If there are more than two, that means someone wants to have an additional path
+                // ref: https://github.com/getsentry/sentry-cocoa/issues/236
+                NSString *path = @"";
+                if ([paths count] > 2) {
+                    [paths removeObjectAtIndex:0]; // We remove the leading /
+                    [paths removeLastObject]; // We remove projectId since we add it later
+                    path = [NSString stringWithFormat:@"/%@", [paths componentsJoinedByString:@"/"]]; // We put together the path
+                }
+                NSURLComponents *components = [NSURLComponents new];
+                components.scheme = url.scheme;
+                components.host = url.host;
+                components.port = url.port;
+                components.path = [NSString stringWithFormat:@"%@/api/%@/store/", path, projectId];
+                _storeEndpoint = components.URL;
+            }
+        }
     }
-    NSURLComponents *components = [NSURLComponents new];
-    components.scheme = url.scheme;
-    components.host = url.host;
-    components.port = url.port;
-    components.path = [NSString stringWithFormat:@"%@/api/%@/store/", path, projectId];
-    return components.URL;
+    return _storeEndpoint;
 }
 
 - (NSURL *_Nullable)convertDsnString:(NSString *)dsnString didFailWithError:(NSError *_Nullable *_Nullable)error {
