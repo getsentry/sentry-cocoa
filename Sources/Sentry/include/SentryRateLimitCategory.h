d73808cf8166ae42ca088e6c43cafae715a05bec
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-04-30 16:58:01 +0200

    Add comment for default category

--- Sources/Sentry/include/SentryRateLimitCategory.h
+++ Sources/Sentry/include/SentryRateLimitCategory.h
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 
+// more categories exist. New categories will be added and old SDKs are expected to fallback to `default`
 static NSString * const SentryRateLimitCategoryDefault = @"default";
 static NSString * const SentryRateLimitCategoryError = @"error";
 static NSString * const SentryRateLimitCategorySession = @"session";
