311b36b8a3f9c5fbfd7cc2ba4ba733695b430b9b
Bruno Garcia <bruno@brunogarcia.com>
2020-05-11 13:07:56 -0300

    test: Make sure basic sessions tests run (#508)
    * ref: Renamed var and fixed typos
    
    * test: run session tests
    
    * Update Sources/Sentry/SentryHub.m
    
    Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>
    
    Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>

--- Sources/Sentry/SentryHub.m
+++ Sources/Sentry/SentryHub.m
@@ -83,7 +83,7 @@
     SentryFileManager *fileManager = [[self getClient] fileManager];
     SentrySession *session = [fileManager readCurrentSession];
     if (nil != session) {
-        [SentryLog logWithMessage:[NSString stringWithFormat:@"A cached session was found."] andLevel:kSentryLogLevelDebug];
+        [SentryLog logWithMessage:@"A cached session was found." andLevel:kSentryLogLevelDebug];
         SentryClient *client = [self getClient];
         if (nil != session && nil != client) { // Make sure there's a client bound.
             if (SentryCrash.sharedInstance.crashedLastLaunch) {
