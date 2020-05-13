958cbc8ea9147b80fec1cb82b82911dbfb7edde2
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-05-13 09:24:26 +0200

    Make tests less flakey

--- Tests/SentryTests/Helper/SentryCurrentDateTests.swift
+++ Tests/SentryTests/Helper/SentryCurrentDateTests.swift
@@ -2,32 +2,32 @@ import XCTest
 @testable import Sentry
 
 class SentryCurrentDateTests: XCTestCase {
-    
+
     func testSetNoCurrentDateProvider() {
-        let expected = Date.init()
-        
-        let actual =  CurrentDate.date()
-        
-        XCTAssertEqual(expected.timeIntervalSinceReferenceDate,
-                       actual.timeIntervalSinceReferenceDate, accuracy: 0.001)
+        let firstDate = Date.init()
+        let secondDate = CurrentDate.date()
+        let thirdDate = Date.init()
+
+        XCTAssertGreaterThanOrEqual(secondDate, firstDate)
+        XCTAssertGreaterThanOrEqual(thirdDate, secondDate)
     }
 
-    func testDefaultCurrentDateProvider()  {
+    func testDefaultCurrentDateProvider() {
         CurrentDate.setCurrentDateProvider(DefaultCurrentDateProvider())
-        let expected = Date.init()
-        
-        let actual =  CurrentDate.date()
-        
-        XCTAssertEqual(expected.timeIntervalSinceReferenceDate,
-                       actual.timeIntervalSinceReferenceDate, accuracy: 0.001)
+        let firstDate = Date.init()
+        let secondDate = CurrentDate.date()
+        let thirdDate = Date.init()
+
+        XCTAssertGreaterThanOrEqual(secondDate, firstDate)
+        XCTAssertGreaterThanOrEqual(thirdDate, secondDate)
     }
-    
+
     func testTestCurrentDateProvider() {
         CurrentDate.setCurrentDateProvider(TestCurrentDateProvider())
         let expected = Date.init(timeIntervalSinceReferenceDate: 0)
-        
+
         let actual = CurrentDate.date()
-        
+
         XCTAssertEqual(expected, actual)
     }
 }
