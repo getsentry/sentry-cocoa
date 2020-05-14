906cfecb1a9e84c0fb3e3fd532594c5d0ed0acec
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-05-14 08:27:48 +0200

    test: Add test for SentryCurrentDate (#513)
    Add test when no current date provider is set to reach 100%
    code coverage for SentryCurrentDate.

--- Tests/SentryTests/Helper/SentryCurrentDateTests.swift
+++ Tests/SentryTests/Helper/SentryCurrentDateTests.swift
@@ -3,22 +3,31 @@ import XCTest
 
 class SentryCurrentDateTests: XCTestCase {
 
-    func testDefaultCurrentDateProvider()  {
-        CurrentDate.setCurrentDateProvider(DefaultCurrentDateProvider())
-        let expected = Date.init()
-        
-        let actual =  CurrentDate.date()
-        
-        XCTAssertEqual(expected.timeIntervalSinceReferenceDate,
-                       actual.timeIntervalSinceReferenceDate, accuracy: 0.001)
+    func testSetNoCurrentDateProvider() {
+        let firstDate = Date.init()
+        let secondDate = CurrentDate.date()
+        let thirdDate = Date.init()
+
+        XCTAssertGreaterThanOrEqual(secondDate, firstDate)
+        XCTAssertGreaterThanOrEqual(thirdDate, secondDate)
     }
-    
+
+    func testDefaultCurrentDateProvider() {
+        CurrentDate.setCurrentDateProvider(DefaultCurrentDateProvider())
+        let firstDate = Date.init()
+        let secondDate = CurrentDate.date()
+        let thirdDate = Date.init()
+
+        XCTAssertGreaterThanOrEqual(secondDate, firstDate)
+        XCTAssertGreaterThanOrEqual(thirdDate, secondDate)
+    }
+
     func testTestCurrentDateProvider() {
         CurrentDate.setCurrentDateProvider(TestCurrentDateProvider())
         let expected = Date.init(timeIntervalSinceReferenceDate: 0)
-        
+
         let actual = CurrentDate.date()
-        
+
         XCTAssertEqual(expected, actual)
     }
 }
