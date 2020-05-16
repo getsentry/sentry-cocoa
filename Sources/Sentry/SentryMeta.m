3484b9f3f7a4778734e26ab40f14e3dd9a1b5bcd
Bruno Garcia <bruno@brunogarcia.com>
2020-05-16 16:45:47 -0300

    release: 5.0.4

--- Sources/Sentry/SentryMeta.m
+++ Sources/Sentry/SentryMeta.m
@@ -2,7 +2,7 @@
 
 @implementation SentryMeta
 
-NSString *const versionString = @"5.0.3";
+NSString *const versionString = @"5.0.4";
 NSString *const sdkName = @"sentry.cocoa";
 
 + (NSString *)versionString
