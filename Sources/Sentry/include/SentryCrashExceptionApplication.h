39fb262cef98200f0af512027896b5771029deeb
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-05-20 10:28:26 +0200

    fix: Failing MacOS build
    Import only NSApplication and import TargetConditionals.
    
    Fixes #526

--- Sources/Sentry/include/SentryCrashExceptionApplication.h
+++ Sources/Sentry/include/SentryCrashExceptionApplication.h
@@ -1,5 +1,8 @@
+// TargetConditionals.h is needed so that `#if TARGET_OS_OSX` is working
+// fine. If we remove this the SDK breaks for MacOS.
+#import "TargetConditionals.h"
 #if TARGET_OS_OSX
-#    import <Cocoa/Cocoa.h>
+#    import <AppKit/NSApplication.h>
 @interface SentryCrashExceptionApplication : NSApplication
 #else
 #    import <Foundation/Foundation.h>
