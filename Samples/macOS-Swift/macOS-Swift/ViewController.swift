e13046bf61d2fb764fa7e4850f2627d236b6ab34
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-05-26 08:48:07 +0200

    test: Add breadcrumb and message to macOS
    Add a button to add breadcrumbs and another button to capture a message
    for macOS-Swift.

--- Samples/macOS-Swift/macOS-Swift/ViewController.swift
+++ Samples/macOS-Swift/macOS-Swift/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    @IBAction func addBreadCrumb(_ sender: Any) {
+        let crumb = Breadcrumb()
+        crumb.message = "tapped addBreadcrumb"
+        SentrySDK.addBreadcrumb(crumb: crumb)
+    }
+    
+    @IBAction func captureMessage(_ sender: Any) {
+        let eventId = SentrySDK.capture(message: "Yeah captured a message")
+        // Returns eventId in case of successfull processed event
+        // otherwise nil
+        print("\(String(describing: eventId))")
+    }
 
     @IBAction func crashOnException(_ sender: Any) {
         let exception = NSException(name: NSExceptionName("My Custom exeption"), reason: "User clicked the button", userInfo: nil)
