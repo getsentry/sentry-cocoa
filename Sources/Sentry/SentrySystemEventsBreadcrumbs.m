ce47a1d0171b5916f4c2c5f1025febb060760b2d
Manoel Aranda Neto <marandaneto@gmail.com>
2020-06-02 16:11:28 +0200

    added device orientation breadcrumbs

--- Sources/Sentry/SentrySystemEventsBreadcrumbs.m
+++ Sources/Sentry/SentrySystemEventsBreadcrumbs.m
@@ -1,7 +1,6 @@
 #import "SentrySystemEventsBreadcrumbs.h"
-#import "SentryBreadcrumb.h"
-#import "SentryDefines.h"
 #import "SentrySDK.h"
+#import "SentryLog.h"
 
 #if SENTRY_HAS_UIKIT
 #    import <UIKit/UIKit.h>
@@ -11,30 +10,44 @@
 
 - (void)start
 {
-    [self initBatteryObserver];
+#if SENTRY_HAS_UIKIT
+    UIDevice *currentDevice = [UIDevice currentDevice];
+    if (currentDevice == nil) {
+        [SentryLog logWithMessage:@"currentDevice is null, it won't be able to record battery breadcrumbs."
+                         andLevel:kSentryLogLevelDebug];
+        return;
+    }
+    
+    [self initBatteryObserver:currentDevice];
+    [self initOrientationObserver:currentDevice];
+#else
+    [SentryLog logWithMessage:@"NO UIKit -> [SentrySystemEventsBreadcrumbs.start] does nothing."
+                     andLevel:kSentryLogLevelDebug];
+#endif
 }
 
-- (void)initBatteryObserver
+- (void)initBatteryObserver:(UIDevice*)currentDevice
 {
-    UIDevice *device = [UIDevice currentDevice];
-    
-    if (device.batteryMonitoringEnabled == NO) {
-        device.batteryMonitoringEnabled = YES;
+    if (currentDevice.batteryMonitoringEnabled == NO) {
+        currentDevice.batteryMonitoringEnabled = YES;
     }
     
     // https://developer.apple.com/documentation/uikit/uidevicebatteryleveldidchangenotification
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batteryStateChanged:) name:@"UIDeviceBatteryLevelDidChangeNotification" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batteryStateChanged:) name:UIDeviceBatteryLevelDidChangeNotification object:currentDevice];
     // https://developer.apple.com/documentation/uikit/uidevicebatterystatedidchangenotification
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batteryStateChanged:) name:@"UIDeviceBatteryStateDidChangeNotification" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batteryStateChanged:) name:UIDeviceBatteryStateDidChangeNotification object:currentDevice];
     
     // for testing only
-//    [[NSNotificationCenter defaultCenter] postNotificationName:@"UIDeviceBatteryStateDidChangeNotification" object:nil];
+//    [[NSNotificationCenter defaultCenter] postNotificationName:@"UIDeviceBatteryStateDidChangeNotification" object:currentDevice];
+//    [[NSNotificationCenter defaultCenter] postNotificationName:@"UIDeviceOrientationDidChangeNotification" object:currentDevice];
 }
 
 - (void)batteryStateChanged:(NSNotification*)notification
 {
+    UIDevice *currentDevice = notification.object;
+    
     // Notifications for battery level change are sent no more frequently than once per minute
-    NSDictionary* batteryData = [self getBatteryStatus];
+    NSDictionary* batteryData = [self getBatteryStatus:currentDevice];
     [batteryData setValue:@"BATTERY_STATE_CHANGE" forKey:@"action"];
     
     SentryBreadcrumb *crumb =
@@ -45,10 +58,9 @@
     [SentrySDK addBreadcrumb:crumb];
 }
 
-- (NSDictionary*)getBatteryStatus
+- (NSDictionary*)getBatteryStatus:(UIDevice*)currentDevice
 {
     // borrowed and adapted from https://github.com/apache/cordova-plugin-battery-status/blob/master/src/ios/CDVBattery.m
-    UIDevice* currentDevice = [UIDevice currentDevice];
     UIDeviceBatteryState currentState = [currentDevice batteryState];
 
     BOOL isPlugged = NO; // UIDeviceBatteryStateUnknown or UIDeviceBatteryStateUnplugged
@@ -63,10 +75,43 @@
     if ((currentState != UIDeviceBatteryStateUnknown) || (currentLevel != -1.0)) {
         float w3cLevel = (currentLevel * 100);
         [batteryData setValue:[NSNumber numberWithFloat:w3cLevel] forKey:@"level"];
+    } else {
+        [SentryLog logWithMessage:@"batteryLevel is unknown."
+                         andLevel:kSentryLogLevelDebug];
     }
     [batteryData setValue:[NSNumber numberWithBool:isPlugged] forKey:@"plugged"];
 
     return batteryData;
+}
+
+- (void)initOrientationObserver:(UIDevice*)currentDevice
+{
+    if (currentDevice.isGeneratingDeviceOrientationNotifications == NO) {
+        [currentDevice beginGeneratingDeviceOrientationNotifications];
+    }
+    // https://developer.apple.com/documentation/uikit/uideviceorientationdidchangenotification
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batteryStateChanged:) name:UIDeviceOrientationDidChangeNotification object:currentDevice];
+}
+
+- (void)orientationChanged:(NSNotification*)notification
+{
+    UIDevice *currentDevice = notification.object;
+    
+    SentryBreadcrumb *crumb =
+        [[SentryBreadcrumb alloc] initWithLevel:kSentryLevelInfo
+                                       category:@"device.orientation"];
+    
+//TODO: look at https://github.com/apache/cordova-plugin-screen-orientation/blob/master/src/ios/CDVOrientation.m
+    // it uses [UIApplication sharedApplication].statusBarOrientation as well, should we use it? maybe device orientation and screen/app orientation
+    
+    if (UIDeviceOrientationIsLandscape(currentDevice.orientation)){
+        crumb.data = @{ @"position": @"landscape"};
+    }
+    else {
+        crumb.data = @{ @"position": @"portrait"};
+    }
+    crumb.type = @"navigation";
+    [SentrySDK addBreadcrumb:crumb];
 }
 
 @end
