fb3ee7edc68b19703ffcb4733b761d8a0df6a661
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-06-17 08:49:11 +0200

    test: Add tests for SentryClient (#582)

--- Tests/SentryTests/Networking/TestTransport.swift
+++ Tests/SentryTests/Networking/TestTransport.swift
@@ -4,11 +4,13 @@ import Foundation
 public class TestTransport: NSObject, Transport {
     
     var lastSentEvent: Event?
+    var lastSentEnvelope: SentryEnvelope?
    
     public func send(event: Event, completion completionHandler: SentryRequestFinished? = nil) {
         lastSentEvent = event
     }
     
     public func send(envelope: SentryEnvelope, completion completionHandler: SentryRequestFinished? = nil) {
+        lastSentEnvelope = envelope
     }
 }
