5f656dea0ccd58c0c079f44f1ce6626a8d753f07
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-06-18 15:08:10 +0200

    Simplify isEqual

--- Tests/SentryTests/Protocol/SentryDebugMetaEquality.swift
+++ Tests/SentryTests/Protocol/SentryDebugMetaEquality.swift
@@ -3,31 +3,12 @@ import Foundation
 extension DebugMeta   {
     open override func isEqual(_ object: Any?) -> Bool {
         if  let other = object as? DebugMeta {
-            if uuid != other.uuid {
-                return false
-            }
-            
-            if type != other.type {
-                return false
-            }
-            
-            if name != other.name {
-                return false
-            }
-            
-            if imageSize != other.imageSize {
-                return false
-            }
-            
-            if imageAddress != other.imageAddress {
-                return false
-            }
-            
-            if imageVmAddress != other.imageVmAddress {
-                return false
-            }
-            
-            return true
+            return  uuid == other.uuid &&
+                type == other.type &&
+                name == other.name  &&
+                imageSize == other.imageSize &&
+                imageAddress == other.imageAddress &&
+                imageVmAddress == other.imageVmAddress
         } else {
             return false
         }
