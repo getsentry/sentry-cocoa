358293efded97bb8d6219f0730ee4c9b345f9a32
Jonathan Downing <jd@jonathandowning.uk>
2020-06-24 09:07:40 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
     name: "Sentry",
     platforms: [
-     .macOS(.v10_10),
-     .iOS(.v9),
-     .tvOS(.v9),
-     .watchOS(.v2)
+        .macOS(.v10_10),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)
     ],
     products: [
         .library(
