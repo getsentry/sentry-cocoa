5c1a13b3be0faa5c67ea1a21acf9048998be71bc
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-07-09 17:45:39 +0200

    TestNotificationCenter supports multiple os

--- Tests/SentryTests/Integrations/TestNotificationCenter.swift
+++ Tests/SentryTests/Integrations/TestNotificationCenter.swift
@@ -1,23 +1,46 @@
 import Foundation
 
 class TestNotificationCenter {
+
+    #if os(tvOS) || os(iOS) 
+    private static let willEnterForegroundNotification = UIApplication.willEnterForegroundNotification
+    private static let didBecomeActiveNotification = UIApplication.didBecomeActiveNotification
+    private static let willResignActiveNotification = UIApplication.willResignActiveNotification
+    private static let didEnterBackgroundNotification = UIApplication.didEnterBackgroundNotification
+    private static let willTerminateNotification = UIApplication.willTerminateNotification
+    #elseif os(macOS)
+    private static let didBecomeActiveNotification = NSApplication.didBecomeActiveNotification;
+    private static let willResignActiveNotification = NSApplication.willResignActiveNotification;
+    private static let willTerminateNotification = NSApplication.willTerminateNotification;
+    #endif
     
     static func willEnterForeground() {
-        NotificationCenter.default.post(Notification(name: UIApplication.willEnterForegroundNotification))
+        #if os(tvOS) || os(iOS)
+        NotificationCenter.default.post(Notification(name: willEnterForegroundNotification))
+        #endif
     }
     
     static func didBecomeActive() {
-        NotificationCenter.default.post(Notification(name: UIApplication.didBecomeActiveNotification))
+        #if os(tvOS) || os(iOS) || os(macOS)
+        NotificationCenter.default.post(Notification(name: didBecomeActiveNotification))
+        #endif
     }
     
     static func willResignActive() {
-        NotificationCenter.default.post(Notification(name: UIApplication.willResignActiveNotification))
+        #if os(tvOS) || os(iOS) || os(macOS)
+        NotificationCenter.default.post(Notification(name: willResignActiveNotification))
+        #endif
     }
+
     static func didEnterBackground() {
-        NotificationCenter.default.post(Notification(name: UIApplication.didEnterBackgroundNotification))
+        #if os(tvOS) || os(iOS)
+        NotificationCenter.default.post(Notification(name: didEnterBackgroundNotification))
+        #endif
     }
-    
+
     static func willTerminate() {
-        NotificationCenter.default.post(Notification(name: UIApplication.willTerminateNotification))
+        #if os(tvOS) || os(iOS) || os(macOS)
+        NotificationCenter.default.post(Notification(name: willTerminateNotification))
+        #endif
     }
 }
