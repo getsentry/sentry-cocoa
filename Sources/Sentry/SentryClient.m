f1304ce8a88772dd915faeafad8872032645e72d
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-07-20 16:58:54 +0200

    ref: No lazy init in SentryClient (#621)
    Don't lazily initialize SentryFileManager and SentryHttpTransport in SentryClient
    because they have to be initialized anyway.

--- Sources/Sentry/SentryClient.m
+++ Sources/Sentry/SentryClient.m
@@ -63,6 +63,17 @@ SentryClient ()
         self.threadInspector =
             [[SentryThreadInspector alloc] initWithStacktraceBuilder:stacktraceBuilder
                                             andMachineContextWrapper:machineContextWrapper];
+
+        NSError *error = nil;
+        self.fileManager = [[SentryFileManager alloc] initWithDsn:self.options.parsedDsn
+                                                 didFailWithError:&error];
+        if (nil != error) {
+            [SentryLog logWithMessage:error.localizedDescription andLevel:kSentryLogLevelError];
+            return nil;
+        }
+
+        self.transport = [SentryTransportFactory initTransport:self.options
+                                             sentryFileManager:self.fileManager];
     }
     return self;
 }
@@ -80,27 +91,8 @@ SentryClient ()
     return self;
 }
 
-- (id<SentryTransport>)transport
-{
-    if (_transport == nil) {
-        _transport = [SentryTransportFactory initTransport:self.options
-                                         sentryFileManager:self.fileManager];
-    }
-    return _transport;
-}
-
 - (SentryFileManager *)fileManager
 {
-    if (_fileManager == nil) {
-        NSError *error = nil;
-        SentryFileManager *fileManager =
-            [[SentryFileManager alloc] initWithDsn:self.options.parsedDsn didFailWithError:&error];
-        if (nil != error) {
-            [SentryLog logWithMessage:(error).localizedDescription andLevel:kSentryLogLevelError];
-            return nil;
-        }
-        _fileManager = fileManager;
-    }
     return _fileManager;
 }
 
