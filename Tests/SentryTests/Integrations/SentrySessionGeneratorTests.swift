567273ece4e78627551e4e87d2e6cb02be575c24
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-07-22 09:19:18 +0200

    test: Extract delayNonBlocking to be reusable (#630)

--- Tests/SentryTests/Integrations/SentrySessionGeneratorTests.swift
+++ Tests/SentryTests/Integrations/SentrySessionGeneratorTests.swift
@@ -113,18 +113,6 @@ class SentrySessionGeneratorTests: XCTestCase {
         autoSessionTrackingIntegration.install(with: options)
     }
     
-    private func delayNonBlocking(timeout: Double = 0.2) {
-        let group = DispatchGroup()
-        group.enter()
-        let queue = DispatchQueue(label: "delay", qos: .background, attributes: [])
-        
-        queue.asyncAfter(deadline: .now() + timeout) {
-            group.leave()
-        }
-        
-        group.wait()
-    }
-    
     private func goToForeground(forSeconds: TimeInterval = 0.2) {
         TestNotificationCenter.willEnterForeground()
         TestNotificationCenter.didBecomeActive()
