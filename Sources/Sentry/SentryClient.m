1926076ffafc2b235e664b87cbec76fcd248e525
Bruno Garcia <bruno@brunogarcia.com>
2020-09-29 11:34:59 -0400

    Update Sources/Sentry/SentryClient.m
    Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>

--- Sources/Sentry/SentryClient.m
+++ Sources/Sentry/SentryClient.m
@@ -129,9 +129,7 @@ SentryClient ()
 - (SentryEvent *)buildExceptionEvent:(NSException *)exception
 {
     SentryEvent *event = [[SentryEvent alloc] initWithLevel:kSentryLevelError];
-    SentryException *sentryException =
-        [[SentryException alloc] initWithValue:[NSString stringWithFormat:@"%@", exception.reason]
-                                          type:exception.name];
+    SentryException *sentryException = [[SentryException alloc] initWithValue:exception.reason type:exception.name];
     event.exceptions = @[ sentryException ];
     [self setUserInfo:exception.userInfo withEvent:event];
     return event;
