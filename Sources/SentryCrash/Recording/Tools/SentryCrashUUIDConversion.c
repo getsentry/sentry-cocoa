e83f07ff52ab467d1ec5e1b0d8758cc535757521
Clang Robot <clang-robot@sentry.io>
2020-10-19 07:57:40 +0000

    Committing formatted code

--- Sources/SentryCrash/Recording/Tools/SentryCrashUUIDConversion.c
+++ Sources/SentryCrash/Recording/Tools/SentryCrashUUIDConversion.c
@@ -1,6 +1,7 @@
 #include "SentryCrashUUIDConversion.h"
 
-void sentrycrashdl_convertBinaryImageUUID(const unsigned char *src, char *dst)
+void
+sentrycrashdl_convertBinaryImageUUID(const unsigned char *src, char *dst)
 {
     const char g_hexNybbles[]
         = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
