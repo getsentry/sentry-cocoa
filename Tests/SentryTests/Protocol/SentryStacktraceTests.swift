1bcacaaeb474fef0b129a0f63ff194d3cb891156
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-11-17 18:58:32 +0100

    test: Add tests for serialization (#846)
    Many classes that adopt SentrySerializable didn't have any tests for serialize. With
    GH-842 the serialization of SentryScope was fixed. These tests are added to avoid such
    an issue in the future and to make sure such an issue is not present in other serialization
    implementations.

---
+++ Tests/SentryTests/Protocol/SentryStacktraceTests.swift
@@ -0,0 +1,34 @@
+import XCTest
+
+class SentryStacktraceTests: XCTestCase {
+
+    func testSerialize() {
+        let stacktrace = TestData.stacktrace
+        
+        let actual = stacktrace.serialize()
+        
+        // Changing the original doesn't modify the serialized
+        stacktrace.frames.removeAll()
+        stacktrace.registers.removeAll()
+        
+        let frames = actual["frames"] as? [Any]
+        XCTAssertEqual(1, frames?.count)
+        XCTAssertEqual(["register": "one"], actual["registers"] as? [String: String])
+    }
+    
+    func testSerializeNoRegisters() {
+        let stacktrace = TestData.stacktrace
+        stacktrace.registers = [:]
+        let actual = stacktrace.serialize()
+
+        XCTAssertNil(actual["registers"] as? [String: String])
+    }
+    
+    func testSerializeNoFrames() {
+        let stacktrace = TestData.stacktrace
+        stacktrace.frames = []
+        let actual = stacktrace.serialize()
+        
+        XCTAssertNil(actual["frames"] as? [Any])
+    }
+}
