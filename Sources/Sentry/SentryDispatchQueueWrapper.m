6004e2788d3cd8c6093d86c58bec47299900eb35
Bruno Garcia <bruno@brunogarcia.com>
2020-12-08 16:14:31 -0500

    ref: drop gloal dispatch queue (thanks @tmm1 )

--- Sources/Sentry/SentryDispatchQueueWrapper.m
+++ Sources/Sentry/SentryDispatchQueueWrapper.m
@@ -3,11 +3,26 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@interface SentryDispatchQueueWrapper ()
+
+@property (nonatomic, strong) dispatch_queue_t queue;
+
+@end
+
 @implementation SentryDispatchQueueWrapper
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.queue = dispatch_queue_create("sentry-dispatch", DISPATCH_QUEUE_SERIAL);
+    }
+    return self;
+}
 
 - (void)dispatchAsyncWithBlock:(void (^)(void))block
 {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul), block);
+    dispatch_async(self.queue, block);
 }
 
 - (void)dispatchOnce:(dispatch_once_t *)predicate block:(void (^)(void))block
