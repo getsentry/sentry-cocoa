1dd9ee65aee3ad5f64f1bb4ddfda093baec3edd0
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-12-09 10:04:36 +0100

    Add comment about autoreleasepool

--- Sources/Sentry/SentryDispatchQueueWrapper.m
+++ Sources/Sentry/SentryDispatchQueueWrapper.m
@@ -16,6 +16,8 @@ SentryDispatchQueueWrapper ()
 {
     self = [super init];
     if (self) {
+        // DISPATCH_QUEUE_SERIAL_WITH_AUTORELEASE_POOL is requires iOS 10. Since we are targeting
+        // iOS 9 we need to manually add the autoreleasepool.
         self.queue = dispatch_queue_create("sentry-dispatch", DISPATCH_QUEUE_SERIAL);
     }
     return self;
