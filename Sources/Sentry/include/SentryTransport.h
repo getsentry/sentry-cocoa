7c7aa5fa75d01f5d3d497ab025128c82c7f64cea
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-12-14 11:45:55 +0100

    ref: Remove out of date comment in Transport
    The comment for sendEvent in SentryTransport is out of date and can be removed
    as SentryTransport is currently not public.

--- Sources/Sentry/include/SentryTransport.h
+++ Sources/Sentry/include/SentryTransport.h
@@ -5,22 +5,9 @@
 NS_ASSUME_NONNULL_BEGIN
 
 // TODO: align with unified SDK api
-/**
- * Sends data to the Sentry server.
- */
 NS_SWIFT_NAME(Transport)
 @protocol SentryTransport <NSObject>
 
-/**
- * Sends an event to sentry.
- * Triggerd when a event occurs. Thus the first try to upload an event.
- * CompletionHandler will be called if set.
- *
- * Failure to send will most likely keep this event on disk to batch upload it
- * on next app launch.
- *
- * @param event SentryEvent that should be sent
- */
 - (void)sendEvent:(SentryEvent *)event NS_SWIFT_NAME(send(event:));
 
 - (void)sendEvent:(SentryEvent *)event withSession:(SentrySession *)session;
