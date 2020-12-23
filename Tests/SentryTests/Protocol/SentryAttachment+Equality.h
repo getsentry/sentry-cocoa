b5ecd320c58099b431370f9003901c28c939eb49
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-12-23 09:04:21 +0100

    Add isEqual and hash to test code

---
+++ Tests/SentryTests/Protocol/SentryAttachment+Equality.h
@@ -0,0 +1,15 @@
+#import <Sentry/Sentry.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SentryAttachment (Equality)
+
+- (BOOL)isEqual:(id _Nullable)other;
+
+- (BOOL)isEqualToAttachment:(SentryAttachment *)attachment;
+
+- (NSUInteger)hash;
+
+@end
+
+NS_ASSUME_NONNULL_END
