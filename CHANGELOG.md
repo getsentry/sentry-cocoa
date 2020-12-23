2d525f454582fb74fb0ac8095a1ecb0ef09e3ca6
Philipp Hofmann <philipp.hofmann@sentry.io>
2020-12-23 09:49:04 +0100

    ref: Remove SentryAttachment.isEqual and hash (#886)
    It doesn't make sense to add the implementation for isEqual and hash for SentryAttachment.
    It was only added because the SentryScope also implemented these functions. As these methods
    were removed with #884 we can also remove them from SentryAttachment.
    
    Fixes GH-885

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## unreleased
 
+- ref: Remove SentryAttachment.isEqual and hash #885
 - ref: Remove SentryScope.isEqual and hash #884
 
 ## 6.1.0-alpha.0
---
+++ Tests/SentryTests/Protocol/SentryAttachment+Equality.m
@@ -0,0 +1,44 @@
+#import "SentryAttachment+Equality.h"
+
+@implementation SentryAttachment (Equality)
+
+- (BOOL)isEqual:(id _Nullable)other
+{
+    if (other == self)
+        return YES;
+    if (!other || ![[other class] isEqual:[self class]])
+        return NO;
+
+    return [self isEqualToAttachment:other];
+}
+
+- (BOOL)isEqualToAttachment:(SentryAttachment *)attachment
+{
+    if (self == attachment)
+        return YES;
+    if (attachment == nil)
+        return NO;
+    if (self.data != attachment.data && ![self.data isEqualToData:attachment.data])
+        return NO;
+    if (self.path != attachment.path && ![self.path isEqualToString:attachment.path])
+        return NO;
+    if (self.filename != attachment.filename
+        && ![self.filename isEqualToString:attachment.filename])
+        return NO;
+    return !(self.contentType != attachment.contentType
+        && ![self.contentType isEqualToString:attachment.contentType]);
+}
+
+- (NSUInteger)hash
+{
+    NSUInteger hash = 17;
+
+    hash = hash * 23 + [self.data hash];
+    hash = hash * 23 + [self.path hash];
+    hash = hash * 23 + [self.filename hash];
+    hash = hash * 23 + [self.contentType hash];
+
+    return hash;
+}
+
+@end
