eca1d62c89097773b2aaa32e0009be9e85911504
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-01-29 08:36:24 +0100

    ci: Use Xcode 12.4

--- scripts/ci-select-xcode.sh
+++ scripts/ci-select-xcode.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo xcode-select -s /Applications/Xcode_12.app/Contents/Developer
+# For available Xcode versions on Github Action, see 
+# https://github.com/actions/virtual-environments/blob/main/images/macos/macos-10.15-Readme.md#xcode
+
+sudo xcode-select -s /Applications/Xcode_12.4.app/Contents/Developer
