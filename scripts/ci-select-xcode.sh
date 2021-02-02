3d0fc11e0735fe96b1a1dfbd6501beabb3c582ef
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-02-02 11:23:35 +0100

    ci: Switch back to Xcode 12.3
    GitHub Actions can't find Xcode 12.4 and therefore the builds are failing.

--- scripts/ci-select-xcode.sh
+++ scripts/ci-select-xcode.sh
@@ -3,4 +3,4 @@
 # For available Xcode versions on Github Action, see 
 # https://github.com/actions/virtual-environments/blob/main/images/macos/macos-10.15-Readme.md#xcode
 
-sudo xcode-select -s /Applications/Xcode_12.4.app/Contents/Developer
+sudo xcode-select -s /Applications/Xcode_12.3.app/Contents/Developer
