b3a4efad840b1365163c9f257bdc4296a2fa4465
Philipp Hofmann <ph0hofmann@gmail.com>
2021-02-22 17:31:22 +0100

    Add comments

--- Sources/Sentry/include/SentryFrameInAppLogic.h
+++ Sources/Sentry/include/SentryFrameInAppLogic.h
@@ -3,6 +3,29 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * This class detects whether an image belongs to the app or not. We differentiate between three
+ * different types of images.
+ *
+ * First, the main executable of the app, which's name can be retrieved by CFBundleExecutable. To
+ * mark this image as inApp the caller needs to pass in the CFBundleExecutable to InAppIncludes.
+ *
+ * Next, there are private frameworks embedded in the application bundle. Both app supporting
+ * frameworks as CocoaLumberJack, Sentry, RXSwift, etc., and frameworks written by the user fall
+ * into this category. These frameworks can be both inApp or not. As we expect most frameworks of
+ * this category to be supporting frameworks, we mark them not as inApp. If a user wants such a
+ * framework to be inApp he needs to pass the name into inAppInclude. For dynamic frameworks, the
+ * location is usually in the bundle under /Frameworks/FrameworkName.framework/FrameworkName. As for
+ * static frameworks, the location is the same as the main executable; this class marks all static
+ * frameworks as inApp. To remove static frameworks from being inApp, Sentry uses stack trace
+ * grouping rules.
+ *
+ * Last, this class marks all public frameworks as not inApp. Such frameworks are bound dynamically
+ * and are usually located at /Library/Frameworks or ~/Library/Frameworks. For simulators, the
+ * location can be something like
+ * /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/System/Library/...
+ *
+ */
 @interface SentryFrameInAppLogic : NSObject
 SENTRY_NO_INIT
 
