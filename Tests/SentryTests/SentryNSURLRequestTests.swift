c5c3d13a30e783500423941159558a9d57153eff
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-03-03 00:50:35 +0100

    test: DSN per TestCase (#969)
    Using a DSN per test case gives each test case a separate folder to store the envelopes,
    reducing the chance of interference. This PR also contains a few minor fixes for other tests.

--- Tests/SentryTests/SentryNSURLRequestTests.swift
+++ Tests/SentryTests/SentryNSURLRequestTests.swift
@@ -3,12 +3,15 @@ import XCTest
 
 class SentryNSURLRequestTests: XCTestCase {
     
+    private static let dsnAsString = TestConstants.dsnAsString(username: "SentryNSURLRequestTests")
+    private static let dsn = TestConstants.dsn(username: "SentryNSURLRequestTests")
+    
     func testRequestWithEnvelopeEndpoint() {
-        let request = try! SentryNSURLRequest(envelopeRequestWith: TestConstants.dsn, andData: Data())
+        let request = try! SentryNSURLRequest(envelopeRequestWith: SentryNSURLRequestTests.dsn, andData: Data())
         XCTAssertTrue(request.url!.absoluteString.hasSuffix("/envelope/"))
     }
     func testRequestWithStoreEndpoint() {
-        let request = try! SentryNSURLRequest(storeRequestWith: TestConstants.dsn, andData: Data())
+        let request = try! SentryNSURLRequest(storeRequestWith: SentryNSURLRequestTests.dsn, andData: Data())
         XCTAssertTrue(request.url!.absoluteString.hasSuffix("/store/"))
     }
 }
