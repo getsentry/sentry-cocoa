1252c9ac27c313f6560708588d4fe94caad37b80
Philipp Hofmann <ph0hofmann@gmail.com>
2021-03-03 09:31:17 +0100

    ci: Fix TestFlight upload for previews
    App Store connect requires a period-separated list of at most three non-negative integers
    for the version. Therefore the version bump script must not add the suffixes of preview
    releases. This is fixed now.

--- Utils/VersionBump/main.swift
+++ Utils/VersionBump/main.swift
@@ -7,7 +7,14 @@ let fromVersionFile = "./Sentry.podspec"
 let files = [
     "./Sentry.podspec",
     "./Sources/Sentry/SentryMeta.m",
-    "./Sources/Configuration/Sentry.xcconfig",
+    "./Sources/Configuration/Sentry.xcconfig"
+]
+
+// We upload the sample project to App Store Connect.
+// The version for it in the Info.plist must be a period-separated list of at most three non-negative integers.
+// Find out more at https://developer.apple.com/documentation/bundleresources/information_property_list/cfbundleshortversionstring"
+// Therefore we remove the alpha and beta suffixes
+let filesWithoutPreviewSuffix = [
     "./Samples/iOS-Swift/iOS-Swift.xcodeproj/project.pbxproj"
 ]
 
@@ -22,16 +29,28 @@ if regex.firstMatch(in: args[1]) == nil {
 let fromVersionFileHandler = try open(fromVersionFile)
 let fromFileContent: String = fromVersionFileHandler.read()
 
+func updateVersion(_ file: String, _ fromVersion: String, _ toVersion: String) throws {
+    let readFile = try open(file)
+    let contents: String = readFile.read()
+    let newContents = contents.replacingOccurrences(of: fromVersion, with: toVersion)
+    let overwriteFile = try! open(forWriting: file, overwrite: true)
+    overwriteFile.write(newContents)
+    overwriteFile.close()
+}
+
+func removePreviewSuffix(_ version: String) -> String {
+    return version.split(separator: "-").first?.base ?? version
+}
+
 for match in Regex(semver, options: [.dotMatchesLineSeparators]).allMatches(in: fromFileContent) {
     let fromVersion = match.matchedString
     let toVersion = args[1]
 
     for file in files {
-        let readFile = try open(file)
-        let contents: String = readFile.read()
-        let newContents = contents.replacingOccurrences(of: fromVersion, with: toVersion)
-        let overwriteFile = try! open(forWriting: file, overwrite: true)
-        overwriteFile.write(newContents)
-        overwriteFile.close()
+        try updateVersion(file, fromVersion, toVersion)
+    }
+
+    for file in filesWithoutPreviewSuffix {
+        try updateVersion(file, removePreviewSuffix(fromVersion), removePreviewSuffix(toVersion))
     }
 }
