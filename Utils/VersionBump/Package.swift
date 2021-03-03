efb71026c312d8d01c2f68de5d239db75435939a
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-03-03 10:46:25 +0100

    Remove preview version suffix also for Sentry

--- Utils/VersionBump/Package.swift
+++ Utils/VersionBump/Package.swift
@@ -7,9 +7,7 @@ let package = Package(
         .executable(name: "VersionBump", targets: ["VersionBump"])
     ],
     dependencies: [
-        // We need to use the 5.1.0-beta.1, because otherwise we can't compile with Swift 5.3
-        // see https://github.com/kareman/SwiftShell/releases/tag/5.1.0-beta.1
-        .package(url: "https://github.com/kareman/SwiftShell.git", from: "5.1.0-beta.1"),
+        .package(url: "https://github.com/kareman/SwiftShell.git", from: "5.1.0"),
         .package(url: "https://github.com/sharplet/Regex.git", from: "2.1.1")
     ],
     targets: [
