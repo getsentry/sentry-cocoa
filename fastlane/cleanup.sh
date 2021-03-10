d553a1425b5e792c892350f6ee02919523777fe2
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-03-10 14:02:49 +0100

    ci: Encrypt Match and Appfile of Fastlane
    We don't want to have the details in the Appfile and Matchfile of Fastlane public.
    Therefore we store them encrypted.

---
+++ fastlane/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -f ./fastlane/Appfile
+rm -f ./fastlane/Matchfile
---
+++ fastlane/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ENCRYPT_KEY=$1
+
+# Decrypt Matchfile
+openssl aes-256-cbc -md sha256 -d -in ./fastlane/Matchfile.aes -out ./fastlane/Matchfile -k $ENCRYPT_KEY
+
+# Decrypt Appfile
+openssl aes-256-cbc -md sha256 -d -in ./fastlane/Appfile.aes -out ./fastlane/Appfile -k $ENCRYPT_KEY
