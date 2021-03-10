07b75dbb30978f6bebefcbaebd9133c59d598c5a
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-03-10 14:31:29 +0100

    Use -pass password

--- fastlane/setup.sh
+++ fastlane/setup.sh
@@ -3,7 +3,7 @@
 ENCRYPT_KEY=$1
 
 # Decrypt Matchfile
-openssl aes-256-cbc -md sha256 -d -in ./fastlane/Matchfile.aes -out ./fastlane/Matchfile -k $ENCRYPT_KEY
+openssl aes-256-cbc -md sha256 -d -in ./fastlane/Matchfile.aes -out ./fastlane/Matchfile -pass password -k $ENCRYPT_KEY
 
 # Decrypt Appfile
-openssl aes-256-cbc -md sha256 -d -in ./fastlane/Appfile.aes -out ./fastlane/Appfile -k $ENCRYPT_KEY
+openssl aes-256-cbc -md sha256 -d -in ./fastlane/Appfile.aes -out ./fastlane/Appfile -pass password -k $ENCRYPT_KEY
