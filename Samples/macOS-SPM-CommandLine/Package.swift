dd880f3da9884282cd0cde294af6118638b275cb
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-04-30 15:23:10 +0200

    ci: Add sample to validate SPM (#1103)
    Add a simple command-line sample that includes Sentry via Swift Package Manager.
    Build this sample in CI to validate SPM.
    
    Co-authored-by: Sentry Github Bot <bot+github-bot@sentry.io>
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

---
+++ Samples/macOS-SPM-CommandLine/Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "macOS-SPM-CommandLine",
+    dependencies: [
+        // branch is replaced in CI to the current sha
+        .package(name: "Sentry", url: "https://github.com/getsentry/sentry-cocoa", .branch("master") )
+    ],
+    targets: [
+        .target(
+            name: "macOS-SPM-CommandLine",
+            dependencies: ["Sentry"])
+    ]
+)
