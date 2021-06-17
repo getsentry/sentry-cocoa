d2ec546d42360d1b73bef651145af94756e947ef
Dhiogo Brustolin <dhiogorb@gmail.com>
2021-06-17 07:31:20 -0300

    Update Sources/Sentry/include/SentrySDK+Private.h
    Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>

--- Sources/Sentry/include/SentrySDK+Private.h
+++ Sources/Sentry/include/SentrySDK+Private.h
@@ -19,7 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (SentryHub *)currentHub;
 
-+ (nullable SentryOptions *)options;
+@property (nonatomic, nullable, readonly, class) SentryOptions *options;
 
 /**
  * Needed by hybrid SDKs as react-native to synchronously store an envelope to disk.
