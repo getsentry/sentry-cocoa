921eba595c6920ec9246884f3c65531005320cbc
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-06-21 08:57:30 +0200

    Update Samples/Carthage-Validation/Framework/Framework/AppDelegate.swift
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

--- Samples/Carthage-Validation/Framework/Framework/AppDelegate.swift
+++ Samples/Carthage-Validation/Framework/Framework/AppDelegate.swift
@@ -9,7 +9,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         SentrySDK.start { options in
             options.dsn = "https://a92d50327ac74b8b9aa4ea80eccfb267@o447951.ingest.sentry.io/5428557"
             options.debug = true
-            options.sessionTrackingIntervalMillis = 5_000
         }
         
         return true
