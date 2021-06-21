729a3b5d9e5dceb091a572d231db0b61ce214fc9
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-06-21 11:19:26 +0200

    Update select xcode link

--- scripts/ci-select-xcode.sh
+++ scripts/ci-select-xcode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # For available Xcode versions on Github Action, see 
-# https://github.com/actions/virtual-environments/blob/main/images/macos/macos-11-Readme.md#xcode
+https://github.com/actions/virtual-environments/blob/main/images/macos/macos-11-Readme.md#xcode
 
 sudo xcode-select -s /Applications/Xcode_12.5.app/Contents/Developer
