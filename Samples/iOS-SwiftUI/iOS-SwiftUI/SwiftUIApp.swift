6e2942a222d3a66c347c42a921e3022e668624ef
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-07-19 10:37:08 +0200

    Fix SwiftLint

--- Samples/iOS-SwiftUI/iOS-SwiftUI/SwiftUIApp.swift
+++ Samples/iOS-SwiftUI/iOS-SwiftUI/SwiftUIApp.swift
@@ -2,7 +2,7 @@ import Sentry
 import SwiftUI
 
 @main
-struct iOS_SwiftUIApp: App {
+struct SwiftUIApp: App {
     init() {
         SentrySDK.start { options in
             options.dsn = "https://a92d50327ac74b8b9aa4ea80eccfb267@o447951.ingest.sentry.io/5428557"
