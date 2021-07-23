3a691c0fbd6a5d71ad7265a11cad73969cb80642
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-07-23 12:18:34 +0200

    Use returns in observeValueForKeyPath to reduce nesting

--- Sources/Sentry/SentryNetworkTracker.m
+++ Sources/Sentry/SentryNetworkTracker.m
@@ -111,33 +111,37 @@ SentryNetworkTracker ()
                         change:(NSDictionary<NSKeyValueChangeKey, id> *)change
                        context:(void *)context
 {
-    if ([keyPath isEqualToString:NSStringFromSelector(@selector(state))]) {
-        NSURLSessionTask *sessionTask = object;
-        if (sessionTask.state != NSURLSessionTaskStateRunning) {
-            id<SentrySpan> netSpan;
-            @synchronized(sessionTask) {
-                netSpan
-                    = objc_getAssociatedObject(sessionTask, &SENTRY_NETWORK_REQUEST_TRACKER_SPAN);
-                // We'll just go through once
-                objc_setAssociatedObject(sessionTask, &SENTRY_NETWORK_REQUEST_TRACKER_SPAN, nil,
-                    OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-            }
-
-            if (netSpan != nil) {
-                NSInteger responseStatusCode = [self urlResponseStatusCode:sessionTask.response];
-
-                if (responseStatusCode != -1) {
-                    [netSpan setDataValue:[NSNumber numberWithInteger:responseStatusCode]
-                                   forKey:@"http.status_code"];
-                }
-
-                [netSpan finishWithStatus:[self statusForSessionTask:sessionTask]];
-                [sessionTask removeObserver:self forKeyPath:NSStringFromSelector(@selector(state))];
-                [SentryLog logWithMessage:@"Finished HTTP span for sessionTask"
-                                 andLevel:kSentryLevelDebug];
-            }
-        }
+    if (![keyPath isEqualToString:NSStringFromSelector(@selector(state))]) {
+        return;
     }
+
+    NSURLSessionTask *sessionTask = object;
+    if (sessionTask.state == NSURLSessionTaskStateRunning) {
+        return;
+    }
+
+    id<SentrySpan> netSpan;
+    @synchronized(sessionTask) {
+        netSpan = objc_getAssociatedObject(sessionTask, &SENTRY_NETWORK_REQUEST_TRACKER_SPAN);
+        // We'll just go through once
+        objc_setAssociatedObject(sessionTask, &SENTRY_NETWORK_REQUEST_TRACKER_SPAN, nil,
+            OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+
+    if (netSpan == nil) {
+        return;
+    }
+
+    NSInteger responseStatusCode = [self urlResponseStatusCode:sessionTask.response];
+
+    if (responseStatusCode != -1) {
+        [netSpan setDataValue:[NSNumber numberWithInteger:responseStatusCode]
+                       forKey:@"http.status_code"];
+    }
+
+    [netSpan finishWithStatus:[self statusForSessionTask:sessionTask]];
+    [sessionTask removeObserver:self forKeyPath:NSStringFromSelector(@selector(state))];
+    [SentryLog logWithMessage:@"Finished HTTP span for sessionTask" andLevel:kSentryLevelDebug];
 }
 
 - (NSInteger)urlResponseStatusCode:(NSURLResponse *)response
