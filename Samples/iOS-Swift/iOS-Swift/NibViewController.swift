f0e5223bf330ec27cee95b9072c9e14cba05988b
Dhiogo Brustolin <dhiogorb@gmail.com>
2021-08-19 16:17:33 -0300

    added a nib controller

---
+++ Samples/iOS-Swift/iOS-Swift/NibViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NibViewController.swift
+//  iOS-Swift
+//
+//  Created by Dhiogo Brustolin on 19/08/21.
+//  Copyright © 2021 Sentry. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NibViewController : UIViewController {
+    
+    @IBOutlet var button : UIButton!
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
