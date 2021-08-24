c35098ad8216a4d3215db6db3b039c63fe86d081
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-08-24 10:04:42 +0200

    Add span

--- Samples/iOS-Swift/iOS-Swift/LoremIpsumViewController.swift
+++ Samples/iOS-Swift/iOS-Swift/LoremIpsumViewController.swift
@@ -9,10 +9,11 @@ class LoremIpsumViewController: UIViewController {
         super.viewDidLoad()
         
         let dispatchQueue = DispatchQueue(label: "LoremIpsumViewController")
-        
         dispatchQueue.async {
+            let span = SentrySDK.span?.startChild(operation: "io", description: "Read Lorem Ipsum")
             if let path = Bundle.main.path(forResource: "LoremIpsum", ofType: "txt") {
                 if let contents = FileManager.default.contents(atPath: path) {
+                    span?.finish(status: .ok)
                     DispatchQueue.main.async {
                         self.textView.text = String(data: contents, encoding: .utf8)
                     }
