fc318f3eb8a743598f9d2bc015c0c5a0f9329a91
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-10-19 10:58:01 +0200

    test: Add UIHostingController to iOS-Swift sample (#1403)

---
+++ Samples/iOS-Swift/iOS-Swift/SwiftUI.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SwiftUI: View {
+    var body: some View {
+        Text("SwiftUI!")
+    }
+}
+
+struct SwiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUI()
+    }
+}
