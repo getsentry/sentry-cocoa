ed0cac85094cc6f84f89111bdbaf86208b8600a7
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-11-18 14:27:58 +0100

    test: Add Vapor Test Server

--- Tests/SentryTests/Integrations/Performance/Network/SentryNetworkTrackerIntegrationTests.swift
+++ Tests/SentryTests/Integrations/Performance/Network/SentryNetworkTrackerIntegrationTests.swift
@@ -3,7 +3,7 @@ import XCTest
 class SentryNetworkTrackerIntegrationTests: XCTestCase {
     
     private static let dsnAsString = TestConstants.dsnAsString(username: "SentryNetworkTrackerIntegrationTests")
-    private static let testURL = URL(fileURLWithPath: "")
+    private static let testURL = URL(string: "https://localhost:8080/sentry-logo-black.png")!
     private static let transactionName = "TestTransaction"
     private static let transactionOperation = "Test"
     
---
+++ test-server/Sources/App/routes.swift
@@ -0,0 +1,11 @@
+import Vapor
+
+func routes(_ app: Application) throws {
+    app.get { req in
+        return "It works!"
+    }
+
+    app.get("hello") { req -> String in
+        return "Hello, world!"
+    }
+}
