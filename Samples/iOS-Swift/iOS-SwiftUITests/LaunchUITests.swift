875223c6df91f278199ed4e54638c772af34b95d
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-11-19 16:12:27 +0100

    Refactor tests

--- Samples/iOS-Swift/iOS-SwiftUITests/LaunchUITests.swift
+++ Samples/iOS-Swift/iOS-SwiftUITests/LaunchUITests.swift
@@ -3,42 +3,44 @@ import XCTest
 class LaunchUITests: XCTestCase {
     
     private let timeout: TimeInterval = 10
+    private let app: XCUIApplication = XCUIApplication()
 
     override func setUpWithError() throws {
         try super.setUpWithError()
         continueAfterFailure = false
-    }
-
-    func testLaunch() {
-        let app = XCUIApplication()
-        app.launch()
         
+        app.launch()
         XCUIDevice.shared.orientation = .portrait
         
         waitForExistenseOfMainScreen()
-        
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        app.terminate()
+    }
+
+    func testLoremIpsum() {
+        app.buttons["Lorem Ipsum"].tap()
+        XCTAssertTrue(app.textViews.firstMatch.waitForExistence(timeout: timeout), "Lorem Ipsum not loaded.")
+    }
+    
+    func testNavigationTransaction() {
         app.buttons["Test Navigation Transaction"].tap()
         XCTAssertTrue(app.images.firstMatch.waitForExistence(timeout: timeout), "Navigation transaction not loaded.")
-        app.swipeDown(velocity: .fast)
-        
-        waitForExistenseOfMainScreen()
-        
+    }
+    
+    func testShowNib() {
         app.buttons["Show Nib"].tap()
         XCTAssertTrue(app.buttons["Button"].waitForExistence(timeout: timeout), "Show Nib not loaded.")
-        app.swipeDown(velocity: .fast)
-        
-        waitForExistenseOfMainScreen()
-        
+    }
+    
+    func testShowSwiftUI() {
         app.buttons["Show SwiftUI"].tap()
         XCTAssertTrue(app.staticTexts["SwiftUI!"].waitForExistence(timeout: timeout), "SwiftUI not loaded.")
-        app.swipeDown(velocity: .fast)
-        
-        waitForExistenseOfMainScreen()
-        
-        app.terminate()
-        
-        func waitForExistenseOfMainScreen() {
-            XCTAssertTrue(app.buttons["captureMessage"].waitForExistence(timeout: timeout), "Home Screen doesn't exist.")
-        }
+    }
+    
+    private func waitForExistenseOfMainScreen() {
+        XCTAssertTrue(app.buttons["captureMessage"].waitForExistence(timeout: timeout), "Home Screen doesn't exist.")
     }
 }
