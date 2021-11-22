56113cce105bfebecaacb6ab80ced4a455b8f0b3
Dhiogo Ramos Brustolin <dhiogorb@gmail.com>
2021-11-22 10:10:16 -0300

    iOS swift 10 sample

---
+++ Samples/iOS-Swift/iOS10-Swift/AppDelegate.swift
@@ -0,0 +1,32 @@
+import UIKit
+import Sentry
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    static let defaultDSN = "https://a92d50327ac74b8b9aa4ea80eccfb267@o447951.ingest.sentry.io/5428557"
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // For testing purposes, we want to be able to change the DSN and store it to disk. In a real app, you shouldn't need this behavior.
+        let dsn = DSNStorage.shared.getDSN() ?? AppDelegate.defaultDSN
+        DSNStorage.shared.saveDSN(dsn: dsn)
+        
+        SentrySDK.start { options in
+            options.dsn = dsn
+            options.beforeSend = { event in
+                return event
+            }
+            options.debug = true
+            // Sampling 100% - In Production you probably want to adjust this
+            options.tracesSampleRate = 1.0
+            options.sessionTrackingIntervalMillis = 5_000
+        }
+        
+        return true
+    }
+
+}
+
