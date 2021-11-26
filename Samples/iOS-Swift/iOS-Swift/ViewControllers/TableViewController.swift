73f9b4972f1a1cdae1c16e3cf643582db1cd5e7d
Dhiogo Brustolin <dhiogorb@gmail.com>
2021-11-26 10:33:29 -0300

    test: Add different types of UIViewController to Swift sample (#1478)
    Added more UIViewController types to our test sample.
    
    Closes #1469, #1470
    
    Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>

---
+++ Samples/iOS-Swift/iOS-Swift/ViewControllers/TableViewController.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL") ?? UITableViewCell(style: .default, reuseIdentifier: "CELL")
+        
+        cell.backgroundColor = UIColor(white: 1.0 - CGFloat(Double(indexPath.row) / 99.0), alpha: 1)
+        
+        return cell
+    }
+    
+}
