c622afa893b2ec9b764d7a262094ea03ef512084
Dhiogo Ramos Brustolin <dhiogorb@gmail.com>
2021-11-26 08:53:22 -0300

    Update TableViewController.swift

--- Samples/iOS-Swift/iOS-Swift/ViewControllers/TableViewController.swift
+++ Samples/iOS-Swift/iOS-Swift/ViewControllers/TableViewController.swift
@@ -14,10 +14,7 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CELL") ?? UITableViewCell(style: .default, reuseIdentifier: "CELL")
         
-        let g = Double(indexPath.row) / 99
-        let r = 1.0 - g
-        
-        cell.backgroundColor = UIColor(red: r, green: g, blue: 0, alpha: 1)
+        cell.backgroundColor = UIColor(white: 1.0 - Double(indexPath.row) / 99.0, alpha: 1)
         
         return cell
     }
