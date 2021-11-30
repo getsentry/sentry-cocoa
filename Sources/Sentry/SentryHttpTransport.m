954126f198bb959a4169fe5180cc74da1d4e2e57
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-11-30 14:21:24 +0100

    ref: Remove not needed todos in HttpTransport
    The todos in SentryHttpTransport are out of date and don't make
    any sense. Hence, they can be removed.

--- Sources/Sentry/SentryHttpTransport.m
+++ Sources/Sentry/SentryHttpTransport.m
@@ -145,7 +145,6 @@ SentryHttpTransport ()
 
 #pragma mark private methods
 
-// TODO: This has to move somewhere else, we are missing the whole beforeSend flow
 - (void)sendAllCachedEnvelopes
 {
     @synchronized(self) {
@@ -207,7 +206,6 @@ SentryHttpTransport ()
     [self.requestManager
                addRequest:request
         completionHandler:^(NSHTTPURLResponse *_Nullable response, NSError *_Nullable error) {
-            // TODO: How does beforeSend work here
 
             // If the response is not nil we had an internet connection.
             // We don't worry about errors here.
