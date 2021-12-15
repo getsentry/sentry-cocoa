147038abf12a2cf6fe678abee2af6d5d9fa093ee
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-12-15 18:16:40 +0100

    comment for apply-patch

--- scripts/apply-patch.sh
+++ scripts/apply-patch.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+# Downloads a patch file from sentry-cocoa and replaces the __GITHUB_REVISION_PLACEHOLDER__ in the patch file
+# with the specified git commit hash.
+# We use github.event.pull_request.head.sha instead of github.sha when available as 
+# the github.sha is the pre merge commit id for PRs.
+# See https://github.community/t/github-sha-isnt-the-value-expected/17903/17906.
+
 PULL_REQUEST_SHA="${1}"
 GITHUB_SHA="${2}"
 PATCH_FILE="${3}"
