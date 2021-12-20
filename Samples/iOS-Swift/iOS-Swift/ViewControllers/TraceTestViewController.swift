c8bc1d16c711fed7a278e4e18cdca7b83965e869
Philipp Hofmann <philipp.hofmann@sentry.io>
2021-12-20 11:45:42 +0100

    Undo changes in TraceTestViewController

--- Samples/iOS-Swift/iOS-Swift/ViewControllers/TraceTestViewController.swift
+++ Samples/iOS-Swift/iOS-Swift/ViewControllers/TraceTestViewController.swift
@@ -5,10 +5,13 @@ import UIKit
 class TraceTestViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    var spanObserver: SpanObserver?
+    var lifeCycleSteps = ["loadView"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
+        appendLifeCycleStep("viewDidLoad")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -23,10 +26,52 @@ class TraceTestViewController: UIViewController {
                     SentrySDK.capture(error: err)
                 } else if let image = data {
                     self.imageView.image = UIImage(data: image)
+                    self.appendLifeCycleStep("GET https://sentry-brand.storage.googleapis.com/sentry-logo-black.png")
                 }
             }
         }
         
         dataTask.resume()
+        spanObserver = createTransactionObserver(forCallback: assertTransaction)
+        appendLifeCycleStep("viewWillAppear")
+        appendLifeCycleStep("viewAppearing")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        appendLifeCycleStep("viewDidAppear")
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        appendLifeCycleStep("viewWillLayoutSubviews")
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        appendLifeCycleStep("viewDidLayoutSubviews")
+    }
+    
+    func appendLifeCycleStep(_ name: String) {
+        if spanObserver?.span.isFinished == false {
+            lifeCycleSteps.append(name)
+        }
+    }
+    
+    func assertTransaction(span: Span) {
+        spanObserver?.releaseOnFinish()
+        guard let children = span.children() else {
+            UIAssert.fail("Transaction has no children")
+            return
+        }
+ 
+        guard let child = children.first(where: { $0.context.operation == "http.client" }) else {
+            UIAssert.fail("Did not found http request child")
+            return
+        }
+        
+        UIAssert.isEqual(child.data?["url"] as? String, "/sentry-logo-black.png", "Could not read url data value")
+        
+        UIAssert.isEqual(child.tags["http.status_code"], "200", "Could not read status_code tag value")
+                
+        UIAssert.checkForViewControllerLifeCycle(span, expectingSpans: 12, viewController: "TraceTestViewController", stepsToCheck: lifeCycleSteps)
     }
 }
