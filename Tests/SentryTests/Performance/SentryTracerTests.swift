9af612e2c05886bd5937fd8f27fc36c9e545651d
Dhiogo Brustolin <dhiogorb@gmail.com>
2021-12-28 14:44:06 -0300

    Update Tests/SentryTests/Performance/SentryTracerTests.swift
    Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>

--- Tests/SentryTests/Performance/SentryTracerTests.swift
+++ Tests/SentryTests/Performance/SentryTracerTests.swift
@@ -236,12 +236,12 @@ class SentryTracerTests: XCTestCase {
     }
     
     func testFinish_WithUnfinishedChildren() {
+        CurrentDate.setCurrentDateProvider(DefaultCurrentDateProvider.sharedInstance())
         let sut = fixture.getSut(waitForChildren: false)
         let child1 = sut.startChild(operation: fixture.transactionOperation)
         let child2 = sut.startChild(operation: fixture.transactionOperation)
         let child3 = sut.startChild(operation: fixture.transactionOperation)
         child2.finish()
-        fixture.currentDateProvider.setDate(date: Date(timeIntervalSinceNow: 0.1))
         sut.finish()
         
         XCTAssertTrue(child1.isFinished)
