c3610a196939ade81fa9553ab3cf6652a74b804c
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-01-04 11:28:58 +0100

    another fix

--- scripts/tests-with-thread-sanitizer.sh
+++ scripts/tests-with-thread-sanitizer.sh
@@ -4,7 +4,6 @@
 # the logs only show failing tests, but don't highlight the threading issues.
 # Therefore we print a hint to find the threading issues.
 
-# Don't use xcpretty -t to be able to see the log output of the ThreadSanitizer
 env NSUnbufferedIO=YES xcodebuild -workspace Sentry.xcworkspace -scheme Sentry -configuration Test -enableThreadSanitizer YES -destination "platform=iOS Simulator,OS=latest,name=iPhone 11" test | tee thread-sanitizer.log | xcpretty -t
 
 testStatus=$?
@@ -13,6 +12,6 @@ if [ $testStatus -eq 0 ]; then
     echo "ThreadSanitizer didn't find problems."
     exit 0
 else
-    echo "ThreadSanitizer found problems or one of the tests failed. Search for \"ThreadSanitizer\" in the log for more details."
+    echo "ThreadSanitizer found problems or one of the tests failed. Search for \"ThreadSanitizer\" in the thread-sanitizer.log artifact for more details."
     exit 1
 fi
