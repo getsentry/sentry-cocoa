34257ca1587c2608aa46943d91074798e6f03265
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-01-11 11:06:01 +0100

    disable xcpretty to see logs

--- scripts/xcode-test.sh
+++ scripts/xcode-test.sh
@@ -50,5 +50,5 @@ if [ $PLATFORM == "iOS" -a $OS == "12.4" ]; then
 else 
     env NSUnbufferedIO=YES xcodebuild -workspace Sentry.xcworkspace -scheme Sentry -configuration Test \
         GCC_GENERATE_TEST_COVERAGE_FILES=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES -destination "$DESTINATION" \
-        test | xcpretty -t && exit ${PIPESTATUS[0]}
+        test
 fi
