23095e56a90c191d150783915b790e44f28ad042
Dhiogo Brustolin <dhiogorb@gmail.com>
2022-01-24 12:09:03 +0100

    Adding extra test

---
+++ Samples/iOS-Swift/iOS-SwiftUITests/SentryFileIOTrackingIntegrationTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+
+class SentryFileIOTrackingIntegrationTests: XCTestCase {
+    
+    private let app: XCUIApplication = XCUIApplication()
+    
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+        XCUIDevice.shared.orientation = .portrait
+        app.launch()
+        
+        waitForExistenseOfMainScreen()
+    }
+    
+    override func tearDown() {
+        app.terminate()
+        super.tearDown()
+    }
+
+    func testLoremIpsum() {
+        app.buttons["loremIpsumButton"].tap()
+        XCTAssertTrue(app.textViews.firstMatch.waitForExistence(), "Lorem Ipsum not loaded.")
+    }
+    
+    private func waitForExistenseOfMainScreen() {
+        XCTAssertTrue(app.buttons["captureMessageButton"].waitForExistence(), "Home Screen doesn't exist.")
+    }
+}
