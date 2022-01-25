bf62c127a9e41d6d41dac3e65264707cd60dff6e
Dhiogo Brustolin <dhiogorb@gmail.com>
2022-01-25 13:37:41 +0100

    Update Tests/SentryTests/Integrations/Performance/IO/SentryFileIOTrackingIntegrationTests.swift
    Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>

--- Tests/SentryTests/Integrations/Performance/IO/SentryFileIOTrackingIntegrationTests.swift
+++ Tests/SentryTests/Integrations/Performance/IO/SentryFileIOTrackingIntegrationTests.swift
@@ -2,9 +2,10 @@ import Foundation
 import Sentry
 import XCTest
  
-//This test is also executed under iOS-SwiftUITests
-//Thats why we need to keep it generic, without access
-//to any private part of the sdk
+// This test is also executed under iOS-SwiftUITests, because
+// GitHub Actions doesn't have simulators for iOS 11 and 10.
+// That's why we need to keep it generic, without access
+// to any private part of the SDK.
 class SentryFileIOTrackingIntegrationTests: XCTestCase {
 
     private class Fixture {
