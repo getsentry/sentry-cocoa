469f83d242ae8caaec2499d21ede6a140a8e933a
Michael Link <mlink@falkor.com>
2022-02-03 15:51:12 -0600

    Two other lines where path was omitted from log format
    Add `path` back to log lines where it was missing

--- Sources/SentryCrash/Recording/Tools/SentryCrashFileUtils.c
+++ Sources/SentryCrash/Recording/Tools/SentryCrashFileUtils.c
@@ -96,7 +96,7 @@ dirContents(const char *path, char ***entries, int *count)
     }
     dir = opendir(path);
     if (dir == NULL) {
-        SentryCrashLOG_ERROR("Error reading directory %s: %s", strerror(errno));
+        SentryCrashLOG_ERROR("Error reading directory %s: %s", path, strerror(errno));
         goto done;
     }
 
@@ -144,7 +144,7 @@ deletePathContents(const char *path, bool deleteTopLevelPathAlso)
 {
     struct stat statStruct = { 0 };
     if (stat(path, &statStruct) != 0) {
-        SentryCrashLOG_ERROR("Could not stat %s: %s", strerror(errno));
+        SentryCrashLOG_ERROR("Could not stat %s: %s", path, strerror(errno));
         return false;
     }
     if (S_ISDIR(statStruct.st_mode)) {
