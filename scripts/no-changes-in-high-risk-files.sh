58bdec44d553c6e1d338f9607c40d1f7c5e8c349
Dhiogo Brustolin <dhiogorb@gmail.com>
2022-03-01 07:50:29 -0300

    Update no-changes-in-high-risk-files.sh

--- scripts/no-changes-in-high-risk-files.sh
+++ scripts/no-changes-in-high-risk-files.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # To update the sha run shasum -a 256 ./Sources/Sentry/SentryNSURLSessionTaskSearch.m and copy the result in EXPECTED.
 
 ACTUAL=$(shasum -a 256 ./Sources/Sentry/SentryNSURLSessionTaskSearch.m)
-EXPECTED="ef3e89f281909766117c179e54dd189c19e302a1a9d8132639f26d390b95ad8f  ./Sources/Sentry/SentryNSURLSessionTaskSearch.m"
+EXPECTED="e07d82b30d1b66c75c1722c7c5b17be1dc273aaaaba749c14537d902eda38d4c  ./Sources/Sentry/SentryNSURLSessionTaskSearch.m"
 
 if [ "$ACTUAL" = "$EXPECTED" ]; then
     echo "No changes in high risk files."
