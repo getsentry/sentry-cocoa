74fcd2e6432dcdd5a93e6d5d120c6da60348ae1f
Indragie Karunaratne <indragie.karunaratne@sentry.io>
2022-03-08 14:55:31 -0800

    Add missing import

--- Sources/Sentry/Profiling/SentryAsyncSafeLogging.h
+++ Sources/Sentry/Profiling/SentryAsyncSafeLogging.h
@@ -1,3 +1,5 @@
+#include <unistd.h>
+
 // write(2) is async signal safe:
 // http://man7.org/linux/man-pages/man7/signal-safety.7.html
 #define __SENTRY_LOG_ASYNC_SAFE(fd, str) write(fd, str, sizeof(str) - 1)
