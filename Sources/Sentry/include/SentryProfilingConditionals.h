a01b236fe628e2f2aa2adf716a4eb23ebe1287aa
Indragie Karunaratne <indragie.karunaratne@sentry.io>
2022-03-12 13:36:41 -0800

    Fix SENTRY_TARGET_PROFILING_SUPPORTED

--- Sources/Sentry/include/SentryProfilingConditionals.h
+++ Sources/Sentry/include/SentryProfilingConditionals.h
@@ -5,7 +5,10 @@
 
 // tvOS and watchOS do not support the kernel APIs required by our profiler
 // e.g. mach_msg, thread_suspend, thread_resume
-#define SENTRY_TARGET_PROFILING_SUPPORTED                                                          \
-    (TARGET_OS_IOS || TARGET_OS_MACCATALYST || TARGET_OS_SIMULATOR || TARGET_OS_OSX)
+#if TARGET_OS_WATCH || TARGET_OS_TV
+#    define SENTRY_TARGET_PROFILING_SUPPORTED 0
+#else
+#    define SENTRY_TARGET_PROFILING_SUPPORTED 1
+#endif
 
 #endif /* SentryProfilingConditionals_h */
