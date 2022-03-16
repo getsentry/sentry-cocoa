095d268a1e57a082abd820bba98e0ad72bb1b3c0
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-03-16 11:49:53 +0100

    Small comment

--- Tests/SentryTests/Helper/SentryTestThreadWrapper.swift
+++ Tests/SentryTests/Helper/SentryTestThreadWrapper.swift
@@ -3,7 +3,7 @@ import Foundation
 class SentryTestThreadWrapper: SentryThreadWrapper {
     
     override func sleep(forTimeInterval timeInterval: TimeInterval) {
-        
+        // Don't sleep. Do nothing.
     }
 
 }
