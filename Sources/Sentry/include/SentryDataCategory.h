3d96708fcbee16e9d3fd9bc2ad900e44296ac21d
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-03-24 12:11:34 +0100

    ref: Fix code comment on DataCategory (#1722)
    For context see https://github.com/getsentry/develop/pull/529.

--- Sources/Sentry/include/SentryDataCategory.h
+++ Sources/Sentry/include/SentryDataCategory.h
@@ -1,9 +1,9 @@
 #import <Foundation/Foundation.h>
 
 /**
- * The data category used for envelopes: https://develop.sentry.dev/sdk/envelopes/#data-model, rate
- * limits: https://develop.sentry.dev/sdk/rate-limiting/#definitions and client reports:
- * https://develop.sentry.dev/sdk/client-reports/#envelope-item-payload.
+ * The data category rate limits: https://develop.sentry.dev/sdk/rate-limiting/#definitions and
+ * client reports: https://develop.sentry.dev/sdk/client-reports/#envelope-item-payload. Be aware
+ * that these categories are different from the envelope item types.
  */
 typedef NS_ENUM(NSUInteger, SentryDataCategory) {
     kSentryDataCategoryAll,
