758519cbd4c38b8ee11927e11e005ad81b5f744d
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-04-21 17:11:40 +0200

    fix: User feedback crash (#1766)
    All fields are nonnull. Setting these to an empty string in the init
    solves the problem.
    
    Fixes GH-1765

--- Tests/SentryTests/Protocol/SentryUserFeedbackTests.swift
+++ Tests/SentryTests/Protocol/SentryUserFeedbackTests.swift
@@ -2,6 +2,14 @@ import XCTest
 
 class SentryUserFeedbackTests: XCTestCase {
     
+    func testPropertiesAreSetToEmptyString() {
+        let userFeedback = UserFeedback(eventId: SentryId())
+        
+        XCTAssertEqual("", userFeedback.comments)
+        XCTAssertEqual("", userFeedback.email)
+        XCTAssertEqual("", userFeedback.name)
+    }
+    
     func testSerialize() {
         let userFeedback = UserFeedback(eventId: SentryId())
         userFeedback.comments = "Fix this please."
@@ -14,5 +22,15 @@ class SentryUserFeedbackTests: XCTestCase {
         XCTAssertEqual(userFeedback.comments, actual["comments"] as? String)
         XCTAssertEqual(userFeedback.email, actual["email"] as? String)
         XCTAssertEqual(userFeedback.name, actual["name"] as? String)
+    }
+    
+    func testSerialize_WithoutSettingProperties_AllAreEmptyStrings() {
+        let userFeedback = UserFeedback(eventId: SentryId())
+        
+        let actual = userFeedback.serialize()
+        
+        XCTAssertEqual("", actual["comments"] as? String)
+        XCTAssertEqual("", actual["email"] as? String)
+        XCTAssertEqual("", actual["name"] as? String)
     }
 }
