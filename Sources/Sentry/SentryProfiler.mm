a052545132b72511c5839b6b8f85a1ccd70118ef
Indragie Karunaratne <indragie.karunaratne@sentry.io>
2022-04-26 23:01:46 -0700

    fix: Fix parsing of output from backtrace_symbols()

--- Sources/Sentry/SentryProfiler.mm
+++ Sources/Sentry/SentryProfiler.mm
@@ -64,6 +64,31 @@ isSimulatorBuild()
     return false;
 #    endif
 }
+
+// Parses a symbol that is returned from `backtrace_symbols()`, which encodes information
+// like the frame index, image name, function name, and offset in a single string.
+// e.g.
+// For the input:
+// 2   UIKitCore                           0x00000001850d97ac -[UIFieldEditor _fullContentInsetsFromFonts] + 160
+// This function would return:
+// -[UIFieldEditor _fullContentInsetsFromFonts]
+//
+// If the format does not match the expected format, this returns the input string.
+NSString *parseFunctionName(const char *symbol) {
+    static NSRegularExpression *regex = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        regex = [NSRegularExpression regularExpressionWithPattern:@"\\d+\\s+\\S+\\s+0[xX][0-9a-fA-F]+\\s+(.+)\\s+\\+\\s+\\d+" options:0 error:nil];
+    });
+    const auto symbolNSStr =  [NSString stringWithUTF8String:symbol];
+    const auto match = [regex firstMatchInString:symbolNSStr
+                                         options:0
+                                           range:NSMakeRange(0, [symbolNSStr length])];
+    if (match == nil) {
+        return symbolNSStr;
+    }
+    return [symbolNSStr substringWithRange:[match rangeAtIndex:1]];
+}
 } // namespace
 
 @implementation SentryProfiler {
@@ -133,7 +158,7 @@ isSimulatorBuild()
                     const auto frame = [NSMutableDictionary<NSString *, id> dictionary];
                     frame[@"instruction_addr"] = sentry_formatHexAddress(@(backtrace.addresses[i]));
 #    if defined(DEBUG)
-                    frame[@"function"] = [NSString stringWithUTF8String:symbols[i]];
+                    frame[@"function"] = parseFunctionName(symbols[i]);
 #    endif
                     [frames addObject:frame];
                 }
