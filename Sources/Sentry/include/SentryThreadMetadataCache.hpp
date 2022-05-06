c33d6d91d3ccd0686cbcb9c48c6c3691caeeed7a
Indragie Karunaratne <indragie.karunaratne@sentry.io>
2022-05-06 11:54:55 -0400

    Revert "feat: Collect queue label information for profiles (#1787)"
    This reverts commit 46cefbd20568701b2a017f2b969f081c6e3e8b22.

--- Sources/Sentry/include/SentryThreadMetadataCache.hpp
+++ Sources/Sentry/include/SentryThreadMetadataCache.hpp
@@ -6,7 +6,6 @@
 
 #    include "SentryThreadHandle.hpp"
 
-#    include <cstdint>
 #    include <memory>
 #    include <string>
 
@@ -18,14 +17,9 @@ namespace profiling {
         int priority;
     };
 
-    struct QueueMetadata {
-        std::uint64_t address;
-        std::string label;
-    };
-
     /**
-     * Caches thread and queue metadata (name, priority, etc.) for reuse while profiling,
-     * since querying that metadata every time can be expensive.
+     * Caches thread metadata (name, priority, etc.) for reuse while profiling, since querying that
+     * metadata from the thread every time can be expensive.
      *
      * @note This class is not thread-safe.
      */
@@ -40,15 +34,6 @@ namespace profiling {
          */
         ThreadMetadata metadataForThread(const ThreadHandle &thread);
 
-        /**
-         * Returns the metadata for the queue at the specified address.
-         * @param address The address of the queue to retrieve metadata from.
-         * @note This function assumes that the queue address is valid. If the address
-         * is invalid, the behavior is non-deterministic and will probably crash.
-         * @return @c QueueMetadata for the queue at the specified address.
-         */
-        QueueMetadata metadataForQueue(std::uint64_t address);
-
         ThreadMetadataCache() = default;
         ThreadMetadataCache(const ThreadMetadataCache &) = delete;
         ThreadMetadataCache &operator=(const ThreadMetadataCache &) = delete;
@@ -58,8 +43,7 @@ namespace profiling {
             ThreadHandle::NativeHandle handle;
             ThreadMetadata metadata;
         };
-        std::vector<const ThreadHandleMetadataPair> threadMetadataCache_;
-        std::vector<const QueueMetadata> queueMetadataCache_;
+        std::vector<const ThreadHandleMetadataPair> cache_;
     };
 
 } // namespace profiling
--- Tests/SentryTests/Profiling/SentryThreadMetadataCacheTests.mm
+++ Tests/SentryTests/Profiling/SentryThreadMetadataCacheTests.mm
@@ -35,7 +35,7 @@ threadSpin(void *name)
 
 @implementation SentryThreadMetadataCacheTests
 
-- (void)testRetrievesThreadMetadata
+- (void)testRetrievesMetadata
 {
     pthread_t thread;
     char name[] = "SentryThreadMetadataCacheTests";
@@ -60,7 +60,7 @@ threadSpin(void *name)
     XCTAssertEqual(pthread_join(thread, nullptr), 0);
 }
 
-- (void)testReturnsCachedThreadMetadata
+- (void)testReturnsCachedMetadata
 {
     pthread_t thread;
     char name[] = "SentryThreadMetadataCacheTests";
@@ -102,24 +102,6 @@ threadSpin(void *name)
 
     XCTAssertEqual(pthread_cancel(thread), 0);
     XCTAssertEqual(pthread_join(thread, nullptr), 0);
-}
-
-- (void)testRetrievesQueueMetadata
-{
-    const auto label = "io.sentry.SentryThreadMetadataCacheTests.testQueue";
-    const auto queue = dispatch_queue_create(label, DISPATCH_QUEUE_SERIAL);
-    const auto cache = std::make_shared<ThreadMetadataCache>();
-
-    XCTAssertTrue(cache->metadataForQueue(reinterpret_cast<std::uint64_t>(&queue)).label == label);
-}
-
-- (void)testNullQueueAddressReturnsNoMetadata
-{
-    const auto cache = std::make_shared<ThreadMetadataCache>();
-    const auto metadata = cache->metadataForQueue(0);
-
-    XCTAssertEqual(metadata.address, static_cast<unsigned long long>(0));
-    XCTAssertEqual(metadata.label, "");
 }
 
 @end
