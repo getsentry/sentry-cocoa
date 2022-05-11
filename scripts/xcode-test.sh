bab5134b09ca08059d825e80f1f84e3d1ba11d2b
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-05-11 09:45:38 +0200

    ci: Raw test log as artifact

--- scripts/xcode-test.sh
+++ scripts/xcode-test.sh
@@ -45,9 +45,9 @@ if [ $PLATFORM == "iOS" -a $OS == "12.4" ]; then
         -skip-testing:"SentryTests/SentryNetworkTrackerIntegrationTests/testGetRequest_SpanCreatedAndTraceHeaderAdded" \
         -skip-testing:"SentryTests/SentrySDKTests/testMemoryFootprintOfAddingBreadcrumbs" \
         -skip-testing:"SentryTests/SentrySDKTests/testMemoryFootprintOfTransactions" \
-        test | xcpretty -t && exit ${PIPESTATUS[0]}
+        test | tee raw-test-output.log | xcpretty -t && exit ${PIPESTATUS[0]}
 else 
     env NSUnbufferedIO=YES xcodebuild -workspace Sentry.xcworkspace -scheme Sentry -configuration Test \
         GCC_GENERATE_TEST_COVERAGE_FILES=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES -destination "$DESTINATION" \
-        test | xcpretty -t && exit ${PIPESTATUS[0]}
+        test | tee raw-test-output.log | xcpretty -t && exit ${PIPESTATUS[0]}
 fi
