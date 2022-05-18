9fe486a7cb9f85365241ce7a59b8a69084f9c94b
Andrew McKnight <andrew.mcknight@sentry.io>
2022-05-17 20:15:06 -0800

    fix: last fixed invocation in Sentry build phase

--- scripts/run-swiftlint.sh
+++ scripts/run-swiftlint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-set -x
-env
-
 # Run swiftlint for local builds so issues show up in Xcode. In CI, swiftlint is called externally and so should not run as part of building any targets.
 
-if [[ $SENTRY_CI -eq 1 ]]; then
+if [[ -n ${CI+x} ]]; then
     echo "Detected CI environment. `swiftlint` will run in a dedicated workflow."
     exit 0;
-else
-    echo "Did not detect CI environment. Running swiftlint..."
 fi
 
 /opt/homebrew/bin/swiftlint lint --force-exclude
