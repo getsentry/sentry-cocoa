1baab9cafebf7fc11460e06ba95be4f878ebf72c
Andrew McKnight <andrew.mcknight@sentry.io>
2022-05-17 19:44:09 -0800

    [TEMP] add some debugging output

--- scripts/run-swiftlint.sh
+++ scripts/run-swiftlint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -x
+env
+
 # Run swiftlint for local builds so issues show up in Xcode. In CI, swiftlint is called externally and so should not run as part of building any targets.
 
 if [[ -n ${CI+x} ]]; then
