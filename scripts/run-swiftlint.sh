cf02545a610e8e1cbe4a34d3bc96da3024a77039
Andrew McKnight <andrew.mcknight@sentry.io>
2022-05-18 00:37:42 -0800

    fix: swiftlint fixes and updates (#1838)
    * fix: correct exclusion for any nested Carthage directory
    
    * chore: add swiftlint build phase to Xcode targets
    
    * fix: update swiftlint rule name
    
    due to warning:
    
    ```
    'whitelist_rules' has been renamed to 'only_rules' and will be completely removed in a future release.
    ```
    
    * chore: .gitignore generated swiftlint docs
    
    * fix: swiftlint runs locally and on CI
    
    Run swiftlint in targets' build phases to get errors and warnings integrated into Xcode, but skip it on CI as there's a dedicated workflow to run it against all code.
    
    * fix: correctly check for CI environment variable
    
    * [TEMP] add some debugging output
    
    * [TEMP] add more debug output
    
    * [TEMP] try setting our own env var
    
    * fix: last fixed invocation in Sentry build phase
    
    * fix: correct path to swiftlint script from framework target build phase

---
+++ scripts/run-swiftlint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run swiftlint for local builds so issues show up in Xcode. In CI, swiftlint is called externally and so should not run as part of building any targets.
+
+if [[ -n ${CI+x} ]]; then
+    echo "Detected CI environment. `swiftlint` will run in a dedicated workflow."
+    exit 0;
+fi
+
+/opt/homebrew/bin/swiftlint lint --force-exclude
