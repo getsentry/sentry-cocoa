d05322b3714e04b2458ac93d3d553e2095100ce8
Andrew McKnight <andrew.mcknight@sentry.io>
2022-05-18 17:19:45 -0800

    [TEMP] try thread-based approaches to running a sampler

--- Samples/TrendingMovies/TrendingMovies/SentryBenchmarking.mm
+++ Samples/TrendingMovies/TrendingMovies/SentryBenchmarking.mm
@@ -6,6 +6,8 @@
 #    import <thread>
 #    include <mach/clock.h>
 
+#define SENTRY_BENCHMARKING_THREAD_NAME "io.sentry.BenchmarkingSamplingProfiler"
+
 namespace {
 /// @note: Implementation ported from @c SentryThreadHandle.hpp .
 NSDictionary<NSString *, NSNumber *> *cpuInfoByThread() {
@@ -14,7 +16,6 @@ NSDictionary<NSString *, NSNumber *> *cpuInfoByThread() {
     thread_act_array_t list;
 
     if (task_threads(mach_task_self(), &list, &count) == KERN_SUCCESS) {
-        const auto mainThreadID = pthread_mach_thread_np(pthread_self());
         for (decltype(count) i = 0; i < count; i++) {
             const auto thread = list[i];
 
@@ -28,13 +29,13 @@ NSDictionary<NSString *, NSNumber *> *cpuInfoByThread() {
             if (pthread_getname_np(handle, name, sizeof(name)) != 0) {
                 continue;
             }
-            if (thread == mainThreadID) {
-                namestr = "main";
-            } else {
-                namestr = std::string(name);
-                if (namestr.length() == 0) {
-                    namestr = std::to_string(thread);
-                }
+            namestr = std::string(name);
+            if (namestr.length() == 0) {
+                namestr = std::to_string(thread);
+            }
+
+            if (namestr == SENTRY_BENCHMARKING_THREAD_NAME) {
+                continue;
             }
 
             mach_msg_type_number_t count = THREAD_BASIC_INFO_COUNT;
@@ -52,16 +53,16 @@ NSDictionary<NSString *, NSNumber *> *cpuInfoByThread() {
 
 NSMutableArray<NSDictionary<NSString *, NSNumber *> *> *samples = [NSMutableArray<NSDictionary<NSString *, NSNumber *> *> array];
 
+// MARK: CPUPlugin-based approach
+
 dispatch_source_t source;
 dispatch_queue_t queue;
-}
 
-@implementation SentryBenchmarking
+const auto frequencyHz = 10;
+const auto intervalNs = 1e9 / frequencyHz;
 
-+ (void)startBenchmarkProfile {
+void cpuPluginBasedApproach() {
     const auto attr = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_CONCURRENT, QOS_CLASS_USER_INTERACTIVE, 0);
-    const auto frequencyHz = 10;
-    const auto intervalNs = 1e9 / frequencyHz;
     const auto leewayNs = intervalNs / 2;
     queue = dispatch_queue_create("io.sentry.benchmarking.gcd-scheduler", attr);
     source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
@@ -70,6 +71,90 @@ dispatch_queue_t queue;
     });
     dispatch_source_set_timer(source, dispatch_time(DISPATCH_TIME_NOW, intervalNs), intervalNs, leewayNs);
     dispatch_resume(source);
+}
+
+// MARK: thread-based approach
+
+void
+samplingThreadCleanup(void *buf)
+{
+    free(buf);
+}
+
+void *
+benchmarkingSamplingThreadMain(mach_port_t port, clock_serv_t clock, mach_timespec_t delaySpec)
+{
+    assert(pthread_setname_np(SENTRY_BENCHMARKING_THREAD_NAME) == 0);
+    const int maxSize = 512;
+    const auto bufRequest = reinterpret_cast<mig_reply_error_t *>(malloc(maxSize));
+    pthread_cleanup_push(samplingThreadCleanup, bufRequest);
+    while (true) {
+        pthread_testcancel();
+        if (mach_msg(&bufRequest->Head, MACH_RCV_MSG, 0, maxSize, port, MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL) != MACH_MSG_SUCCESS) {
+            break;
+        }
+        if (clock_alarm(clock, TIME_RELATIVE, delaySpec, port) != KERN_SUCCESS) {
+            break;
+        }
+
+        [samples addObject:cpuInfoByThread()];
+    }
+    pthread_cleanup_pop(1);
+    return nullptr;
+}
+
+mach_timespec_t delaySpec_;
+std::mutex lock_;
+std::thread thread_;
+clock_serv_t clock_;
+mach_port_t port_;
+void samplingThreadMainBasedApproach() {
+    assert(host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &clock_) == KERN_SUCCESS);
+    assert(mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &port_) == KERN_SUCCESS);
+    const auto intervalNs = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<float>(1) / frequencyHz).count();
+    delaySpec_ = { .tv_sec = static_cast<unsigned int>(intervalNs / 1000000000ULL),
+        .tv_nsec = static_cast<clock_res_t>(intervalNs % 1000000000ULL) };
+    std::lock_guard<std::mutex> l(lock_);
+    thread_ = std::thread(benchmarkingSamplingThreadMain, port_, clock_, delaySpec_);
+
+    int policy;
+    sched_param param;
+    const auto pthreadHandle = thread_.native_handle();
+    if (pthread_getschedparam(pthreadHandle, &policy, &param) == 0) {
+        // A priority of 50 is higher than user input, according to:
+        // https://chromium.googlesource.com/chromium/src/base/+/master/threading/platform_thread_mac.mm#302
+        // Run at a higher priority than the main thread so that we can capture main thread
+        // backtraces even when it's busy.
+        param.sched_priority = 50;
+        assert(pthread_setschedparam(pthreadHandle, policy, &param) == 0);
+    }
+
+    assert(clock_alarm(clock_, TIME_RELATIVE, delaySpec_, port_) == 0);
+}
+
+// MARK: NSThread based approach
+
+NSThread *thread;
+void nsthreadBasedApproach() {
+    thread = [[NSThread alloc] initWithBlock:^{
+        assert(pthread_setname_np("io.sentry.BenchmarkingSamplingProfiler") == 0);
+        while(true) {
+            [samples addObject:cpuInfoByThread()];
+            [NSThread sleepForTimeInterval:1.0/frequencyHz];
+        }
+    }];
+    thread.name = @SENTRY_BENCHMARKING_THREAD_NAME;
+    thread.qualityOfService = NSQualityOfServiceUserInteractive;
+    [thread start];
+}
+}
+
+@implementation SentryBenchmarking
+
++ (void)startBenchmarkProfile {
+//    cpuPluginBasedApproach();
+    samplingThreadMainBasedApproach();
+//    nsthreadBasedApproach();
 }
 
 + (double)retrieveBenchmarks {
