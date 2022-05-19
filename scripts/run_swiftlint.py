8d4291e1e7680d5a74c930f429976ceaa309cdfa
Andrew McKnight <andrew.mcknight@sentry.io>
2022-05-19 13:59:29 -0800

    fix: only bypass swiftlint for CI from within Xcode

--- scripts/run_swiftlint.py
+++ scripts/run_swiftlint.py
@@ -5,8 +5,8 @@ import sys
 from lib.git_repo_root import git_repo_root
 from lib.swiftlint_git_diff import get_swiftlint_issues_in_diff
 
-if 'CI' in os.environ:
-    print('Will not run swiftlint as a build phase for this target in CI. It runs in a dedicated GitHub action workflow separately.')
+if 'XCODE_PRODUCT_BUILD_VERSION' in os.environ and 'CI' in os.environ:
+    print('Will not run swiftlint as a build phase for this target in CI via Xcode. It will run in a dedicated GitHub action workflow separately.')
     sys.exit(0)
 
 get_swiftlint_issues_in_diff()
