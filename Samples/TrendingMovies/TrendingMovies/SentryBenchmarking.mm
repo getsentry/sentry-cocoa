7b66f03f30d6d2eeacc15727e4e2c88b5f799a6e
Andrew McKnight <andrew.mcknight@sentry.io>
2022-05-20 16:44:17 -0800

    fix: clean up benchmark sampler
    - rename a few functions
    - remove the mutex lock

--- Samples/TrendingMovies/TrendingMovies/SentryBenchmarking.mm
+++ Samples/TrendingMovies/TrendingMovies/SentryBenchmarking.mm
@@ -6,6 +6,10 @@
 #include <string>
 #import <thread>
 
+//#define SENTRY_BENCHMARK_GCD_BASED
+//#define SENTRY_BENCHMARK_NSTHREAD_BASED
+#define SENTRY_BENCHMARK_PTHREAD_BASED
+
 #define SENTRY_BENCHMARKING_THREAD_NAME "io.sentry.BenchmarkingSamplingProfiler"
 
 namespace {
@@ -57,32 +61,37 @@ cpuInfoByThread()
     return dict;
 }
 
+const auto frequencyHz = 10;
+const auto intervalNs = 1e9 / frequencyHz;
+
+bool cancel_ = false;
+
 NSMutableArray<NSDictionary<NSString *, NSNumber *> *> *samples =
     [NSMutableArray<NSDictionary<NSString *, NSNumber *> *> array];
 
-// MARK: CPUPlugin-based approach
+// MARK: GCD-based approach
 
 dispatch_source_t source;
 dispatch_queue_t queue;
 
-const auto frequencyHz = 10;
-const auto intervalNs = 1e9 / frequencyHz;
-
 void
-cpuPluginBasedApproach()
+gcdBasedApproach()
 {
     const auto attr = dispatch_queue_attr_make_with_qos_class(
         DISPATCH_QUEUE_CONCURRENT, QOS_CLASS_USER_INTERACTIVE, 0);
     const auto leewayNs = intervalNs / 2;
     queue = dispatch_queue_create("io.sentry.benchmarking.gcd-scheduler", attr);
     source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
-    dispatch_source_set_event_handler(source, ^{ [samples addObject:cpuInfoByThread()]; });
+    dispatch_source_set_event_handler(source, ^{
+        [samples addObject:cpuInfoByThread()];
+    });
     dispatch_source_set_timer(
         source, dispatch_time(DISPATCH_TIME_NOW, intervalNs), intervalNs, leewayNs);
     dispatch_resume(source);
+
 }
 
-// MARK: thread-based approach
+// MARK: pthread-based approach
 
 void
 samplingThreadCleanup(void *buf)
@@ -93,7 +102,7 @@ samplingThreadCleanup(void *buf)
 void *
 benchmarkingSamplingThreadMain(mach_port_t port, clock_serv_t clock, mach_timespec_t delaySpec)
 {
-    assert(pthread_setname_np(SENTRY_BENCHMARKING_THREAD_NAME) == 0);
+    assert(pthread_setname_np(SENTRY_BENCHMARKING_THREAD_NAME) == KERN_SUCCESS);
     const int maxSize = 512;
     const auto bufRequest = reinterpret_cast<mig_reply_error_t *>(malloc(maxSize));
     pthread_cleanup_push(samplingThreadCleanup, bufRequest);
@@ -115,12 +124,11 @@ benchmarkingSamplingThreadMain(mach_port_t port, clock_serv_t clock, mach_timesp
 }
 
 mach_timespec_t delaySpec_;
-std::mutex lock_;
 std::thread thread_;
 clock_serv_t clock_;
 mach_port_t port_;
 void
-samplingThreadMainBasedApproach()
+pthreadBasedApproach()
 {
     assert(host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &clock_) == KERN_SUCCESS);
     assert(mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &port_) == KERN_SUCCESS);
@@ -129,22 +137,21 @@ samplingThreadMainBasedApproach()
                                 .count();
     delaySpec_ = { .tv_sec = static_cast<unsigned int>(intervalNs / 1000000000ULL),
         .tv_nsec = static_cast<clock_res_t>(intervalNs % 1000000000ULL) };
-    std::lock_guard<std::mutex> l(lock_);
     thread_ = std::thread(benchmarkingSamplingThreadMain, port_, clock_, delaySpec_);
 
     int policy;
     sched_param param;
     const auto pthreadHandle = thread_.native_handle();
-    if (pthread_getschedparam(pthreadHandle, &policy, &param) == 0) {
+    if (pthread_getschedparam(pthreadHandle, &policy, &param) == KERN_SUCCESS) {
         // A priority of 50 is higher than user input, according to:
         // https://chromium.googlesource.com/chromium/src/base/+/master/threading/platform_thread_mac.mm#302
         // Run at a higher priority than the main thread so that we can capture main thread
         // backtraces even when it's busy.
         param.sched_priority = 50;
-        assert(pthread_setschedparam(pthreadHandle, policy, &param) == 0);
+        assert(pthread_setschedparam(pthreadHandle, policy, &param) == KERN_SUCCESS);
     }
 
-    assert(clock_alarm(clock_, TIME_RELATIVE, delaySpec_, port_) == 0);
+    assert(clock_alarm(clock_, TIME_RELATIVE, delaySpec_, port_) == KERN_SUCCESS);
 }
 
 // MARK: NSThread based approach
@@ -154,8 +161,11 @@ void
 nsthreadBasedApproach()
 {
     thread = [[NSThread alloc] initWithBlock:^{
-        assert(pthread_setname_np("io.sentry.BenchmarkingSamplingProfiler") == 0);
+        assert(pthread_setname_np("io.sentry.BenchmarkingSamplingProfiler") == KERN_SUCCESS);
         while (true) {
+            if (cancel_) {
+                break;
+            }
             [samples addObject:cpuInfoByThread()];
             [NSThread sleepForTimeInterval:1.0 / frequencyHz];
         }
@@ -170,13 +180,28 @@ nsthreadBasedApproach()
 
 + (void)startBenchmarkProfile
 {
-    //    cpuPluginBasedApproach();
-    samplingThreadMainBasedApproach();
-    //    nsthreadBasedApproach();
+    cancel_ = false;
+
+#if defined(SENTRY_BENCHMARK_GCD_BASED)
+    gcdBasedApproach();
+#elif defined(SENTRY_BENCHMARK_PTHREAD_BASED)
+    pthreadBasedApproach();
+#elif defined(SENTRY_BENCHMARK_NSTHREAD_BASED)
+    nsthreadBasedApproach();
+#endif
 }
 
 + (double)retrieveBenchmarks
 {
+#if defined(SENTRY_BENCHMARK_GCD_BASED)
+        dispatch_cancel(source);
+#elif defined(SENTRY_BENCHMARK_PTHREAD_BASED)
+        assert(pthread_cancel(thread_.native_handle()) == KERN_SUCCESS);
+        thread_.join();
+#elif defined(SENTRY_BENCHMARK_NSTHREAD_BASED)
+        [thread cancel];
+#endif
+
     [samples addObject:cpuInfoByThread()];
     const auto totals = [NSMutableDictionary<NSString *, NSNumber *> dictionary];
     for (auto i = 0; i < samples.count - 2; i++) {
@@ -208,17 +233,6 @@ nsthreadBasedApproach()
                 totals[key] = @(thisDelta + totals[key].integerValue);
             }
         }
-
-        // extrapolate to estimate the remaining work done by a thread that's destroyed before this
-        // sample?
-        //        for (NSString *key : destroyedThreads) {
-        //            const auto thisDelta = before[key].integerValue;
-        //            if (!totals[key]) {
-        //                totals[key] = @(thisDelta);
-        //            } else {
-        //                totals[key] = @(thisDelta + totals[key].integerValue);
-        //            }
-        //        }
     }
 
     const auto samplingThreadUsage = totals[@"io.sentry.SamplingProfiler"].integerValue;
