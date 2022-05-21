2bf75c6ede6175813a5fe3921b608181eb5c0f09
Andrew McKnight <andrew.mcknight@sentry.io>
2022-05-20 16:37:27 -0800

    docs: add headerdoc comments to public API

--- Samples/TrendingMovies/TrendingMovies/SentryBenchmarking.h
+++ Samples/TrendingMovies/TrendingMovies/SentryBenchmarking.h
@@ -4,7 +4,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SentryBenchmarking : NSObject
 
+/** Begin a Sentry transaction, starting the profiler components. */
 + (void)startBenchmarkProfile;
+
+/** @return The % CPU overhead incurred by running the sampling profiler in the Sentry SDK in the test app. */
 + (double)retrieveBenchmarks;
 
 @end
