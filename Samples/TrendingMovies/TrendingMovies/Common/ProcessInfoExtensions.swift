6db0a9dd19268b7161d42ac5fe53c45619bf2d7e
Andrew McKnight <andrew.mcknight@sentry.io>
2022-05-23 21:23:45 -0800

    fix: invert check to disable profiling in movies app

--- Samples/TrendingMovies/TrendingMovies/Common/ProcessInfoExtensions.swift
+++ Samples/TrendingMovies/TrendingMovies/Common/ProcessInfoExtensions.swift
@@ -2,4 +2,5 @@ import Foundation
 
 extension ProcessInfo {
     static var isBenchmarking = ProcessInfo().arguments.contains("--io.sentry.ui-test.benchmarking")
+    static var disableProfiling = ProcessInfo().arguments.contains("--io.sentry.disable-profiling")
 }
--- Samples/TrendingMovies/TrendingMovies/Utilities/Tracer.swift
+++ Samples/TrendingMovies/TrendingMovies/Utilities/Tracer.swift
@@ -31,8 +31,7 @@ extension Tracer {
             options.dsn = "https://a92d50327ac74b8b9aa4ea80eccfb267@o447951.ingest.sentry.io/5428557"
             options.environment = "integration-tests"
             options.debug = true
-
-            if ProcessInfo().arguments.contains("--io.sentry.enable-profiling") {
+            if !ProcessInfo.disableProfiling {
                 options.enableProfiling = true
             }
         }
