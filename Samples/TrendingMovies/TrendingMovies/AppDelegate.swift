dda55a9274fdbda8e6c5ef7c3be61b4b1f0825bd
Andrew McKnight <andrew.mcknight@sentry.io>
2022-06-07 16:15:49 -0400

    fix: remove duplicate init of sentry SDK

--- Samples/TrendingMovies/TrendingMovies/AppDelegate.swift
+++ Samples/TrendingMovies/TrendingMovies/AppDelegate.swift
@@ -28,18 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("[TrendingMovies] didFinishLaunchingWithOptions")
         Tracer.setUp(finishedLaunching: true)
-        
-        SentrySDK.start { options in
-            options.dsn = "https://a92d50327ac74b8b9aa4ea80eccfb267@o447951.ingest.sentry.io/5428557"
-            options.debug = true
-            options.sessionTrackingIntervalMillis = 5_000
-            // Sampling 100% - In Production you probably want to adjust this
-            options.tracesSampleRate = 1.0
-            options.enableFileIOTracking = true
-            options.enableCoreDataTracking = true
-            options.enableProfiling = true
-            options.attachScreenshot = true
-        }
 
         window = UIWindow(frame: UIScreen.main.bounds)
         let tabBarController = createTabBarController(items: [
