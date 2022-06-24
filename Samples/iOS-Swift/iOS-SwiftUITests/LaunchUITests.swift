0acf9c51637d9002f8692d120cab9f3ad369b686
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-06-24 15:39:16 +0200

    Fix high percentage of slow frames (#1915)
    The SentryFramesTracker didn't handle fps changes from
    the CADisplayLink. This is fixed now by calculating the
    actual frame rate as pointed out by the docs.
    
    Fixes GH-1881

--- Samples/iOS-Swift/iOS-SwiftUITests/LaunchUITests.swift
+++ Samples/iOS-Swift/iOS-SwiftUITests/LaunchUITests.swift
@@ -12,6 +12,7 @@ class LaunchUITests: XCTestCase {
         app.launch()
         
         waitForExistenseOfMainScreen()
+        checkSlowAndFrozenFrames()
     }
     
     override func tearDown() {
@@ -68,6 +69,25 @@ class LaunchUITests: XCTestCase {
         
     private func waitForExistenseOfMainScreen() {
         XCTAssertTrue(app.buttons["captureMessageButton"].waitForExistence(), "Home Screen doesn't exist.")
+    }
+    
+    private func checkSlowAndFrozenFrames() {
+        let frameStatsLabel = app.staticTexts["framesStatsLabel"]
+        XCTAssertTrue(frameStatsLabel.waitForExistence(), "Frame statistics message not found.")
+        
+        let frameStatsAsStringArray = frameStatsLabel.label.components(separatedBy: CharacterSet.decimalDigits.inverted)
+        let frameStats = frameStatsAsStringArray.filter { $0 != "" }.map { Int($0) }
+        
+        XCTAssertEqual(3, frameStats.count)
+        guard let totalFrames = frameStats[0] else { XCTFail("No total frames found."); return }
+        guard let slowFrames = frameStats[1] else { XCTFail("No slow frames found."); return }
+        guard let frozenFrames = frameStats[1] else { XCTFail("No frozen frames found."); return }
+
+        let slowFramesPercentage = Double(slowFrames) / Double(totalFrames)
+        XCTAssertTrue(0.5 > slowFramesPercentage, "Too many slow frames.")
+        
+        let frozenFramesPercentage = Double(frozenFrames) / Double(totalFrames)
+        XCTAssertTrue(0.5 > frozenFramesPercentage, "Too many frozen frames.")
     }
     
     private func assertApp() {
