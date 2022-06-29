a723c7a22d1b70b198678f0b9c950fc254ace151
Dhiogo Brustolin <dhiogorb@gmail.com>
2022-06-29 13:01:29 +0200

    Report sink test

--- Tests/SentryTests/SentryCrash/SentryCrashReportSinkTest.swift
+++ Tests/SentryTests/SentryCrash/SentryCrashReportSinkTest.swift
@@ -1,9 +1,42 @@
 import XCTest
 
-class SentryCrashReportSinkTests: XCTestCase {
-    
-    func testFilterReports_withScreenShots() {
+class SentryCrashReportSinkTests: SentrySDKIntegrationTestsBase {
         
+    func testFilterReports_withScreenShots() {
+        givenSdkWithHub()
+        
+        let reportSink = SentryCrashReportSink(inAppLogic: SentryInAppLogic(inAppIncludes: [], inAppExcludes: []))
+        let expect = expectation(description: "Callback Called")
+        
+        let report = ["screenshots":["file.png"]];
+        
+        reportSink.filterReports([report]) { reports, completed, error in
+            self.assertCrashEventWithScope { event, scope in
+                XCTAssertEqual(scope?.attachments.count, 1)
+                expect.fulfill()
+            }
+        }
+                
+        wait(for: [expect], timeout: 1)
     }
     
+    func testFilterReports_CopyHubScope() {
+        givenSdkWithHub()
+        SentrySDK.currentHub().scope.setEnvironment("testFilterReports_CopyHubScope")
+        
+        let reportSink = SentryCrashReportSink(inAppLogic: SentryInAppLogic(inAppIncludes: [], inAppExcludes: []))
+        let expect = expectation(description: "Callback Called")
+        
+        let report = [String:Any]();
+        
+        reportSink.filterReports([report]) { reports, completed, error in
+            self.assertCrashEventWithScope { event, scope in
+                let data = scope?.serialize()
+                XCTAssertEqual(data?["environment"] as? String, "testFilterReports_CopyHubScope")
+                expect.fulfill()
+            }
+        }
+                
+        wait(for: [expect], timeout: 1)
+    }
 }
