4c565eff05552db5a94cc2e9344379d372d0f379
Kevin Renskers <kevin@loopwerk.io>
2022-07-18 16:17:00 +0200

    Push permission checking

--- Sources/Sentry/SentryPermissionsObserver.m
+++ Sources/Sentry/SentryPermissionsObserver.m
@@ -1,13 +1,13 @@
 #import "SentryPermissionsObserver.h"
 #import <CoreLocation/CoreLocation.h>
+#import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface
 SentryPermissionsObserver () <CLLocationManagerDelegate>
-
 @property (nonatomic, strong) CLLocationManager *locationManager;
-
 @end
 
 @implementation SentryPermissionsObserver
@@ -16,19 +16,57 @@ SentryPermissionsObserver () <CLLocationManagerDelegate>
 {
     self = [super init];
     if (self) {
+        // Set initial values
+        [self checkPushPermissions];
+        [self setLocationPermissionFromStatus:[CLLocationManager authorizationStatus]];
+
+        // Listen for location permission updates
         self.locationManager = [[CLLocationManager alloc] init];
         self.locationManager.delegate = self;
 
-        CLAuthorizationStatus locationStatus = [CLLocationManager authorizationStatus];
-        [self setLocationPermissionFromStatus:locationStatus];
+        // We can't listen for push permission updates directly, there simply is no API for that.
+        // Instead we re-check when the application comes back to the foreground.
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(checkPushPermissions)
+                                                     name:UIApplicationDidBecomeActiveNotification
+                                                   object:nil];
     }
 
     return self;
 }
 
-- (void)setLocationPermissionFromStatus:(CLAuthorizationStatus)locationStatus
+- (void)checkPushPermissions
 {
-    switch (locationStatus) {
+    if (@available(iOS 10, *)) {
+        [[UNUserNotificationCenter currentNotificationCenter]
+            getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings *settings) {
+                [self setPushPermissionFromStatus:settings.authorizationStatus];
+            }];
+    }
+}
+
+- (void)setPushPermissionFromStatus:(UNAuthorizationStatus)status
+{
+    switch (status) {
+    case UNAuthorizationStatusNotDetermined:
+        self.hasLocationPermission = kPermissionStatusUnknown;
+        break;
+
+    case UNAuthorizationStatusDenied:
+        self.hasLocationPermission = kPermissionStatusDenied;
+        break;
+
+    case UNAuthorizationStatusAuthorized:
+    case UNAuthorizationStatusProvisional:
+    case UNAuthorizationStatusEphemeral:
+        self.hasLocationPermission = kPermissionStatusGranted;
+        break;
+    }
+}
+
+- (void)setLocationPermissionFromStatus:(CLAuthorizationStatus)status
+{
+    switch (status) {
     case kCLAuthorizationStatusNotDetermined:
         self.hasLocationPermission = kPermissionStatusUnknown;
         break;
