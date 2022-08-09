9f7c07822b8003c66e7eb6b097a14f6041873f7b
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-08-09 23:33:43 +0200

    wip: perf test - include sentry & prepare fastlane script

--- Tests/Perf/test-app-sentry/test-app-sentry/test-app-sentry/test_app_sentryApp.swift
+++ Tests/Perf/test-app-sentry/test-app-sentry/test-app-sentry/test_app_sentryApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Sentry
 
 @main
 struct test_app_sentryApp: App {
+    init() {
+        SentrySDK.start { options in
+            options.dsn = "https://a92d50327ac74b8b9aa4ea80eccfb267@o447951.ingest.sentry.io/5428557"
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
