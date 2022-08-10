e543dda626038156c77030e34e38e8a425cf7001
Indragie Karunaratne <i@indragie.com>
2022-08-10 12:57:04 -0700

    fix: Default environment for profile payload should be production (#2041)
    For transactions this defaults to production when no explicit environment is set, but we don't copy the same default for the profiling payload. This fixes that.
    
    Co-authored-by: Sentry Github Bot <bot+github-bot@sentry.io>

--- Sources/Sentry/SentryProfiler.mm
+++ Sources/Sentry/SentryProfiler.mm
@@ -253,7 +253,7 @@ isSimulatorBuild()
     profile[@"device_is_emulator"] = @(isSimulatorBuild());
     profile[@"device_physical_memory_bytes"] =
         [@(NSProcessInfo.processInfo.physicalMemory) stringValue];
-    profile[@"environment"] = transaction.environment;
+    profile[@"environment"] = transaction.environment ?: @"production";
     profile[@"platform"] = transaction.platform;
     profile[@"transaction_id"] = transaction.eventId.sentryIdString;
     profile[@"trace_id"] = transaction.trace.context.traceId.sentryIdString;
