70d36c07be2098e31bf51d10cd34654f054be01b
Dhiogo Brustolin <dhiogorb@gmail.com>
2022-09-05 15:29:45 +0200

    Update SentryPermissionsObserver.h

--- Sources/Sentry/include/SentryPermissionsObserver.h
+++ Sources/Sentry/include/SentryPermissionsObserver.h
@@ -5,6 +5,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SentryPermissionsObserver : NSObject
 
+/*
+ * We want as many permissions as possible,
+ * but we had to remove the media permission because it was preventing some developers from
+ * publishing their apps. Apple was requiring developers to include NSAppleMusicUsageDescription in
+ * their plist files, even when they don't use this feature. More info at
+ * https://github.com/getsentry/sentry-cocoa/issues/2065#issuecomment-1237016049
+ */
+
 @property (nonatomic) SentryPermissionStatus pushPermissionStatus;
 @property (nonatomic) SentryPermissionStatus locationPermissionStatus;
 @property (nonatomic) SentryPermissionStatus photoLibraryPermissionStatus;
