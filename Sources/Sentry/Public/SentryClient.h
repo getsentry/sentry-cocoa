4402525dd0b5b47de202c1f5c9e21c3fdd783cc6
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-09-15 11:00:37 +0200

    Code doc

--- Sources/Sentry/Public/SentryClient.h
+++ Sources/Sentry/Public/SentryClient.h
@@ -113,6 +113,13 @@ SENTRY_NO_INIT
 
 - (void)captureEnvelope:(SentryEnvelope *)envelope NS_SWIFT_NAME(capture(envelope:));
 
+/**
+ * Waits synchronously for the SDK to flush out all queued and cached items. Returns if the SDK
+ * doesn't complete the flush before the specified timeout period has elapsed or if there is no
+ * internet connection. The SDK doesn't dispose the client or the hub.
+ *
+ * @param timeout The time to wait for the SDK to complete the flush.
+ */
 - (void)flush:(NSTimeInterval)timeout NS_SWIFT_NAME(flush(timeout:));
 
 @end
