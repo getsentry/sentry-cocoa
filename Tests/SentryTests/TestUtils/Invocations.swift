973d73aed462da007b1eb9a2e4bb4be59f74282f
Andrew McKnight <andrew.mcknight@sentry.io>
2022-09-15 21:51:28 -0800

    put Invocations.swift back

--- Tests/SentryTests/TestUtils/Invocations.swift
+++ Tests/SentryTests/TestUtils/Invocations.swift
@@ -4,44 +4,44 @@ import Foundation
  * For recording invocations of methods in a list in a thread safe manner.
  */
 class Invocations<T> {
-
-   private let queue = DispatchQueue(label: "Invocations", attributes: .concurrent)
-
-   private var _invocations = NSMutableArray()
-
-   var invocations: [T] {
-       return queue.sync {
-           return self._invocations as! [T]
-       }
-   }
-
-   var count: Int {
-       return queue.sync {
-           return self._invocations.count
-       }
-   }
-
-   var first: T? {
-       return queue.sync {
-           return self._invocations.firstObject as? T
-       }
-   }
-
-   var last: T? {
-       return queue.sync {
-           return self._invocations.lastObject as? T
-       }
-   }
-
-   var isEmpty: Bool {
-       return queue.sync {
-           return self._invocations.count == 0
-       }
-   }
-
-   func record(_ invocation: T) {
-       queue.async(flags: .barrier) {
-           self._invocations.add(invocation)
-       }
-   }
+    
+    private let queue = DispatchQueue(label: "Invocations", attributes: .concurrent)
+    
+    private var _invocations: [T] = []
+    
+    var invocations: [T] {
+        return queue.sync {
+            return self._invocations
+        }
+    }
+    
+    var count: Int {
+        return queue.sync {
+            return self._invocations.count
+        }
+    }
+    
+    var first: T? {
+        return queue.sync {
+            return self._invocations.first
+        }
+    }
+    
+    var last: T? {
+        return queue.sync {
+            return self._invocations.last
+        }
+    }
+    
+    var isEmpty: Bool {
+        return queue.sync {
+            return self._invocations.isEmpty
+        }
+    }
+    
+    func record(_ invocation: T) {
+        queue.async(flags: .barrier) {
+            self._invocations.append(invocation)
+        }
+    }
 }
