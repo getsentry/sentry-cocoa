eee6686aedd96b2123230e10c992e47df6af1929
Dhiogo Brustolin <dhiogorb@gmail.com>
2022-09-28 17:28:48 +0200

    add descriptor

---
+++ Sources/SentrySwift/Integration/SentryDescriptorIntegration.swift
@@ -0,0 +1,9 @@
+import Foundation
+import SentryObjc
+
+public class SentryDescriptorIntegration: NSObject, SentryIntegrationProtocol {
+    public func install(with options: Options) -> Bool {
+        
+        return true
+    }
+}
