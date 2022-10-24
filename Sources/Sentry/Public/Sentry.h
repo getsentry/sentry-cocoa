048259e7b82d8cf45c527f961f5cf2b841e32138
Manoel Aranda Neto <marandaneto@gmail.com>
2022-10-24 14:46:32 +0200

    ref sentry request header

--- Sources/Sentry/Public/Sentry.h
+++ Sources/Sentry/Public/Sentry.h
@@ -50,3 +50,4 @@ FOUNDATION_EXPORT const unsigned char SentryVersionString[];
 #import "SentryTransactionContext.h"
 #import "SentryUser.h"
 #import "SentryUserFeedback.h"
+#import "SentryRequest.h"
--- Sources/Sentry/SentryNetworkTracker.m
+++ Sources/Sentry/SentryNetworkTracker.m
@@ -313,7 +313,8 @@ SentryNetworkTracker ()
 
     // TODO: how would I conver the error to sentry's error? thsi could be useful in case something
     // goes wrong
-    //    sessionTask.error
+//    if (nil != sessionTask.error) {
+//    }
 
     NSString *message = [NSString
         stringWithFormat:@"HTTP Client Error with status code: %ld", (long)myResponse.statusCode];
