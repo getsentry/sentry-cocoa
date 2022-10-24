getsentry/sentry-cocoa

commit fbeb49aa4bf63d1ce6537cbc4d509564d1510617

ref: Remove a possible race condition related to reading previous app state (#2250)

Sources/Sentry/SentryFileManager.m
@@ -23,6 +23,7 @@ SentryFileManager ()
 @property (nonatomic, copy) NSString *currentSessionFilePath;
 @property (nonatomic, copy) NSString *crashedSessionFilePath;
 @property (nonatomic, copy) NSString *lastInForegroundFilePath;
+@property (nonatomic, copy) NSString *previousAppStateFilePath;
 @property (nonatomic, copy) NSString *appStateFilePath;
 @property (nonatomic, copy) NSString *timezoneOffsetFilePath;
 @property (nonatomic, assign) NSUInteger currentFileCounter;
@@ -65,6 +66,8 @@ SentryFileManager ()
         self.lastInForegroundFilePath =
             [self.sentryPath stringByAppendingPathComponent:@"lastInForeground.timestamp"];
 
+        self.previousAppStateFilePath =
+            [self.sentryPath stringByAppendingPathComponent:@"previous.app.state"];
         self.appStateFilePath = [self.sentryPath stringByAppendingPathComponent:@"app.state"];
         self.timezoneOffsetFilePath =
             [self.sentryPath stringByAppendingPathComponent:@"timezone.offset"];
@@ -395,32 +398,71 @@ SentryFileManager ()
     }
 }
 
+- (void)moveAppStateToPreviousAppState
+{
+    @synchronized(self.appStateFilePath) {
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        NSError *error = nil;
+        [fileManager moveItemAtPath:self.appStateFilePath
+                             toPath:self.previousAppStateFilePath
+                              error:&error];
+
+        // We don't want to log an error if the file doesn't exist.
+        if (nil != error && error.code != NSFileNoSuchFileError) {
+            [SentryLog
+                logWithMessage:[NSString
+                                   stringWithFormat:
+                                       @"Failed to move app state to previous app state: %@", error]
+                      andLevel:kSentryLevelError];
+        }
+    }
+}
+
 - (SentryAppState *_Nullable)readAppState
+{
+    @synchronized(self.appStateFilePath) {
+        return [self readAppStateFrom:self.appStateFilePath];
+    }
+}
+
+- (SentryAppState *_Nullable)readPreviousAppState
+{
+    @synchronized(self.previousAppStateFilePath) {
+        return [self readAppStateFrom:self.previousAppStateFilePath];
+    }
+}
+
+- (SentryAppState *_Nullable)readAppStateFrom:(NSString *)path
 {
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSData *currentData = nil;
-    @synchronized(self.appStateFilePath) {
-        currentData = [fileManager contentsAtPath:self.appStateFilePath];
-        if (nil == currentData) {
-            return nil;
-        }
+    currentData = [fileManager contentsAtPath:path];
+    if (nil == currentData) {
+        return nil;
     }
     return [SentrySerialization appStateWithData:currentData];
 }
 
 - (void)deleteAppState
 {
+    @synchronized(self.appStateFilePath) {
+        [self deleteAppStateFrom:self.appStateFilePath];
+        [self deleteAppStateFrom:self.previousAppStateFilePath];
+    }
+}
+
+- (void)deleteAppStateFrom:(NSString *)path
+{
     NSError *error = nil;
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    @synchronized(self.appStateFilePath) {
-        [fileManager removeItemAtPath:self.appStateFilePath error:&error];
+    [fileManager removeItemAtPath:path error:&error];
 
-        // We don't want to log an error if the file doesn't exist.
-        if (nil != error && error.code != NSFileNoSuchFileError) {
-            [SentryLog
-                logWithMessage:[NSString stringWithFormat:@"Failed to delete app state %@", error]
-                      andLevel:kSentryLevelError];
-        }
+    // We don't want to log an error if the file doesn't exist.
+    if (nil != error && error.code != NSFileNoSuchFileError) {
+        [SentryLog
+            logWithMessage:[NSString stringWithFormat:@"Failed to delete app state from %@: %@",
+                                     path, error]
+                  andLevel:kSentryLevelError];
     }
 }
 

Tests/SentryTests/Helper/SentryFileManagerTests.swift
@@ -439,6 +439,15 @@ class SentryFileManagerTests: XCTestCase {
         sut.deleteAppState()
         XCTAssertNil(sut.readAppState())
     }
+
+    func testDeletePreviousAppState() {
+        sut.store(TestData.appState)
+        sut.moveAppStateToPreviousAppState()
+        sut.deleteAppState()
+
+        XCTAssertNil(sut.readAppState())
+        XCTAssertNil(sut.readPreviousAppState())
+    }
     
     func testStore_WhenFileImmutable_AppStateIsNotOverwritten() {
         sut.store(TestData.appState)
@@ -469,6 +478,14 @@ class SentryFileManagerTests: XCTestCase {
         
         sut.deleteAppState()
         XCTAssertNotNil(sut.readAppState())
+    }
+
+    func testMoveAppStateAndReadPreviousAppState() {
+        sut.store(TestData.appState)
+        sut.moveAppStateToPreviousAppState()
+
+        let actual = sut.readPreviousAppState()
+        XCTAssertEqual(TestData.appState, actual)
     }
 
     func testStoreAndReadTimezoneOffset() {
@@ -600,7 +617,6 @@ class SentryFileManagerTests: XCTestCase {
     private func assertValidAppStateStored() {
         let actual = sut.readAppState()
         XCTAssertEqual(TestData.appState, actual)
-        
     }
 
     private func advanceTime(bySeconds: TimeInterval) {

Sources/Sentry/SentryAppStateManager.m
@@ -57,6 +57,11 @@ SentryAppStateManager ()
                                    systemBootTimestamp:self.sysctl.systemBootTimestamp];
 }
 
+- (SentryAppState *)loadPreviousAppState
+{
+    return [self.fileManager readPreviousAppState];
+}
+
 - (SentryAppState *)loadCurrentAppState
 {
     return [self.fileManager readAppState];
@@ -67,7 +72,7 @@ SentryAppStateManager ()
     [self.fileManager storeAppState:[self buildCurrentAppState]];
 }
 
-- (void)removeCurrentAppState
+- (void)deleteAppState
 {
     [self.fileManager deleteAppState];
 }

Sources/Sentry/SentryOutOfMemoryLogic.m
@@ -40,7 +40,7 @@ SentryOutOfMemoryLogic ()
     }
 
 #if SENTRY_HAS_UIKIT
-    SentryAppState *previousAppState = [self.appStateManager loadCurrentAppState];
+    SentryAppState *previousAppState = [self.appStateManager loadPreviousAppState];
     SentryAppState *currentAppState = [self.appStateManager buildCurrentAppState];
 
     // If there is no previous app state, we can't do anything.

Sources/Sentry/SentryOutOfMemoryTracker.m
@@ -129,7 +129,7 @@ SentryOutOfMemoryTracker ()
         removeObserver:self
                   name:SentryNSNotificationCenterWrapper.willTerminateNotificationName
                 object:nil];
-    [self.appStateManager removeCurrentAppState];
+    [self.appStateManager deleteAppState];
 #endif
 }
 

Sources/Sentry/SentrySDK.m
@@ -146,7 +146,10 @@ static NSUInteger startInvocations;
     startInvocations++;
 
     [SentryLog configure:options.debug diagnosticLevel:options.diagnosticLevel];
+
     SentryClient *newClient = [[SentryClient alloc] initWithOptions:options];
+    [newClient.fileManager moveAppStateToPreviousAppState];
+
     // The Hub needs to be initialized with a client so that closing a session
     // can happen.
     [SentrySDK setCurrentHub:[[SentryHub alloc] initWithClient:newClient andScope:nil]];

Sources/Sentry/include/SentryAppStateManager.h
@@ -26,11 +26,13 @@ SENTRY_NO_INIT
  */
 - (SentryAppState *)buildCurrentAppState;
 
+- (SentryAppState *)loadPreviousAppState;
+
 - (SentryAppState *)loadCurrentAppState;
 
 - (void)storeCurrentAppState;
 
-- (void)removeCurrentAppState;
+- (void)deleteAppState;
 
 - (void)updateAppState:(void (^)(SentryAppState *))block;
 

Sources/Sentry/include/SentryFileManager.h
@@ -60,7 +60,9 @@ SENTRY_NO_INIT
 - (NSString *)storeDictionary:(NSDictionary *)dictionary toPath:(NSString *)path;
 
 - (void)storeAppState:(SentryAppState *)appState;
+- (void)moveAppStateToPreviousAppState;
 - (SentryAppState *_Nullable)readAppState;
+- (SentryAppState *_Nullable)readPreviousAppState;
 - (void)deleteAppState;
 
 - (NSNumber *_Nullable)readTimezoneOffset;

commit 64225be5808d92970f1efab0da188427fed632f8

fix: Fix moving app state to previous app state (#2321)
This is a very high priority fix for an issue introduced in #2250

Sources/Sentry/SentryFileManager.m
@@ -402,6 +402,11 @@ SentryFileManager ()
 {
     @synchronized(self.appStateFilePath) {
         NSFileManager *fileManager = [NSFileManager defaultManager];
+
+        // We first need to remove the old previous app state file,
+        // or we can't move the current app state file to it.
+        [self removeFileAtPath:self.previousAppStateFilePath];
+
         NSError *error = nil;
         [fileManager moveItemAtPath:self.appStateFilePath
                              toPath:self.previousAppStateFilePath

Tests/SentryTests/Helper/SentryFileManagerTests.swift
@@ -488,6 +488,18 @@ class SentryFileManagerTests: XCTestCase {
         XCTAssertEqual(TestData.appState, actual)
     }
 
+    func testMoveAppStateWhenPreviousAppStateAlreadyExists() {
+        sut.store(TestData.appState)
+        sut.moveAppStateToPreviousAppState()
+
+        let newAppState = SentryAppState(releaseName: "2.0.0", osVersion: "14.4.1", vendorId: "12345678-1234-1234-1234-12344567890AB", isDebugging: false, systemBootTimestamp: Date(timeIntervalSince1970: 10))
+        sut.store(newAppState)
+        sut.moveAppStateToPreviousAppState()
+
+        let actual = sut.readPreviousAppState()
+        XCTAssertEqual(newAppState, actual)
+    }
+
     func testStoreAndReadTimezoneOffset() {
         sut.storeTimezoneOffset(7_200)
         XCTAssertEqual(sut.readTimezoneOffset(), 7_200)