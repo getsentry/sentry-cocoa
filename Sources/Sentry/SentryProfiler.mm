f08f6444f1151de36a75b1d5813055102fd132cf
Andrew McKnight <andrew.mcknight@sentry.io>
2022-10-31 11:33:27 -0800

    fix: dont require profiler to start on main thread

--- Sources/Sentry/SentryProfiler.mm
+++ Sources/Sentry/SentryProfiler.mm
@@ -108,11 +108,6 @@ profilerTruncationReasonName(SentryProfilerTruncationReason reason)
 #    if SENTRY_TARGET_PROFILING_SUPPORTED
 - (instancetype)init
 {
-    if (![NSThread isMainThread]) {
-        SENTRY_LOG_ERROR(@"SentryProfiler must be initialized on the main thread");
-        return nil;
-    }
-
     if (!(self = [super init])) {
         return nil;
     }
@@ -148,6 +143,10 @@ profilerTruncationReasonName(SentryProfilerTruncationReason reason)
 
     if (_gCurrentProfiler == nil) {
         _gCurrentProfiler = [[SentryProfiler alloc] init];
+        if (_gCurrentProfiler == nil) {
+            SENTRY_LOG_WARN(@"Profiler was not initialized, will not proceed.");
+            return;
+        }
 #        if SENTRY_HAS_UIKIT
         [SentryFramesTracker.sharedInstance resetProfilingTimestamps];
 #        endif // SENTRY_HAS_UIKIT
