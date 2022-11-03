a744465218bf4b48da65bf38e95af0c3186b325b
Dhiogo Brustolin <dhiogorb@gmail.com>
2022-11-03 16:48:52 +0100

    Update PrivateSentrySDKOnly.h

--- Sources/Sentry/include/PrivateSentrySDKOnly.h
+++ Sources/Sentry/include/PrivateSentrySDKOnly.h
@@ -1,3 +1,6 @@
+// We need this because if Sentry library is add as a Framework
+// the reference should be in the form of <module/header>.
+// Otherwise, the reference is direct.
 #if __has_include(<Sentry/SentryDefines.h>)
 #    import <Sentry/SentryDefines.h>
 #else
