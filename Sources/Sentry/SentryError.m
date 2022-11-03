3fe877a6c23b9e7f0e7ebf769f9aa182ec6ea49f
Andrew McKnight <andrew.mcknight@sentry.io>
2022-11-02 16:06:43 -0800

    feat: build errors with exceptions and underling errors

--- Sources/Sentry/SentryError.m
+++ Sources/Sentry/SentryError.m
@@ -4,11 +4,33 @@ NS_ASSUME_NONNULL_BEGIN
 
 NSString *const SentryErrorDomain = @"SentryErrorDomain";
 
+NSError *_Nullable NSErrorFromSentryErrorWithUnderlyingError(
+    SentryError error, NSString *description, NSError *underlyingError)
+{
+    return [NSError errorWithDomain:SentryErrorDomain
+                               code:error
+                           userInfo:@ {
+                               NSLocalizedDescriptionKey : description,
+                               NSUnderlyingErrorKey : underlyingError
+                           }];
+}
+
+NSError *_Nullable NSErrorFromSentryErrorWithException(
+    SentryError error, NSString *description, NSException *exception)
+{
+    return [NSError errorWithDomain:SentryErrorDomain
+                               code:error
+                           userInfo:@ {
+                               NSLocalizedDescriptionKey : [NSString
+                                   stringWithFormat:@"%@ (%@)", description, exception.reason],
+                           }];
+}
+
 NSError *_Nullable NSErrorFromSentryError(SentryError error, NSString *description)
 {
-    NSMutableDictionary *userInfo = [NSMutableDictionary new];
-    [userInfo setValue:description forKey:NSLocalizedDescriptionKey];
-    return [NSError errorWithDomain:SentryErrorDomain code:error userInfo:userInfo];
+    return [NSError errorWithDomain:SentryErrorDomain
+                               code:error
+                           userInfo:@ { NSLocalizedDescriptionKey : description }];
 }
 
 NS_ASSUME_NONNULL_END
