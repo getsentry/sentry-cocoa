295e1cb5a76b719b242ecceee040d7605c105391
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-11-28 10:47:44 +0100

    Don't disable options

--- Sources/Sentry/SentrySDK.m
+++ Sources/Sentry/SentrySDK.m
@@ -418,7 +418,6 @@ static NSUInteger startInvocations;
 
     // close the client
     SentryClient *client = [hub getClient];
-    client.options.enabled = NO;
     [hub bindClient:nil];
 
     [SentrySDK setCurrentHub:nil];
