7405e8d496bc26ce003bb80d321a8fe5a0306802
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-11-28 08:23:46 +0100

    Disable zombie

--- Sources/SentryCrash/Recording/Monitors/SentryCrashMonitor_Signal.c
+++ Sources/SentryCrash/Recording/Monitors/SentryCrashMonitor_Signal.c
@@ -216,10 +216,6 @@ uninstallSignalHandler(void)
 static void
 setEnabled(bool isEnabled)
 {
-    bool yes = true;
-    if (yes)
-        return;
-
     if (isEnabled != g_isEnabled) {
         g_isEnabled = isEnabled;
         if (isEnabled) {
