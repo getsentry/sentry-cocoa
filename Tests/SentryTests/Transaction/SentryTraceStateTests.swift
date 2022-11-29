9f8d429dca2ca9e5c8114031925bed4c524d9dfb
Dhiogo Brustolin <dhiogorb@gmail.com>
2022-11-29 12:08:01 -0300

    revert rename of sentryuser (#2462)
    Reverted renaming of User to SentryUser

--- Tests/SentryTests/Transaction/SentryTraceStateTests.swift
+++ Tests/SentryTests/Transaction/SentryTraceStateTests.swift
@@ -28,7 +28,7 @@ class SentryTraceContextTests: XCTestCase {
             tracer = SentryTracer(transactionContext: TransactionContext(name: transactionName, operation: transactionOperation), hub: nil)
             
             scope = Scope()
-            scope.setUser(SentryUser(userId: userId))
+            scope.setUser(User(userId: userId))
             scope.userObject?.segment = userSegment
             scope.span = tracer
             
