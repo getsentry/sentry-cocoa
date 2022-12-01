fce6ba39e011f9334764da22574ada981908c0f7
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-12-01 18:05:10 +0100

    Fix test build failure

--- Tests/SentryTests/Integrations/MetricKit/SentryMetricKitIntegrationTests.swift
+++ Tests/SentryTests/Integrations/MetricKit/SentryMetricKitIntegrationTests.swift
@@ -8,7 +8,7 @@ final class SentryMetricKitIntegrationTests: SentrySDKIntegrationTestsBase {
     var callStackTree: SentryMXCallStackTree!
     
     override func setUpWithError() throws {
-        super.setUpWithError()
+        try super.setUpWithError()
         
         let contents = try contentsOfResource("metric-kit-callstack-tree-simple")
         callStackTree = try SentryMXCallStackTree.from(data: contents)
