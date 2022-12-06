7eeb4e419b17814cab229612c4f863b47d932360
Kevin Renskers <kevin@loopwerk.io>
2022-12-06 10:38:01 +0100

    Fix tests

--- Tests/SentryTests/Helper/TestNSNotificationCenterWrapper.swift
+++ Tests/SentryTests/Helper/TestNSNotificationCenterWrapper.swift
@@ -8,15 +8,18 @@ import Foundation
 
     public override func addObserver(_ observer: Any, selector aSelector: Selector, name aName: NSNotification.Name) {
         addObserverInvocations.record((observer, aSelector, aName))
+        NotificationCenter.default.addObserver(observer, selector: aSelector, name: aName, object: nil)
     }
 
     var removeObserverWithNameInvocations = Invocations<(observer: Any, name: NSNotification.Name)>()
     public override func removeObserver(_ observer: Any, name aName: NSNotification.Name) {
         removeObserverWithNameInvocations.record((observer, aName))
+        NotificationCenter.default.removeObserver(observer, name: aName, object: nil)
     }
 
     var removeObserverInvocations = Invocations<Any>()
     public override func removeObserver(_ observer: Any) {
         removeObserverInvocations.record(observer)
+        NotificationCenter.default.removeObserver(observer)
     }
 }
