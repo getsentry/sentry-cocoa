2c6f3bc351341b74f7867234934a286cda62aa47
Philipp Hofmann <philipp.hofmann@sentry.io>
2022-12-28 08:45:18 +0100

    Fis test

--- Tests/SentryTests/Networking/SentryHttpTransportTests.swift
+++ Tests/SentryTests/Networking/SentryHttpTransportTests.swift
@@ -581,7 +581,8 @@ class SentryHttpTransportTests: XCTestCase {
             dispatchAfterBlock.block()
         }
         
-        assertEnvelopesStored(envelopeCount: 10)
+        // The amount of sent envelopes is non deterministic as it depends on how fast ARC deallocates the sut above. We only want to ensure that not all envelopes are sent, so 7 should be fine.
+        XCTAssertLessThan(7, fixture.fileManager.getAllEnvelopes().count)
     }
     
     func testBuildingRequestFailsAndRateLimitActive_RecordsLostEvents() {
