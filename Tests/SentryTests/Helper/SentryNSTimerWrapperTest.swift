08c0979e17481f740be9b184c9c55579ca9c5dc3
Andrew McKnight <andrew.mcknight@sentry.io>
2023-01-13 11:57:06 -0500

    add nstimer wrapper tests

---
+++ Tests/SentryTests/Helper/SentryNSTimerWrapperTest.swift
@@ -0,0 +1,30 @@
+import XCTest
+
+class SentryNSTimerWrapperTests: XCTestCase {
+    struct Fixture {
+        lazy var timerWrapper = SentryNSTimerWrapper()
+    }
+    lazy var fixture = Fixture()
+
+    func testNonrepeatingTimer() {
+        let exp = expectation(description: "timer fires exactly once")
+        fixture.timerWrapper.scheduledTimer(withTimeInterval: 0.1, repeats: false) { _ in
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 1)
+    }
+
+    func testRepeatingTimer() {
+        var count = 0
+        let exp = expectation(description: "timer fires multiple times")
+        exp.expectedFulfillmentCount = 2
+        fixture.timerWrapper.scheduledTimer(withTimeInterval: 0.1, repeats: true) {
+            if count == exp.expectedFulfillmentCount {
+                $0.invalidate()
+            }
+            count += 1
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 1)
+    }
+}
