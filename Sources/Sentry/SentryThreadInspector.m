766556e9832b687732ece18cbdfbad26a558081c
Dhiogo Brustolin <dhiogorb@gmail.com>
2023-01-20 10:25:59 +0100

    Update Sources/Sentry/SentryThreadInspector.m

--- Sources/Sentry/SentryThreadInspector.m
+++ Sources/Sentry/SentryThreadInspector.m
@@ -30,7 +30,6 @@ getStackEntriesFromThread(SentryCrashThread thread, struct SentryCrashMachineCon
 {
     sentrycrashmc_getContextForThread(thread, context, false);
     SentryCrashStackCursor stackCursor;
-    stackCursor.useDlAddr = 0;
 
     sentrycrashsc_initWithMachineContext(&stackCursor, MAX_STACKTRACE_LENGTH, context);
 
