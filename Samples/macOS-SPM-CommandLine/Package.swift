fdfe96b4aaa8a8fd38e7608d9c68b26e343b738c
Dhiogo Brustolin <dhiogorb@gmail.com>
2023-01-24 08:39:39 +0100

    Update Package.swift (#2632)
    Added SentrySwiftUI with SPM in a sample project to validate it during CI.

--- Samples/macOS-SPM-CommandLine/Package.swift
+++ Samples/macOS-SPM-CommandLine/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .target(
             name: "macOS-SPM-CommandLine",
-            dependencies: ["Sentry"], 
+            dependencies: ["Sentry", .product(name: "SentrySwiftUI", package: "Sentry")],
             swiftSettings: [
                 .unsafeFlags(["-warnings-as-errors"])
             ])
