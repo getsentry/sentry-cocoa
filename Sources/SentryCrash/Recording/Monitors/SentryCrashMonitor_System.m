6426c9d8402f56e650466b1855c040313c085712
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-02-01 12:06:25 +0100

    chore: Fix typo in CrashMonitor_System (#2674)

--- Sources/SentryCrash/Recording/Monitors/SentryCrashMonitor_System.m
+++ Sources/SentryCrash/Recording/Monitors/SentryCrashMonitor_System.m
@@ -277,7 +277,7 @@ getAppUUID()
 
 /** Get the current CPU's architecture.
  *
- * @return The current CPU archutecture.
+ * @return The current CPU architecture.
  */
 static const char *
 getCPUArchForCPUType(cpu_type_t cpuType, cpu_subtype_t subType)
