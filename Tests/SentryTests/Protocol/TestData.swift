c38a4ea85c7a67b0e198234133e6980adf1ea412
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-02-17 11:16:51 +0100

    impr: Change debug image to macho
    Change debug image from apple to macho cause apple was
    deprecated.
    
    Fixes GH-2591

--- Tests/SentryTests/Protocol/TestData.swift
+++ Tests/SentryTests/Protocol/TestData.swift
@@ -72,8 +72,10 @@ class TestData {
         debugMeta.imageSize = 352_256
         debugMeta.imageVmAddress = "0x00007fff51af0000"
         debugMeta.name = "/tmp/scratch/dyld_sim"
-        debugMeta.type = "apple"
+        debugMeta.codeFile = "/tmp/scratch/dyld_sim"
+        debugMeta.type = "macho"
         debugMeta.uuid = "84BAEBDA-AD1A-33F4-B35D-8A45F5DAF322"
+        debugMeta.debugID = "84BAEBDA-AD1A-33F4-B35D-8A45F5DAF321"
         
         return debugMeta
     }
