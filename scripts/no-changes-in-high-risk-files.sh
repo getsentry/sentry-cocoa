bd054788823ac0162b74774f8b4f9ab17754880c
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-04-13 08:42:41 +0200

    ci: Remove iOS 12 simulator (#2891)
    Remove running unit tests on iOS 12 simulator as the xcode-install tool
    is being sunset. Instead, we now run UI tests in saucelabs on iOS 12.
    
    Fixes GH-2862

--- scripts/no-changes-in-high-risk-files.sh
+++ scripts/no-changes-in-high-risk-files.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-# To update the sha run shasum -a 256 ./Sources/Sentry/SentryNSURLSessionTaskSearch.m and copy the result in EXPECTED.
+# To update the sha run the command in ACTUAL and copy the result in EXPECTED.
 
-ACTUAL=$(shasum -a 256 ./Sources/Sentry/SentryNSURLSessionTaskSearch.m)
-EXPECTED="819d5ca5e3db2ac23c859b14c149b7f0754d3ae88bea1dba92c18f49a81da0e1  ./Sources/Sentry/SentryNSURLSessionTaskSearch.m"
+ACTUAL=$(shasum -a 256 ./Sources/Sentry/SentryNSURLSessionTaskSearch.m ./Sources/Sentry/SentryNetworkTracker.m)
+EXPECTED="819d5ca5e3db2ac23c859b14c149b7f0754d3ae88bea1dba92c18f49a81da0e1  ./Sources/Sentry/SentryNSURLSessionTaskSearch.m
+58d5414b4f0a4c821b20fc1a16f88bda3116401e905b7bc1d18af828be75e431  ./Sources/Sentry/SentryNetworkTracker.m"
 
 if [ "$ACTUAL" = "$EXPECTED" ]; then
     echo "No changes in high risk files."
