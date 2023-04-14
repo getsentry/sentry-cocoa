ddf1628baf063cc8074e768fc6b10b373e5cad63
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-04-14 10:18:25 +0200

    test: Make TestLogOutput thread safe
    When running the tests locally, I got a SIGABRT in the TestLogOutput when writing to
    the loggedMessages. I added a test to reproduce the same error by writing and reading
    the loggedMessages from different threads. The problem is fixed now by using a
    dispatch queue.

--- Tests/SentryTests/TestLogOutput.swift
+++ Tests/SentryTests/TestLogOutput.swift
@@ -1,9 +1,50 @@
 import Foundation
 
 class TestLogOutput: SentryLogOutput {
-    var loggedMessages: [String] = []
+    
+    private let queue = DispatchQueue(label: "TestLogOutput", attributes: .concurrent)
+    
+    private var _loggedMessages: [String] = []
+    
+    var loggedMessages: [String] {
+        get {
+            queue.sync {
+                return _loggedMessages
+            }
+        }
+    }
+    
     override func log(_ message: String) {
         super.log(message)
-        loggedMessages.append(message)
+        queue.async(flags: .barrier) {
+            self._loggedMessages.append(message)
+        }
+    }
+}
+
+class TestLogOutPutTests: XCTestCase {
+    
+    func testLoggingFromMulitpleThreads() {
+        let sut = TestLogOutput()
+        
+        let queue = DispatchQueue(label: "TestLogOutPutTests", qos: .userInteractive, attributes: [.concurrent, .initiallyInactive])
+        let group = DispatchGroup()
+        
+        for _ in 0...2 {
+            group.enter()
+            queue.async {
+                
+                for i in 0...1_000 {
+                    sut.log("Some message \(i)")
+                }
+                
+                XCTAssertNotNil(sut.loggedMessages)
+                
+                group.leave()
+            }
+        }
+        
+        queue.activate()
+        group.waitWithTimeout(timeout: 500)
     }
 }
