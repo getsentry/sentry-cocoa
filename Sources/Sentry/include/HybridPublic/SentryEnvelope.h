7a1815e1629d4528aa02c15bb0d25d4528e465b6
Denis Andrasec <denrase@gmail.com>
2023-04-17 10:40:55 +0200

    document field

--- Sources/Sentry/include/HybridPublic/SentryEnvelope.h
+++ Sources/Sentry/include/HybridPublic/SentryEnvelope.h
@@ -63,6 +63,14 @@ SENTRY_NO_INIT
 
 @property (nullable, nonatomic, readonly, copy) SentryTraceContext *traceContext;
 
+/**
+ * The timestamp when the event was sent from the SDK as string in RFC 3339 format. Used
+ * for clock drift correction of the event timestamp. The time zone must be UTC.
+ *
+ * The timestamp should be generated as close as possible to the transmision of the event,
+ * so that the delay between sending the envelope and receiving it on the server-side is
+ * minimized.
+ */
 @property (nullable, nonatomic, copy) NSDate *sentAt;
 
 @end
