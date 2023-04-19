cf434a1b7970fce65b7503d104a6953827f9764f
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-04-19 10:58:22 +0200

    add network tracker

--- Sources/Sentry/include/SentryTraceOrigins.h
+++ Sources/Sentry/include/SentryTraceOrigins.h
@@ -7,5 +7,6 @@ static NSString *const SentryTraceOriginAuto = @"auto";
 static NSString *const SentryTraceOriginAutoAppStart = @"auto.app.start";
 static NSString *const SentryTraceOriginAutoFile = @"auto.file";
 static NSString *const SentryTraceOriginAutoDBCoreData = @"auto.db.core_data";
+static NSString *const SentryTraceOriginAutoHttpNSRLSession = @"auto.http.ns_url_session";
 static NSString *const SentryTraceOriginAutoUIViewController = @"auto.ui.view_controller";
 static NSString *const SentryTraceOriginAutoUITimeToDisplay = @"auto.ui.time_to_display";
