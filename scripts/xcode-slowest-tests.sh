0f9e3d68ae3841bbafed74c476f5c5e7160dd6a8
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-04-27 14:16:20 +0200

    fix broken pipe

--- scripts/xcode-slowest-tests.sh
+++ scripts/xcode-slowest-tests.sh
@@ -7,4 +7,4 @@ RAW_TEST_OUTPUT_LOG=${1:-raw-test-output.log}
 NUMBER_OF_SLOWEST_TEST="${2:-10}"
 
 echo "The $NUMBER_OF_SLOWEST_TEST slowest test cases:" 
-cat $RAW_TEST_OUTPUT_LOG | grep 'Test\ Case.*seconds' | awk -F '[()]' '{print $2 " -> " $1}' | sort -rn | head -$NUMBER_OF_SLOWEST_TEST
+cat $RAW_TEST_OUTPUT_LOG | grep 'Test\ Case.*seconds' | awk -F '[()]' '{print $2 " -> " $1}' | sort -rn | head -n $NUMBER_OF_SLOWEST_TEST
