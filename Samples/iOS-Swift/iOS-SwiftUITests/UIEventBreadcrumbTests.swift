4f0963e8f0b7ec63ab36a25b2533a68143662059
Dhiogo Ramos Brustolin <dhiogo.brustolin@sentry.io>
2023-05-03 11:01:10 +0200

    Update UIEventBreadcrumbTests.swift

--- Samples/iOS-Swift/iOS-SwiftUITests/UIEventBreadcrumbTests.swift
+++ Samples/iOS-Swift/iOS-SwiftUITests/UIEventBreadcrumbTests.swift
@@ -22,17 +22,15 @@ class UIEventBreadcrumbTests: XCTestCase {
         app.buttons["Extra"].tap()
         app.buttons["UI event tests"].tap()
 
-        let label = app.staticTexts["breadcrumbLabel"]
-
         //Trigger a change in textfield
         app.buttons["editingChangedButton"].tap()
         //Check the last breadcrumb is the button being pressed
-        XCTAssertEqual(label.label, "performEditingChangedPressed:")
+        XCTAssertEqual(app.staticTexts["breadcrumbLabel"].label, "performEditingChangedPressed:")
 
         //Trigger an endEditing in textfield
         app.buttons["editingDidEndButton"].tap()
         //Check the last breadcrumb is the endEditing from the textfield and not the button being pressed
-        XCTAssertEqual(label.label, "textFieldEndChanging:")
+        XCTAssertEqual(app.staticTexts["breadcrumbLabel"].label, "textFieldEndChanging:")
     }
 
     func waitForExistenceOfMainScreen() {
