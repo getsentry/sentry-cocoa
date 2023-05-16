2bfe5bb7072448b9c4dbd36d5ce3bcf66474f820
Indragie Karunaratne <indragie.karunaratne@sentry.io>
2023-05-15 22:47:05 -0700

    fix: additional profiling data races

--- Sources/Sentry/include/SentryProfiler+Test.h
+++ Sources/Sentry/include/SentryProfiler+Test.h
@@ -1,5 +1,6 @@
 #include "SentryBacktrace.hpp"
 #import "SentryProfiler.h"
+#import "SentryProfiler+Private.h"
 #import "SentryProfilingConditionals.h"
 
 #if SENTRY_TARGET_PROFILING_SUPPORTED
@@ -10,14 +11,6 @@
 @class SentryTransaction;
 
 NS_ASSUME_NONNULL_BEGIN
-
-void processBacktrace(const sentry::profiling::Backtrace &backtrace,
-    NSMutableDictionary<NSString *, NSMutableDictionary *> *threadMetadata,
-    NSMutableDictionary<NSString *, NSDictionary *> *queueMetadata,
-    NSMutableArray<SentrySample *> *samples, NSMutableArray<NSArray<NSNumber *> *> *stacks,
-    NSMutableArray<NSDictionary<NSString *, id> *> *frames,
-    NSMutableDictionary<NSString *, NSNumber *> *frameIndexLookup,
-    NSMutableDictionary<NSString *, NSNumber *> *stackIndexLookup);
 
 NSDictionary<NSString *, id> *serializedProfileData(NSDictionary<NSString *, id> *profileData,
     SentryTransaction *transaction, SentryId *profileID, NSString *truncationReason,
