ab10443b7076efb0762f1eff85c570cc763e0734
Dhiogo Brustolin <dhiogo.brustolin@sentry.io>
2023-06-14 12:31:34 +0200

    Update Sources/Sentry/include/SentryBinaryImageCache.h

--- Sources/Sentry/include/SentryBinaryImageCache.h
+++ Sources/Sentry/include/SentryBinaryImageCache.h
@@ -9,6 +9,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic) uint64_t size;
 @end
 
+/**
+* This class listen to `SentryCrashBinaryImageCache` to keep a copy of the loaded binaries information in a sorted collection that will be used to symbolicate frames with better performance.
+*/
 @interface SentryBinaryImageCache : NSObject
 SENTRY_NO_INIT
 
