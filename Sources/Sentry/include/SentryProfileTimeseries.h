ba971cbaee48143a5a68fe62259e31ac73c0cdc7
Andrew McKnight <andrew.mcknight@sentry.io>
2023-07-06 16:18:23 -0800

    extract implementation of SentryProfilerState; extract SentrySample

--- Sources/Sentry/include/SentryProfileTimeseries.h
+++ Sources/Sentry/include/SentryProfileTimeseries.h
@@ -4,19 +4,11 @@
 
 #    import "SentryDefines.h"
 #    import <Foundation/Foundation.h>
-#    import <mutex>
 
+@class SentrySample;
 @class SentryTransaction;
 
 NS_ASSUME_NONNULL_BEGIN
-
-/** A storage class to hold the data associated with a single profiler sample. */
-@interface SentrySample : NSObject
-@property (nonatomic, assign) uint64_t absoluteTimestamp;
-@property (nonatomic, strong) NSNumber *stackIndex;
-@property (nonatomic, assign) uint64_t threadID;
-@property (nullable, nonatomic, copy) NSString *queueAddress;
-@end
 
 NSArray<SentrySample *> *_Nullable slicedProfileSamples(
     NSArray<SentrySample *> *samples, SentryTransaction *transaction);
