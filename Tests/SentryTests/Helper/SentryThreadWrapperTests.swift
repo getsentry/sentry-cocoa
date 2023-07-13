2022d05b7e29da8aac49b0a2f0ca3f2aa5b9d3e1
Andrew McKnight <andrew.mcknight@sentry.io>
2023-07-12 16:17:16 -0800

    add test

---
+++ Tests/SentryTests/Helper/SentryThreadWrapperTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+
+final class SentryThreadWrapperTests: XCTestCase {
+    func testOnMainThreadFromMainThread() {
+        let e = expectation(description: "Asserted that execution happened on main thread")
+        SentryThreadWrapper.onMainThread {
+            XCTAssertTrue(Thread.isMainThread)
+            e.fulfill()
+        }
+        waitForExpectations(timeout: 1)
+    }
+
+    func testOnMainThreadFromNonmainContext() {
+        let e = expectation(description: "Asserted that execution happened on main thread")
+        let q = DispatchQueue(label: "a nonmain queue", qos: .background)
+        q.async {
+            XCTAssertFalse(Thread.isMainThread)
+            SentryThreadWrapper.onMainThread {
+                XCTAssertTrue(Thread.isMainThread)
+                e.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 1)
+    }
+}
