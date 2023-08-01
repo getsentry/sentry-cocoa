3f03cbcab00276440364fdbfe0f663e66417a9c1
Dhiogo Ramos Brustolin <dhiogo.brustolin@sentry.io>
2023-08-01 16:59:25 +0200

    Update no-changes-in-high-risk-files.sh

--- scripts/no-changes-in-high-risk-files.sh
+++ scripts/no-changes-in-high-risk-files.sh
@@ -5,7 +5,7 @@ set -euox pipefail
 
 ACTUAL=$(shasum -a 256 ./Sources/Sentry/SentryNSURLSessionTaskSearch.m ./Sources/Sentry/SentryNetworkTracker.m ./Sources/Sentry/SentryUIViewControllerSwizzling.m ./Sources/Sentry/SentryNSDataSwizzling.m ./Sources/Sentry/SentrySubClassFinder.m ./Sources/Sentry/SentryCoreDataSwizzling.m ./Sources/Sentry/SentrySwizzleWrapper.m ./Sources/Sentry/include/SentrySwizzle.h ./Sources/Sentry/SentrySwizzle.m)
 EXPECTED="819d5ca5e3db2ac23c859b14c149b7f0754d3ae88bea1dba92c18f49a81da0e1  ./Sources/Sentry/SentryNSURLSessionTaskSearch.m
-797f1e4f1f4d0986770c749c6614243417373d3858044a0aa6e16bd4a52533ed  ./Sources/Sentry/SentryNetworkTracker.m
+6d0920d91649a7173a52b4229db0b204b66677cb4033d9ca28917f3423276517  ./Sources/Sentry/SentryNetworkTracker.m
 128dee523c75c121e9b8b767d84a2a137e5b60498c9f3ba604aad3c935d780d0  ./Sources/Sentry/SentryUIViewControllerSwizzling.m
 e95e62ec7363984f20c78643bb7d992a41a740f97e1befb71525ac34caf88b37  ./Sources/Sentry/SentryNSDataSwizzling.m
 9ad05dd8dd29788cba994736fdcd3bbde59a94e32612640d11f4f9c38ad6610e  ./Sources/Sentry/SentrySubClassFinder.m
