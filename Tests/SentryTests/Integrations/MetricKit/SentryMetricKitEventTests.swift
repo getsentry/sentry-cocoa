6f8dd55094c74bff62ea812c004eee75566e5ecc
Andrew McKnight <andrew.mcknight@sentry.io>
2023-08-18 14:25:06 -0800

    ref: move decl/impl from SentryEvent (MetricKit) to SenryEvent .h/.m as interface extension

--- Tests/SentryTests/Integrations/MetricKit/SentryMetricKitEventTests.swift
+++ Tests/SentryTests/Integrations/MetricKit/SentryMetricKitEventTests.swift
@@ -5,27 +5,27 @@ import XCTest
 final class SentryMetricKitEventTests: XCTestCase {
 
     func testMXCPUException_IsMetricKitEvent() {
-        XCTAssertTrue(TestData.metricKitEvent.isMetricKitEvent)
+        XCTAssertTrue(TestData.metricKitEvent.isMetricKitEvent())
     }
     
     func testMXDiskWriteException_IsMetricKitEvent() {        
-        XCTAssertTrue(createMetricKitEventWith(mechanismType: SentryMetricKitDiskWriteExceptionMechanism).isMetricKitEvent)
+        XCTAssertTrue(createMetricKitEventWith(mechanismType: SentryMetricKitDiskWriteExceptionMechanism).isMetricKitEvent())
     }
     
     func testMXHangDiagnostic_IsMetricKitEvent() {
-        XCTAssertTrue(createMetricKitEventWith(mechanismType: SentryMetricKitHangDiagnosticMechanism).isMetricKitEvent)
+        XCTAssertTrue(createMetricKitEventWith(mechanismType: SentryMetricKitHangDiagnosticMechanism).isMetricKitEvent())
     }
     
     func testWatchDogEvent_IsNotMetricKitEvent() {
-        XCTAssertFalse(TestData.oomEvent.isMetricKitEvent)
+        XCTAssertFalse(TestData.oomEvent.isMetricKitEvent())
     }
     
     func testNormalEvent_IsNotMetricKitEvent() {
-        XCTAssertFalse(TestData.event.isMetricKitEvent)
+        XCTAssertFalse(TestData.event.isMetricKitEvent())
     }
     
     func testEmptyEvent_IsNotMetricKitEvent() {
-        XCTAssertFalse(Event().isMetricKitEvent)
+        XCTAssertFalse(Event().isMetricKitEvent())
     }
     
     private func createMetricKitEventWith(mechanismType: String) -> Event {
