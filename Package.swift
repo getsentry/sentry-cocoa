826eeb7d8c1e48c95c6fb426d54fb76611f2fece
Andrew McKnight <andrew.mcknight@sentry.io>
2023-10-04 14:17:29 -0700

    fixup! fixup! set uikit macro in SPM/CocoaPods specs

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
                 "SentryCrash/"
             ],
             publicHeadersPath: "Sentry/Public/",
+            cSettings: [.unsafeFlags(["-DSENTRY_UIKIT_LINKED=1"])],
             cxxSettings: [
                 .define("GCC_ENABLE_CPP_EXCEPTIONS", to: "YES"),
                 .headerSearchPath("Sentry/include"),
@@ -31,8 +32,7 @@ let package = Package(
                 .headerSearchPath("SentryCrash/Reporting/Filters"),
                 .headerSearchPath("SentryCrash/Reporting/Filters/Tools"),
                 .headerSearchPath("SentryCrash/Reporting/Tools")
-            ],
-            cSettings: [.unsafeFlags(["-DSENTRY_UIKIT_LINKED=1"])]
+            ]
         ),
         .target( name: "SentryPrivate",
                  path: "Sources",
