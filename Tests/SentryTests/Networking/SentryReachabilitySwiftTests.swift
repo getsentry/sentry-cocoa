7d14178f49d821048d29dc0914d424ab511da566
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-11-17 10:58:41 +0100

    test: Skip callbacks for Swift Reachability Tests (#3425)
    Calling the methods of SCNetworkReachability in a tight loop from
    multiple threads is not an actual use case, and it leads to flaky test
    results. With this test, we want to test if the adding and removing
    observers are adequately synchronized and not if we call
    SCNetworkReachability correctly.

--- Tests/SentryTests/Networking/SentryReachabilitySwiftTests.swift
+++ Tests/SentryTests/Networking/SentryReachabilitySwiftTests.swift
@@ -4,6 +4,12 @@ final class SentryReachabilitySwiftTests: XCTestCase {
 
     func testAddRemoveFromMultipleThreads() throws {
         let sut = SentryReachability()
+        // Calling the methods of SCNetworkReachability in a tight loop from
+        // multiple threads is not an actual use case, and it leads to flaky test
+        // results. With this test, we want to test if the adding and removing
+        // observers are adequately synchronized and not if we call
+        // SCNetworkReachability correctly.
+        sut.skipRegisteringActualCallbacks = true
         testConcurrentModifications(writeWork: {_ in
             sut.add(TestReachabilityObserver())
         }, readWork: {
