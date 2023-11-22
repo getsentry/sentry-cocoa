24839d46b72733f3c42dc526940b4fba78a9e8bc
Dhiogo Brustolin <dhiogorb@gmail.com>
2023-11-22 10:41:25 +0100

    Update ExternalUIViewController.swift

--- SentryTestUtils/ExternalUIViewController.swift
+++ SentryTestUtils/ExternalUIViewController.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-#if os(iOS) || os(tvOS)
+#if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
 import UIKit
 
 //This class is used to test swizzling of view controllers in external libs
