b8c516217f512fa6c3fd39a8d6c6edfcee6cf3a6
Andrew McKnight <andrew.mcknight@sentry.io>
2023-11-29 11:41:55 -0900

    ci: remove TrendingMovies and profile data generation CI job (#3453)

--- Samples/TrendingMovies/TrendingMovies/Common/NSLayoutConstraintExtensions.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-extension NSLayoutConstraint {
-    func withPriority(_ priority: UILayoutPriority) -> NSLayoutConstraint {
-        self.priority = priority
-        return self
-    }
-}
