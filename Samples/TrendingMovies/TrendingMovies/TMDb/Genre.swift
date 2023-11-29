a3c4915ecd4b726c43163a749b47b548bd7d4917
Andrew McKnight <andrew.mcknight@sentry.io>
2023-11-29 11:41:38 -0900

    remove trending movies sources

--- Samples/TrendingMovies/TrendingMovies/TMDb/Genre.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// https://developers.themoviedb.org/3/genres/get-movie-list
-struct Genre: Codable {
-    let id: Int
-    let name: String
-}
--- Samples/TrendingMovies/TrendingMovies/TMDb/Person.swift
+++
@@ -1,8 +0,0 @@
-/// Protocol for a type that represents the profile of someone who received
-/// attribution in the credits.
-protocol Person {
-    var id: Int { get }
-    var name: String { get }
-    var profilePath: String? { get }
-    var role: String { get }
-}
