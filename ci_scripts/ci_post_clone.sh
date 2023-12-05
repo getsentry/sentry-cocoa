170d4f022f292ba091ecc427b98fd69278e093e0
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2023-12-05 14:52:47 +0100

    use custom sentry-cli branch for experimenting

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,5 +6,5 @@ source ~/.cargo/env
 cd ~
 git clone https://github.com/getsentry/sentry-cli.git
 cd sentry-cli
-git revert 40f86f6
+git checkout szokeasaurusrex/check-revert-optimizations
 cargo build --release
