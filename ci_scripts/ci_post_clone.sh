b45b44ecf9de5c7193cdf0eab3698f383a2c35b1
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2023-12-06 13:22:23 +0100

    verify

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source ~/.cargo/env
-
-cd ~
-git clone https://github.com/getsentry/sentry-cli.git
-cd sentry-cli
-git checkout szokeasaurusrex/check-revert-optimizations
-cargo build --release
+brew install getsentry/tools/sentry-cli
