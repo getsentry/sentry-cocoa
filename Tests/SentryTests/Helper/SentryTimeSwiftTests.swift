db876e90965a030edbd7540ee83092abd6ff4aab
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-12-06 12:22:55 +0100

    again fix tests

--- Tests/SentryTests/Helper/SentryTimeSwiftTests.swift
+++ Tests/SentryTests/Helper/SentryTimeSwiftTests.swift
@@ -7,13 +7,15 @@ final class SentryTimeSwiftTests: XCTestCase {
         expect(timeIntervalToNanoseconds(0.0)) == UInt64(0)
         expect(timeIntervalToNanoseconds(0.5)) == UInt64(500_000_000)
         expect(timeIntervalToNanoseconds(1.0)) == UInt64(1_000_000_000)
-        let maxTimeInterval = nanosecondsToTimeInterval(UInt64.max)
-        expect(timeIntervalToNanoseconds(maxTimeInterval)) == UInt64.max
+        expect(timeIntervalToNanoseconds(1.123456789)) == UInt64(1_123_456_789)
+        expect(timeIntervalToNanoseconds(123_456_789.123456)) == UInt64(123_456_789_123_456_000)
     }
 
     func testNanosecondsToTimeInterval() {
         expect(nanosecondsToTimeInterval(0)).to(beCloseTo(0.0, within: 1e-9))
         expect(nanosecondsToTimeInterval(500_000_000)).to(beCloseTo(0.5, within: 1e-9))
         expect(nanosecondsToTimeInterval(1_000_000_000)).to(beCloseTo(1.0, within: 1e-9))
+        expect(nanosecondsToTimeInterval(1_123_456_789)).to(beCloseTo(1.123456789, within: 1e-9))
+        expect(nanosecondsToTimeInterval(123_456_789_123_456_000)).to(beCloseTo(123_456_789.123456, within: 1e-9))
     }
 }
