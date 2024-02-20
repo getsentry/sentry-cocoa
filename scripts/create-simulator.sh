9465cc303c94150519d8758146b7bde33c3b5c1d
Philipp Hofmann <philipp.hofmann@sentry.io>
2024-02-20 15:52:50 +0100

    again a fix

--- scripts/create-simulator.sh
+++ scripts/create-simulator.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 
 XCODE_VERSION="${1}"
 SIM_RUNTIME="${2}"
-SIM_RUNTIME_UNDERSCORE="${3}"
+SIM_RUNTIME_WITH_DASH="${3}"
 
 sudo mkdir -p /Library/Developer/CoreSimulator/Profiles/Runtimes
 sudo ln -s /Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ ${SIM_RUNTIME}.simruntime
-xcrun simctl create custom-test-device "iPhone 8" "com.apple.CoreSimulator.SimRuntime.iOS-${SIM_RUNTIME_UNDERSCORE}"
+xcrun simctl create custom-test-device "iPhone 8" "com.apple.CoreSimulator.SimRuntime.iOS-${SIM_RUNTIME_WITH_DASH}"
