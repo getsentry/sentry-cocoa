914d06903ad6768bc4d9ba87207700d27b441940
Dhiogo Brustolin <dhiogo.brustolin@sentry.io>
2024-02-26 14:49:07 +0100

    Apply suggestions from code review

--- Sources/Sentry/include/SentrySwift.h
+++ Sources/Sentry/include/SentrySwift.h
@@ -1,9 +1,7 @@
 #ifndef SentrySwift_h
 #define SentrySwift_h
 
-#if SWIFT_PACKAGE
-@import SentryPrivate;
-#elif __has_include("Sentry-Swift.h")
+#if __has_include("Sentry-Swift.h")
 #    import "Sentry-Swift.h"
 #else
 #    import "Sentry/Sentry-Swift.h"
