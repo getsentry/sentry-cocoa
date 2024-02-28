0a6d52bb6e9a443192c62ec7c87d117191bb5926
Dhiogo Brustolin <dhiogorb@gmail.com>
2024-02-28 14:05:32 +0100

    fixing test

--- Package.swift
+++ Package.swift
@@ -12,7 +12,12 @@ let package = Package(
         .binaryTarget(
                     name: "Sentry",
                     url: "https://github.com/getsentry/sentry-cocoa/releases/download/8.22.0-alpha.0/Sentry.xcframework.zip",
-                    checksum: "86156301aee5c8774a8cd5c240286f914f6e7721aaac5a7c9d049ea613a4b730"
+                    checksum: "86156301aee5c8774a8cd5c240286f914f6e7721aaac5a7c9d049ea613a4b730" //Sentry-Static
+                ),
+        .binaryTarget(
+                    name: "Sentry",
+                    url: "https://github.com/getsentry/sentry-cocoa/releases/download/8.22.0-alpha.0/Sentry-Dynamic.xcframework.zip",
+                    checksum: "86156301aee5c8774a8cd5c240286f914f6e7721aaac5a7c9d049ea613a4b730" //Sentry-Dynamic
                 ),
         .target ( name: "SentrySwiftUI",
                   dependencies: ["Sentry", "SentryInternal"],
--- scripts/update-package-sha.sh
+++ scripts/update-package-sha.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-NEW_CHECKSUM=$(shasum -a 256 Carthage/Sentry.xcframework.zip | awk '{print $1}')
+NEW_CHECKSUM_STATIC=$(shasum -a 256 Carthage/Sentry.xcframework.zip | awk '{print $1}')
+NEW_CHECKSUM_DYNAMIC=$(shasum -a 256 Carthage/Sentry-Dynamic.xcframework.zip | awk '{print $1}')
 
 os=$(uname)
 
 if [ "$os" == "Linux" ]; then
-    sed -i "s/checksum: \".*\"/checksum: \"$NEW_CHECKSUM\"/" Package.swift
+    sed -i "s/checksum: \".*\" \/\/Sentry-Static/checksum: \"$NEW_CHECKSUM_STATIC\" \/\/Sentry-Static/" Package.swift
+    sed -i "s/checksum: \".*\" \/\/Sentry-Dynamic/checksum: \"$NEW_CHECKSUM_DYNAMIC\" \/\/Sentry-Dynamic/" Package.swift
 else
-    sed -i "" "s/checksum: \".*\"/checksum: \"$NEW_CHECKSUM\"/" Package.swift
+    sed -i "" "s/checksum: \".*\" \/\/Sentry-Static/checksum: \"$NEW_CHECKSUM_STATIC\" \/\/Sentry-Static/" Package.swift
+    sed -i "" "s/checksum: \".*\" \/\/Sentry-Dynamic/checksum: \"$NEW_CHECKSUM_DYNAMIC\" \/\/Sentry-Dynamic/" Package.swift
 fi
 
 echo $GITHUB_RUN_ID > .github/last-release-runid
