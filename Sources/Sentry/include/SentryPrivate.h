b55c88cf7319665ee1355c98a52cb3f3c0712335
Dhiogo Brustolin <dhiogorb@gmail.com>
2024-03-14 16:32:18 +0100

    SessionReplayIntegration in swift

--- Sources/Sentry/include/SentryPrivate.h
+++ Sources/Sentry/include/SentryPrivate.h
@@ -1,7 +1,10 @@
 // Sentry internal headers that are needed for swift code
 
 #import "SentryBaggage.h"
-#import "SentryBaseIntegration.h"
+#import "SentryDependencyContainer.h"
+#import "SentryGlobalEventProcessor.h"
 #import "SentryRandom.h"
 #import "SentrySdkInfo.h"
+#import "SentrySessionReplay.h"
 #import "SentryTime.h"
+#import "SentryUIApplication.h"
