6733b7f594daaa75daf3ef09a1faae21cdef1527
Philipp Hofmann <philipp.hofmann@sentry.io>
2024-04-08 11:32:41 +0200

    feat(metrics): Add rate limits
    This adds the handling of rate limits for the new metric_bucket category
    including handling the metric namespace.
    
    Fixes GH-3805

--- Tests/SentryTests/Networking/SentryDataCategoryMapperTests.swift
+++ Tests/SentryTests/Networking/SentryDataCategoryMapperTests.swift
@@ -3,13 +3,14 @@ import Nimble
 import XCTest
 
 class SentryDataCategoryMapperTests: XCTestCase {
+    
     func testEnvelopeItemType() {
         expect(sentryDataCategoryForEnvelopItemType("event")) == .error
         expect(sentryDataCategoryForEnvelopItemType("session")) == .session
         expect(sentryDataCategoryForEnvelopItemType("transaction")) == .transaction
         expect(sentryDataCategoryForEnvelopItemType("attachment")) == .attachment
         expect(sentryDataCategoryForEnvelopItemType("profile")) == .profile
-        expect(sentryDataCategoryForEnvelopItemType("statsd")) == .statsd
+        expect(sentryDataCategoryForEnvelopItemType("statsd")) == .metricBucket
         expect(sentryDataCategoryForEnvelopItemType("unknown item type")) == .default
     }
 
@@ -22,7 +23,7 @@ class SentryDataCategoryMapperTests: XCTestCase {
         expect(sentryDataCategoryForNSUInteger(5)) == .attachment
         expect(sentryDataCategoryForNSUInteger(6)) == .userFeedback
         expect(sentryDataCategoryForNSUInteger(7)) == .profile
-        expect(sentryDataCategoryForNSUInteger(8)) == .statsd
+        expect(sentryDataCategoryForNSUInteger(8)) == .metricBucket
         expect(sentryDataCategoryForNSUInteger(9)) == .unknown
 
         XCTAssertEqual(.unknown, sentryDataCategoryForNSUInteger(10), "Failed to map unknown category number to case .unknown")
@@ -37,7 +38,7 @@ class SentryDataCategoryMapperTests: XCTestCase {
         expect(sentryDataCategoryForString(kSentryDataCategoryNameAttachment)) == .attachment
         expect(sentryDataCategoryForString(kSentryDataCategoryNameUserFeedback)) == .userFeedback
         expect(sentryDataCategoryForString(kSentryDataCategoryNameProfile)) == .profile
-        expect(sentryDataCategoryForString(kSentryDataCategoryNameStatsd)) == .statsd
+        expect(sentryDataCategoryForString(kSentryDataCategoryNameMetricBucket)) == .metricBucket
         expect(sentryDataCategoryForString(kSentryDataCategoryNameUnknown)) == .unknown
 
         XCTAssertEqual(.unknown, sentryDataCategoryForString("gdfagdfsa"), "Failed to map unknown category name to case .unknown")
@@ -52,7 +53,7 @@ class SentryDataCategoryMapperTests: XCTestCase {
         expect(nameForSentryDataCategory(.attachment)) == kSentryDataCategoryNameAttachment
         expect(nameForSentryDataCategory(.userFeedback)) == kSentryDataCategoryNameUserFeedback
         expect(nameForSentryDataCategory(.profile)) == kSentryDataCategoryNameProfile
-        expect(nameForSentryDataCategory(.statsd)) == kSentryDataCategoryNameStatsd
+        expect(nameForSentryDataCategory(.metricBucket)) == kSentryDataCategoryNameMetricBucket
         expect(nameForSentryDataCategory(.unknown)) == kSentryDataCategoryNameUnknown
     }
 }
