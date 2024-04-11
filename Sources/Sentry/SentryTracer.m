008f9c7fb705af612c11b8561ecd0c26200ef505
Andrew McKnight <andrew.mcknight@sentry.io>
2024-04-11 11:10:22 -0800

    remove conditional logic change for later impl branch

--- Sources/Sentry/SentryTracer.m
+++ Sources/Sentry/SentryTracer.m
@@ -189,8 +189,7 @@ static BOOL appStartMeasurementRead;
 #endif // SENTRY_HAS_UIKIT
 
 #if SENTRY_TARGET_PROFILING_SUPPORTED
-    if (!hub.client.options.enableContinuousProfiling
-        && (_configuration.profilesSamplerDecision.decision == kSentrySampleDecisionYes
+    if ((_configuration.profilesSamplerDecision.decision == kSentrySampleDecisionYes
             || isTracingAppLaunch)) {
         _internalID = [[SentryId alloc] init];
         if ((_isProfiling = [SentryProfiler startWithTracer:_internalID])) {
