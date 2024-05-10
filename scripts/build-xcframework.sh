getsentry/sentry-cocoa

commit 626b91bc890ecd47b9e08dd197494d5098b55377

fix: Sentry.framework does not support the minimum OS Version specified in the Info.plist (#3774)
Static framework was failing Xcode validation because of minimum os version

scripts/build-xcframework.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eou pipefail
+
 sdks=( iphoneos iphonesimulator macosx appletvos appletvsimulator watchos watchsimulator xros xrsimulator )
 
 rm -rf Carthage/
@@ -18,9 +20,20 @@ generate_xcframework() {
     
     for sdk in "${sdks[@]}"; do
         if [[ -n "$(grep "${sdk}" <<< "$ALL_SDKS")" ]]; then
-            xcodebuild archive -project Sentry.xcodeproj/ -scheme "$scheme" -configuration Release -sdk "$sdk" -archivePath ./Carthage/archive/${scheme}${sufix}/${sdk}.xcarchive CODE_SIGNING_REQUIRED=NO SKIP_INSTALL=NO CODE_SIGN_IDENTITY= CARTHAGE=YES MACH_O_TYPE=$MACH_O_TYPE
-            
+            xcodebuild archive -project Sentry.xcodeproj/ -scheme "$scheme" -configuration Release -sdk "$sdk" -archivePath ./Carthage/archive/${scheme}${sufix}/${sdk}.xcarchive CODE_SIGNING_REQUIRED=NO SKIP_INSTALL=NO CODE_SIGN_IDENTITY= CARTHAGE=YES MACH_O_TYPE=$MACH_O_TYPE ENABLE_CODE_COVERAGE=NO
+                 
             createxcframework+="-framework Carthage/archive/${scheme}${sufix}/${sdk}.xcarchive/Products/Library/Frameworks/${scheme}.framework "
+
+            if [ "$MACH_O_TYPE" = "staticlib" ]; then
+                local infoPlist="Carthage/archive/${scheme}${sufix}/${sdk}.xcarchive/Products/Library/Frameworks/${scheme}.framework/Info.plist"
+                
+                if [ ! -e "$infoPlist" ]; then
+                    infoPlist="Carthage/archive/${scheme}${sufix}/${sdk}.xcarchive/Products/Library/Frameworks/${scheme}.framework/Resources/Info.plist"
+                fi
+                # This workaround is necessary to make Sentry Static framework to work
+                #More information in here: https://github.com/getsentry/sentry-cocoa/issues/3769
+                plutil -replace "MinimumOSVersion" -string "9999" "$infoPlist"
+            fi
             
             if [ -d "Carthage/archive/${scheme}${sufix}/${sdk}.xcarchive/dSYMs/${scheme}.framework.dSYM" ]; then
                 # Has debug symbols
@@ -32,7 +45,12 @@ generate_xcframework() {
     done
     
     #Create framework for mac catalyst
-    xcodebuild -project Sentry.xcodeproj/ -scheme "$scheme" -configuration Release -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' -derivedDataPath ./Carthage/DerivedData CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY= CARTHAGE=YES MACH_O_TYPE=$MACH_O_TYPE SUPPORTS_MACCATALYST=YES
+    xcodebuild -project Sentry.xcodeproj/ -scheme "$scheme" -configuration Release -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' -derivedDataPath ./Carthage/DerivedData CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY= CARTHAGE=YES MACH_O_TYPE=$MACH_O_TYPE SUPPORTS_MACCATALYST=YES ENABLE_CODE_COVERAGE=NO
+    
+    if [ "$MACH_O_TYPE" = "staticlib" ]; then
+        local infoPlist="Carthage/DerivedData/Build/Products/Release-maccatalyst/${scheme}.framework/Resources/Info.plist"
+        plutil -replace "MinimumOSVersion" -string "9999" "$infoPlist"
+    fi
     
     createxcframework+="-framework Carthage/DerivedData/Build/Products/Release-maccatalyst/${scheme}.framework "
     if [ -d "Carthage/DerivedData/Build/Products/Release-maccatalyst/${scheme}.framework.dSYM" ]; then

Sentry.podspec
@@ -42,8 +42,7 @@ Pod::Spec.new do |s|
   
   s.subspec 'HybridSDK' do |sp|
       sp.source_files = "Sources/Sentry/**/*.{h,hpp,m,mm,c,cpp}",
-        "Sources/SentryCrash/**/*.{h,hpp,m,mm,c,cpp}", "Sources/Swift/**/*.{swift,h,hpp,m,mm,c,cpp}",
-        "Sources/Sentry/include/module.modulemap"
+        "Sources/SentryCrash/**/*.{h,hpp,m,mm,c,cpp}", "Sources/Swift/**/*.{swift,h,hpp,m,mm,c,cpp}"
         
       sp.public_header_files =
         "Sources/Sentry/Public/*.h", "Sources/Sentry/include/HybridPublic/*.h"

commit 4c0b15137001db76ed776f24dd430b9121a977ba

fix: Clang error for Xcode 15.4 (#3958)
The hacky solution introduced in the PR #3774 is breaking compilation for Xcode 15.4

This solution will keep both Xcode 15 (.3 and .4) working.

I believe we can remove the version replacement after a few months after Xcode 15.4 release.

scripts/build-xcframework.sh
@@ -38,8 +38,9 @@ generate_xcframework() {
                     infoPlist="Carthage/archive/${scheme}${sufix}/${sdk}.xcarchive/Products/Library/Frameworks/${scheme}.framework/Resources/Info.plist"
                 fi
                 # This workaround is necessary to make Sentry Static framework to work
-                #More information in here: https://github.com/getsentry/sentry-cocoa/issues/3769
-                plutil -replace "MinimumOSVersion" -string "9999" "$infoPlist"
+                # More information in here: https://github.com/getsentry/sentry-cocoa/issues/3769
+                # The version 100 seems to work with all Xcode up to 15.4
+                plutil -replace "MinimumOSVersion" -string "100.0" "$infoPlist"
             fi
             
             if [ -d "Carthage/archive/${scheme}${sufix}/${sdk}.xcarchive/dSYMs/${scheme}.framework.dSYM" ]; then
@@ -56,7 +57,7 @@ generate_xcframework() {
 
     if [ "$MACH_O_TYPE" = "staticlib" ]; then
         local infoPlist="Carthage/DerivedData/Build/Products/"$configuration"-maccatalyst/${scheme}.framework/Resources/Info.plist"
-        plutil -replace "MinimumOSVersion" -string "9999" "$infoPlist"
+        plutil -replace "MinimumOSVersion" -string "100.0" "$infoPlist"
     fi
     
     createxcframework+="-framework Carthage/DerivedData/Build/Products/"$configuration"-maccatalyst/${scheme}.framework "