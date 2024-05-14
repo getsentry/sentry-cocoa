getsentry/sentry-cocoa

commit cbd77256d1229ff6ca0cc60d94bc69560179ea37

feat: Send GraphQL "operationName" in HTTP breadcrumbs (#3931)
This PR attempts to support sending GraphQL operation names with existing HTTP breadcrumbs.


Co-authored-by: Max Chuquimia <>
Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>

Sentry.xcodeproj/project.pbxproj
@@ -72,6 +72,8 @@
 		15E0A8F22411A45A00F044E3 /* SentrySession.m in Sources */ = {isa = PBXBuildFile; fileRef = 15E0A8F12411A45A00F044E3 /* SentrySession.m */; };
 		33042A0D29DAF79A00C60085 /* SentryExtraContextProvider.m in Sources */ = {isa = PBXBuildFile; fileRef = 33042A0C29DAF79A00C60085 /* SentryExtraContextProvider.m */; };
 		33042A1729DC2C4300C60085 /* SentryExtraContextProviderTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 33042A1629DC2C4300C60085 /* SentryExtraContextProviderTests.swift */; };
+		51B15F7E2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift in Sources */ = {isa = PBXBuildFile; fileRef = 51B15F7D2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift */; };
+		51B15F802BE88D510026A2F2 /* URLSessionTaskTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 51B15F7F2BE88D510026A2F2 /* URLSessionTaskTests.swift */; };
 		620379DB2AFE1415005AC0C1 /* SentryBuildAppStartSpans.h in Headers */ = {isa = PBXBuildFile; fileRef = 620379DA2AFE1415005AC0C1 /* SentryBuildAppStartSpans.h */; };
 		620379DD2AFE1432005AC0C1 /* SentryBuildAppStartSpans.m in Sources */ = {isa = PBXBuildFile; fileRef = 620379DC2AFE1432005AC0C1 /* SentryBuildAppStartSpans.m */; };
 		621D9F2F2B9B0320003D94DE /* SentryCurrentDateProvider.swift in Sources */ = {isa = PBXBuildFile; fileRef = 621D9F2E2B9B0320003D94DE /* SentryCurrentDateProvider.swift */; };
@@ -1026,6 +1028,8 @@
 		33042A0B29DAF5F400C60085 /* SentryExtraContextProvider.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = SentryExtraContextProvider.h; sourceTree = "<group>"; };
 		33042A0C29DAF79A00C60085 /* SentryExtraContextProvider.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = SentryExtraContextProvider.m; sourceTree = "<group>"; };
 		33042A1629DC2C4300C60085 /* SentryExtraContextProviderTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SentryExtraContextProviderTests.swift; sourceTree = "<group>"; };
+		51B15F7D2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = URLSessionTaskExtensions.swift; sourceTree = "<group>"; };
+		51B15F7F2BE88D510026A2F2 /* URLSessionTaskTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = URLSessionTaskTests.swift; sourceTree = "<group>"; };
 		620379DA2AFE1415005AC0C1 /* SentryBuildAppStartSpans.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; name = SentryBuildAppStartSpans.h; path = include/SentryBuildAppStartSpans.h; sourceTree = "<group>"; };
 		620379DC2AFE1432005AC0C1 /* SentryBuildAppStartSpans.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = SentryBuildAppStartSpans.m; sourceTree = "<group>"; };
 		621D9F2E2B9B0320003D94DE /* SentryCurrentDateProvider.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SentryCurrentDateProvider.swift; sourceTree = "<group>"; };
@@ -2085,6 +2089,7 @@
 			isa = PBXGroup;
 			children = (
 				62872B622BA1B86100A4FA7D /* NSLockTests.swift */,
+				51B15F7F2BE88D510026A2F2 /* URLSessionTaskTests.swift */,
 			);
 			path = Extensions;
 			sourceTree = "<group>";
@@ -3783,6 +3788,7 @@
 			children = (
 				D8F016B52B962548007B9AFB /* StringExtensions.swift */,
 				62872B5E2BA1B7F300A4FA7D /* NSLock.swift */,
+				51B15F7D2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift */,
 			);
 			path = Extensions;
 			sourceTree = "<group>";
@@ -4042,6 +4048,7 @@
 				63FE716F20DA4C1100CDBAE8 /* SentryCrashCPU_Apple.h in Headers */,
 				639FCFA81EBC80CC00778193 /* SentryFrame.h in Headers */,
 				D8BFE37229A3782F002E73F3 /* SentryTimeToDisplayTracker.h in Headers */,
+				8E8C57A625EEFC43001CEEFA /* SentrySampling.h in Headers */,
 				8E8C57A625EEFC43001CEEFA /* SentrySampling.h in Headers */,
 				7B634599280EB9D100CFA05A /* SentryUIEventTrackingIntegration.h in Headers */,
 				63FE716D20DA4C1100CDBAE8 /* SentryCrashSysCtl.h in Headers */,
@@ -4387,6 +4394,7 @@
 				7B3B473825D6CC7E00D01640 /* SentryNSError.m in Sources */,
 				D8ACE3C82762187200F5A213 /* SentryNSDataTracker.m in Sources */,
 				7BE3C77D2446112C00A38442 /* SentryRateLimitParser.m in Sources */,
+				51B15F7E2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift in Sources */,
 				D8B088B729C9E3FF00213258 /* SentryTracerConfiguration.m in Sources */,
 				8ECC674A25C23A20000E2BF6 /* SentryTransactionContext.mm in Sources */,
 				03BCC38C27E1C01A003232C7 /* SentryTime.mm in Sources */,
@@ -4722,6 +4730,7 @@
 				63FE721420DA66EC00CDBAE8 /* SentryCrashMemory_Tests.m in Sources */,
 				62885DA729E946B100554F38 /* TestConncurrentModifications.swift in Sources */,
 				63FE720520DA66EC00CDBAE8 /* FileBasedTestCase.m in Sources */,
+				51B15F802BE88D510026A2F2 /* URLSessionTaskTests.swift in Sources */,
 				63EED6C32237989300E02400 /* SentryOptionsTest.m in Sources */,
 				7BBD18B22451804C00427C76 /* SentryRetryAfterHeaderParserTests.swift in Sources */,
 				7BD337E424A356180050DB6E /* SentryCrashIntegrationTests.swift in Sources */,

Sources/Sentry/SentryNetworkTracker.m
@@ -43,6 +43,7 @@ SentryNetworkTracker ()
 @property (nonatomic, assign) BOOL isNetworkTrackingEnabled;
 @property (nonatomic, assign) BOOL isNetworkBreadcrumbEnabled;
 @property (nonatomic, assign) BOOL isCaptureFailedRequestsEnabled;
+@property (nonatomic, assign) BOOL isGraphQLOperationTrackingEnabled;
 
 @end
 
@@ -62,6 +63,7 @@ SentryNetworkTracker ()
         _isNetworkTrackingEnabled = NO;
         _isNetworkBreadcrumbEnabled = NO;
         _isCaptureFailedRequestsEnabled = NO;
+        _isGraphQLOperationTrackingEnabled = NO;
     }
     return self;
 }
@@ -87,12 +89,20 @@ SentryNetworkTracker ()
     }
 }
 
+- (void)enableGraphQLOperationTracking
+{
+    @synchronized(self) {
+        _isGraphQLOperationTrackingEnabled = YES;
+    }
+}
+
 - (void)disable
 {
     @synchronized(self) {
         _isNetworkBreadcrumbEnabled = NO;
         _isNetworkTrackingEnabled = NO;
         _isCaptureFailedRequestsEnabled = NO;
+        _isGraphQLOperationTrackingEnabled = NO;
     }
 }
 
@@ -440,6 +450,11 @@ SentryNetworkTracker ()
     }
 
     context[@"response"] = response;
+
+    if (self.isGraphQLOperationTrackingEnabled) {
+        context[@"graphql_operation_name"] = [sessionTask getGraphQLOperationName];
+    }
+
     event.context = context;
 
     [SentrySDK captureEvent:event];
@@ -489,6 +504,10 @@ SentryNetworkTracker ()
         breadcrumbData[@"status_code"] = statusCode;
         breadcrumbData[@"reason"] =
             [NSHTTPURLResponse localizedStringForStatusCode:responseStatusCode];
+
+        if (self.isGraphQLOperationTrackingEnabled) {
+            breadcrumbData[@"graphql_operation_name"] = [sessionTask getGraphQLOperationName];
+        }
     }
 
     if (urlComponents.query != nil) {

Sources/Swift/Extensions/URLSessionTaskExtensions.swift
@@ -1,0 +1,19 @@
+import Foundation
+
+public extension URLSessionTask {
+
+    @objc
+    func getGraphQLOperationName() -> String? {
+        guard originalRequest?.value(forHTTPHeaderField: "Content-Type") == "application/json" else { return nil }
+        guard let requestBody = originalRequest?.httpBody else { return nil }
+
+        let requestInfo = try? JSONDecoder().decode(GraphQLRequest.self, from: requestBody)
+
+        return requestInfo?.operationName
+    }
+
+}
+
+private struct GraphQLRequest: Decodable {
+    let operationName: String
+}

Sources/Sentry/Public/SentryOptions.h
@@ -143,6 +143,12 @@ NS_SWIFT_NAME(Options)
 @property (nonatomic, assign) BOOL enableAutoSessionTracking;
 
 /**
+ * Whether to attach the top level `operationName` node of HTTP json requests to HTTP breadcrumbs
+ * @note Default is @c NO.
+ */
+@property (nonatomic, assign) BOOL enableGraphQLOperationTracking;
+
+/**
  * Whether to enable Watchdog Termination tracking or not.
  * @note This feature requires the @c SentryCrashIntegration being enabled, otherwise it would
  * falsely report every crash as watchdog termination.

Sources/Sentry/SentryNetworkTrackingIntegration.m
@@ -29,6 +29,10 @@
         [SentryNetworkTracker.sharedInstance enableCaptureFailedRequests];
     }
 
+    if (options.enableGraphQLOperationTracking) {
+        [SentryNetworkTracker.sharedInstance enableGraphQLOperationTracking];
+    }
+
     if (shouldEnableNetworkTracking || options.enableNetworkBreadcrumbs
         || options.enableCaptureFailedRequests) {
         [SentryNetworkTrackingIntegration swizzleURLSessionTask];

Sources/Sentry/SentryOptions.m
@@ -94,6 +94,7 @@ NSString *const kSentryDefaultEnvironment = @"production";
         _integrations = SentryOptions.defaultIntegrations;
         self.sampleRate = SENTRY_DEFAULT_SAMPLE_RATE;
         self.enableAutoSessionTracking = YES;
+        self.enableGraphQLOperationTracking = NO;
         self.enableWatchdogTerminationTracking = YES;
         self.sessionTrackingIntervalMillis = [@30000 unsignedIntValue];
         self.attachStacktrace = YES;
@@ -352,6 +353,9 @@ NSString *const kSentryDefaultEnvironment = @"production";
 
     [self setBool:options[@"enableAutoSessionTracking"]
             block:^(BOOL value) { self->_enableAutoSessionTracking = value; }];
+
+    [self setBool:options[@"enableGraphQLOperationTracking"]
+            block:^(BOOL value) { self->_enableGraphQLOperationTracking = value; }];
 
     [self setBool:options[@"enableWatchdogTerminationTracking"]
             block:^(BOOL value) { self->_enableWatchdogTerminationTracking = value; }];

Sources/Sentry/include/SentryNetworkTracker.h
@@ -18,12 +18,14 @@ static NSString *const SENTRY_NETWORK_REQUEST_TRACKER_BREADCRUMB
 - (void)enableNetworkTracking;
 - (void)enableNetworkBreadcrumbs;
 - (void)enableCaptureFailedRequests;
+- (void)enableGraphQLOperationTracking;
 - (BOOL)isTargetMatch:(NSURL *)URL withTargets:(NSArray *)targets;
 - (void)disable;
 
 @property (nonatomic, readonly) BOOL isNetworkTrackingEnabled;
 @property (nonatomic, readonly) BOOL isNetworkBreadcrumbEnabled;
 @property (nonatomic, readonly) BOOL isCaptureFailedRequestsEnabled;
+@property (nonatomic, readonly) BOOL isGraphQLOperationTrackingEnabled;
 
 @end
 

Tests/SentryTests/Integrations/Performance/Network/SentryNetworkTrackerIntegrationTests.swift
@@ -228,7 +228,20 @@ class SentryNetworkTrackerIntegrationTests: XCTestCase {
 
         XCTAssertFalse(SentryNetworkTracker.sharedInstance.isCaptureFailedRequestsEnabled)
     }
-    
+
+    func testGraphQLOperationTrackingEnabled() {
+        fixture.options.enableGraphQLOperationTracking = true
+        startSDK()
+
+        XCTAssertTrue(SentryNetworkTracker.sharedInstance.isGraphQLOperationTrackingEnabled)
+    }
+
+    func testGraphQLOperationTrackingDisabled() {
+        startSDK()
+
+        XCTAssertFalse(SentryNetworkTracker.sharedInstance.isGraphQLOperationTrackingEnabled)
+    }
+
     func testGetCaptureFailedRequestsEnabled() {
         let expect = expectation(description: "Request completed")
 

commit 1ac58a6fe712abf054c94ceac8e7b2a72b87d6e4

fix: Convert Swift extension to helper class for use by ObjC (#3973)
Resolved a crash introduced with #3931.

Co-authored-by: Max Chuquimia <>
Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>

Sentry.xcodeproj/project.pbxproj
@@ -72,8 +72,8 @@
 		15E0A8F22411A45A00F044E3 /* SentrySession.m in Sources */ = {isa = PBXBuildFile; fileRef = 15E0A8F12411A45A00F044E3 /* SentrySession.m */; };
 		33042A0D29DAF79A00C60085 /* SentryExtraContextProvider.m in Sources */ = {isa = PBXBuildFile; fileRef = 33042A0C29DAF79A00C60085 /* SentryExtraContextProvider.m */; };
 		33042A1729DC2C4300C60085 /* SentryExtraContextProviderTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 33042A1629DC2C4300C60085 /* SentryExtraContextProviderTests.swift */; };
-		51B15F7E2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift in Sources */ = {isa = PBXBuildFile; fileRef = 51B15F7D2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift */; };
-		51B15F802BE88D510026A2F2 /* URLSessionTaskTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 51B15F7F2BE88D510026A2F2 /* URLSessionTaskTests.swift */; };
+		51B15F7E2BE88A7C0026A2F2 /* URLSessionTaskHelper.swift in Sources */ = {isa = PBXBuildFile; fileRef = 51B15F7D2BE88A7C0026A2F2 /* URLSessionTaskHelper.swift */; };
+		51B15F802BE88D510026A2F2 /* URLSessionTaskHelperTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 51B15F7F2BE88D510026A2F2 /* URLSessionTaskHelperTests.swift */; };
 		620379DB2AFE1415005AC0C1 /* SentryBuildAppStartSpans.h in Headers */ = {isa = PBXBuildFile; fileRef = 620379DA2AFE1415005AC0C1 /* SentryBuildAppStartSpans.h */; };
 		620379DD2AFE1432005AC0C1 /* SentryBuildAppStartSpans.m in Sources */ = {isa = PBXBuildFile; fileRef = 620379DC2AFE1432005AC0C1 /* SentryBuildAppStartSpans.m */; };
 		621D9F2F2B9B0320003D94DE /* SentryCurrentDateProvider.swift in Sources */ = {isa = PBXBuildFile; fileRef = 621D9F2E2B9B0320003D94DE /* SentryCurrentDateProvider.swift */; };
@@ -1035,8 +1035,8 @@
 		33042A0B29DAF5F400C60085 /* SentryExtraContextProvider.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = SentryExtraContextProvider.h; sourceTree = "<group>"; };
 		33042A0C29DAF79A00C60085 /* SentryExtraContextProvider.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = SentryExtraContextProvider.m; sourceTree = "<group>"; };
 		33042A1629DC2C4300C60085 /* SentryExtraContextProviderTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SentryExtraContextProviderTests.swift; sourceTree = "<group>"; };
-		51B15F7D2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = URLSessionTaskExtensions.swift; sourceTree = "<group>"; };
-		51B15F7F2BE88D510026A2F2 /* URLSessionTaskTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = URLSessionTaskTests.swift; sourceTree = "<group>"; };
+		51B15F7D2BE88A7C0026A2F2 /* URLSessionTaskHelper.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = URLSessionTaskHelper.swift; sourceTree = "<group>"; };
+		51B15F7F2BE88D510026A2F2 /* URLSessionTaskHelperTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = URLSessionTaskHelperTests.swift; sourceTree = "<group>"; };
 		620379DA2AFE1415005AC0C1 /* SentryBuildAppStartSpans.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; name = SentryBuildAppStartSpans.h; path = include/SentryBuildAppStartSpans.h; sourceTree = "<group>"; };
 		620379DC2AFE1432005AC0C1 /* SentryBuildAppStartSpans.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = SentryBuildAppStartSpans.m; sourceTree = "<group>"; };
 		621D9F2E2B9B0320003D94DE /* SentryCurrentDateProvider.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SentryCurrentDateProvider.swift; sourceTree = "<group>"; };
@@ -2104,7 +2104,6 @@
 			isa = PBXGroup;
 			children = (
 				62872B622BA1B86100A4FA7D /* NSLockTests.swift */,
-				51B15F7F2BE88D510026A2F2 /* URLSessionTaskTests.swift */,
 			);
 			path = Extensions;
 			sourceTree = "<group>";
@@ -3628,6 +3627,7 @@
 				D8AFC0582BDA899A00118BE1 /* RedactRegionTests.swift */,
 				D8F67AEF2BE0D31A00C9197B /* UIImageHelperTests.swift */,
 				D8F67AF22BE10F7600C9197B /* UIRedactBuilderTests.swift */,
+				51B15F7F2BE88D510026A2F2 /* URLSessionTaskHelperTests.swift */,
 			);
 			name = Tools;
 			sourceTree = "<group>";
@@ -3659,6 +3659,7 @@
 				D8AFC0192BD7A20B00118BE1 /* SentryViewScreenshotProvider.swift */,
 				D8AFC0562BDA895400118BE1 /* UIRedactBuilder.swift */,
 				D8F67AED2BE0D19200C9197B /* UIImageHelper.swift */,
+				51B15F7D2BE88A7C0026A2F2 /* URLSessionTaskHelper.swift */,
 			);
 			path = Tools;
 			sourceTree = "<group>";
@@ -3817,7 +3818,6 @@
 			children = (
 				D8F016B52B962548007B9AFB /* StringExtensions.swift */,
 				62872B5E2BA1B7F300A4FA7D /* NSLock.swift */,
-				51B15F7D2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift */,
 			);
 			path = Extensions;
 			sourceTree = "<group>";
@@ -4424,7 +4424,7 @@
 				7B3B473825D6CC7E00D01640 /* SentryNSError.m in Sources */,
 				D8ACE3C82762187200F5A213 /* SentryNSDataTracker.m in Sources */,
 				7BE3C77D2446112C00A38442 /* SentryRateLimitParser.m in Sources */,
-				51B15F7E2BE88A7C0026A2F2 /* URLSessionTaskExtensions.swift in Sources */,
+				51B15F7E2BE88A7C0026A2F2 /* URLSessionTaskHelper.swift in Sources */,
 				D8B088B729C9E3FF00213258 /* SentryTracerConfiguration.m in Sources */,
 				8ECC674A25C23A20000E2BF6 /* SentryTransactionContext.mm in Sources */,
 				03BCC38C27E1C01A003232C7 /* SentryTime.mm in Sources */,
@@ -4763,7 +4763,7 @@
 				63FE721420DA66EC00CDBAE8 /* SentryCrashMemory_Tests.m in Sources */,
 				62885DA729E946B100554F38 /* TestConncurrentModifications.swift in Sources */,
 				63FE720520DA66EC00CDBAE8 /* FileBasedTestCase.m in Sources */,
-				51B15F802BE88D510026A2F2 /* URLSessionTaskTests.swift in Sources */,
+				51B15F802BE88D510026A2F2 /* URLSessionTaskHelperTests.swift in Sources */,
 				63EED6C32237989300E02400 /* SentryOptionsTest.m in Sources */,
 				7BBD18B22451804C00427C76 /* SentryRetryAfterHeaderParserTests.swift in Sources */,
 				7BD337E424A356180050DB6E /* SentryCrashIntegrationTests.swift in Sources */,

Sources/Sentry/SentryNetworkTracker.m
@@ -452,7 +452,8 @@ SentryNetworkTracker ()
     context[@"response"] = response;
 
     if (self.isGraphQLOperationTrackingEnabled) {
-        context[@"graphql_operation_name"] = [sessionTask getGraphQLOperationName];
+        context[@"graphql_operation_name"] =
+            [URLSessionTaskHelper getGraphQLOperationNameFrom:sessionTask];
     }
 
     event.context = context;
@@ -506,7 +507,8 @@ SentryNetworkTracker ()
             [NSHTTPURLResponse localizedStringForStatusCode:responseStatusCode];
 
         if (self.isGraphQLOperationTrackingEnabled) {
-            breadcrumbData[@"graphql_operation_name"] = [sessionTask getGraphQLOperationName];
+            breadcrumbData[@"graphql_operation_name"] =
+                [URLSessionTaskHelper getGraphQLOperationNameFrom:sessionTask];
         }
     }
 

Sources/Swift/Extensions/URLSessionTaskExtensions.swift
@@ -1,19 +1,0 @@
-import Foundation
-
-public extension URLSessionTask {
-
-    @objc
-    func getGraphQLOperationName() -> String? {
-        guard originalRequest?.value(forHTTPHeaderField: "Content-Type") == "application/json" else { return nil }
-        guard let requestBody = originalRequest?.httpBody else { return nil }
-
-        let requestInfo = try? JSONDecoder().decode(GraphQLRequest.self, from: requestBody)
-
-        return requestInfo?.operationName
-    }
-
-}
-
-private struct GraphQLRequest: Decodable {
-    let operationName: String
-}

Sources/Swift/Tools/URLSessionTaskHelper.swift
@@ -1,0 +1,20 @@
+import Foundation
+
+@objcMembers
+class URLSessionTaskHelper: NSObject {
+
+    static func getGraphQLOperationName(from task: URLSessionTask?) -> String? {
+        guard let task = task else { return nil }
+        guard task.originalRequest?.value(forHTTPHeaderField: "Content-Type") == "application/json" else { return nil }
+        guard let requestBody = task.originalRequest?.httpBody else { return nil }
+
+        let requestInfo = try? JSONDecoder().decode(GraphQLRequest.self, from: requestBody)
+
+        return requestInfo?.operationName
+    }
+
+}
+
+private struct GraphQLRequest: Decodable {
+    let operationName: String
+}

Tests/SentryTests/URLSessionTaskHelperTests.swift
@@ -3,7 +3,7 @@ import Nimble
 @testable import Sentry
 import XCTest
 
-final class URLSessionTaskTests: XCTestCase {
+final class URLSessionTaskHelperTests: XCTestCase {
 
     func testHTTPContentTypeInvalid() {
         let task = makeTask(
@@ -11,8 +11,8 @@ final class URLSessionTaskTests: XCTestCase {
             body: "8J+YiQo="
         )
 
-        let operationName = task.getGraphQLOperationName()
-        
+        let operationName = URLSessionTaskHelper.getGraphQLOperationName(from: task)
+
         expect(operationName) == nil
     }
 
@@ -22,7 +22,7 @@ final class URLSessionTaskTests: XCTestCase {
             body: "not json"
         )
 
-        let operationName = task.getGraphQLOperationName()
+        let operationName = URLSessionTaskHelper.getGraphQLOperationName(from: task)
 
         expect(operationName) == nil
     }
@@ -33,7 +33,7 @@ final class URLSessionTaskTests: XCTestCase {
             body: nil
         )
 
-        let operationName = task.getGraphQLOperationName()
+        let operationName = URLSessionTaskHelper.getGraphQLOperationName(from: task)
 
         expect(operationName) == nil
     }
@@ -52,14 +52,14 @@ final class URLSessionTaskTests: XCTestCase {
             """
         )
 
-        let operationName = task.getGraphQLOperationName()
+        let operationName = URLSessionTaskHelper.getGraphQLOperationName(from: task)
 
         expect(operationName) == "MyOperation"
     }
 
 }
 
-private extension URLSessionTaskTests {
+private extension URLSessionTaskHelperTests {
 
     func makeTask(headers: [String: String], body: String?) -> URLSessionTask {
         var request = URLRequest(url: URL(string: "https://anything.com")!)