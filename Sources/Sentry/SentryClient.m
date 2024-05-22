5365396a4a7da9465970252af07fb9efa4dc1f52
Bruno Guidolim <bguidolim@gmail.com>
2024-05-22 15:54:37 +0200

    Run format command

--- Sources/Sentry/SentryClient.m
+++ Sources/Sentry/SentryClient.m
@@ -634,16 +634,16 @@ NSString *const DropSessionLogMessage = @"Session has no release name. Won't sen
 #if SENTRY_HAS_UIKIT
         if (!isCrashEvent) {
             NSMutableDictionary *context =
-            [event.context mutableCopy] ?: [NSMutableDictionary dictionary];
+                [event.context mutableCopy] ?: [NSMutableDictionary dictionary];
             if (context[@"app"] == nil
                 || ([context[@"app"] isKindOfClass:NSDictionary.self]
                     && context[@"app"][@"in_foreground"] == nil)) {
-                NSMutableDictionary *app =
-                [(NSDictionary *)context[@"app"] mutableCopy] ?: [NSMutableDictionary dictionary];
+                NSMutableDictionary *app = [(NSDictionary *)context[@"app"] mutableCopy]
+                    ?: [NSMutableDictionary dictionary];
                 context[@"app"] = app;
-                
+
                 UIApplicationState appState =
-                [SentryDependencyContainer sharedInstance].application.applicationState;
+                    [SentryDependencyContainer sharedInstance].application.applicationState;
                 BOOL inForeground = appState == UIApplicationStateActive;
                 app[@"in_foreground"] = @(inForeground);
                 event.context = context;
