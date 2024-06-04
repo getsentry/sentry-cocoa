getsentry/sentry-cocoa

commit 96af87cea3bb92d5e696bf7996decb7aeee09dfd

impr: Speed up adding breadcrumbs (#4029)
Adding breadcrumbs was O(n) because the SDK used removeObjectAtIndex:0
when reaching the max breadcrumb amount, which requires reshifting the
whole array. Now, we use a ring buffer making adding breadcrumbs O(1).

Fixes GH-3341

Co-authored-by: Sentry Github Bot <bot+github-bot@sentry.io>

Sources/Sentry/SentryScope.m
@@ -45,7 +45,9 @@ SentryScope ()
  */
 @property (atomic) enum SentryLevel levelEnum;
 
-@property (atomic) NSInteger maxBreadcrumbs;
+@property (atomic) NSUInteger maxBreadcrumbs;
+@property (atomic) NSUInteger currentBreadcrumbIndex;
+@property (atomic, strong) NSMutableArray<SentryBreadcrumb *> *breadcrumbArray;
 
 @property (atomic, strong) NSMutableArray<SentryAttachment *> *attachmentArray;
 
@@ -62,8 +64,9 @@ SentryScope ()
 - (instancetype)initWithMaxBreadcrumbs:(NSInteger)maxBreadcrumbs
 {
     if (self = [super init]) {
-        self.maxBreadcrumbs = maxBreadcrumbs;
-        self.breadcrumbArray = [NSMutableArray new];
+        _maxBreadcrumbs = MAX(0, maxBreadcrumbs);
+        _currentBreadcrumbIndex = 0;
+        _breadcrumbArray = [[NSMutableArray alloc] initWithCapacity:_maxBreadcrumbs];
         self.tagDictionary = [NSMutableDictionary new];
         self.extraDictionary = [NSMutableDictionary new];
         self.contextDictionary = [NSMutableDictionary new];
@@ -87,7 +90,9 @@ SentryScope ()
         [_extraDictionary addEntriesFromDictionary:[scope extras]];
         [_tagDictionary addEntriesFromDictionary:[scope tags]];
         [_contextDictionary addEntriesFromDictionary:[scope context]];
-        [_breadcrumbArray addObjectsFromArray:[scope breadcrumbs]];
+        NSArray<SentryBreadcrumb *> *crumbs = [scope breadcrumbs];
+        _currentBreadcrumbIndex = crumbs.count;
+        [_breadcrumbArray addObjectsFromArray:crumbs];
         [_fingerprintArray addObjectsFromArray:[scope fingerprints]];
         [_attachmentArray addObjectsFromArray:[scope attachments]];
 
@@ -117,10 +122,14 @@ SentryScope ()
     }
     SENTRY_LOG_DEBUG(@"Add breadcrumb: %@", crumb);
     @synchronized(_breadcrumbArray) {
-        [_breadcrumbArray addObject:crumb];
-        if ([_breadcrumbArray count] > self.maxBreadcrumbs) {
-            [_breadcrumbArray removeObjectAtIndex:0];
-        }
+        // Use a ring buffer making adding breadcrumbs O(1).
+        // In a prior version, we added the new breadcrumb at the end of the array and used
+        // removeObjectAtIndex:0 when reaching the max breadcrumb amount. removeObjectAtIndex:0 is
+        // O(n) because it needs to reshift the whole array. So when the breadcrumbs array was full
+        // every add operation was O(n).
+
+        _breadcrumbArray[_currentBreadcrumbIndex] = crumb;
+        _currentBreadcrumbIndex = (_currentBreadcrumbIndex + 1) % _maxBreadcrumbs;
 
         for (id<SentryScopeObserver> observer in self.observers) {
             [observer addSerializedBreadcrumb:[crumb serialize]];
@@ -150,9 +159,7 @@ SentryScope ()
     // references instead of self we remove all objects instead of creating new instances. Removing
     // all objects is usually O(n). This is acceptable as we don't expect a huge amount of elements
     // in the arrays or dictionaries, that would slow down the performance.
-    @synchronized(_breadcrumbArray) {
-        [_breadcrumbArray removeAllObjects];
-    }
+    [self clearBreadcrumbs];
     @synchronized(_tagDictionary) {
         [_tagDictionary removeAllObjects];
     }
@@ -183,7 +190,8 @@ SentryScope ()
 - (void)clearBreadcrumbs
 {
     @synchronized(_breadcrumbArray) {
-        [_breadcrumbArray removeAllObjects];
+        _currentBreadcrumbIndex = 0;
+        _breadcrumbArray = [[NSMutableArray alloc] initWithCapacity:_maxBreadcrumbs];
 
         for (id<SentryScopeObserver> observer in self.observers) {
             [observer clearBreadcrumbs];
@@ -194,7 +202,18 @@ SentryScope ()
 - (NSArray<SentryBreadcrumb *> *)breadcrumbs
 {
     @synchronized(_breadcrumbArray) {
-        return _breadcrumbArray.copy;
+        NSMutableArray<SentryBreadcrumb *> *crumbs = [NSMutableArray new];
+        for (int i = 0; i < _maxBreadcrumbs; i++) {
+            // Crumbs use a ring buffer. We need to start at the current crumb to get the
+            // crumbs in the correct order.
+            NSInteger index = (_currentBreadcrumbIndex + i) % _maxBreadcrumbs;
+
+            if (index < _breadcrumbArray.count) {
+                [crumbs addObject:_breadcrumbArray[index]];
+            }
+        }
+
+        return crumbs;
     }
 }
 

Tests/SentryTests/SentryScopeSwiftTests.swift
@@ -579,6 +579,59 @@ class SentryScopeSwiftTests: XCTestCase {
         XCTAssertEqual(2, observer.clearInvocations)
     }
     
+    func testDefaultBreadcrumbCapacity() {
+        let scope = Scope()
+        for i in 0..<197 {
+            let crumb = Breadcrumb()
+            crumb.message = "\(i)"
+            scope.addBreadcrumb(crumb)
+        }
+
+        let scopeSerialized = scope.serialize()
+        let scopeCrumbs = scopeSerialized["breadcrumbs"] as? [[String: Any]]
+        XCTAssertEqual(100, scopeCrumbs?.count ?? 0)
+        
+        var j = 0
+        for i in 97..<197 {
+            let actualMessage = scopeCrumbs?[j]["message"] as? String
+            XCTAssertEqual("\(i)", actualMessage)
+            
+            j += 1
+        }
+    }
+    
+    func testBreadcrumbsNotFull() {
+        let scope = Scope()
+        for i in 0..<97 {
+            let crumb = Breadcrumb()
+            crumb.message = "\(i)"
+            scope.addBreadcrumb(crumb)
+        }
+
+        let scopeSerialized = scope.serialize()
+        let scopeCrumbs = scopeSerialized["breadcrumbs"] as? [[String: Any]]
+        XCTAssertEqual(97, scopeCrumbs?.count ?? 0)
+        
+        for i in 0..<97 {
+            let actualMessage = scopeCrumbs?[i]["message"] as? String
+            XCTAssertEqual("\(i)", actualMessage)
+        }
+    }
+    
+    func testClearBreadcrumb() {
+        let scope = Scope()
+        scope.clearBreadcrumbs()
+        for _ in 0..<101 {
+            scope.addBreadcrumb(fixture.breadcrumb)
+        }
+        scope.clearBreadcrumbs()
+        
+        let scopeSerialized = scope.serialize()
+        
+        let scopeCrumbs = scopeSerialized["breadcrumbs"] as? [[String: Any]]
+        XCTAssertEqual(0, scopeCrumbs?.count ?? 0)
+    }
+    
     class TestScopeObserver: NSObject, SentryScopeObserver {
         var tags: [String: String]?
         func setTags(_ tags: [String: String]?) {

Sources/Sentry/SentrySessionReplay.m
@@ -292,7 +292,7 @@ SentrySessionReplay ()
     __block NSArray<SentryRRWebEvent *> *events;
 
     [SentrySDK.currentHub configureScope:^(SentryScope *_Nonnull scope) {
-        events = [self->_breadcrumbConverter convertWithBreadcrumbs:scope.breadcrumbArray
+        events = [self->_breadcrumbConverter convertWithBreadcrumbs:scope.breadcrumbs
                                                                from:videoInfo.start
                                                               until:videoInfo.end];
     }];

Sources/Sentry/include/SentryScope+Private.h
@@ -23,10 +23,7 @@ SentryScope ()
 
 @property (atomic, strong) SentryPropagationContext *propagationContext;
 
-/**
- * Contains the breadcrumbs which will be sent with the event
- */
-@property (atomic, strong) NSMutableArray<SentryBreadcrumb *> *breadcrumbArray;
+- (NSArray<SentryBreadcrumb *> *)breadcrumbs;
 
 /**
  * used to add values in event context.

Tests/SentryTests/SentryScopeTests.m
@@ -57,18 +57,6 @@
     XCTAssertEqual(expectedMaxBreadcrumb, [scope2Crumbs count]);
 }
 
-- (void)testDefaultMaxCapacity
-{
-    SentryScope *scope = [[SentryScope alloc] init];
-    for (int i = 0; i < 2000; ++i) {
-        [scope addBreadcrumb:[[SentryBreadcrumb alloc] init]];
-    }
-
-    NSDictionary<NSString *, id> *scopeSerialized = [scope serialize];
-    NSArray *scopeCrumbs = [scopeSerialized objectForKey:@"breadcrumbs"];
-    XCTAssertEqual(100, [scopeCrumbs count]);
-}
-
 - (void)testSetTagValueForKey
 {
     NSDictionary<NSString *, NSString *> *excpected = @{ @"A" : @"1", @"B" : @"2", @"C" : @"" };
@@ -161,15 +149,6 @@
     NSString *expectedReplayId = @"Some_replay_id";
     [scope setReplayId:expectedReplayId];
     XCTAssertEqualObjects([[scope serialize] objectForKey:@"replay_id"], expectedReplayId);
-}
-
-- (void)testClearBreadcrumb
-{
-    SentryScope *scope = [[SentryScope alloc] init];
-    [scope clearBreadcrumbs];
-    [scope addBreadcrumb:[self getBreadcrumb]];
-    [scope clearBreadcrumbs];
-    XCTAssertTrue([[[scope serialize] objectForKey:@"breadcrumbs"] count] == 0);
 }
 
 @end

commit 4075106cd2e58fbb25ba5ac48c7a7eca69b7da1d

fix: Scope addCrumb threading issues
The SentryScopeTest.testModifyingFromMultipleThreads was failing
frequently in CI, because of some threading issues introduced with
#4029. This is fixed now by not swapping the array reference and
other minor improvements.

Sources/Sentry/SentryScope.m
@@ -91,6 +91,7 @@ SentryScope ()
         [_tagDictionary addEntriesFromDictionary:[scope tags]];
         [_contextDictionary addEntriesFromDictionary:[scope context]];
         NSArray<SentryBreadcrumb *> *crumbs = [scope breadcrumbs];
+        _breadcrumbArray = [[NSMutableArray alloc] initWithCapacity:scope.maxBreadcrumbs];
         _currentBreadcrumbIndex = crumbs.count;
         [_breadcrumbArray addObjectsFromArray:crumbs];
         [_fingerprintArray addObjectsFromArray:[scope fingerprints]];
@@ -129,6 +130,7 @@ SentryScope ()
         // every add operation was O(n).
 
         _breadcrumbArray[_currentBreadcrumbIndex] = crumb;
+
         _currentBreadcrumbIndex = (_currentBreadcrumbIndex + 1) % _maxBreadcrumbs;
 
         for (id<SentryScopeObserver> observer in self.observers) {
@@ -191,7 +193,7 @@ SentryScope ()
 {
     @synchronized(_breadcrumbArray) {
         _currentBreadcrumbIndex = 0;
-        _breadcrumbArray = [[NSMutableArray alloc] initWithCapacity:_maxBreadcrumbs];
+        [_breadcrumbArray removeAllObjects];
 
         for (id<SentryScopeObserver> observer in self.observers) {
             [observer clearBreadcrumbs];
@@ -201,8 +203,8 @@ SentryScope ()
 
 - (NSArray<SentryBreadcrumb *> *)breadcrumbs
 {
+    NSMutableArray<SentryBreadcrumb *> *crumbs = [NSMutableArray new];
     @synchronized(_breadcrumbArray) {
-        NSMutableArray<SentryBreadcrumb *> *crumbs = [NSMutableArray new];
         for (int i = 0; i < _maxBreadcrumbs; i++) {
             // Crumbs use a ring buffer. We need to start at the current crumb to get the
             // crumbs in the correct order.
@@ -212,9 +214,9 @@ SentryScope ()
                 [crumbs addObject:_breadcrumbArray[index]];
             }
         }
-
-        return crumbs;
     }
+
+    return crumbs;
 }
 
 - (void)setContextValue:(NSDictionary<NSString *, id> *)value forKey:(NSString *)key

Tests/SentryTests/SentryScopeSwiftTests.swift
@@ -415,7 +415,55 @@ class SentryScopeSwiftTests: XCTestCase {
         // The number is kept small for the CI to not take too long.
         // If you really want to test this increase to 100_000 or so.
         testConcurrentModifications(asyncWorkItems: 2, writeLoopCount: 10, writeWork: { _ in
-            self.modifyScope(scope: scope)
+            
+            let key = "key"
+            
+            _ = Scope(scope: scope)
+            
+            for _ in 0...100 {
+                scope.addBreadcrumb(self.fixture.breadcrumb)
+            }
+            
+            scope.serialize()
+            scope.clearBreadcrumbs()
+            scope.addBreadcrumb(self.fixture.breadcrumb)
+            
+            scope.applyTo(session: SentrySession(releaseName: "1.0.0", distinctId: "some-id"))
+            
+            scope.setFingerprint(nil)
+            scope.setFingerprint(["finger", "print"])
+            
+            scope.setContext(value: ["some": "value"], key: key)
+            scope.removeContext(key: key)
+            
+            scope.setExtra(value: 1, key: key)
+            scope.removeExtra(key: key)
+            scope.setExtras(["value": "1", "value2": "2"])
+            
+            scope.applyTo(event: TestData.event, maxBreadcrumbs: 5)
+            
+            scope.setTag(value: "value", key: key)
+            scope.removeTag(key: key)
+            scope.setTags(["tag1": "hello", "tag2": "hello"])
+            
+            scope.addAttachment(TestData.fileAttachment)
+            scope.clearAttachments()
+            scope.addAttachment(TestData.fileAttachment)
+            
+            for _ in 0...10 {
+                scope.addBreadcrumb(self.fixture.breadcrumb)
+            }
+            scope.serialize()
+            
+            scope.setUser(self.fixture.user)
+            scope.setDist("dist")
+            scope.setEnvironment("env")
+            scope.setLevel(SentryLevel.debug)
+            
+            scope.applyTo(session: SentrySession(releaseName: "1.0.0", distinctId: "some-id"))
+            scope.applyTo(event: TestData.event, maxBreadcrumbs: 5)
+            
+            scope.serialize()
         })
     }
     
@@ -687,56 +735,5 @@ class SentryScopeSwiftTests: XCTestCase {
         func setUser(_ user: User?) {
             self.user = user
         }
-    }
-
-    private func modifyScope(scope: Scope) {
-        let key = "key"
-        
-        _ = Scope(scope: scope)
-        
-        for _ in 0...100 {
-            scope.addBreadcrumb(self.fixture.breadcrumb)
-        }
-        
-        scope.serialize()
-        scope.clearBreadcrumbs()
-        scope.addBreadcrumb(self.fixture.breadcrumb)
-        
-        scope.applyTo(session: SentrySession(releaseName: "1.0.0", distinctId: "some-id"))
-        
-        scope.setFingerprint(nil)
-        scope.setFingerprint(["finger", "print"])
-        
-        scope.setContext(value: ["some": "value"], key: key)
-        scope.removeContext(key: key)
-        
-        scope.setExtra(value: 1, key: key)
-        scope.removeExtra(key: key)
-        scope.setExtras(["value": "1", "value2": "2"])
-        
-        scope.applyTo(event: TestData.event, maxBreadcrumbs: 5)
-        
-        scope.setTag(value: "value", key: key)
-        scope.removeTag(key: key)
-        scope.setTags(["tag1": "hello", "tag2": "hello"])
-        
-        scope.addAttachment(TestData.fileAttachment)
-        scope.clearAttachments()
-        scope.addAttachment(TestData.fileAttachment)
-        
-        for _ in 0...10 {
-            scope.addBreadcrumb(self.fixture.breadcrumb)
-        }
-        scope.serialize()
-        
-        scope.setUser(self.fixture.user)
-        scope.setDist("dist")
-        scope.setEnvironment("env")
-        scope.setLevel(SentryLevel.debug)
-        
-        scope.applyTo(session: SentrySession(releaseName: "1.0.0", distinctId: "some-id"))
-        scope.applyTo(event: TestData.event, maxBreadcrumbs: 5)
-        
-        scope.serialize()
     }
 }