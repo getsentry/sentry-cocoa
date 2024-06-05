4593a8a9a371621a6075525a2529a44cd5c73eee
Andrew McKnight <andrew.mcknight@sentry.io>
2024-06-05 14:37:27 -0800

    this is already handled by the profilesSampleRate override env var

--- Samples/iOS-Swift/iOS-Swift/AppDelegate.swift
+++ Samples/iOS-Swift/iOS-Swift/AppDelegate.swift
@@ -79,7 +79,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             options.enableTimeToFullDisplayTracing = true
             options.enablePerformanceV2 = true
             options.enableMetrics = true
-            options.profilesSampleRate = ProcessInfo.processInfo.arguments.contains("--io.sentry.enable-continuous-profiling") ? nil : 1
             
             options.add(inAppInclude: "iOS_External")
 
