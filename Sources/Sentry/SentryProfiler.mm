8492fa0366217e6008c8581b6dccceda9b5596e8
Andrew McKnight <andrew.mcknight@sentry.io>
2024-06-20 12:07:23 -0800

    add back in the change from shared_ptr to unique_ptr

--- Sources/Sentry/SentryProfiler.mm
+++ Sources/Sentry/SentryProfiler.mm
@@ -56,7 +56,7 @@ sentry_manageTraceProfilerOnStartSDK(SentryOptions *options, SentryHub *hub)
 }
 
 @implementation SentryProfiler {
-    std::shared_ptr<SamplingProfiler> _samplingProfiler;
+    std::unique_ptr<SamplingProfiler> _samplingProfiler;
 }
 
 + (void)load
@@ -163,7 +163,7 @@ sentry_manageTraceProfilerOnStartSDK(SentryOptions *options, SentryHub *hub)
 
     SentryProfilerState *const state = [[SentryProfilerState alloc] init];
     self.state = state;
-    _samplingProfiler = std::make_shared<SamplingProfiler>(
+    _samplingProfiler = std::make_unique<SamplingProfiler>(
         [state](auto &backtrace) {
             Backtrace backtraceCopy = backtrace;
             backtraceCopy.absoluteTimestamp
