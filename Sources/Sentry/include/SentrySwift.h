d15d002e04b556c56d578c5c5d152d40e2f5cb0a
Andrew McKnight <andrew.mcknight@sentry.io>
2024-07-08 13:45:59 -0800

    also use the check for available header instead of user defined setting in Swift.h umbrella header

--- Sources/Sentry/include/SentrySwift.h
+++ Sources/Sentry/include/SentrySwift.h
@@ -7,18 +7,18 @@
 #    endif
 #endif
 
-#ifdef SENTRY_NO_UIKIT
+#if __has_include(<SentryWithoutUIKit/Sentry.h>)
 #    if __has_include("SentryWithoutUIKit-Swift.h")
 #        import "SentryWithoutUIKit-Swift.h"
 #    else
 #        import <SentryWithoutUIKit/SentryWithoutUIKit-Swift.h>
 #    endif
-#else
+#else // !__has_include(<SentryWithoutUIKit/Sentry.h>)
 #    if __has_include("Sentry-Swift.h")
 #        import "Sentry-Swift.h"
 #    else
 #        import <Sentry/Sentry-Swift.h>
 #    endif
-#endif
+#endif // __has_include(<SentryWithoutUIKit/Sentry.h>)
 
 #endif
