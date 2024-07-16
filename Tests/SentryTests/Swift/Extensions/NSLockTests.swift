cbfb9bca9126afb4d973d109b7374f5dd37188a0
Dhiogo Brustolin <dhiogorb@gmail.com>
2024-07-16 15:47:20 +0200

    Update NSLockTests.swift

--- Tests/SentryTests/Swift/Extensions/NSLockTests.swift
+++ Tests/SentryTests/Swift/Extensions/NSLockTests.swift
@@ -45,6 +45,40 @@ final class NSLockTests: XCTestCase {
         
         wait(for: [expectation], timeout: 0.1)
     }
+    
+    func testCheckFlagIsFalse() {
+        var flag = false
+        var executed = false
+        let lock = NSLock()
+        _ = lock.checkFlag(flag: &flag) {
+            executed = true
+        }
+        XCTAssertTrue(executed)
+        XCTAssertTrue(flag)
+    }
+    
+    func testCheckFlagIsTrue() {
+        var flag = true
+        var skipped = true
+        let lock = NSLock()
+        _ = lock.checkFlag(flag: &flag) {
+            skipped = false
+        }
+        XCTAssertTrue(skipped)
+        XCTAssertTrue(flag)
+    }
+    
+    func testStressFlag() {
+        let lock = NSLock()
+        var flag = false
+        var value = 0
+        
+        testConcurrentModifications(asyncWorkItems: 100, writeLoopCount: 9, writeWork: { _ in
+            _ = lock.checkFlag(flag: &flag) { value += 1 }
+        })
+        
+        XCTAssertEqual(value, 1)
+    }
 
     enum NSLockError: Error {
         case runtimeError(String)
