488ce8272f37efb96c702422e27fcd1a52ebbe4b
Dhiogo Brustolin <dhiogorb@gmail.com>
2024-07-18 15:57:41 +0200

    lint

--- SentryTestUtils/TestHub.swift
+++ SentryTestUtils/TestHub.swift
@@ -49,9 +49,8 @@ public class TestHub: SentryHub {
         capturedTransactionsWithScope.record((transaction.serialize(), scope))
         super.capture(transaction, with: scope)
     }
-    
-    
-    public var onReplayCapture: (()->Void)?
+
+    public var onReplayCapture: (() -> Void)?
     public var capturedReplayRecordingVideo = Invocations<(replay: SentryReplayEvent, recording: SentryReplayRecording, video: URL)>()
     public override func capture(_ replayEvent: SentryReplayEvent, replayRecording: SentryReplayRecording, video videoURL: URL) {
         capturedReplayRecordingVideo.record((replayEvent, replayRecording, videoURL))
