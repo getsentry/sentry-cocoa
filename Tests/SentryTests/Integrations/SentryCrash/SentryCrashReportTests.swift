de749526db62a315afd1ffe4cc066a65ccdf2519
Philipp Hofmann <philipp.hofmann@sentry.io>
2024-08-05 09:36:43 +0200

    fix: Missing mach info for crash reports
    The SentryInternalCDefines didn't define the SENTRY_HOST_APPLE, which
    led to missing mach info for crash reports, as the SentryCrashReport
    relies on the SENTRY_HOST_APPLE to write that info. For reference, the
    SentryCrashCRASH_HOST_APPLE was changed to SENTRY_HOST_APPLE in GH-4101.
    The problem is fixed now by defining SENTRY_HOST_APPLE in
    SentryInternalCDefines.
    
    Fixes GH-4227

--- Tests/SentryTests/Integrations/SentryCrash/SentryCrashReportTests.swift
+++ Tests/SentryTests/Integrations/SentryCrash/SentryCrashReportTests.swift
@@ -117,13 +117,48 @@ class SentryCrashReportTests: XCTestCase {
         XCTAssertFalse(crashReportContentsAsString.contains("boot_time"), "The crash report must not contain boot_time because Apple forbids sending this information off device see: https://developer.apple.com/documentation/bundleresources/privacy_manifest_files/describing_use_of_required_reason_api#4278394.")
     }
     
-    private func writeCrashReport() {
+    func testCrashReportContainsMachInfo() throws {
+        
         var monitorContext = SentryCrash_MonitorContext()
+        monitorContext.mach.type = EXC_BAD_ACCESS
+        monitorContext.mach.code = 1
+        monitorContext.mach.subcode = 12
+        
+        writeCrashReport(monitorContext: monitorContext)
+        
+        let crashReportContents = try XCTUnwrap( FileManager.default.contents(atPath: fixture.reportPath))
+        
+        let crashReport: CrashReport = try JSONDecoder().decode(CrashReport.self, from: crashReportContents)
+        
+        let mach = try XCTUnwrap(crashReport.crash.error.mach)
+        XCTAssertEqual(1, mach.exception)
+        XCTAssertEqual("EXC_BAD_ACCESS", mach.exception_name)
+        XCTAssertEqual(1, mach.code)
+        XCTAssertEqual("KERN_INVALID_ADDRESS", mach.code_name)
+        XCTAssertEqual(12, mach.subcode)
+    }
+    
+    func testCrashReportContainsStandardMachInfo_WhenMachInfoIsEmpty() throws {
+        writeCrashReport()
+        
+        let crashReportContents = try XCTUnwrap( FileManager.default.contents(atPath: fixture.reportPath))
+        
+        let crashReport: CrashReport = try JSONDecoder().decode(CrashReport.self, from: crashReportContents)
+        
+        let mach = try XCTUnwrap(crashReport.crash.error.mach)
+        XCTAssertEqual(0, mach.exception)
+        XCTAssertNil(mach.exception_name)
+        XCTAssertEqual(0, mach.code)
+        XCTAssertNil(mach.code_name)
+        XCTAssertEqual(0, mach.subcode)
+    }
+    
+    private func writeCrashReport(monitorContext: SentryCrash_MonitorContext? = nil) {
+        var localMonitorContext = monitorContext ?? SentryCrash_MonitorContext()
         
         let api = sentrycrashcm_system_getAPI()
-        api?.pointee.addContextualInfoToEvent(&monitorContext)
-        
-        sentrycrashreport_writeStandardReport(&monitorContext, fixture.reportPath)
+        api?.pointee.addContextualInfoToEvent(&localMonitorContext)
+        sentrycrashreport_writeStandardReport(&localMonitorContext, fixture.reportPath)
     }
     
     /**
@@ -167,12 +202,21 @@ class SentryCrashReportTests: XCTestCase {
         let type: String?
         let reason: String?
         let nsexception: NSException?
+        let mach: Mach?
     }
     
     struct NSException: Decodable, Equatable {
         let name: String?
         let userInfo: String?
         let reason: String?
+    }
+    
+    struct Mach: Decodable, Equatable {
+        let exception: Int?
+        let exception_name: String?
+        let code: Int?
+        let code_name: String?
+        let subcode: Int?
     }
 
     struct CrashReportUserInfo: Decodable, Equatable {
