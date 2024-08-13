bf373269046b91c2b00744c1b0d939f6a8b0bb65
Philipp Hofmann <philipp.hofmann@sentry.io>
2024-08-13 15:44:30 +0200

    cleanup

--- Sources/Swift/Integrations/FramesTracking/SentryFramesDelayData.swift
+++ Sources/Swift/Integrations/FramesTracking/SentryFramesDelayData.swift
@@ -4,7 +4,6 @@ import Foundation
 class SentryFramesDelayResult: NSObject {
     /// The frames delay for the passed time period. If frame delay can't be calculated this is -1.
     let delayDuration: CFTimeInterval
-    /// The system time stamp of the last rendered frame
     let framesCount: UInt
     
     init(delayDuration: CFTimeInterval, framesCount: UInt) {
