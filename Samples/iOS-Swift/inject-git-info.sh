9d50aaf942bf5007bca57bccfb9d78949b69c73f
Andrew McKnight <andrew.mcknight@sentry.io>
2024-09-09 13:57:19 -0800

    set default value to overwrite if needed

--- Samples/iOS-Swift/inject-git-info.sh
+++ Samples/iOS-Swift/inject-git-info.sh
@@ -8,5 +8,6 @@ COMMIT_HASH=$(git rev-parse --short HEAD)
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 /usr/libexec/PlistBuddy -c "Set :GIT_BRANCH $BRANCH_NAME" "${INFOPLIST_FILE}"
 
+GIT_STATUS_CLEAN=1
 GIT_STATUS_CLEAN=$(test -n "$(git status --porcelain)")
 /usr/libexec/PlistBuddy -c "Set :GIT_STATUS_CLEAN $GIT_STATUS_CLEAN" "${INFOPLIST_FILE}"
