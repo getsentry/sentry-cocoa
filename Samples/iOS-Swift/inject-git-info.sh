8bbbdb8f675000b0c2d08d6c720637fa3d2af37c
Andrew McKnight <andrew.mcknight@sentry.io>
2024-09-09 14:04:20 -0800

    try another test for clean git status

--- Samples/iOS-Swift/inject-git-info.sh
+++ Samples/iOS-Swift/inject-git-info.sh
@@ -6,8 +6,7 @@ set -x
 # will be empty if `test` below returns a result of no modifications
 # to the index, which will be interpreted as a falsy value in the
 # swift code
-GIT_STATUS_CLEAN=1
-GIT_STATUS_CLEAN=$(test -n "$(git status --porcelain)")
+GIT_STATUS_CLEAN=$(test "$(git status --porcelain | wc -l)" -eq 0)
 /usr/libexec/PlistBuddy -c "Set :GIT_STATUS_CLEAN $GIT_STATUS_CLEAN" "${INFOPLIST_FILE}"
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
