070809754b97fd52928f38b2cae9712a7ebc2f5d
Andrew McKnight <andrew.mcknight@sentry.io>
2024-09-09 14:09:57 -0800

    fix clean status check

--- Samples/iOS-Swift/inject-git-info.sh
+++ Samples/iOS-Swift/inject-git-info.sh
@@ -2,11 +2,10 @@
 
 set -x
 
-# if we don't supply the default happy path value, this variable
-# will be empty if `test` below returns a result of no modifications
-# to the index, which will be interpreted as a falsy value in the
-# swift code
-GIT_STATUS_CLEAN=$(test "$(git status --porcelain | wc -l)" -eq 0)
+GIT_STATUS_CLEAN=1
+if test -n "$(git status --porcelain)"; then
+    GIT_STATUS_CLEAN=0
+fi
 /usr/libexec/PlistBuddy -c "Set :GIT_STATUS_CLEAN $GIT_STATUS_CLEAN" "${INFOPLIST_FILE}"
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
