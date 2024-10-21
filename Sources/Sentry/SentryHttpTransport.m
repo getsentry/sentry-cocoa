f88da377430f72dd88caa70bb80de485fd180a5f
Ramiz Wachtler <r.wachtler@outlook.com>
2024-10-21 19:22:17 +0200

    ref(transport): Log a warning when an envelope item is dropped

--- Sources/Sentry/SentryHttpTransport.m
+++ Sources/Sentry/SentryHttpTransport.m
@@ -232,6 +232,7 @@
 - (void)envelopeItemDropped:(SentryEnvelopeItem *)envelopeItem
                withCategory:(SentryDataCategory)dataCategory;
 {
+    SENTRY_LOG_WARN(@"Envelope item dropped due to exceeding rate limit. Category: %@", nameForSentryDataCategory(dataCategory));
     [self recordLostEvent:dataCategory reason:kSentryDiscardReasonRateLimitBackoff];
     [self recordLostSpans:envelopeItem reason:kSentryDiscardReasonRateLimitBackoff];
 }
