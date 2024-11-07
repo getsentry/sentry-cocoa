6ecd34d6c0cc90e82e2784c4f9de5cd91b335b8e
Dhiogo Brustolin <dhiogorb@gmail.com>
2024-11-07 15:35:27 +0100

    fix

--- Samples/iOS-Swift/iOS-Swift6/SceneDelegate.swift
+++ Samples/iOS-Swift/iOS-Swift6/SceneDelegate.swift
@@ -1,11 +1,10 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-       guard let _ = (scene as? UIWindowScene) else { return }
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
