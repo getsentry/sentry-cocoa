f66ad3b82e8a82911ed27496ee0a8d26c8535fce
Andrew McKnight <andrew.mcknight@sentry.io>
2024-11-14 15:47:01 -0900

    put back integration objc

--- Sources/Sentry/SentryUserFeedbackIntegration.m
+++ Sources/Sentry/SentryUserFeedbackIntegration.m
@@ -1,15 +1,8 @@
 #import "SentryUserFeedbackIntegration.h"
 #import "SentryOptions+Private.h"
 #import "SentrySwift.h"
-#import "SentryEnvelope.h"
-#import "SentrySDK+Private.h"
-#import "SentryUserFeedback.h"
 
 #if TARGET_OS_IOS && SENTRY_HAS_UIKIT
-
-@interface SentryUserFeedbackIntegration()
-
-@end
 
 @implementation SentryUserFeedbackIntegration {
     SentryUserFeedbackIntegrationDriver *_driver;
@@ -22,7 +15,7 @@
     }
 
     _driver = [[SentryUserFeedbackIntegrationDriver alloc]
-               initWithConfiguration:options.userFeedbackConfiguration];
+        initWithConfiguration:options.userFeedbackConfiguration];
     return YES;
 }
 
