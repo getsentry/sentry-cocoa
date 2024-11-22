15c4a4eaf680c8a2a54494eb5b436a340771c0d8
Dhiogo Brustolin <dhiogo.brustolin@sentry.io>
2024-11-22 13:49:53 +0100

    Apply suggestions from code review

--- Tests/DuplicatedSDKTest/DuplicatedSDKTest/DuplicatedSDKTestApp.swift
+++ Tests/DuplicatedSDKTest/DuplicatedSDKTest/DuplicatedSDKTestApp.swift
@@ -11,7 +11,7 @@ struct DuplicatedSDKTestApp: App {
     
     init () {
         SentrySDK.start { options in
-            options.dsn = "https://a@o.sentry.io/1"
+            options.dsn = "https://6cc9bae94def43cab8444a99e0031c28@o447951.ingest.sentry.io/5428557"
         }
     }
     
--- Tests/DuplicatedSDKTest/DuplicatedSDKTest/UITest.swift
+++ Tests/DuplicatedSDKTest/DuplicatedSDKTest/UITest.swift
@@ -3,9 +3,9 @@ import XCTest
 final class UITestDuplicatedSDK: XCTestCase {
 
     @MainActor
-    func testExample() throws {
+    func testLoadIntegrations() throws {
         let app = XCUIApplication()
         app.launch()
-        XCTAssertEqual(app.staticTexts["TEST_RESULT"].label, "true")
+        XCTAssertEqual(app.staticTexts["TEST_RESULT"].label, "true", "SentrySDK integrations are not being loaded from the same binary. This will lead to undefined behavior.")
     }
 }
