57768ad9e0a8654f2c68f11953299ce8d9a0884d
Dhiogo Brustolin <dhiogorb@gmail.com>
2024-12-06 09:27:01 +0100

    Update SentryRedactModifierTests.swift

--- Samples/iOS-SwiftUI/iOS-SwiftUI-UITests/SentryRedactModifierTests.swift
+++ Samples/iOS-SwiftUI/iOS-SwiftUI-UITests/SentryRedactModifierTests.swift
@@ -5,19 +5,17 @@ import SwiftUI
 import XCTest
 
 class SentryRedactModifierTests: XCTestCase {
-
     func testViewMask() throws {
         let redactedText = Text("Hello, World!").sentryReplayMask()
         let description = String(describing: redactedText)
-        XCTAssertTrue(description.starts(with: "AnyView(ModifiedContent<Text, SentryReplayModifier>"))
+        XCTAssertTrue(description.starts(with: "AnyView(ModifiedContent<Text, SentryReplayModifier>"), "Redacted text is \(description)")
     }
     
     func testViewUnmask() throws {
-        let redactedText = Text("Hello, World!").sentryReplayUnmask()
-        let description = String(describing: redactedText)
-        XCTAssertTrue(description.starts(with: "AnyView(ModifiedContent<Text, SentryReplayModifier>"))
+        let unmaskedText = Text("Hello, World!").sentryReplayUnmask()
+        let description = String(describing: unmaskedText)
+        XCTAssertTrue(description.starts(with: "AnyView(ModifiedContent<Text, SentryReplayModifier>"), "Unmasked text is \(description)")
     }
-
 }
 
 #endif
