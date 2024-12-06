250a5155341bda414bc75545204d83632fe857bf
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-12-06 10:17:06 +0100

    add link to docs

--- Sources/Sentry/include/SentrySwift.h
+++ Sources/Sentry/include/SentrySwift.h
@@ -35,5 +35,9 @@
 #        import <Sentry/Sentry-Swift.h>
 #    endif
 #endif // __has_include(<SentryWithoutUIKit/Sentry.h>)
+// This class is used to check if APPLICATION_EXTENSION_API_ONLY is set to YES for Sentry.
+// If this is not set, the build will fail.
+// For more information, see
+// https://docs.sentry.io/platforms/apple/troubleshooting/#unknown-receiver-somereceiver-use-of-undeclared-identifier-someidentifier
 static If_You_See_This_Set_APPLICATION_EXTENSION_API_ONLY_To_YES_For_Sentry *SentrySwiftHeaderCheck;
 #endif
--- Sources/Swift/Tools/SentrySwiftHeaderCheck.swift
+++ Sources/Swift/Tools/SentrySwiftHeaderCheck.swift
@@ -1,5 +1,10 @@
 import Foundation
 
-@objc
-class If_You_See_This_Set_APPLICATION_EXTENSION_API_ONLY_To_YES_For_Sentry: NSObject {}
+/**
+ * This class is used to check if APPLICATION_EXTENSION_API_ONLY is set to YES for Sentry.
+ * If this is not set, the build will fail.
+ *
+ * For more information, see https://docs.sentry.io/platforms/apple/troubleshooting/#unknown-receiver-somereceiver-use-of-undeclared-identifier-someidentifier
+ */
+@objc class If_You_See_This_Set_APPLICATION_EXTENSION_API_ONLY_To_YES_For_Sentry: NSObject {}
 // swiftlint:disable:previous type_name
