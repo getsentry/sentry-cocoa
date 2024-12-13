a2764d80ee5718b243ff35450b68f211e799cb93
Andrew McKnight <andrew.mcknight@sentry.io>
2024-12-12 17:05:20 -0900

    default user.name in addition to user.username

--- Samples/iOS-Swift/iOS-Swift/AppDelegate.swift
+++ Samples/iOS-Swift/iOS-Swift/AppDelegate.swift
@@ -152,9 +152,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 user.email = env["--io.sentry.user.email"] ?? "tony@example.com"
                 // first check if the username has been overridden in the scheme for testing purposes; then try to use the system username so each person gets an automatic way to easily filter things on the dashboard; then fall back on a hardcoded value if none of these are present
                 let username = env["--io.sentry.user.username"] ?? (env["SIMULATOR_HOST_HOME"] as? NSString)?
-                    .lastPathComponent ?? "cocoa developer"
+                    .lastPathComponent ?? "cocoadev"
                 user.username = username
-                user.name = env["--io.sentry.user.name"]
+                user.name = env["--io.sentry.user.name"] ?? "cocoa developer"
                 scope.setUser(user)
 
                 if let path = Bundle.main.path(forResource: "Tongariro", ofType: "jpg") {
