64ab6604c1fe4b6b650c792e05215a9d828695b9
Andrew McKnight <andrew.mcknight@sentry.io>
2024-12-12 18:15:43 -0900

    add serialization test

---
+++ Tests/SentryTests/Integrations/Feedback/SentryFeedbackTests.swift
@@ -0,0 +1,20 @@
+import Foundation
+@testable import Sentry
+import XCTest
+
+class SentryFeedbackTests: XCTestCase {
+    func testSerialize() throws {
+        let sut = SentryFeedback(message: "Test feedback message", name: "Test feedback provider", email: "test-feedback-provider@sentry.io", screenshot: Data())
+        
+        let serialization = sut.serialize()
+        XCTAssertEqual(try XCTUnwrap(serialization["message"] as? String), "Test feedback message")
+        XCTAssertEqual(try XCTUnwrap(serialization["name"] as? String), "Test feedback provider")
+        XCTAssertEqual(try XCTUnwrap(serialization["contact_email"] as? String), "test-feedback-provider@sentry.io")
+        XCTAssertEqual(try XCTUnwrap(serialization["source"] as? String), "widget")
+        
+        let attachments = sut.attachments()
+        XCTAssertEqual(attachments.count, 1)
+        XCTAssertEqual(try XCTUnwrap(attachments.first).filename, "screenshot.png")
+        XCTAssertEqual(try XCTUnwrap(attachments.first).contentType, "application/png")
+    }
+}
