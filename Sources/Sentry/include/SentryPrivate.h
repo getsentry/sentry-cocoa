880be534a3e83dad98dfd6820de6f0fea3474688
Andrew McKnight <andrew.mcknight@sentry.io>
2024-12-13 11:35:13 -0900

    implement access to internal objc user api

--- Sources/Sentry/include/SentryPrivate.h
+++ Sources/Sentry/include/SentryPrivate.h
@@ -1,11 +1,11 @@
-// Sentry internal headers that are needed for swift code
+// Sentry internal headers that are needed for swift code; you cannot import headers that depend on
+// public interfaces here
 #import "NSLocale+Sentry.h"
 #import "SentryDispatchQueueWrapper.h"
 #import "SentryNSDataUtils.h"
 #import "SentryRandom.h"
-#import "SentrySDK+Private.h"
-#import "SentryScope+Private.h"
 #import "SentryTime.h"
+#import "SentryUserAccess.h"
 
 // Headers that also import SentryDefines should be at the end of this list
 // otherwise it wont compile
