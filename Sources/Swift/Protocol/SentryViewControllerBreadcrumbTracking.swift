efd328d07cd4b7bda678b25307331532f6b5e8d0
Michał Zaborowski <m1entus@gmail.com>
2024-12-18 13:33:23 +0100

    Update SentryViewControllerBreadcrumbTracking.swift
    Co-authored-by: Dhiogo Brustolin <dhiogorb@gmail.com>

--- Sources/Swift/Protocol/SentryViewControllerBreadcrumbTracking.swift
+++ Sources/Swift/Protocol/SentryViewControllerBreadcrumbTracking.swift
@@ -1,7 +1,13 @@
 import Foundation
 
+///
+/// Use this protocol to customize the name used in the automatic
+/// UIViewController performance tracker.
+///
 @objc
 public protocol SentryViewControllerBreadcrumbTracking: NSObjectProtocol {
 
+    /// The custom name of the UIViewController
+    /// that will be used for the transaction name.
     var screenName: String { get }
 }
