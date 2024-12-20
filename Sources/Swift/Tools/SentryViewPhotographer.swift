bd8b1bf348c49a23dd70ce840d77c5e189c99b08
Dhiogo Brustolin <dhiogo.brustolin@sentry.io>
2024-12-20 10:06:39 +0100

    fix: Session Redact wrong clipping order (#4651)
    Fix an error where SR clipping happens in the wrong order.

--- Sources/Swift/Tools/SentryViewPhotographer.swift
+++ Sources/Swift/Tools/SentryViewPhotographer.swift
@@ -66,7 +66,6 @@ class SentryViewPhotographer: NSObject, SentryViewScreenshotProvider {
             let imageRect = CGRect(origin: .zero, size: size)
             context.cgContext.addRect(CGRect(origin: CGPoint.zero, size: size))
             context.cgContext.clip(using: .evenOdd)
-            UIColor.blue.setStroke()
             
             context.cgContext.interpolationQuality = .none
             image.draw(at: .zero)
@@ -79,10 +78,9 @@ class SentryViewPhotographer: NSObject, SentryViewScreenshotProvider {
                 
                 defer { latestRegion = region }
                 
-                guard latestRegion?.canReplace(as: region) != true && imageRect.intersects(path.boundingBoxOfPath) else { continue }
-                
                 switch region.type {
                 case .redact, .redactSwiftUI:
+                    guard latestRegion?.canReplace(as: region) != true && imageRect.intersects(path.boundingBoxOfPath) else { continue }
                     (region.color ?? UIImageHelper.averageColor(of: context.currentImage, at: rect.applying(region.transform))).setFill()
                     context.cgContext.addPath(path)
                     context.cgContext.fillPath()
