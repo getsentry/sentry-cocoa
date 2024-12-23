6ddc8314cf7b3dd61177da3e8114984cb396b03b
Andrew McKnight <andrew.mcknight@sentry.io>
2024-12-23 09:21:59 -0900

    remove public from function

--- Sources/Swift/Integrations/UserFeedback/SentryFeedback.swift
+++ Sources/Swift/Integrations/UserFeedback/SentryFeedback.swift
@@ -32,7 +32,7 @@ class SentryFeedback: NSObject, SentrySerializable {
         super.init()
     }
     
-    public func serialize() -> [String: Any] {
+    func serialize() -> [String: Any] {
         let numberOfOptionalItems = (name == nil ? 0 : 1) + (email == nil ? 0 : 1) + (associatedEventId == nil ? 0 : 1)
         var dict = [String: Any](minimumCapacity: 2 + numberOfOptionalItems)
         dict["message"] = message
