5cbab61937651191fc28f15a4e8d927de062fbf0
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-01-03 09:29:57 +0100

    add example failing test

---
+++ Tests/SentryTests/ExampleTest.swift
@@ -0,0 +1,21 @@
+//
+//  ExampleTest.swift
+//  Sentry
+//
+//  Created by Philip Niedertscheider on 03.01.25.
+//  Copyright © 2025 Sentry. All rights reserved.
+//
+
+@testable import Sentry
+import XCTest
+
+final class ExampleTest: XCTestCase {
+    
+    func testExample_shouldFail() {
+        XCTFail("This test should fail")
+    }
+    
+    func testExample_shouldPass() {
+        XCTAssertTrue(true)
+    }
+}
