c46d0c2fa9b100f0919254965c179a79573de445
Andrew McKnight <andrew.mcknight@sentry.io>
2025-01-03 15:49:07 -0900

    remove app support directory too

--- Samples/iOS-Swift/iOS-Swift/AppDelegate.swift
+++ Samples/iOS-Swift/iOS-Swift/AppDelegate.swift
@@ -54,9 +54,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private func removeAppData() {
         print("[iOS-Swift] [debug] removing app data")
         let cache = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first!
-        guard let files = FileManager.default.enumerator(atPath: cache) else { return }
-        for item in files {
-            try! FileManager.default.removeItem(atPath: (cache as NSString).appendingPathComponent((item as! String)))
+        let appSupport = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true).first!
+        [cache, appSupport].forEach {
+            guard let files = FileManager.default.enumerator(atPath: $0) else { return }
+            for item in files {
+                try! FileManager.default.removeItem(atPath: ($0 as NSString).appendingPathComponent((item as! String)))
+            }
         }
     }
 }
