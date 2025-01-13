f29de608b4839b9dff7d50c1781e367d53439a50
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-01-13 15:20:31 +0100

    remove unused imported headers

--- Sources/Sentry/include/SentryPrivate.h
+++ Sources/Sentry/include/SentryPrivate.h
@@ -9,17 +9,11 @@
 
 // Headers that also import SentryDefines should be at the end of this list
 // otherwise it wont compile
-#import "SentryByteCountFormatter.h"
-#import "SentryClient+Private.h"
 #import "SentryDateUtil.h"
 #import "SentryDisplayLinkWrapper.h"
 #import "SentryFileIOTracker.h"
-#import "SentryFileManager.h"
 #import "SentryLevelHelper.h"
 #import "SentryLogC.h"
 #import "SentryRandom.h"
-#import "SentrySDK+Private.h"
 #import "SentrySdkInfo.h"
 #import "SentrySession.h"
-#import "SentrySpanOperations.h"
-#import "SentryTraceOrigins.h"
