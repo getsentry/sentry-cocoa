4ff6f4e78135791f01879d7222352fd17e582f6a
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-01-13 14:36:04 +0100

    revert changes in SentrySpanOperations.h

--- Sources/Sentry/SentryFileOperations.m
+++
@@ -1,7 +0,0 @@
-#import "SentrySpanOperations.h"
-
-NSString *const SentrySpanOperationUILoad = @"ui.load";
-NSString *const SentrySpanOperationUILoadInitialDisplay = @"ui.load.initial_display";
-NSString *const SentrySpanOperationUILoadFullDisplay = @"ui.load.full_display";
-NSString *const SentrySpanOperationUIAction = @"ui.action";
-NSString *const SentrySpanOperationUIActionClick = @"ui.action.click";
--- Sources/Sentry/include/SentrySpanOperations.h
+++ Sources/Sentry/include/SentrySpanOperations.h
@@ -1,11 +1,10 @@
-#import "SentryDefines.h"
 #import <Foundation/Foundation.h>
 
 // Note: Consider adding new operations to the `SentrySpanOperation` enum in
 // `SentrySpanOperations.swift` instead of adding them here.
 
-SENTRY_EXTERN NSString *const SentrySpanOperationUILoad;
-SENTRY_EXTERN NSString *const SentrySpanOperationUILoadInitialDisplay;
-SENTRY_EXTERN NSString *const SentrySpanOperationUILoadFullDisplay;
-SENTRY_EXTERN NSString *const SentrySpanOperationUIAction;
-SENTRY_EXTERN NSString *const SentrySpanOperationUIActionClick;
+static NSString *const SentrySpanOperationUILoad = @"ui.load";
+static NSString *const SentrySpanOperationUILoadInitialDisplay = @"ui.load.initial_display";
+static NSString *const SentrySpanOperationUILoadFullDisplay = @"ui.load.full_display";
+static NSString *const SentrySpanOperationUIAction = @"ui.action";
+static NSString *const SentrySpanOperationUIActionClick = @"ui.action.click";
