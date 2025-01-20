650a64bca7b8228a64de48294b94150a04c2ab37
Dhiogo Brustolin <dhiogorb@gmail.com>
2025-01-20 09:19:14 +0100

    Update SentrySwiftUI.h

--- Sources/SentrySwiftUI/SentrySwiftUI.h
+++ Sources/SentrySwiftUI/SentrySwiftUI.h
@@ -6,8 +6,6 @@ FOUNDATION_EXPORT double SentryUIVersionNumber;
 //! Project version string for SentryUI.
 FOUNDATION_EXPORT const unsigned char SentryUIVersionString[];
 
-extern NSString *const SENTRY_XCODE_PREVIEW_ENVIRONMENT_KEY;
-
 #if __has_include("SentryInternal.h")
 #    import "SentryInternal.h"
 #endif
