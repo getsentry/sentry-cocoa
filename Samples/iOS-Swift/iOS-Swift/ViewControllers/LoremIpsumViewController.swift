0ca0bcbd4e152f63f62011d53ae55439ab5068c1
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-01-23 15:28:57 +0100

    revert change in sample

--- Samples/iOS-Swift/iOS-Swift/ViewControllers/LoremIpsumViewController.swift
+++ Samples/iOS-Swift/iOS-Swift/ViewControllers/LoremIpsumViewController.swift
@@ -11,23 +11,12 @@ class LoremIpsumViewController: UIViewController {
         let dispatchQueue = DispatchQueue(label: "LoremIpsumViewController")
         dispatchQueue.async {
             if let path = Bundle.main.path(forResource: "LoremIpsum", ofType: "txt") {
-                if let contents = try? Data(
-                    contentsOfUrlWithSentryTracing: URL(fileURLWithPath: path)
-                ) {
+                if let contents = FileManager.default.contents(atPath: path) {
                     DispatchQueue.main.async {
                         self.textView.text = String(data: contents, encoding: .utf8)
-
-                        dispatchQueue.async {
-                            let tempURL = FileManager.default.temporaryDirectory
-                                .appendingPathComponent("output.txt")
-                            try! contents
-                                .writeWithSentryTracing(
-                                    to: tempURL,
-                                    options: .atomic
-                                )
-                            dispatchQueue.asyncAfter(deadline: .now() + 0.1) {
-                                SentrySDK.reportFullyDisplayed()
-                            }
+                        
+                        dispatchQueue.asyncAfter(deadline: .now() + 0.1) {
+                            SentrySDK.reportFullyDisplayed()
                         }
                     }
                 }
