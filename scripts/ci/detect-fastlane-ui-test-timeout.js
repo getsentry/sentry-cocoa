c62381cbd3ff678aba9b8a4f1d90d2ac63c76c2c
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-02-03 10:42:34 +0100

    refactor scripts

---
+++ scripts/ci/detect-fastlane-ui-test-timeout.js
@@ -0,0 +1,33 @@
+module.exports = ({ github, context, core }) => {
+  const fs = require("fs");
+
+  try {
+    const logsDir = path.join("~/Library/Logs/scan");
+    if (!fs.existsSync(logsDir)) {
+      throw new Error("No logs directory found");
+    }
+    const logs = fs.readdirSync(logsDir);
+    const lastLog = logs[logs.length - 1];
+    if (!lastLog) {
+      throw new Error("No logs found");
+    }
+    const lastLogPath = path.join(logsDir, lastLog);
+    const lastLogContent = fs.readFileSync(lastLogPath, "utf8");
+    const retryReasonRegexs = [
+      /Test\srunner\snever\sbegan\sexecuting\stests\safter\slaunching/,
+    ];
+    const retryReason = retryReasonRegexs.find((regex) =>
+      lastLogContent.match(regex)
+    );
+    if (retryReason) {
+      core.warning("Retry condition found, retrying...");
+      core.setOutput("RETRY_TEST", "true");
+
+      return;
+    }
+
+    throw new Error("Retry condition not found!");
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
