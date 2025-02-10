e9e18ab85c2171ef4ca3d3e760e1c4c2ba679550
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-02-10 12:02:40 +0100

    tests: fixed execution and compilation of SentrySwiftUI tests (#4809)

--- Tests/SentrySwiftUITests/SentryRedactModifierTests.swift
+++ Tests/SentrySwiftUITests/SentryRedactModifierTests.swift
@@ -6,15 +6,44 @@ import XCTest
 
 class SentryRedactModifierTests: XCTestCase {
     func testViewMask() throws {
+        // -- Arrange --
         let text = Text("Hello, World!")
+        // -- Act --
         let redactedText = text.sentryReplayMask()
-        XCTAssertTrue(redactedText is ModifiedContent<Text, SentryReplayModifier>)
+        // -- Assert --
+        // We can not use the `type(of:)` or `is` to compare the response, because the type is erased to `AnyView`.
+        let typeOfRedactedText = String(describing: redactedText)
+
+        // As the actual type is part of SwiftUI and somewhat unpredictable, we need to have multiple assertions.
+        // Eventually this can be replaced with a more stable solution.
+        let candidates = [
+            "ModifiedContent<Text, SentryReplayModifier>",
+            "SwiftUI.ModifiedContent<SwiftUI.Text, SentrySwiftUI.SentryReplayModifier>"
+        ]
+        XCTAssertTrue(
+            candidates.contains(where: { typeOfRedactedText.contains($0) }),
+            "Type did not match candidates: \(typeOfRedactedText)"
+        )
     }
     
     func testViewUnmask() throws {
+        // -- Arrange --
         let text = Text("Hello, World!")
+        // -- Act --
         let redactedText = text.sentryReplayUnmask()
-        XCTAssertTrue(redactedText is ModifiedContent<Text, SentryReplayModifier>)
+        // -- Assert --
+        // We can not use the `type(of:)` or `is` to compare the response, because the type is erased to `AnyView`.
+        let typeOfRedactedText = String(describing: redactedText)
+        // As the actual type is part of SwiftUI and somewhat unpredictable, we need to have multiple assertions.
+        // Eventually this can be replaced with a more stable solution.
+        let candidates = [
+            "ModifiedContent<Text, SentryReplayModifier>",
+            "SwiftUI.ModifiedContent<SwiftUI.Text, SentrySwiftUI.SentryReplayModifier>"
+        ]
+        XCTAssertTrue(
+            candidates.contains(where: { typeOfRedactedText.contains($0) }),
+            "Type did not match candidates: \(typeOfRedactedText)"
+        )
     }
 }
 
--- Tests/SentrySwiftUITests/SentryTraceViewModelTest.swift
+++ Tests/SentrySwiftUITests/SentryTraceViewModelTest.swift
@@ -1,5 +1,6 @@
 #if canImport(UIKit) && canImport(SwiftUI)
 @testable import Sentry
+@_implementationOnly import SentryInternal
 @testable import SentrySwiftUI
 import XCTest
 
@@ -78,7 +79,14 @@ class SentryTraceViewModelTestCase: XCTestCase {
         
         viewModel.finishSpan(spanId)
         viewModel.viewDidAppear()
-        
+
+        // The span is finished in the next main cycle, therefore we need to wait for it.
+        let expectation = XCTestExpectation(description: "Wait for span to be finished.")
+        DispatchQueue.main.async {
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.5)
+
         // Verify that the span was popped and finished
         XCTAssertNil(SentryPerformanceTracker.shared.activeSpanId(), "Active span should be nil after finishing the span.")
         
