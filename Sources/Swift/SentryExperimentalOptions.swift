12a3b9169017825d2a3e31b5b333e53c03efc41e
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-02-20 17:36:18 +0100

    revert experimental option of file manager swizzling

--- Sources/Swift/SentryExperimentalOptions.swift
+++ Sources/Swift/SentryExperimentalOptions.swift
@@ -15,8 +15,9 @@ public class SentryExperimentalOptions: NSObject {
      * - Note: Swizzling is enabled by setting ``SentryOptions.enableSwizzling`` to `true`.
      *         This option allows you to disable swizzling for `NSFileManager` only, while keeping swizzling enabled for other classes.
      *         This is useful if you want to use manual tracing for file operations.
+     * - Experiment: This is an experimental feature and is therefore disabled by default. We'll enable it by default in a future release.
      */
-    public var enableFileManagerSwizzling = true
+    public var enableFileManagerSwizzling = false
 
     func validateOptions(_ options: [String: Any]?) {
     }
