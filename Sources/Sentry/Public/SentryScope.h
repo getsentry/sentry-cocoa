a500e984bedd6b7e3d3ee6b24f5cf6601c1a194f
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-02-25 13:56:36 +0100

    Add longer deprectation notice

--- Sources/Sentry/Public/SentryScope.h
+++ Sources/Sentry/Public/SentryScope.h
@@ -158,7 +158,11 @@ NS_SWIFT_NAME(Scope)
  * Mutates the current transaction atomically.
  * @param callback the SentrySpanCallback.
  */
-- (void)useSpan:(SentrySpanCallback)callback DEPRECATED_MSG_ATTRIBUTE("use `span` instead");
+- (void)useSpan:(SentrySpanCallback)callback
+    DEPRECATED_MSG_ATTRIBUTE(
+        "This method was used to create an atomic block that could be used to mutate the current "
+        "span. It is not atomic anymore and due to issues with memory safety in `NSBlock` it is "
+        "now considered unsafe and deprecated. Use `span` instead.");
 
 /**
  * Returns the current span.
