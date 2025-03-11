8260d64553d58878055764a3d84a0d2dd8560568
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-03-11 14:10:45 +0100

    add note on experimental mask renderer sbuclass of mask renderer

--- Sources/Swift/Tools/ViewCapture/SentryExperimentalMaskRenderer.swift
+++ Sources/Swift/Tools/ViewCapture/SentryExperimentalMaskRenderer.swift
@@ -8,6 +8,7 @@ class SentryExperimentalMaskRenderer: SentryDefaultMaskRenderer {
         // The `SentryDefaultMaskRenderer` is also using an display scale of 1, therefore we also use 1 here.
         // This could be evaluated in future iterations to view performance impact vs quality.
         let image = SentryGraphicsImageRenderer(size: size, scale: 1).image { context in
+            // The experimental mask renderer only uses a different graphics renderer and can reuse the default masking logic.
             applyMasking(to: context, image: image, size: size, masking: masking)
         }
         return image
