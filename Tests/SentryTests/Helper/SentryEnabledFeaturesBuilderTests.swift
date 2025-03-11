82cac0c4f2648bdf638be23aa379ce70e0b239fa
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-03-11 15:52:46 +0100

    feat(session-replay): Add experimental flags to use a more performant view renderer for Session Replay (#4940)

--- Tests/SentryTests/Helper/SentryEnabledFeaturesBuilderTests.swift
+++ Tests/SentryTests/Helper/SentryEnabledFeaturesBuilderTests.swift
@@ -4,13 +4,18 @@ import XCTest
 final class SentryEnabledFeaturesBuilderTests: XCTestCase {
 
     func testDefaultFeatures() throws {
-        let features = SentryEnabledFeaturesBuilder.getEnabledFeatures(options: Options())
-        
+        // -- Arrange --
+        let options = Options()
+
+        // -- Act --
+        let features = SentryEnabledFeaturesBuilder.getEnabledFeatures(options: options)
+
+        // -- Assert --
         XCTAssertEqual(features, ["captureFailedRequests"])
     }
-    
+
     func testEnableAllFeatures() throws {
-        
+        // -- Arrange --
         let options = Options()
         options.enablePerformanceV2 = true
         options.enableTimeToFullDisplayTracing = true
@@ -25,41 +30,45 @@ final class SentryEnabledFeaturesBuilderTests: XCTestCase {
         options.enablePreWarmedAppStartTracing = true
 #endif // canImport(UIKit)
 #endif // os(iOS) || os(tvOS)
-      
+
 #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
         options.enableAppHangTrackingV2 = true
 #endif //os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
-        
+
+        // -- Act --
         let features = SentryEnabledFeaturesBuilder.getEnabledFeatures(options: options)
-        
+
+        // -- Assert --
         XCTAssert(features.contains("captureFailedRequests"))
         XCTAssert(features.contains("performanceV2"))
         XCTAssert(features.contains("timeToFullDisplayTracing"))
         XCTAssert(features.contains("swiftAsyncStacktraces"))
-        
+
 #if os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
         XCTAssert(features.contains("appLaunchProfiling"))
 #endif // os(iOS) || os(macOS) || targetEnvironment(macCatalyst)
-    
+
 #if os(iOS) || os(tvOS)
 #if canImport(UIKit) && !SENTRY_NO_UIKIT
         XCTAssert(features.contains("preWarmedAppStartTracing"))
 #endif // canImport(UIKit)
 #endif // os(iOS) || os(tvOS)
-        
+
 #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
         XCTAssert(features.contains("appHangTrackingV2"))
 #endif //os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
-        
     }
-    
+
     func testEnablePersistingTracesWhenCrashing() {
+        // -- Arrange --
         let options = Options()
-        
+
         options.enablePersistingTracesWhenCrashing = true
-        
+
+        // -- Act --
         let features = SentryEnabledFeaturesBuilder.getEnabledFeatures(options: options)
-        
+
+        // -- Assert --
         XCTAssert(features.contains("persistingTracesWhenCrashing"))
     }
 
@@ -69,5 +78,39 @@ final class SentryEnabledFeaturesBuilderTests: XCTestCase {
 
         // -- Assert --
         XCTAssertEqual(result, [])
+    }
+
+    func testEnableExperimentalViewRenderer_isEnabled_shouldAddFeature() throws {
+#if os(iOS)
+        // -- Arrange --
+        let options = Options()
+
+        options.sessionReplay.enableExperimentalViewRenderer = true
+
+        // -- Act --
+        let features = SentryEnabledFeaturesBuilder.getEnabledFeatures(options: options)
+
+        // -- Assert --
+        XCTAssert(features.contains("experimentalViewRenderer"))
+#else
+        throw XCTSkip("Test not supported on this platform")
+#endif
+    }
+
+    func testEnableFastViewRendering_isEnabled_shouldAddFeature() throws {
+#if os(iOS)
+        // -- Arrange --
+        let options = Options()
+
+        options.sessionReplay.enableFastViewRendering = true
+
+        // -- Act --
+        let features = SentryEnabledFeaturesBuilder.getEnabledFeatures(options: options)
+
+        // -- Assert --
+        XCTAssert(features.contains("fastViewRendering"))
+#else
+        throw XCTSkip("Test not supported on this platform")
+#endif
     }
 }
