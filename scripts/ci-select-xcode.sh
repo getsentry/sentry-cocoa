d62f07f18813eaa386415afae2c6b508cbb9a8ac
Philipp Hofmann <philipp.hofmann@sentry.io>
2025-03-24 10:53:49 +0100

    ci: Use xcodes in ci-select-xcode.sh
    Replace manual selecting Xcode version with xcodes.

--- scripts/ci-select-xcode.sh
+++ scripts/ci-select-xcode.sh
@@ -3,11 +3,12 @@
 # For available Xcode versions see:
 # - https://github.com/actions/runner-images/blob/main/images/macos/macos-13-Readme.md
 # - https://github.com/actions/runner-images/blob/main/images/macos/macos-14-Readme.md
+# - https://github.com/actions/runner-images/blob/main/images/macos/macos-15-Readme.md
 
 set -euo pipefail
 
 # 14.3 is the default
 XCODE_VERSION="${1:-14.3}"
 
-sudo xcode-select -s "/Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer"
+xcodes select "$XCODE_VERSION"
 swiftc --version
