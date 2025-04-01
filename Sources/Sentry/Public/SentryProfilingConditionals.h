4fd672c15790abe54e1e2a82a28da10a25c8465c
Andrew McKnight <andrew.mcknight@sentry.io>
2025-04-01 12:19:02 -0800

    trying to work around building with tooling versions both before and after visionOS becamse a thing

--- Sources/Sentry/Public/SentryProfilingConditionals.h
+++ Sources/Sentry/Public/SentryProfilingConditionals.h
@@ -6,7 +6,7 @@
 // tvOS and watchOS do not support the kernel APIs required by our profiler
 // e.g. mach_msg, thread_suspend, thread_resume; we haven't yet tested on
 // visionOS
-#if TARGET_OS_WATCH || TARGET_OS_TV || TARGET_OS_VISION
+#if TARGET_OS_WATCH || TARGET_OS_TV || (defined(TARGET_OS_VISION) && TARGET_OS_VISION)
 #    define SENTRY_TARGET_PROFILING_SUPPORTED 0
 #else
 #    define SENTRY_TARGET_PROFILING_SUPPORTED 1
