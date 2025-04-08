df8c91e9f15f5459e124e8702c954a77f918a982
Andrew McKnight <andrew.mcknight@sentry.io>
2025-04-07 17:04:23 -0800

    make analyzer happy

--- Sources/Sentry/SentrySpan.m
+++ Sources/Sentry/SentrySpan.m
@@ -111,7 +111,7 @@ NS_ASSUME_NONNULL_BEGIN
 #endif // SENTRY_TARGET_PROFILING_SUPPORTED
     }
 
-    if (_parentSpanId == nil) {
+    if (_parentSpanId == context.parentSpanId) {
         SENTRY_LOG_DEBUG(@"Started root span with id %@", _spanId.sentrySpanIdString);
     } else {
         SENTRY_LOG_DEBUG(@"Started span with id %@; parent id %@", _spanId.sentrySpanIdString,
