0866d62798fb4095b46581cfc32ea70fd02fd8f0
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-04-08 12:17:07 +0200

    feat add switches to sample to restart SDK with options

--- Samples/SessionReplay-CameraTest/Sources/AppDelegate.swift
+++ Samples/SessionReplay-CameraTest/Sources/AppDelegate.swift
@@ -4,14 +4,28 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    static var isSessionReplayEnabled = true
+    static var isExperimentalViewRendererEnabled = true
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        AppDelegate.reloadSentrySDK()
+
+        return true
+    }
+
+    static func reloadSentrySDK() {
+        if SentrySDK.isEnabled {
+            print("SentrySDK already started, closing it")
+            SentrySDK.close()
+        }
+
         SentrySDK.start { options in
             options.dsn = "https://6cc9bae94def43cab8444a99e0031c28@o447951.ingest.sentry.io/5428557"
             options.debug = true
 
             options.tracesSampleRate = 1.0
             options.profilesSampleRate = 1.0
-            options.sessionReplay.sessionSampleRate = 1.0
+            options.sessionReplay.sessionSampleRate = Self.isSessionReplayEnabled ? 1.0 : 0.0
 
             options.initialScope = { scope in
                 scope.injectGitInformation()
@@ -20,12 +34,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
 
             // Experimental features
-            options.sessionReplay.enableExperimentalViewRenderer = true
+            options.sessionReplay.enableExperimentalViewRenderer = Self.isExperimentalViewRendererEnabled
             // Disable the fast view renderering, because we noticed parts (like the tab bar) are not rendered correctly
             options.sessionReplay.enableFastViewRendering = false
         }
-
-        return true
     }
 
     // MARK: UISceneSession Lifecycle
--- Samples/SessionReplay-CameraTest/Sources/ViewController.swift
+++ Samples/SessionReplay-CameraTest/Sources/ViewController.swift
@@ -5,6 +5,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var backgroundLabel: UILabel!
+    @IBOutlet weak var controlsContainerView: UIView!
+
+    @IBOutlet weak var enableSessionReplaySwitch: UISwitch!
+    @IBOutlet weak var useExperimentalViewRendererSwitch: UISwitch!
 
     private weak var previewView: PreviewView!
     private weak var errorLabel: UILabel!
@@ -25,7 +29,7 @@ class ViewController: UIViewController {
     private func setupPreviewView() {
         let previewView = PreviewView()
         self.previewView = previewView
-        view.addSubview(previewView)
+        view.insertSubview(previewView, belowSubview: controlsContainerView)
 
         previewView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -78,6 +82,19 @@ class ViewController: UIViewController {
 
         // Assign the video output to the preview layer and start the session
         videoPreviewLayer.session = captureSession
-        captureSession.startRunning()
+        DispatchQueue.global(qos: .userInitiated).async {
+            captureSession.startRunning()
+        }
+    }
+
+    @IBAction func didChangeToggleValue(_ sender: UISwitch) {
+        if sender === enableSessionReplaySwitch {
+            AppDelegate.isSessionReplayEnabled = sender.isOn
+            print("Enable session replay flag changed to: \(AppDelegate.isSessionReplayEnabled)")
+        } else if sender === useExperimentalViewRendererSwitch {
+            AppDelegate.isExperimentalViewRendererEnabled = sender.isOn
+            print("Use experimental view renderer flag changed to: \(AppDelegate.isExperimentalViewRendererEnabled)")
+        }
+        AppDelegate.reloadSentrySDK()
     }
 }
