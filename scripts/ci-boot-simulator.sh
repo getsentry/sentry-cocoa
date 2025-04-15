4be9cdea2c5adf1936237fcc60ccf96fe068dfa5
Krystof Woldrich <krystof.woldrich@sentry.io>
2025-04-15 13:51:02 +0200

    wait for boot, fix scripts vars

--- scripts/ci-boot-simulator.sh
+++ scripts/ci-boot-simulator.sh
@@ -14,51 +14,43 @@ set -euo pipefail
 # Parse named arguments
 XCODE_VERSION="16.2" # Default value
 
-# Only allow one argument type
-if [[ $# -gt 2 ]]; then
-    echo "Error: Cannot specify both --xcode and --device"
-    echo "Usage: $0 [-x|--xcode <version>] OR [-d|--device <device>]"
-    exit 1
-fi
-
 while [[ $# -gt 0 ]]; do
     case $1 in
         -x|--xcode)
             XCODE_VERSION="$2"
             shift 2
             ;;
-        -d|--device)
-            SIMULATOR="$2"
-            shift 2
-            ;;
         *)
             echo "Unknown argument: $1"
-            echo "Usage: $0 [-x|--xcode <version>] [-d|--device <device>]"
+            echo "Usage: $0 [-x|--xcode <version>]"
             exit 1
             ;;
     esac
 done
 
+SIMULATOR="iPhone 16"
+
 # Select simulator based on Xcode version
-if [[ -z "${SIMULATOR}" ]]; then
-    case "$XCODE_VERSION" in
-        "14.3.1")
-            SIMULATOR="iPhone 14"
-            ;;
-        "15.4")
-            SIMULATOR="iPhone 15"
-            ;;
-        "16.2")
-            SIMULATOR="iPhone 16"
-            ;;
-        *)
-            SIMULATOR="iPhone 16" # Default fallback
-            ;;
-    esac
-fi
+case "$XCODE_VERSION" in
+    "14.3.1")
+        SIMULATOR="iPhone 14"
+        ;;
+    "15.4")
+        SIMULATOR="iPhone 15"
+        ;;
+    "16.2")
+        SIMULATOR="iPhone 16"
+        ;;
+    *)
+        SIMULATOR="iPhone 16" # Default fallback
+        ;;
+esac
 
 echo "Booting simulator $SIMULATOR"
 xcrun simctl boot "$SIMULATOR"
+
+# Wait for the simulator to boot
+xcrun simctl bootstatus "$SIMULATOR"
 
 # Print details about the booted simulator, iOS version, etc.
 xcrun simctl list devices --json | jq '.devices | to_entries[] | select(.value[] | .state == "Booted")'
