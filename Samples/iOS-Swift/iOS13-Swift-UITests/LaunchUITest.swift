4a695e7ac10586e922fd9beee7f0592ca4777318
Andrew McKnight <andrew.mcknight@sentry.io>
2025-04-21 22:41:27 -0800

    Revert "host iOS13-Swift from within Samples/, not Samples/iOS13-Swift/"
    This reverts commit 27fe5ba93f9a99c55538917cb5d5f1748bf80e5d.

---
+++ Samples/iOS-Swift/iOS13-Swift-UITests/LaunchUITest.swift
@@ -0,0 +1,36 @@
+import XCTest
+
+class LaunchUITests: XCTestCase {
+    
+    private let timeout: TimeInterval = 10
+    private let app: XCUIApplication = XCUIApplication()
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        continueAfterFailure = false
+        
+        app.launch()
+        XCUIDevice.shared.orientation = .portrait
+        
+        waitForExistenceOfMainScreen()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        app.terminate()
+    }
+   
+    func testShowSwiftUI() {
+        app.buttons["Show SwiftUI"].tap()
+        XCTAssertTrue(app.staticTexts["SwiftUI!"].waitForExistence(timeout: timeout), "SwiftUI not loaded.")
+    }
+    
+    func testNavigationTransaction() {
+        app.buttons["Test Navigation Transaction"].tap()
+        XCTAssertEqual(app.state, .runningForeground)
+    }
+    
+    private func waitForExistenceOfMainScreen() {
+        XCTAssertTrue(app.buttons["captureMessage"].waitForExistence(timeout: timeout), "Home Screen doesn't exist.")
+    }
+}
