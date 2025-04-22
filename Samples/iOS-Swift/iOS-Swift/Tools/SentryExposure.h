cd64d820ffd5655d883e5b5443f9ee3775e6335f
Andrew McKnight <andrew.mcknight@sentry.io>
2025-04-21 17:30:19 -0800

    host iOS-Swift from within Samples/, not another redundant level down

--- Samples/iOS-Swift/iOS-Swift/Tools/SentryExposure.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-@interface SentryBreadcrumbTracker : NSObject
-
-+ (NSDictionary *)extractDataFromView:(UIView *)view
-          withAccessibilityIdentifier:(BOOL)includeIdentifier;
-
-@end
