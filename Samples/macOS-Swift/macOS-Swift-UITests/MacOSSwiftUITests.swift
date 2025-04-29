07786e89d8218029f8b4a12029e19bf4171e938e
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-04-29 15:32:01 +0200

    remove unused boilerplate code

--- Samples/macOS-Swift/macOS-Swift-UITests/MacOSSwiftUITests.swift
+++ Samples/macOS-Swift/macOS-Swift-UITests/MacOSSwiftUITests.swift
@@ -3,16 +3,7 @@ import XCTest
 final class MacOSSwiftUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = true
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     @MainActor
