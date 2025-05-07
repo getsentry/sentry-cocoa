77309c50e7230fd8cd577ea5fa7b30cf680c5d2a
Philipp Hofmann <philipp.hofmann@sentry.io>
2025-05-07 13:23:29 +0200

    fix

--- Utils/VersionBump/main.swift
+++ Utils/VersionBump/main.swift
@@ -13,6 +13,13 @@ let files = [
     "./Tests/HybridSDKTest/HybridPod.podspec"
 ]
 
+// Files that only accept the format x.x.x in order to release an app using the framework.
+// This will enable publishing apps with SDK beta version.
+let restrictFiles = [
+    "./Sources/Configuration/SDK.xcconfig",
+    "./Sources/Configuration/Versioning.xcconfig"
+]
+
 let args = CommandLine.arguments
 
 let semver: StaticString = "([0-9]+)\\.([0-9]+)\\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\\.[0-9A-Za-z-]+)*))?(?:\\+[0-9A-Za-z-]+)?"
@@ -35,7 +42,9 @@ if let match = Regex(semver, options: [.dotMatchesLineSeparators]).firstMatch(in
     fromVersion = extractVersionOnly(fromVersion)
     toVersion = extractVersionOnly(toVersion)
     
-    try updateVersion("./Sources/Configuration/Versioning.xcconfig", fromVersion, toVersion)
+    for file in restrictFiles {
+        try updateVersion(file, fromVersion, toVersion)
+    }
 }
 
 func updateVersion(_ file: String, _ fromVersion: String, _ toVersion: String) throws {
