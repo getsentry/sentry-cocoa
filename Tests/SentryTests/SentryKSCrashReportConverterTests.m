38c40a545bbf616bc6340797fcec9c2bd35178bf
Philipp Hofmann <philipp.hofmann@sentry.io>
2025-05-07 10:55:17 +0200

    fix: Reporting unhandled C++ exception message
    The SDK was not reporting the exception message of unhandled C++.
    This is fixed now.

--- Tests/SentryTests/SentryKSCrashReportConverterTests.m
+++ Tests/SentryTests/SentryKSCrashReportConverterTests.m
@@ -234,7 +234,16 @@
 
 - (void)testCPPException
 {
-    [self isValidReport:@"Resources/CPPException"];
+    [self isValidReport:@"Resources/CPP Exception"];
+
+    NSDictionary *rawCrash = [self getCrashReport:@"Resources/CPPException"];
+    SentryCrashReportConverter *reportConverter =
+        [[SentryCrashReportConverter alloc] initWithReport:rawCrash inAppLogic:self.inAppLogic];
+    SentryEvent *event = [reportConverter convertReportToEvent];
+
+    SentryException *exception = event.exceptions.firstObject;
+
+    XCTAssertEqualObjects(exception.value, @"MyException: Something bad happened...");
 }
 
 - (void)testNXPage
