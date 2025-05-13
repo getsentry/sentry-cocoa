3ece86a8fcaab8535a38bbcbbfd0fae659fbe0de
Andrew McKnight <andrew.mcknight@sentry.io>
2025-05-13 00:07:17 -0800

    standardize ios-swift6

--- Samples/iOS-Swift6/iOS-Swift6/SceneDelegate.swift
+++ Samples/iOS-Swift6/iOS-Swift6/SceneDelegate.swift
@@ -3,11 +3,9 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    lazy var s = SampleAppDebugMenu()
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
-
-        s.display()
+        SentrySDKWrapper.shared.startSentry()
     }
 }
