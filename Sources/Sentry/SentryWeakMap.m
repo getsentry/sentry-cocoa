5b2218294e024d438a4ed34abaf517b0366cc69c
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-05-14 10:41:44 +0200

    replace nstablemap with dictionary-based storage

--- Sources/Sentry/SentryWeakMap.m
+++ Sources/Sentry/SentryWeakMap.m
@@ -1,49 +1,124 @@
 #import "SentryWeakMap.h"
 
-@interface SentryWeakMap <KeyType, ObjectType>()
-@property (nonatomic, strong) NSMapTable<KeyType, ObjectType> *mapTable;
+@interface SentryWeakBox : NSObject <NSCopying>
+
+@property (nonatomic, weak, readonly) id key;
+
+- (instancetype)initWithKey:(id)key;
+
 @end
 
-@implementation SentryWeakMap
+@implementation SentryWeakBox {
+    __weak id _key;
+    NSUInteger _hash;
+}
 
-- (instancetype)init
+- (instancetype)initWithKey:(id)key
 {
     if (self = [super init]) {
-        _mapTable = [NSMapTable weakToStrongObjectsMapTable];
+        _key = key;
+        // We need to cache the hash value because weak keys can be deallocated.
+        // This hash is used to identify the weak box by the wrapped key in the storage.
+        _hash = [key hash];
     }
     return self;
 }
 
-- (nullable id)objectForKey:(nullable id)aKey
+- (id)key
 {
-    id obj = [self.mapTable objectForKey:aKey];
-    [self prune];
-    return obj;
+    return _key;
+}
+
+- (NSUInteger)hash
+{
+    return _hash;
+}
+
+- (BOOL)isEqual:(id)object
+{
+    if (![object isKindOfClass:[SentryWeakBox class]]) {
+        return NO;
+    }
+    id selfKey = self.key;
+    id otherKey = ((SentryWeakBox *)object).key;
+    return selfKey && otherKey && [selfKey isEqual:otherKey];
+}
+
+- (nonnull id)copyWithZone:(nullable NSZone *)zone
+{
+    return [[[SentryWeakBox class] allocWithZone:zone] initWithKey:self.key];
+}
+
+@end
+
+@interface SentryWeakMap <KeyType, ObjectType>()
+@property (nonatomic, strong) NSMutableDictionary<SentryWeakBox *, id> *storage;
+@end
+
+@implementation SentryWeakMap
+
+// This class was originally a wrapper around NSMapTable with weak keys.
+// Due to undeterministic behavior of NSMapTable, we had to implement our own weak key storage.
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _storage = [NSMutableDictionary dictionary];
+    }
+    return self;
 }
 
 - (void)setObject:(nullable id)anObject forKey:(nullable id)aKey
 {
-    [self.mapTable setObject:anObject forKey:aKey];
+    if (!aKey || !anObject) {
+        return;
+    }
+
     [self prune];
+
+    SentryWeakBox *box = [[SentryWeakBox alloc] initWithKey:aKey];
+    self.storage[box] = anObject;
+}
+
+- (nullable id)objectForKey:(nullable id)aKey
+{
+    if (!aKey) {
+        return nil;
+    }
+
+    [self prune];
+
+    SentryWeakBox *box = [[SentryWeakBox alloc] initWithKey:aKey];
+    return self.storage[box];
 }
 
 - (void)removeObjectForKey:(nullable id)aKey
 {
-    [self.mapTable removeObjectForKey:aKey];
+    if (!aKey) {
+        return;
+    }
+
     [self prune];
+
+    SentryWeakBox *box = [[SentryWeakBox alloc] initWithKey:aKey];
+    [self.storage removeObjectForKey:box];
 }
 
 - (NSUInteger)count
 {
-    return self.mapTable.count;
+    // Do not prune here, to make this method a direct proxy for the underlying dictionary.
+    return self.storage.count;
 }
 
 - (void)prune
 {
-    for (id key in self.mapTable.keyEnumerator) {
-        // No-op: iterating triggers internal cleanup of nil keys
-        (void)key;
+    NSMutableArray *keysToRemove = [NSMutableArray array];
+    for (SentryWeakBox *box in self.storage.keyEnumerator) {
+        if (box.key == nil) {
+            [keysToRemove addObject:box];
+        }
     }
+    [self.storage removeObjectsForKeys:keysToRemove];
 }
 
 @end
