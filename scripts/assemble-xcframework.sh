889ea3a028f05b78471d957e2d2d218c33a3610f
Andrew McKnight <andrew.mcknight@sentry.io>
2025-05-17 09:28:34 -0800

    interleave framework/debugsymbol args

--- scripts/assemble-xcframework.sh
+++ scripts/assemble-xcframework.sh
@@ -1,13 +1,32 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -x
 
-search_path=${1}
-xcframework_name=${2}
+search_path="$1"
+xcframework_name="$2"
 
-framework_args=$(find "$search_path" -name "*.framework" | awk '{print "-framework " $0}' | xargs)
-dsym_args=$(find "$search_path" -name "*.framework.dSYM" | awk '{print "-debug-symbols " $0}' | xargs)
+if [ "$xcframework_name" = "SentrySwiftUI" ]; then
+    find "$search_path" -name "Sentry.framework" -print0 | xargs -0 rm -rf
+    find "$search_path" -name "Sentry.framework.dSYM" -print0 | xargs -0 rm -rf
+fi
 
-eval "xcodebuild -create-xcframework $framework_args $dsym_args -output $xcframework_name.xcframework"
+xcodebuild_cmd="xcodebuild -create-xcframework"
+
+# Iterate over each slice directory
+for slice_dir in "$search_path"/xcframework-*; do
+    framework_path=$(find "$slice_dir/Products" -name '*.framework' -print -quit)
+    dsym_path=$(find "$slice_dir/dSYMs" -name '*.framework.dSYM' -print -quit)
+
+    if [[ -n "$framework_path" && -n "$dsym_path" ]]; then
+        xcodebuild_cmd+=" -framework \"$framework_path\""
+        xcodebuild_cmd+=" -debug-symbols \"$dsym_path\""
+    fi
+done
+
+# Construct the xcodebuild command
+xcodebuild_cmd+=" -output \"$xcframework_name.xcframework\""
+
+# Execute the command
+eval "$xcodebuild_cmd"
 
 ditto -c -k -X --rsrc --keepParent "$xcframework_name.xcframework" "$xcframework_name.xcframework.zip"
