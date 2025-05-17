054633257e086629f5afe69b8ef2c2d7b4231e65
Andrew McKnight <andrew.mcknight@sentry.io>
2025-05-16 23:48:53 -0800

    dont need to append path prefix

--- scripts/assemble-xcframework.sh
+++ scripts/assemble-xcframework.sh
@@ -5,8 +5,8 @@ set -x
 search_path=${1}
 xcframework_name=${2}
 
-framework_args=$(find "$search_path" -name "*.framework" | awk -v sp="$search_path" '{print "-framework " sp "/" $0}' | xargs)
-dsym_args=$(find "$search_path" -name "*.framework.dSYM" | awk -v sp="$search_path" '{print "-debug-symbols " sp "/" $0}' | xargs)
+framework_args=$(find "$search_path" -name "*.framework" | awk -v '{print "-framework " $0}' | xargs)
+dsym_args=$(find "$search_path" -name "*.framework.dSYM" | awk -v '{print "-debug-symbols " $0}' | xargs)
 
 xcodebuild -create-xcframework "$framework_args" "$dsym_args" -output "$xcframework_name.xcframework"
 
