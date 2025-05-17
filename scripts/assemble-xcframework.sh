1e7c5bc695b91050d325dd5665cf412bfe3e7420
Andrew McKnight <andrew.mcknight@sentry.io>
2025-05-16 21:41:20 -0800

    extract assembly to script

---
+++ scripts/assemble-xcframework.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+search_path=${1}
+xcframework_name=${2}
+
+framework_args=$(find "$search_path" -name "*.framework" | awk '{print "-framework $search_path"/"$0}' | xargs)
+dsym_args=$(find "$search_path" -name "*.framework.dSYM" | awk '{print "-debug-symbols $search_path"/"$0}' | xargs)
+
+xcodebuild -create-xcframework "$framework_args" "$dsym_args" -output "$xcframework_name.xcframework"
+
+ditto -c -k -X --rsrc --keepParent "$xcframework_name.xcframework" "$xcframework_name.xcframework.zip"
