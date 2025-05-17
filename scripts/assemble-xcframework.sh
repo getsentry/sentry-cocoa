c69cfd4573967b1046c1af36f0f7f45f80065915
Andrew McKnight <andrew.mcknight@sentry.io>
2025-05-17 09:37:51 -0800

    do the sentry dep build removal before even archiving

--- scripts/assemble-xcframework.sh
+++ scripts/assemble-xcframework.sh
@@ -5,14 +5,8 @@ set -x
 search_path="$1"
 xcframework_name="$2"
 
-if [ "$xcframework_name" = "SentrySwiftUI" ]; then
-    find "$search_path" -name "Sentry.framework" -print0 | xargs -0 rm -rf
-    find "$search_path" -name "Sentry.framework.dSYM" -print0 | xargs -0 rm -rf
-fi
-
 xcodebuild_cmd="xcodebuild -create-xcframework"
 
-# Iterate over each slice directory
 for slice_dir in "$search_path"/xcframework-*; do
     framework_path=$(find "$slice_dir/Products" -name '*.framework' -print -quit)
     dsym_path=$(find "$slice_dir/dSYMs" -name '*.framework.dSYM' -print -quit)
@@ -26,10 +20,8 @@ for slice_dir in "$search_path"/xcframework-*; do
     fi
 done
 
-# Construct the xcodebuild command
 xcodebuild_cmd+=" -output \"$xcframework_name.xcframework\""
 
-# Execute the command
 eval "$xcodebuild_cmd"
 
 ditto -c -k -X --rsrc --keepParent "$xcframework_name.xcframework" "$xcframework_name.xcframework.zip"
