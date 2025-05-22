68b7c0c94f415e9193c3b425b796c3dbc1eee493
Antonis Lilis <antonis.lilis@gmail.com>
2025-05-22 16:07:36 +0300

    Use low priority queue for dispatch queue wrapper

--- Sources/Sentry/SentryDispatchFactory.m
+++ Sources/Sentry/SentryDispatchFactory.m
@@ -11,13 +11,13 @@
     return [[SentryDispatchQueueWrapper alloc] initWithName:name attributes:attributes];
 }
 
-- (SentryDispatchQueueWrapper *)createBackgroundQueueWithName:(const char *)name
-                                             relativePriority:(int)relativePriority
+- (SentryDispatchQueueWrapper *)createLowPriorityQueue:(const char *)name
+                                      relativePriority:(int)relativePriority
 {
     SENTRY_CASSERT(relativePriority <= 0 && relativePriority >= QOS_MIN_RELATIVE_PRIORITY,
         @"Relative priority must be between 0 and %d", QOS_MIN_RELATIVE_PRIORITY);
     dispatch_queue_attr_t attributes = dispatch_queue_attr_make_with_qos_class(
-        DISPATCH_QUEUE_SERIAL, QOS_CLASS_BACKGROUND, relativePriority);
+        DISPATCH_QUEUE_SERIAL, QOS_CLASS_UTILITY, relativePriority);
     return [[SentryDispatchQueueWrapper alloc] initWithName:name attributes:attributes];
 }
 
