2464e8c2603333782755a2261742a851f45e563f
Itay <itay@itaysoft.com>
2025-05-26 18:15:00 -0300

    feat: Add class loaded twice validator

---
+++ Sources/Swift/Core/Tools/LoadValidator.swift
@@ -0,0 +1,69 @@
+//
+//  LoadValidator.swift
+//  Sentry
+//
+//  Created by Itay Brenner on 26/5/25.
+//  Copyright © 2025 Sentry. All rights reserved.
+//
+
+import Foundation
+import MachO
+
+@objc
+class LoadValidator: NSObject {
+    // Any class should be fine
+    static let targetClassName = "PrivateSentrySDKOnly"
+    
+    @objc
+    class func validateLoadedOnce() {
+        let imageCount = _dyld_image_count()
+        
+        var imagesWhereSentryIsFound: [String] = [String]()
+        
+        // Iterate over all images to make sure none of them contains a Sentry implementation twice
+        for i in 0..<imageCount {
+            guard let cImageName = _dyld_get_image_name(i) else { continue }
+            
+            // Skip Apple Frameworks
+            let imagePath = String(cString: cImageName)
+            
+            let systemLibraryPath = "/usr/lib/"
+#if targetEnvironment(simulator)
+            let ignoredPath = "/Library/Developer/CoreSimulator/Profiles/Runtimes/"
+#else
+            let ignoredPath = "/System/Library/"
+#endif
+            if imagePath.contains(ignoredPath) || imagePath.hasPrefix(systemLibraryPath) {
+                continue
+            }
+            
+            var classCount: UInt32 = 0
+            if let classNames = objc_copyClassNamesForImage(cImageName, &classCount) {
+                for j in 0..<Int(classCount) {
+                    let name = String(cString: classNames[j])
+                    if name.contains(self.targetClassName) {
+                        imagesWhereSentryIsFound.append(imagePath)
+                        break
+                    }
+                }
+                free(classNames)
+            }
+        }
+        
+        if imagesWhereSentryIsFound.count > 1 {
+#if DEBUG
+            var message = ["❌ Sentry SDK was loaded multiple times in the binary ❌"]
+            message.append("⚠️ This can cause undefined behavior, crashes, or duplicate reporting.")
+            message.append("Ensure the SDK is linked only once")
+            imagesWhereSentryIsFound.forEach { path in
+                message.append("  - \(path)")
+            }
+            SentryLog.log(message: message.joined(separator: "\n"), andLevel: SentryLevel.debug)
+            // Raise a debugger breakpoint
+            raise(SIGTRAP)
+#else
+            SentryLog.log(message: "❌ Sentry SDK was loaded multiple times in the binary ❌", andLevel: SentryLevel.warning)
+#endif
+        }
+    }
+}
