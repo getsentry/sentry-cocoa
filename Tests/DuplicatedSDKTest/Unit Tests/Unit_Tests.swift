65dd81824c14453ed1a1063a48d35067ce2a1b96
Itay <itay@itaysoft.com>
2025-05-27 00:51:11 -0300

    Add basic test capturing print output

---
+++ Tests/DuplicatedSDKTest/Unit Tests/Unit_Tests.swift
@@ -0,0 +1,40 @@
+//
+//  Unit_Tests.swift
+//  Unit Tests
+//
+//  Created by Itay Brenner on 27/5/25.
+//
+
+import Sentry
+import XCTest
+
+final class TestLog: XCTestCase {
+    func testDuplicatedLoadMessageOnSDKInit() throws {
+      
+      let output = captureStandardOutput {
+        SentrySDK.start(options: Options())
+      }
+      
+      XCTAssertTrue(output.contains("❌ Sentry SDK was loaded multiple times in the binary ❌"))
+    }
+  
+  private func captureStandardOutput(_ action: () -> Void) -> String {
+      let pipe = Pipe()
+      let originalStdOut = dup(fileno(stdout))
+
+      dup2(pipe.fileHandleForWriting.fileDescriptor, fileno(stdout))
+
+      action()
+
+      fflush(stdout)
+      dup2(originalStdOut, fileno(stdout))
+      close(originalStdOut)
+
+      pipe.fileHandleForWriting.closeFile()
+
+      let data = pipe.fileHandleForReading.readDataToEndOfFile()
+      pipe.fileHandleForReading.closeFile()
+
+      return String(data: data, encoding: .utf8) ?? ""
+  }
+}
