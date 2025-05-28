1cf944450c0946eb17b091ed09e772bc20b26e60
Itay <itay@itaysoft.com>
2025-05-28 10:42:40 -0300

    Remove headers

--- Tests/DuplicatedSDKTest/Unit Tests/Unit_Tests.swift
+++ Tests/DuplicatedSDKTest/Unit Tests/Unit_Tests.swift
@@ -1,40 +1,33 @@
-//
-//  Unit_Tests.swift
-//  Unit Tests
-//
-//  Created by Itay Brenner on 27/5/25.
-//
-
 import Sentry
 import XCTest
 
 final class TestLog: XCTestCase {
     func testDuplicatedLoadMessageOnSDKInit() throws {
-      
-      let output = captureStandardOutput {
-        SentrySDK.start(options: Options())
-      }
-      
-      XCTAssertTrue(output.contains("❌ Sentry SDK was loaded multiple times in the binary ❌"))
+
+        let output = captureStandardOutput {
+          SentrySDK.start(options: Options())
+        }
+        
+        XCTAssertTrue(output.contains("❌ Sentry SDK was loaded multiple times in the binary ❌"))
     }
   
-  private func captureStandardOutput(_ action: () -> Void) -> String {
-      let pipe = Pipe()
-      let originalStdOut = dup(fileno(stdout))
+    private func captureStandardOutput(_ action: () -> Void) -> String {
+        let pipe = Pipe()
+        let originalStdOut = dup(fileno(stdout))
 
-      dup2(pipe.fileHandleForWriting.fileDescriptor, fileno(stdout))
+        dup2(pipe.fileHandleForWriting.fileDescriptor, fileno(stdout))
 
-      action()
+        action()
 
-      fflush(stdout)
-      dup2(originalStdOut, fileno(stdout))
-      close(originalStdOut)
+        fflush(stdout)
+        dup2(originalStdOut, fileno(stdout))
+        close(originalStdOut)
 
-      pipe.fileHandleForWriting.closeFile()
+        pipe.fileHandleForWriting.closeFile()
 
-      let data = pipe.fileHandleForReading.readDataToEndOfFile()
-      pipe.fileHandleForReading.closeFile()
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        pipe.fileHandleForReading.closeFile()
 
-      return String(data: data, encoding: .utf8) ?? ""
-  }
+        return String(data: data, encoding: .utf8) ?? ""
+    }
 }
