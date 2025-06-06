3020f7a8c03b335719acda23a36868a1ece85788
Andrew McKnight <andrew.mcknight@sentry.io>
2025-06-05 18:52:21 -0700

    treat maccatalyst as a first class sdk

--- scripts/build-xcframework-slice.sh
+++ scripts/build-xcframework-slice.sh
@@ -18,12 +18,11 @@ suffix="${3:-}"
 MACH_O_TYPE="${4-mh_dylib}"
 configuration_suffix="${5-}"
 
-GCC_GENERATE_DEBUGGING_SYMBOLS="YES"
-
 resolved_configuration="Release$configuration_suffix"
 resolved_product_name="$scheme$configuration_suffix.framework"
 OTHER_LDFLAGS=""
 
+GCC_GENERATE_DEBUGGING_SYMBOLS="YES"
 if [ "$MACH_O_TYPE" = "staticlib" ]; then
     #For static framework we disabled symbols because they are not distributed in the framework causing warnings.
     GCC_GENERATE_DEBUGGING_SYMBOLS="NO"
@@ -41,36 +40,8 @@ fi
 slice_id="${scheme}${suffix}-${sdk}"
 
 carthage_xcarchive_path="Carthage/archive/${scheme}${suffix}"
-sentry_xcarchive_path="$carthage_xcarchive_path/${sdk}.xcarchive"
 
-set -o pipefail && NSUnbufferedIO=YES xcodebuild archive \
-    -project Sentry.xcodeproj/ \
-    -scheme "$scheme" \
-    -configuration "$resolved_configuration" \
-    -sdk "$sdk" \
-    -archivePath "./$sentry_xcarchive_path" \
-    CODE_SIGNING_REQUIRED=NO \
-    SKIP_INSTALL=NO \
-    CODE_SIGN_IDENTITY= \
-    CARTHAGE=YES \
-    MACH_O_TYPE="$MACH_O_TYPE" \
-    ENABLE_CODE_COVERAGE=NO \
-    GCC_GENERATE_DEBUGGING_SYMBOLS="$GCC_GENERATE_DEBUGGING_SYMBOLS" \
-    OTHER_LDFLAGS="$OTHER_LDFLAGS" 2>&1 | tee "${slice_id}.log" | xcbeautify
-
-if [ "$MACH_O_TYPE" = "staticlib" ]; then
-    if [ "$sdk" = "macosx" ]; then
-        infoPlistPath="Resources/Info.plist"
-    else
-        infoPlistPath="Info.plist"
-    fi
-    # This workaround is necessary to make Sentry Static framework to work
-    # More information in here: https://github.com/getsentry/sentry-cocoa/issues/3769
-    # The version 100 seems to work with all Xcode up to 15.4
-    plutil -replace "MinimumOSVersion" -string "100.0" "$sentry_xcarchive_path/Products/Library/Frameworks/${resolved_product_name}/$infoPlistPath"
-fi
-
-if [ "$sdk" = "macosx" ]; then
+if [ "$sdk" = "maccatalyst" ]; then
     #Create framework for mac catalyst
     set -o pipefail && NSUnbufferedIO=YES xcodebuild \
         -project Sentry.xcodeproj/ \
@@ -103,5 +74,34 @@ if [ "$sdk" = "macosx" ]; then
         maccatalyst_archive_dsym_directory="${maccatalyst_archive_directory}/dSYMs"
         mkdir "${maccatalyst_archive_dsym_directory}"
         cp -r "${maccatalyst_build_product_directory}/${resolved_product_name}.dSYM" "${maccatalyst_archive_dsym_directory}"
+    fi
+else
+    sentry_xcarchive_path="$carthage_xcarchive_path/${sdk}.xcarchive"
+
+    set -o pipefail && NSUnbufferedIO=YES xcodebuild archive \
+        -project Sentry.xcodeproj/ \
+        -scheme "$scheme" \
+        -configuration "$resolved_configuration" \
+        -sdk "$sdk" \
+        -archivePath "./$sentry_xcarchive_path" \
+        CODE_SIGNING_REQUIRED=NO \
+        SKIP_INSTALL=NO \
+        CODE_SIGN_IDENTITY= \
+        CARTHAGE=YES \
+        MACH_O_TYPE="$MACH_O_TYPE" \
+        ENABLE_CODE_COVERAGE=NO \
+        GCC_GENERATE_DEBUGGING_SYMBOLS="$GCC_GENERATE_DEBUGGING_SYMBOLS" \
+        OTHER_LDFLAGS="$OTHER_LDFLAGS" 2>&1 | tee "${slice_id}.log" | xcbeautify
+
+    if [ "$MACH_O_TYPE" = "staticlib" ]; then
+        if [ "$sdk" = "macosx" ]; then
+            infoPlistPath="Resources/Info.plist"
+        else
+            infoPlistPath="Info.plist"
+        fi
+        # This workaround is necessary to make Sentry Static framework to work
+        # More information in here: https://github.com/getsentry/sentry-cocoa/issues/3769
+        # The version 100 seems to work with all Xcode up to 15.4
+        plutil -replace "MinimumOSVersion" -string "100.0" "$sentry_xcarchive_path/Products/Library/Frameworks/${resolved_product_name}/$infoPlistPath"
     fi
 fi
