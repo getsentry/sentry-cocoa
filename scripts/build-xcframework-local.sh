93bc1deb95c6599205383ca3a239ff83fd3e5862
Andrew McKnight <andrew.mcknight@sentry.io>
2025-06-08 10:04:08 -0700

    get carthage ios working

--- scripts/build-xcframework-local.sh
+++ scripts/build-xcframework-local.sh
@@ -2,33 +2,38 @@
 
 set -eoux pipefail
 
-sdks="${1:-AllSDKs}"
-variants="${2:-AllVariants}"
+sdks="${1:-all}" # examples: all, ios, macosx, maccatalyst, tvos, watchos, visionos
+variants="${2:-all}" # examples: all, dynamic, static, swiftui, withoutUIKit
 signed="${3:-}"
 
-rm -rf Carthage/
-mkdir Carthage
+mkdir -p Carthage
 
-if [ "$variants" = "DynamicOnly" ] || [ "$variants" = "AllVariants" ]; then
+if [ "$variants" = "dynamic" ] || [ "$variants" = "all" ]; then
+    final_zip_path="Carthage/Sentry-Dynamic.xcframework.zip"
+    rm -rf "$final_zip_path"
     ./scripts/build-xcframework-variant.sh "Sentry" "-Dynamic" "mh_dylib" "" "$sdks"
     ./scripts/compress-xcframework.sh "$signed" Sentry-Dynamic
-    mv Sentry-Dynamic.xcframework.zip Carthage/Sentry-Dynamic.xcframework.zip
+    mv Sentry-Dynamic.xcframework.zip "$final_zip_path"
 fi
 
-if [ "$variants" = "StaticOnly" ] || [ "$variants" = "AllVariants" ]; then
+if [ "$variants" = "static" ] || [ "$variants" = "all" ]; then
+    final_zip_path="Carthage/Sentry.xcframework.zip"
+    rm -rf "$final_zip_path"
     ./scripts/build-xcframework-variant.sh "Sentry" "" "staticlib" "" "$sdks"
     ./scripts/compress-xcframework.sh "$signed" Sentry
-    mv Sentry.xcframework.zip Carthage/Sentry.xcframework.zip
+    mv Sentry.xcframework.zip "$final_zip_path"
 fi
 
-if [ "$variants" = "SwiftUIOnly" ] || [ "$variants" = "AllVariants" ]; then
+if [ "$variants" = "swiftui" ] || [ "$variants" = "all" ]; then
+    final_zip_path="Carthage/SentrySwiftUI.xcframework.zip"
+    rm -rf "$final_zip_path"
     ./scripts/build-xcframework-variant.sh "SentrySwiftUI" "" "mh_dylib" "" "$sdks"
     ./scripts/compress-xcframework.sh "$signed" SentrySwiftUI
-    mv SentrySwiftUI.xcframework.zip Carthage/SentrySwiftUI.xcframework.zip
-fi
+    mv SentrySwiftUI.xcframework.zip "$final_zip_path"fi
 
-if [ "$variants" = "WithoutUIKitOnly" ] || [ "$variants" = "AllVariants" ]; then
+if [ "$variants" = "withoutUIKit" ] || [ "$variants" = "all" ]; then
+    final_zip_path="Carthage/Sentry-WithoutUIKitOrAppKit.xcframework.zip"
+    rm -rf "$final_zip_path"
     ./scripts/build-xcframework-variant.sh "Sentry" "-WithoutUIKitOrAppKit" "mh_dylib" "WithoutUIKit" "$sdks"
     ./scripts/compress-xcframework.sh "$signed" Sentry-WithoutUIKitOrAppKit
-    mv Sentry-WithoutUIKitOrAppKit.xcframework.zip Carthage/Sentry-WithoutUIKitOrAppKit.xcframework.zip
-fi
+    mv Sentry-WithoutUIKitOrAppKit.xcframework.zip "$final_zip_path"fi
--- scripts/package-integration-test-local.sh
+++ scripts/package-integration-test-local.sh
@@ -1,33 +1,67 @@
 #!/bin/bash
 
-set -ou pipefail
+set -oux pipefail
 
-PLATFORM_SDK=${1:-all}
-SDK_VARIANT=${2:-all}
+PLATFORM_SDK=${1:-all} # examples: all, ios, macosx, maccatalyst, tvos, watchos, visionos
+DELIVERY_METHOD=${2:-all} # examples: all, cocoapods, spm, xcframework-static, xcframework-dynamic, carthage
 
-# Build the xcframeworks locally
-./scripts/build-xcframework-local.sh | tee build-xcframework.log
-
-# Prepare the shared framework directory
 FRAMEWORK_DIR="Integration/Frameworks"
-rm -rf $FRAMEWORK_DIR
-mkdir -p $FRAMEWORK_DIR
-mv Sentry.xcframework Sentry-Dynamic.xcframework SentrySwiftUI.xcframework $FRAMEWORK_DIR
-mv Carthage/Sentry.xcframework.zip Carthage/Sentry-Dynamic.xcframework.zip Carthage/SentrySwiftUI.xcframework.zip $FRAMEWORK_DIR
 
-# Initialize lists for succeeded and failed tests
+# cocoapods doesn't use the prebuilt xcframeworks
+if [ "$DELIVERY_METHOD" != "cocoapods" ]; then
+  case "$DELIVERY_METHOD" in
+    spm)
+      ./scripts/build-xcframework-local.sh "$PLATFORM_SDK" "dynamic"
+      ./scripts/build-xcframework-local.sh "$PLATFORM_SDK" "swiftui"
+      XCFRAMEWORK_FILENAMES="Sentry-Dynamic.xcframework SentrySwiftUI.xcframework"
+      XCFRAMEWORK_ZIP_FILENAMES="Carthage/Sentry-Dynamic.xcframework.zip Carthage/SentrySwiftUI.xcframework.zip"
+    ;;
+    xcframework-static)
+      ./scripts/build-xcframework-local.sh "$PLATFORM_SDK" "static"
+      XCFRAMEWORK_FILENAMES="Sentry.xcframework"
+      XCFRAMEWORK_ZIP_FILENAMES="Carthage/Sentry.xcframework.zip"
+    ;;
+    xcframework-dynamic)
+      ./scripts/build-xcframework-local.sh "$PLATFORM_SDK" "dynamic"
+      XCFRAMEWORK_FILENAMES="Sentry-Dynamic.xcframework"
+      XCFRAMEWORK_ZIP_FILENAMES="Carthage/Sentry-Dynamic.xcframework.zip"
+    ;;
+    carthage)
+      ./scripts/build-xcframework-local.sh "$PLATFORM_SDK" "static"
+      ./scripts/build-xcframework-local.sh "$PLATFORM_SDK" "swiftui"
+      XCFRAMEWORK_FILENAMES="Sentry.xcframework SentrySwiftUI.xcframework"
+      XCFRAMEWORK_ZIP_FILENAMES="Carthage/Sentry.xcframework.zip Carthage/SentrySwiftUI.xcframework.zip"
+    ;;
+    all)
+      ./scripts/build-xcframework-local.sh
+      XCFRAMEWORK_FILENAMES="Sentry.xcframework SentrySwiftUI.xcframework Sentry-Dynamic.xcframework Sentry-WithoutUIKitOrAppKit.xcframework"
+      XCFRAMEWORK_ZIP_FILENAMES="Carthage/Sentry.xcframework.zip Carthage/SentrySwiftUI.xcframework.zip Sentry-Dynamic.xcframework.zip Sentry-WithoutUIKitOrAppKit.xcframework.zip"
+    ;;
+    *)
+      echo "Invalid package delivery method"
+      exit 1
+    ;;
+  esac
+  
+  "cp -Rf $XCFRAMEWORK_FILENAMES $FRAMEWORK_DIR"
+  "cp -Rf $XCFRAMEWORK_ZIP_FILENAMES $FRAMEWORK_DIR"
+fi
+
 succeeded_tests=()
 failed_tests=()
 
 # Run the package integration tests for all platform and SDK variant combinations
-# todo: add maccatalyst
+# todo: add maccatalyst, it'll need to be special-cased for platform/destination combination
 for platform in ios tvos watchos macos visionos; do
-  # todo: add carthage
-  for delivery_method in cocoapods spm xcframework-static xcframework-dynamic; do
+  for delivery_method in cocoapods spm xcframework-static xcframework-dynamic carthage; do
     if [ "$PLATFORM_SDK" == "all" ] || [ "$PLATFORM_SDK" == "$platform" ]; then
-      if [ "$SDK_VARIANT" == "all" ] || [ "$SDK_VARIANT" == "$delivery_method" ]; then
-        if [ "$SDK_VARIANT" == "coocapods" ] && [ "$PLATFORM_SDK" == "visionos" ]; then
-          # todo: visionos via cocoapods doesn't work due to the issue initially reported in https://github.com/getsentry/sentry-cocoa/issues/3809
+      if [ "$DELIVERY_METHOD" == "all" ] || [ "$DELIVERY_METHOD" == "$delivery_method" ]; then
+        if [ "$DELIVERY_METHOD" == "coocapods" ] && [ "$PLATFORM_SDK" == "visionos" ]; then
+          echo "visionos via cocoapods doesn't work due to the issue initially reported in https://github.com/getsentry/sentry-cocoa/issues/3809"
+          continue
+        fi
+        if [ "$DELIVERY_METHOD" == "carthage" ] && [ "$PLATFORM_SDK" == "visionos" ]; then
+          echo "carthage doesn't support visionos"
           continue
         fi
 
@@ -42,7 +76,6 @@ for platform in ios tvos watchos macos visionos; do
   done
 done
 
-# Display the summary of results
 echo "Summary of the results:"
 echo "Succeeded tests:"
 for test in "${succeeded_tests[@]}"; do
