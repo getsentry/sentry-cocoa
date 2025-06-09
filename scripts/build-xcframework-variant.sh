cf5f536e7c0e26f0677800da1f776eb2630cee7a
Andrew McKnight <andrew.mcknight@sentry.io>
2025-06-09 09:28:28 -0400

    add make init to right job, remove unused workflow file

--- scripts/build-xcframework-variant.sh
+++ scripts/build-xcframework-variant.sh
@@ -7,13 +7,7 @@ suffix="${2:-}"
 MACH_O_TYPE="${3-mh_dylib}"
 configuration_suffix="${4-}"
 sdks_to_build="${5:-allSDKs}" # examples: allSDKs, ios, macosx, maccatalyst, tvos, watchos, visionos
-verbose="${6:-}" # examples: --verbose)
 
-if [ "${verbose:-}" = "--verbose" ]; then
-    set -x
-else
-    set +x
-fi
 echo "--------------------------------"
 echo "Building XCFramework ${scheme}${suffix} for ${sdks_to_build}"
 echo "--------------------------------"
