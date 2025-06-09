ce7f70dc2e42c4e00567381e0425510742f924ab
Andrew McKnight <andrew.mcknight@sentry.io>
2025-06-09 09:49:00 -0400

    pull out the infoplist modification to the end for all sdks

--- scripts/build-xcframework-slice.sh
+++ scripts/build-xcframework-slice.sh
@@ -32,9 +32,10 @@ fi
 slice_id="${scheme}${suffix}-${sdk}"
 
 carthage_xcarchive_path="Carthage/archive/${scheme}${suffix}"
+sentry_xcarchive_path="$carthage_xcarchive_path/${sdk}.xcarchive"
 
 if [ "$sdk" = "maccatalyst" ]; then
-    #Create framework for mac catalyst
+    # we can't use the "archive" action here because it doesn't support the -destination option, which we need to build the maccatalyst slice. so we'll have to build it manually and then copy the build product to an xcarchive directory we create.
     set -o pipefail && NSUnbufferedIO=YES xcodebuild \
         -project Sentry.xcodeproj/ \
         -scheme "$scheme" \
@@ -53,14 +54,9 @@ if [ "$sdk" = "maccatalyst" ]; then
 
     maccatalyst_build_product_directory="Carthage/DerivedData/Build/Products/$resolved_configuration-maccatalyst"
 
-    if [ "$MACH_O_TYPE" = "staticlib" ]; then
-        infoPlist="${maccatalyst_build_product_directory}/${resolved_product_name}/Resources/Info.plist"
-        plutil -replace "MinimumOSVersion" -string "100.0" "$infoPlist"
-    fi
-
-    maccatalyst_archive_directory="${carthage_xcarchive_path}/maccatalyst.xcarchive/Products/Library/Frameworks"
-    mkdir -p "${maccatalyst_archive_directory}"
-    cp -R "${maccatalyst_build_product_directory}/${resolved_product_name}" "${maccatalyst_archive_directory}"
+    maccatalyst_xcarchive_framework_directory="${sentry_xcarchive_path}/Products/Library/Frameworks"
+    mkdir -p "${maccatalyst_xcarchive_framework_directory}"
+    cp -R "${maccatalyst_build_product_directory}/${resolved_product_name}" "${maccatalyst_xcarchive_framework_directory}"
 
     if [ -d "${maccatalyst_build_product_directory}/${resolved_product_name}.dSYM" ]; then
         maccatalyst_archive_dsym_destination="${carthage_xcarchive_path}/maccatalyst.xcarchive/dSYMs"
@@ -68,8 +64,6 @@ if [ "$sdk" = "maccatalyst" ]; then
         cp -R "${maccatalyst_build_product_directory}/${resolved_product_name}.dSYM" "${maccatalyst_archive_dsym_destination}"
     fi
 else
-    sentry_xcarchive_path="$carthage_xcarchive_path/${sdk}.xcarchive"
-
     set -o pipefail && NSUnbufferedIO=YES xcodebuild archive \
         -project Sentry.xcodeproj/ \
         -scheme "$scheme" \
@@ -84,16 +78,16 @@ else
         ENABLE_CODE_COVERAGE=NO \
         GCC_GENERATE_DEBUGGING_SYMBOLS="$GCC_GENERATE_DEBUGGING_SYMBOLS" \
         OTHER_LDFLAGS="$OTHER_LDFLAGS" 2>&1 | tee "${slice_id}.log" | xcbeautify
+fi
 
-    if [ "$MACH_O_TYPE" = "staticlib" ]; then
-        if [ "$sdk" = "macosx" ]; then
-            infoPlistPath="Resources/Info.plist"
-        else
-            infoPlistPath="Info.plist"
-        fi
-        # This workaround is necessary to make Sentry Static framework to work
-        # More information in here: https://github.com/getsentry/sentry-cocoa/issues/3769
-        # The version 100 seems to work with all Xcode up to 15.4
-        plutil -replace "MinimumOSVersion" -string "100.0" "$sentry_xcarchive_path/Products/Library/Frameworks/${resolved_product_name}/$infoPlistPath"
+if [ "$MACH_O_TYPE" = "staticlib" ]; then
+    if [ "$sdk" = "macosx" ] || [ "$sdk" = "maccatalyst" ]; then
+        infoPlistPath="Resources/Info.plist"
+    else
+        infoPlistPath="Info.plist"
     fi
+    # This workaround is necessary to make Sentry Static framework to work
+    # More information in here: https://github.com/getsentry/sentry-cocoa/issues/3769
+    # The version 100 seems to work with all Xcode up to 15.4
+    plutil -replace "MinimumOSVersion" -string "100.0" "$sentry_xcarchive_path/Products/Library/Frameworks/${resolved_product_name}/$infoPlistPath"
 fi
