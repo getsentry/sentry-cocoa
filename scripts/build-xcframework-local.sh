871dc31bbfbe31e3f0722f8ffbb0e7a67944c45a
Andrew McKnight <andrew.mcknight@sentry.io>
2025-06-10 15:20:45 -0400

    fix script rename

--- scripts/build-xcframework-local.sh
+++ scripts/build-xcframework-local.sh
@@ -11,24 +11,24 @@ mkdir Carthage
 
 if [ "$variants" = "DynamicOnly" ] || [ "$variants" = "AllVariants" ]; then
     ./scripts/build-xcframework-variant.sh "Sentry" "-Dynamic" "mh_dylib" "" "$sdks"
-    ./scripts/compress-xcframework.sh.sh "$signed" Sentry-Dynamic
+    ./scripts/zip_built_sdk.sh "$signed" Sentry-Dynamic
     mv Sentry-Dynamic.xcframework.zip Carthage/Sentry-Dynamic.xcframework.zip
 fi
 
 if [ "$variants" = "StaticOnly" ] || [ "$variants" = "AllVariants" ]; then
     ./scripts/build-xcframework-variant.sh "Sentry" "" "staticlib" "" "$sdks"
-    ./scripts/compress-xcframework.sh.sh "$signed" Sentry
+    ./scripts/zip_built_sdk.sh "$signed" Sentry
     mv Sentry.xcframework.zip Carthage/Sentry.xcframework.zip
 fi
 
 if [ "$variants" = "SwiftUIOnly" ] || [ "$variants" = "AllVariants" ]; then
     ./scripts/build-xcframework-variant.sh "SentrySwiftUI" "" "mh_dylib" "" "$sdks"
-    ./scripts/compress-xcframework.sh.sh "$signed" SentrySwiftUI
+    ./scripts/zip_built_sdk.sh "$signed" SentrySwiftUI
     mv SentrySwiftUI.xcframework.zip Carthage/SentrySwiftUI.xcframework.zip
 fi
 
 if [ "$variants" = "WithoutUIKitOnly" ] || [ "$variants" = "AllVariants" ]; then
     ./scripts/build-xcframework-variant.sh "Sentry" "-WithoutUIKitOrAppKit" "mh_dylib" "WithoutUIKit" "$sdks"
-    ./scripts/compress-xcframework.sh.sh "$signed" Sentry-WithoutUIKitOrAppKit
+    ./scripts/zip_built_sdk.sh "$signed" Sentry-WithoutUIKitOrAppKit
     mv Sentry-WithoutUIKitOrAppKit.xcframework.zip Carthage/Sentry-WithoutUIKitOrAppKit.xcframework.zip
 fi
