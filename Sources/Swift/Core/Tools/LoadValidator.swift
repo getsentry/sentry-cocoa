299a80ad033c3c8453fabf55a574d08d1968941e
Itay Brenner <itay.brenner@sentry.io>
2025-06-20 18:32:51 -0300

    Analyze images on image load

--- Sources/Swift/Core/Tools/LoadValidator.swift
+++ Sources/Swift/Core/Tools/LoadValidator.swift
@@ -1,5 +1,8 @@
+@_implementationOnly import _SentryPrivate
+import Darwin
 import Foundation
 import MachO
+@_implementationOnly import Sentry._Hybrid
 
 @objc
 class LoadValidator: NSObject {
@@ -7,53 +10,55 @@ class LoadValidator: NSObject {
     static let targetClassName = "PrivateSentrySDKOnly"
     
     @objc
-    class func validateLoadedOnce() {
-        let imageCount = _dyld_image_count()
-        
-        var imagesWhereSentryIsFound: [String] = [String]()
-        
-        // Iterate over all images to make sure none of them contains a Sentry implementation twice
-        for i in 0..<imageCount {
-            guard let cImageName = _dyld_get_image_name(i) else { continue }
-            
-            // Skip Apple Frameworks
-            let imagePath = String(cString: cImageName)
-            
+    class func validateSDKPresenceIn(_ image: SentryBinaryImageInfo) {
+        DispatchQueue.global(qos: .background).async {
             let systemLibraryPath = "/usr/lib/"
 #if targetEnvironment(simulator)
             let ignoredPath = "/Library/Developer/CoreSimulator/Profiles/Runtimes/"
 #else
             let ignoredPath = "/System/Library/"
 #endif
-            if imagePath.contains(ignoredPath) || imagePath.hasPrefix(systemLibraryPath) {
-                continue
+            let imageName = image.name
+            guard !imageName.contains(ignoredPath) && !imageName.hasPrefix(systemLibraryPath) else {
+                return
             }
             
+            let loadValidatorAddress = self.getCurrentFrameworkTextPointer()
+            let imageAddress = image.address
+            let imageSize = image.size
+            let loadValidatorAddressValue = UInt(bitPattern: loadValidatorAddress)
+            let isCurrentImageContainingLoadValidator = (loadValidatorAddressValue >= imageAddress) && (loadValidatorAddressValue < (imageAddress + imageSize))
+
             var classCount: UInt32 = 0
-            if let classNames = objc_copyClassNamesForImage(cImageName, &classCount) {
-                for j in 0..<Int(classCount) {
-                    let name = String(cString: classNames[j])
-                    if name.contains(self.targetClassName) {
-                        imagesWhereSentryIsFound.append(imagePath)
-                        break
+            imageName.withCString { cImageName in
+                if let classNames = objc_copyClassNamesForImage(cImageName, &classCount) {
+                    for j in 0..<Int(classCount) {
+                        let name = String(cString: classNames[j])
+                        if name.contains(self.targetClassName) {
+                            if name == self.targetClassName && isCurrentImageContainingLoadValidator {
+                                // Skip our implementation of `PrivateSentrySDKOnly`
+                                continue
+                            }
+                            var message = ["❌ Sentry SDK was loaded multiple times in the binary ❌"]
+                            message.append("⚠️ This can cause undefined behavior, crashes, or duplicate reporting.")
+                            message.append("Ensure the SDK is linked only once, found classes in image paths: \(imageName)")
+                            print(message.joined(separator: "\n"))
+                            
+                            break
+                        }
                     }
+                    free(classNames)
                 }
-                free(classNames)
             }
         }
-        
-        if imagesWhereSentryIsFound.count > 1 {
-            var message = ["❌ Sentry SDK was loaded multiple times in the binary ❌"]
-            message.append("⚠️ This can cause undefined behavior, crashes, or duplicate reporting.")
-            message.append("Ensure the SDK is linked only once")
-            imagesWhereSentryIsFound.forEach { path in
-                message.append("  - \(path)")
-            }
-            print(message.joined(separator: "\n"))
-#if DEBUG
-            // Raise a debugger breakpoint
-            raise(SIGTRAP)
-#endif
-        }
+    }
+    
+    /**
+     * Returns a pointer to a function inside the __TEXT segment of the binary containing this class
+     */
+    class func getCurrentFrameworkTextPointer() -> UnsafeRawPointer {
+        let cFunction: @convention(c) () -> Void = { }
+        let c = unsafeBitCast(cFunction, to: UnsafeRawPointer.self)
+        return c
     }
 }
