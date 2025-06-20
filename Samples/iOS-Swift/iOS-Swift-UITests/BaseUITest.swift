4368d8c02ce8ba4f7bacef812693468f6e02ef0c
Andrew McKnight <andrew.mcknight@sentry.io>
2025-06-20 11:41:25 -0800

    do it in BaseUITest.setUp so it only happens once per test case, on the first launch

--- Samples/iOS-Swift/iOS-Swift-UITests/BaseUITest.swift
+++ Samples/iOS-Swift/iOS-Swift-UITests/BaseUITest.swift
@@ -12,6 +12,9 @@ class BaseUITest: XCTestCase {
         continueAfterFailure = false
         XCUIDevice.shared.orientation = .portrait
         app.launchEnvironment["--io.sentry.sdk-environment"] = "ui-tests"
+        app.launchArguments.append(contentsOf: [
+            "--io.sentry.wipe-data"
+        ])
         if automaticallyLaunchAndTerminateApp {
             launchApp()
         }
@@ -29,9 +32,6 @@ extension BaseUITest {
     func newAppSession() -> XCUIApplication {
         let app = XCUIApplication()
         app.launchEnvironment["--io.sentry.ui-test.test-name"] = name
-        app.launchArguments.append(contentsOf: [
-            "--io.sentry.wipe-data"
-        ])
         return app
     }
     
