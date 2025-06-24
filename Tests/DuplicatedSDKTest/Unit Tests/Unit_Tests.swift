523ae1b1214d7dad5ae0d51f61b1e2d32e53de9e
Itay Brenner <itay.brenner@sentry.io>
2025-06-24 15:59:45 -0300

    Update test

--- Tests/DuplicatedSDKTest/Unit Tests/Unit_Tests.swift
+++ Tests/DuplicatedSDKTest/Unit Tests/Unit_Tests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Sentry
 import XCTest
 
@@ -5,10 +6,25 @@ final class TestLog: XCTestCase {
     func testDuplicatedLoadMessageOnSDKInit() throws {
 
         let output = captureStandardOutput {
-          SentrySDK.start(options: Options())
+            let options = Options()
+            options.debug = true
+            options.diagnosticLevel = .warning
+            SentrySDK.start(options: options)
+            
+            // Force loading another library to trigger validation
+            let path = Bundle(for: type(of: self)).bundlePath
+            let fullpath = "\(path)/../../Frameworks/ModuleA.framework/ModuleA"
+            guard _ = dlopen(fullpath, RTLD_NOW) else {
+                let error = String(cString: dlerror())
+                fatalError("Could not open framework: \(error)")
+            }
+            
+            // Unfortunately, there is no other option rather than wait a couple seconds.
+            // If this test becomes flaky, we will disable it
+            sleep(10)
         }
         
-        XCTAssertTrue(output.contains("❌ Sentry SDK was loaded multiple times in the binary ❌"))
+        XCTAssertTrue(output.contains("Sentry SDK was loaded multiple times in the binary"))
     }
   
     private func captureStandardOutput(_ action: () -> Void) -> String {
