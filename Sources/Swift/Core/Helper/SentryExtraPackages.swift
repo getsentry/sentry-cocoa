82ec5ad0dd0afe1dd505e265d6e6c03787ad7a58
Noah Martin <noahm444@gmail.com>
2025-06-24 09:47:25 -0400

    lock

--- Sources/Swift/Core/Helper/SentryExtraPackages.swift
+++ Sources/Swift/Core/Helper/SentryExtraPackages.swift
@@ -2,7 +2,7 @@ import Foundation
 
 @objc @_spi(Private) public final class SentryExtraPackages: NSObject {
     private static var extraPackages = Set<[String: String]>()
-    private static let syncQueue = DispatchQueue(label: "io.sentry.SentryExtraPackages.sync", attributes: .concurrent)
+    private static let lock = NSRecursiveLock()
 
     @objc
     public static func addPackageName(_ name: String?, version: String?) {
@@ -12,28 +12,21 @@ import Foundation
 
         let newPackage = ["name": name, "version": version]
 
-        // Thread-safe write using a barrier block on a concurrent queue.
-        // Ensures no other reads or writes happen during this insertion.
-        syncQueue.async(flags: .barrier) {
+        _ = lock.synchronized {
             extraPackages.insert(newPackage)
         }
     }
 
     @objc
     public static func getPackages() -> NSMutableSet {
-        var copy: Set<[String: String]> = []
-        // Thread-safe synchronous read on a concurrent queue.
-        // Multiple reads can happen concurrently unless a write barrier is active.
-        // Returns a copy to prevent external mutation of internal state.
-        syncQueue.sync {
-            copy = extraPackages
+        lock.synchronized {
+            NSMutableSet(set: extraPackages as NSSet)
         }
-        return NSMutableSet(set: copy as NSSet)
     }
 
     #if SENTRY_TEST || SENTRY_TEST_CI
     static func clear() {
-        syncQueue.async(flags: .barrier) {
+        lock.synchronized {
             extraPackages.removeAll()
         }
     }
