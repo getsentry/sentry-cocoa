9b58a8fd48f669ef33ac0ce196c700b40c842c59
Noah Martin <noahm444@gmail.com>
2025-06-30 13:41:48 -0400

    Remove app extension only flag

--- Samples/iOS-ObjectiveC/iOS-ObjectiveC/AppDelegate.m
+++ Samples/iOS-ObjectiveC/iOS-ObjectiveC/AppDelegate.m
@@ -122,7 +122,6 @@
                     uiForm.useSentryUser = YES;
                 };
                 config.configureTheme = ^(SentryUserFeedbackThemeConfiguration *_Nonnull theme) {
-                    theme.font = [UIFont fontWithName:@"ChalkboardSE-Regular" size:25];
                     theme.outlineStyle =
                         [[SentryFormElementOutlineStyle alloc] initWithColor:UIColor.purpleColor
                                                                 cornerRadius:10
