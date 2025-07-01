548eb4e81c7cb6862966681328c21dbb4e2222e2
Denis Andrasec <denrase@gmail.com>
2025-07-01 13:53:11 +0200

    remove separate codable files

--- Sources/Swift/Protocol/SentryLogAttribute.swift
+++ Sources/Swift/Protocol/SentryLogAttribute.swift
@@ -1,5 +1,5 @@
 extension SentryLog {
-    enum Attribute {
+    enum Attribute: Codable {
         case string(String)
         case boolean(Bool)
         case integer(Int)
@@ -20,6 +20,46 @@ extension SentryLog {
             case .boolean(let value): return value
             case .integer(let value): return value
             case .double(let value): return value
+            }
+        }
+        
+        private enum CodingKeys: String, CodingKey {
+            case value
+            case type
+        }
+        
+        init(from decoder: any Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+            let type = try container.decode(String.self, forKey: .type)
+            switch type {
+            case "string":
+                self = .string(try container.decode(String.self, forKey: .value))
+            case "boolean":
+                self = .boolean(try container.decode(Bool.self, forKey: .value))
+            case "integer":
+                self = .integer(try container.decode(Int.self, forKey: .value))
+            case "double":
+                self = .double(try container.decode(Double.self, forKey: .value))
+            default:
+                throw DecodingError.dataCorruptedError(forKey: .type, in: container, debugDescription: "Unknown type: \(type)")
+            }
+        }
+        
+        func encode(to encoder: any Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            
+            try container.encode(type, forKey: .type)
+            
+            switch self {
+            case .string(let value):
+                try container.encode(value, forKey: .value)
+            case .boolean(let value):
+                try container.encode(value, forKey: .value)
+            case .integer(let value):
+                try container.encode(value, forKey: .value)
+            case .double(let value):
+                try container.encode(value, forKey: .value)
             }
         }
     }
