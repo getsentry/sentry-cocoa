af21cc9b90e617dd60e18e9b9b4888a672f6c0b4
Noah Martin <noahm444@gmail.com>
2025-07-07 12:46:33 -0400

    PR feedback

--- Tests/SentryTests/SentrySDKThreadTests.swift
+++ Tests/SentryTests/SentrySDKThreadTests.swift
@@ -3,31 +3,32 @@ import XCTest
 
 final class SentrySDKThreadTests: XCTestCase {
     func testRaceWhenBindingClient() {
-        for _ in 0..<10_000 {
-            SentrySDK.start(options: .init())
+
+        let options = Options()
+        let sut = SentryHub(client: SentryClient(options: options), andScope: nil)
+
+        for _ in 0..<100 {
+
             let exp = expectation(description: "wait")
-            let warmupExpectation = expectation(description: "warmup")
-            let warmupCount = 100
-            warmupExpectation.expectedFulfillmentCount = warmupCount
+            exp.expectedFulfillmentCount = 100
+
             let queue = DispatchQueue(label: "com.sentry.test_client", qos: .userInteractive, attributes: [.concurrent, .initiallyInactive])
-            for i in 0..<1_000 {
-                exp.expectedFulfillmentCount += 1
+
+            for _ in 0..<100 {
                 queue.async {
-                    SentrySDK.currentHub().capture(event: .init())
+                    sut.bindClient(SentryClient(options: options))
+                    sut.capture(message: "Test message")
                     exp.fulfill()
-                    if i < warmupCount {
-                        warmupExpectation.fulfill()
-                    }
                 }
             }
-            exp.expectedFulfillmentCount -= 1
-            
+
             queue.activate()
-            
-            wait(for: [warmupExpectation])
-            SentrySDK.currentHub().bindClient(nil)
-            
-            wait(for: [exp])
+
+            for _ in 0..<100 {
+                sut.bindClient(nil)
+            }
+
+            wait(for: [exp], timeout: 1.0)
         }
     }
 }
