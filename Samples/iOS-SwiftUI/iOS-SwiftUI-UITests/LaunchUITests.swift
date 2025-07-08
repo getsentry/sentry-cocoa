515e871467f31d6d9b35d9a7480afdaa1ad71421
Andrew McKnight <andrew.mcknight@sentry.io>
2025-07-08 12:00:25 -0800

    disable launch profiling for swiftui sample app tests

--- Samples/iOS-SwiftUI/iOS-SwiftUI-UITests/LaunchUITests.swift
+++ Samples/iOS-SwiftUI/iOS-SwiftUI-UITests/LaunchUITests.swift
@@ -11,16 +11,16 @@ class LaunchUITests: XCTestCase {
     func testTransactionSpan() {
         let app = newAppSession()
         app.safelyLaunch()
-        
+
         let transactionName = app.staticTexts["TRANSACTION_NAME"]
         let transactionId = app.staticTexts["TRANSACTION_ID"]
         if !transactionName.waitForExistence(timeout: 1) {
             XCTFail("Span operation label not found")
         }
-        
+
         let childParentId = app.staticTexts["CHILD_PARENT_SPANID"]
         let childName = app.staticTexts["CHILD_NAME"]
-        
+
         XCTAssertEqual(childName.label, "Child Span")
         XCTAssertEqual(transactionName.label, "Content View Body")
         XCTAssertEqual(childParentId.label, transactionId.label)
@@ -39,34 +39,44 @@ class LaunchUITests: XCTestCase {
         formScreenNavigationBar/*@START_MENU_TOKEN@*/.buttons["Test"]/*[[".otherElements[\"Test\"].buttons[\"Test\"]",".buttons[\"Test\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         XCTAssertEqual(app.staticTexts["SPAN_ID"].label, "NO SPAN")
     }
-    
+
     func testTTID_TTFD() {
         let app = newAppSession()
         app.safelyLaunch()
         app.buttons["Show TTD"].tap()
-        
+
         XCTAssertEqual(app.staticTexts["TTDInfo"].label, "TTID and TTFD found")
     }
-  
-  func newAppSession() -> XCUIApplication {
-      let app = XCUIApplication()
-      app.launchEnvironment["--io.sentry.ui-test.test-name"] = name
-      app.launchArguments.append("--disable-spotlight")
-      return app
-  }
+
+    func newAppSession() -> XCUIApplication {
+        let app = XCUIApplication()
+        app.launchEnvironment["--io.sentry.ui-test.test-name"] = name
+        app.launchArguments.append(contentsOf: [
+            "--disable-spotlight",
+            "--io.sentry.disable-app-start-profiling,"
+        ])
+        return app
+    }
 }
 
 extension XCUIApplication {
-  public func safelyLaunch() {
-    // Calling activate() and then launch() effectively launches the app twice, interfering with
-    // local debugging. Only call activate if there isn't a debugger attached, which is a decent
-    // proxy for whether this is running in CI.
-    if !isDebugging() {
-      // App prewarming can sometimes cause simulators to get stuck in UI tests, activating them
-      // before launching clears any prewarming state.
-      activate()
+    public func safelyLaunch() {
+        // Calling activate() and then launch() effectively launches the app twice, interfering with
+        // local debugging. Only call activate if there isn't a debugger attached, which is a decent
+        // proxy for whether this is running in CI.
+        if !isDebugging() {
+            // activate() appears to drop launch args and environment variables, so save them beforehand and reset them before subsequent calls to launch()
+            let launchArguments = app.launchArguments
+            let launchEnvironment = app.launchEnvironment
+
+            // App prewarming can sometimes cause simulators to get stuck in UI tests, activating them
+            // before launching clears any prewarming state.
+            activate()
+
+            app.launchArguments = launchArguments
+            app.launchEnvironment = launchEnvironment
+        }
+
+        launch()
     }
-    
-    launch()
-  }
 }
