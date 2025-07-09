6ca5be31793a18cab5a90fc1fd9fdf2bfaf3aa65
Denis Andrasec <denrase@gmail.com>
2025-07-09 16:33:58 +0200

    ref correclty

--- Sources/Sentry/include/SentryClient+Logs.h
+++ Sources/Sentry/include/SentryClient+Logs.h
@@ -1,76 +1,8 @@
 #import "SentryClient.h"
-#import "SentryDataCategory.h"
-#import "SentryDiscardReason.h"
-
-@class SentryAttachment;
-@class SentryEnvelope;
-@class SentryEnvelopeItem;
-@class SentryId;
-@class SentryReplayEvent;
-@class SentryReplayRecording;
-@class SentrySession;
-@class SentryThreadInspector;
 
 NS_ASSUME_NONNULL_BEGIN
 
-@protocol SentryClientAttachmentProcessor <NSObject>
-
-- (NSArray<SentryAttachment *> *)processAttachments:(NSArray<SentryAttachment *> *)attachments
-                                           forEvent:(SentryEvent *)event;
-
-@end
-
 @interface SentryClient ()
-
-@property (nonatomic, strong)
-    NSMutableArray<id<SentryClientAttachmentProcessor>> *attachmentProcessors;
-@property (nonatomic, strong) SentryThreadInspector *threadInspector;
-@property (nonatomic, strong) SentryFileManager *fileManager;
-
-- (SentryId *)captureError:(NSError *)error
-                 withScope:(SentryScope *)scope
-    incrementSessionErrors:(SentrySession * (^)(void))sessionBlock;
-
-- (SentryId *)captureException:(NSException *)exception
-                     withScope:(SentryScope *)scope
-        incrementSessionErrors:(SentrySession * (^)(void))sessionBlock;
-
-- (SentryId *)captureFatalEvent:(SentryEvent *)event withScope:(SentryScope *)scope;
-
-- (SentryId *)captureFatalEvent:(SentryEvent *)event
-                    withSession:(SentrySession *)session
-                      withScope:(SentryScope *)scope;
-
-- (void)saveCrashTransaction:(SentryTransaction *)transaction
-                   withScope:(SentryScope *)scope
-    NS_SWIFT_NAME(saveCrashTransaction(transaction:scope:));
-
-- (SentryId *)captureEvent:(SentryEvent *)event
-                  withScope:(SentryScope *)scope
-    additionalEnvelopeItems:(NSArray<SentryEnvelopeItem *> *)additionalEnvelopeItems
-    NS_SWIFT_NAME(capture(event:scope:additionalEnvelopeItems:));
-
-- (void)captureReplayEvent:(SentryReplayEvent *)replayEvent
-           replayRecording:(SentryReplayRecording *)replayRecording
-                     video:(NSURL *)videoURL
-                 withScope:(SentryScope *)scope;
-
-- (void)captureSession:(SentrySession *)session NS_SWIFT_NAME(capture(session:));
-
-/**
- * Needed by hybrid SDKs as react-native to synchronously store an envelope to disk.
- */
-- (void)storeEnvelope:(SentryEnvelope *)envelope;
-
-- (void)captureEnvelope:(SentryEnvelope *)envelope;
-
-- (void)recordLostEvent:(SentryDataCategory)category reason:(SentryDiscardReason)reason;
-- (void)recordLostEvent:(SentryDataCategory)category
-                 reason:(SentryDiscardReason)reason
-               quantity:(NSUInteger)quantity;
-
-- (void)addAttachmentProcessor:(id<SentryClientAttachmentProcessor>)attachmentProcessor;
-- (void)removeAttachmentProcessor:(id<SentryClientAttachmentProcessor>)attachmentProcessor;
 
 /**
  * Helper to capture encoded logs, as SentryEnvelope can't be used in the Swift SDK.
