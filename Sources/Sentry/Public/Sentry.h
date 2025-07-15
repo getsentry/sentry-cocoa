e9286d9041090aef2899f51b09649013468a4774
Itay Brenner <itay.brenner@sentry.io>
2025-07-15 16:02:21 -0300

    Fix build issue

--- Sources/Sentry/Public/Sentry.h
+++ Sources/Sentry/Public/Sentry.h
@@ -9,6 +9,7 @@ FOUNDATION_EXPORT const unsigned char SentryVersionString[];
 
 #    import <Sentry/SentryAttachment.h>
 #    import <Sentry/SentryBaggage.h>
+#    import <Sentry/SentryBinaryImageInfo.h>
 #    import <Sentry/SentryBreadcrumb.h>
 #    import <Sentry/SentryClient.h>
 #    import <Sentry/SentryCrashExceptionApplication.h>
