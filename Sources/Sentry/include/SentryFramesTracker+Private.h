4463d8cb58e34f57bf1254c85292eaeddad2d40c
Itay Brenner <itay.brenner@sentry.io>
2025-07-16 17:44:54 -0300

    feat(ANR): Ignore frame delays when going into background

---
+++ Sources/Sentry/include/SentryFramesTracker+Private.h
@@ -0,0 +1,12 @@
+#if SENTRY_HAS_UIKIT
+
+#    import "SentryFramesTracker.h"
+
+@interface SentryFramesTracker ()
+
+- (void)pause;
+- (void)unpause;
+
+@end
+
+#endif // SENTRY_HAS_UIKIT
--- Sources/Sentry/include/SentryPrivate.h
+++ Sources/Sentry/include/SentryPrivate.h
@@ -19,6 +19,7 @@
 #import "SentryDisplayLinkWrapper.h"
 #import "SentryFileIOTracker.h"
 #import "SentryFileManager.h"
+#import "SentryFramesTracker+Private.h"
 #import "SentryLevelHelper.h"
 #import "SentryMeta.h"
 #import "SentryNSDictionarySanitize.h"
