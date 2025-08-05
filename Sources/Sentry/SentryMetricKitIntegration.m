7b3d68b59534946a0ab299666b842d8481864f02
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-08-05 13:00:51 +0200

    fix: Add null-handling for debug meta

--- Sources/Sentry/SentryMetricKitIntegration.m
+++ Sources/Sentry/SentryMetricKitIntegration.m
@@ -509,7 +509,9 @@ NS_ASSUME_NONNULL_BEGIN
         uint64_t imageAddress = mxFrame.address - mxFrame.offsetIntoBinaryTextSegment;
         debugMeta.imageAddress = sentry_formatHexAddressUInt64(imageAddress);
 
-        debugMetas[debugMeta.debugID] = debugMeta;
+        if (nil != debugMeta.debugID) {
+            debugMetas[SENTRY_UNWRAP_NULLABLE(NSString, debugMeta.debugID)] = debugMeta;
+        }
     }
 
     return [debugMetas allValues];
