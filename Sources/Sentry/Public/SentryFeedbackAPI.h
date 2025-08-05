59a1edab479fdcb2f20d967019277c9460012c61
Itay Brenner <itay.brenner@sentry.io>
2025-08-05 10:07:55 -0300

    Add NS_EXTENSION_UNAVAILABLE to SentryFeedbackAPI methods

--- Sources/Sentry/Public/SentryFeedbackAPI.h
+++ Sources/Sentry/Public/SentryFeedbackAPI.h
@@ -22,7 +22,8 @@ SENTRY_NO_INIT
  * @seealso See @c SentryOptions.configureUserFeedback to configure the widget.
  * @note User feedback widget is only available for iOS 13 or later.
  */
-- (void)showWidget API_AVAILABLE(ios(13.0));
+- (void)showWidget API_AVAILABLE(ios(13.0))
+    NS_EXTENSION_UNAVAILABLE("Sentry User Feedback UI cannot be used from app extensions.");
 
 /**
  * Hide the feedback widget button.
@@ -30,7 +31,8 @@ SENTRY_NO_INIT
  * @seealso See @c SentryOptions.configureUserFeedback to configure the widget.
  * @note User feedback widget is only available for iOS 13 or later.
  */
-- (void)hideWidget API_AVAILABLE(ios(13.0));
+- (void)hideWidget API_AVAILABLE(ios(13.0))
+    NS_EXTENSION_UNAVAILABLE("Sentry User Feedback UI cannot be used from app extensions.");
 
 @end
 
