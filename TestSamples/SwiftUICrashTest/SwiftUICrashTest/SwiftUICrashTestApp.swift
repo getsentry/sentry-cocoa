5b233214e054e05ef09c8893df0edb9bf9e5e5f7
Itay Brenner <itay.brenner@sentry.io>
2025-08-08 11:36:44 -0300

    ci: collect logs on CI for SwiftUI Crash Test

--- TestSamples/SwiftUICrashTest/SwiftUICrashTest/SwiftUICrashTestApp.swift
+++ TestSamples/SwiftUICrashTest/SwiftUICrashTest/SwiftUICrashTestApp.swift
@@ -5,9 +5,12 @@ import SwiftUI
 struct SwiftUICrashTestApp: App {
 
     init() {
+        // Using `NSLog` so it shows up in the logs and `SwiftUICrashTestApp` to easily find it in the logs.
+        NSLog("SwiftUICrashTestApp - app launched")
         SentrySDK.start { options in
             options.dsn = "https://6cc9bae94def43cab8444a99e0031c28@o447951.ingest.sentry.io/5428557"
         }
+        NSLog("SwiftUICrashTestApp - SDK Started")
     }
 
     var body: some Scene {
@@ -16,10 +19,13 @@ struct SwiftUICrashTestApp: App {
                 .onAppear {
                     let userDefaultsKey = "crash-on-launch"
                     if UserDefaults.standard.bool(forKey: userDefaultsKey) {
+                        NSLog("SwiftUICrashTestApp - will crash")
                         UserDefaults.standard.removeObject(forKey: userDefaultsKey)
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                             SentrySDK.crash()
                         }
+                    } else {
+                        NSLog("SwiftUICrashTestApp - will not crash")
                     }
                 }
         }
