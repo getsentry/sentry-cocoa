39ec2a0a946f73fe59f574b3683aa930704eb77c
Itay Brenner <itay.brenner@sentry.io>
2025-08-08 14:37:04 -0300

    ci: Crash test app onAppear instead of init (#5855)

--- TestSamples/SwiftUICrashTest/SwiftUICrashTest/SwiftUICrashTestApp.swift
+++ TestSamples/SwiftUICrashTest/SwiftUICrashTest/SwiftUICrashTestApp.swift
@@ -8,18 +8,20 @@ struct SwiftUICrashTestApp: App {
         SentrySDK.start { options in
             options.dsn = "https://6cc9bae94def43cab8444a99e0031c28@o447951.ingest.sentry.io/5428557"
         }
-
-        let userDefaultsKey = "crash-on-launch"
-        if UserDefaults.standard.bool(forKey: userDefaultsKey) {
-
-            UserDefaults.standard.removeObject(forKey: userDefaultsKey)
-            SentrySDK.crash()
-        }
     }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    let userDefaultsKey = "crash-on-launch"
+                    if UserDefaults.standard.bool(forKey: userDefaultsKey) {
+                        UserDefaults.standard.removeObject(forKey: userDefaultsKey)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            SentrySDK.crash()
+                        }
+                    }
+                }
         }
     }
 }
