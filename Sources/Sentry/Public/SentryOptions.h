eb1fc611b069f1608572f5d55704179826355415
Noah Martin <noahm444@gmail.com>
2025-08-27 08:24:56 -1000

    feat(V9): Make performance V2 the default in V9

--- Sources/Sentry/Public/SentryOptions.h
+++ Sources/Sentry/Public/SentryOptions.h
@@ -288,6 +288,7 @@ NS_SWIFT_NAME(Options)
  */
 @property (nonatomic, assign) BOOL enableAutoPerformanceTracing;
 
+#if !SDK_V9
 /**
  * We're working to update our Performance product offering in order to be able to provide better
  * insights and highlight specific actions you can take to improve your mobile app's overall
@@ -296,6 +297,7 @@ NS_SWIFT_NAME(Options)
  * UIWindowDidBecomeVisibleNotification. This change will be the default in the next major version.
  */
 @property (nonatomic, assign) BOOL enablePerformanceV2;
+#endif // !SDK_V9
 
 /**
  * @warning This is an experimental feature and may still have bugs.
