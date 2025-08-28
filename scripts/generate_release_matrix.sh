43c05848d3c4df5dea6257cbe08ec576fedd4e9f
Itay Brenner <itay.brenner@sentry.io>
2025-08-28 11:44:05 -0300

    Use JSON and jq to make script easier to read

--- scripts/generate_release_matrix.sh
+++ scripts/generate_release_matrix.sh
@@ -1,85 +1,57 @@
 #!/bin/bash
 
 # This script is used to generate the matrix combinations for the release workflow.
+# Rewritten to use jq as much as possible for maintainability.
 
 set -euo pipefail
 
 # Slices and Variants only needed on PRs
-BASE_SLICES=(
-    '{"name": "Sentry", "macho-type": "mh_dylib", "suffix": "-Dynamic", "id": "sentry-dynamic"}'
-    '{"name": "Sentry", "macho-type": "staticlib", "id": "sentry-static"}'
-    '{"name": "SentrySwiftUI", "macho-type": "mh_dylib", "id": "sentry-swiftui"}'
-    '{"name": "Sentry", "macho-type": "mh_dylib", "suffix": "-WithoutUIKitOrAppKit", "configuration-suffix": "WithoutUIKit", "id": "sentry-withoutuikit-dynamic"}'
-)
-BASE_VARIANTS=(
-    '{"scheme": "Sentry", "macho-type": "mh_dylib", "suffix": "-Dynamic", "id": "sentry-dynamic", "excluded-archs": "arm64e"}'
-    '{"scheme": "Sentry", "macho-type": "staticlib", "id": "sentry-static"}'
-    '{"scheme": "SentrySwiftUI", "macho-type": "mh_dylib", "id": "sentry-swiftui"}'
-    '{"scheme": "Sentry", "macho-type": "mh_dylib", "suffix": "-WithoutUIKitOrAppKit", "configuration-suffix": "WithoutUIKit", "id": "sentry-withoutuikit-dynamic", "excluded-archs": "arm64e"}'
-)
-BASE_SDKS=(
-    '"iphoneos"'
-    '"iphonesimulator"'
-    '"macosx"'
-    '"maccatalyst"'
-    '"appletvos"'
-    '"watchos"'
-    '"xros"'
-)
+BASE_SLICES_JSON='[
+  {"name": "Sentry", "macho-type": "mh_dylib", "suffix": "-Dynamic", "id": "sentry-dynamic"},
+  {"name": "Sentry", "macho-type": "staticlib", "id": "sentry-static"},
+  {"name": "SentrySwiftUI", "macho-type": "mh_dylib", "id": "sentry-swiftui"},
+  {"name": "Sentry", "macho-type": "mh_dylib", "suffix": "-WithoutUIKitOrAppKit", "configuration-suffix": "WithoutUIKit", "id": "sentry-withoutuikit-dynamic"}
+]'
+BASE_VARIANTS_JSON='[
+  {"scheme": "Sentry", "macho-type": "mh_dylib", "suffix": "-Dynamic", "id": "sentry-dynamic", "excluded-archs": "arm64e"},
+  {"scheme": "Sentry", "macho-type": "staticlib", "id": "sentry-static"},
+  {"scheme": "SentrySwiftUI", "macho-type": "mh_dylib", "id": "sentry-swiftui"},
+  {"scheme": "Sentry", "macho-type": "mh_dylib", "suffix": "-WithoutUIKitOrAppKit", "configuration-suffix": "WithoutUIKit", "id": "sentry-withoutuikit-dynamic", "excluded-archs": "arm64e"}
+]'
+BASE_SDKS_JSON='[
+  "iphoneos",
+  "iphonesimulator",
+  "macosx",
+  "maccatalyst",
+  "appletvos",
+  "watchos",
+  "xros"
+]'
 
 # Slices and Variants only needed on main or release
-ADDITIONAL_VARIANTS=(
-    '{"scheme": "Sentry", "macho-type": "mh_dylib", "suffix": "-Dynamic", "id": "sentry-dynamic", "override-name": "Sentry-Dynamic-WithARM64e"}'
-    '{"scheme": "Sentry", "macho-type": "mh_dylib", "suffix": "-WithoutUIKitOrAppKit", "configuration-suffix": "WithoutUIKit", "id": "sentry-withoutuikit-dynamic", "override-name": "Sentry-WithoutUIKitOrAppKit-WithARM64e"}'
-)
-ADDITIONAL_SDKS=(
-    '"appletvsimulator"'
-    '"watchsimulator"'
-    '"xrsimulator"'
-)
-
-build_json_array() {
-    local array_name=$1
-    local result="["
-    local first=true
-    
-    # Get the array elements using indirect expansion
-    local array_values
-    eval "array_values=(\"\${${array_name}[@]}\")"
-    
-    for item in "${array_values[@]}"; do
-        if [ "$first" = true ]; then
-            first=false
-        else
-            result+=","
-        fi
-        result+="$item"
-    done
-    
-    result+="]"
-    echo "$result"
-}
+ADDITIONAL_VARIANTS_JSON='[
+  {"scheme": "Sentry", "macho-type": "mh_dylib", "suffix": "-Dynamic", "id": "sentry-dynamic", "override-name": "Sentry-Dynamic-WithARM64e"},
+  {"scheme": "Sentry", "macho-type": "mh_dylib", "suffix": "-WithoutUIKitOrAppKit", "configuration-suffix": "WithoutUIKit", "id": "sentry-withoutuikit-dynamic", "override-name": "Sentry-WithoutUIKitOrAppKit-WithARM64e"}
+]'
+ADDITIONAL_SDKS_JSON='[
+  "appletvsimulator",
+  "watchsimulator",
+  "xrsimulator"
+]'
 
 if [ "$EVENT_NAME" = "pull_request" ]; then
-    SLICES_COMBINATIONS=$(build_json_array BASE_SLICES)
-    VARIANTS_COMBINATIONS=$(build_json_array BASE_VARIANTS)
-    SDK_LIST=$(build_json_array BASE_SDKS)
+    SLICES_COMBINATIONS="$BASE_SLICES_JSON"
+    VARIANTS_COMBINATIONS="$BASE_VARIANTS_JSON"
+    SDK_LIST="$BASE_SDKS_JSON"
 else
-    # shellcheck disable=SC2034
-    ALL_SLICES=("${BASE_SLICES[@]}")
-    # shellcheck disable=SC2034
-    ALL_VARIANTS=("${BASE_VARIANTS[@]}" "${ADDITIONAL_VARIANTS[@]}")
-    # shellcheck disable=SC2034
-    ALL_SDKS=("${BASE_SDKS[@]}" "${ADDITIONAL_SDKS[@]}")
-    
-    SLICES_COMBINATIONS=$(build_json_array ALL_SLICES)
-    VARIANTS_COMBINATIONS=$(build_json_array ALL_VARIANTS)
-    SDK_LIST=$(build_json_array ALL_SDKS)
+    SLICES_COMBINATIONS="$BASE_SLICES_JSON"
+    VARIANTS_COMBINATIONS=$(jq -c -s '.[0] + .[1]' <(echo "$BASE_VARIANTS_JSON") <(echo "$ADDITIONAL_VARIANTS_JSON"))
+    SDK_LIST=$(jq -c -s '.[0] + .[1]' <(echo "$BASE_SDKS_JSON") <(echo "$ADDITIONAL_SDKS_JSON"))
 fi
 
 {
-  echo "slices=$SLICES_COMBINATIONS"
-  echo "variants=$VARIANTS_COMBINATIONS"
-  echo "sdk-list-array=$SDK_LIST"
+  echo "slices=$(echo "$SLICES_COMBINATIONS" | jq -c '.')"
+  echo "variants=$(echo "$VARIANTS_COMBINATIONS" | jq -c '.')"
+  echo "sdk-list-array=$(echo "$SDK_LIST" | jq -c '.')"
   echo "sdk-list-string=$(echo "$SDK_LIST" | jq -r 'join(",")')"
-} >> "$GITHUB_OUTPUT"
+}  >> "$GITHUB_OUTPUT"
