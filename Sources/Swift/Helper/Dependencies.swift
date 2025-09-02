3cbf585b840e476a80f7b9873fc21d374ec1a606
Itay Brenner <itay.brenner@sentry.io>
2025-09-02 19:20:40 -0300

    Add Swift precompiler directives for uidevice wrapper

--- Sources/Swift/Helper/Dependencies.swift
+++ Sources/Swift/Helper/Dependencies.swift
@@ -1,5 +1,7 @@
 @objc(SentryDependencies) @_spi(Private) public final class Dependencies: NSObject {
     @objc public static let dispatchQueueWrapper = SentryDispatchQueueWrapper()
     @objc public static let dateProvider = SentryDefaultCurrentDateProvider()
+#if !os(watchOS) && !os(macOS) && !SENTRY_NO_UIKIT
     public static let uiDeviceWrapper = SentryDefaultUIDeviceWrapper(queueWrapper: Dependencies.dispatchQueueWrapper)
+#endif // !os(watchOS) && !os(macOS) && !SENTRY_NO_UIKIT
 }
