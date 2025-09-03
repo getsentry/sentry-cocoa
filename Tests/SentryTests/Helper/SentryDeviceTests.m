a91adfd66cc1b208ee4c65f1ac99f39a049903f3
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-09-03 16:25:18 +0200

    adapted implementation based on feedback

--- Tests/SentryTests/Helper/SentryDeviceTests.m
+++ Tests/SentryTests/Helper/SentryDeviceTests.m
@@ -91,11 +91,11 @@
 #if TARGET_OS_OSX
     SENTRY_ASSERT_PREFIX(osVersion, @"10.", @"11.", @"12.", @"13.", @"14.", @"15.", @"26.");
 #elif TARGET_OS_IOS || TARGET_OS_MACCATALYST || TARGET_OS_TV
-    SENTRY_ASSERT_PREFIX(
-        osVersion, @"9.", @"10.", @"11.", @"12.", @"13.", @"14.", @"15.", @"16.", @"17.", @"18.", @"26.");
+    SENTRY_ASSERT_PREFIX(osVersion, @"9.", @"10.", @"11.", @"12.", @"13.", @"14.", @"15.", @"16.",
+        @"17.", @"18.", @"26.");
 #elif TARGET_OS_WATCH
     // TODO: create a watch UI test target to test this branch
-    SENTRY_ASSERT_PREFIX(osVersion, @"2.", @"3.", @"4.", @"5.", @"6.", @"7.", @"8.", @"9.", @"10.", @"26.");
+    SENTRY_ASSERT_PREFIX(osVersion, @"2.", @"3.", @"4.", @"5.", @"6.", @"7.", @"8.", @"9.", @"26.");
 #elif TARGET_OS_VISION
     SENTRY_ASSERT_PREFIX(osVersion, @"1.", @"2.", @"26.");
 #else
@@ -164,96 +164,6 @@
 #else
     XCTFail(@"Unexpected target OS");
 #endif
-}
-
-- (void)testOSVersion26Support
-{
-    // Test that version 26.x is properly supported for all platforms
-    NSString *mockVersion26 = @"26.0.1";
-    
-#if TARGET_OS_OSX
-    // Test macOS 26 support
-    BOOL macOSSupported = [mockVersion26 hasPrefix:@"26."];
-    XCTAssertTrue(macOSSupported, @"macOS 26.x should be supported");
-#elif TARGET_OS_IOS || TARGET_OS_MACCATALYST || TARGET_OS_TV
-    // Test iOS/Mac Catalyst/tvOS 26 support
-    BOOL iOSSupported = [mockVersion26 hasPrefix:@"26."];
-    XCTAssertTrue(iOSSupported, @"iOS/Mac Catalyst/tvOS 26.x should be supported");
-#elif TARGET_OS_WATCH
-    // Test watchOS 26 support
-    BOOL watchOSSupported = [mockVersion26 hasPrefix:@"26."];
-    XCTAssertTrue(watchOSSupported, @"watchOS 26.x should be supported");
-#elif TARGET_OS_VISION
-    // Test visionOS 26 support
-    BOOL visionOSSupported = [mockVersion26 hasPrefix:@"26."];
-    XCTAssertTrue(visionOSSupported, @"visionOS 26.x should be supported");
-#endif
-}
-
-- (void)testOSVersionRange
-{
-    // Test that the version range logic works correctly for edge cases
-    NSArray<NSString *> *testVersions = @[@"18.9.9", @"26.0", @"26.1.2", @"26.99.99"];
-    
-    for (NSString *version in testVersions) {
-#if TARGET_OS_OSX
-        NSArray<NSString *> *macOSVersions = @[@"10.", @"11.", @"12.", @"13.", @"14.", @"15.", @"26."];
-#elif TARGET_OS_IOS || TARGET_OS_MACCATALYST || TARGET_OS_TV
-        NSArray<NSString *> *iOSVersions = @[@"9.", @"10.", @"11.", @"12.", @"13.", @"14.", @"15.", @"16.", @"17.", @"18.", @"26."];
-#elif TARGET_OS_WATCH
-        NSArray<NSString *> *watchOSVersions = @[@"2.", @"3.", @"4.", @"5.", @"6.", @"7.", @"8.", @"9.", @"10.", @"26."];
-#elif TARGET_OS_VISION
-        NSArray<NSString *> *visionOSVersions = @[@"1.", @"2.", @"26."];
-#endif
-        
-        BOOL foundMatch = NO;
-#if TARGET_OS_OSX
-        for (NSString *prefix in macOSVersions) {
-#elif TARGET_OS_IOS || TARGET_OS_MACCATALYST || TARGET_OS_TV
-        for (NSString *prefix in iOSVersions) {
-#elif TARGET_OS_WATCH
-        for (NSString *prefix in watchOSVersions) {
-#elif TARGET_OS_VISION
-        for (NSString *prefix in visionOSVersions) {
-#endif
-            if ([version hasPrefix:prefix]) {
-                foundMatch = YES;
-                break;
-            }
-        }
-        
-        if ([version hasPrefix:@"18."] || [version hasPrefix:@"26."]) {
-            XCTAssertTrue(foundMatch, @"Version %@ should be supported", version);
-        }
-    }
-}
-
-- (void)testUnsupportedOSVersionsAreRejected
-{
-    // Test that non-existent OS versions (19-25) would be rejected
-    NSArray<NSString *> *unsupportedVersions = @[@"19.0", @"20.1", @"21.2", @"22.3", @"23.4", @"24.5", @"25.6"];
-    
-    for (NSString *version in unsupportedVersions) {
-#if TARGET_OS_OSX
-        NSArray<NSString *> *supportedVersions = @[@"10.", @"11.", @"12.", @"13.", @"14.", @"15.", @"26."];
-#elif TARGET_OS_IOS || TARGET_OS_MACCATALYST || TARGET_OS_TV
-        NSArray<NSString *> *supportedVersions = @[@"9.", @"10.", @"11.", @"12.", @"13.", @"14.", @"15.", @"16.", @"17.", @"18.", @"26."];
-#elif TARGET_OS_WATCH
-        NSArray<NSString *> *supportedVersions = @[@"2.", @"3.", @"4.", @"5.", @"6.", @"7.", @"8.", @"9.", @"10.", @"26."];
-#elif TARGET_OS_VISION
-        NSArray<NSString *> *supportedVersions = @[@"1.", @"2.", @"26."];
-#endif
-        
-        BOOL foundMatch = NO;
-        for (NSString *prefix in supportedVersions) {
-            if ([version hasPrefix:prefix]) {
-                foundMatch = YES;
-                break;
-            }
-        }
-        
-        XCTAssertFalse(foundMatch, @"Unsupported version %@ should be rejected", version);
-    }
 }
 
 - (void)testOSBuildNumber
