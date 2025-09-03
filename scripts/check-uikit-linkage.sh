588dd7cf59b5b9ca76568fb4837dd288a6c9aff6
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-09-03 07:47:24 -0700

    chore(ci): Replace latest simulator OS with deterministic versions (#6053)

--- scripts/check-uikit-linkage.sh
+++ scripts/check-uikit-linkage.sh
@@ -4,34 +4,71 @@
 
 set -eou pipefail
 
+# Disable SC1091 because it won't work with pre-commit
+# shellcheck source=./scripts/ci-utils.sh disable=SC1091
+source "$(cd "$(dirname "$0")" && pwd)/ci-utils.sh"
+
 CONFIGURATION="${1}"
 DERIVED_DATA_PATH="${2}"
 LINKAGE_TEST="${3}"
 MODULE_NAME="${4}"
 
+log_notice "Checking UIKit linkage for:"
+log_notice " - Configuration:     $CONFIGURATION"
+log_notice " - Derived Data Path: $DERIVED_DATA_PATH"
+log_notice " - Linkage Test:      $LINKAGE_TEST"
+log_notice " - Module Name:       $MODULE_NAME"
+
+# Define the path to the Sentry build product.
 SENTRY_BUILD_PRODUCT_PATH="$DERIVED_DATA_PATH/Build/Products/$CONFIGURATION-iphonesimulator/$MODULE_NAME.framework/$MODULE_NAME"
+log_notice "Checking build product path: $SENTRY_BUILD_PRODUCT_PATH"
 
-stat "$SENTRY_BUILD_PRODUCT_PATH"
+if [ ! -f "$SENTRY_BUILD_PRODUCT_PATH" ]; then
+    log_error "Sentry build product not found at path: $SENTRY_BUILD_PRODUCT_PATH"
+    exit 1
+fi
 
-MATCHES=$(otool -L "$SENTRY_BUILD_PRODUCT_PATH" | grep -c -e "UIKit.framework/UIKit" -e "libswiftUIKit.dylib" ||:)
+# For some frameworks, the binary in the framework root is a symlink to the binary in the Versions directory.
+# We need to check resolve the symlink and check the binary in the Versions directory.
+if [ -L "$SENTRY_BUILD_PRODUCT_PATH" ]; then
+    log_notice "Sentry build product is a symlink, resolving it."
+    SENTRY_BUILD_PRODUCT_PATH=$(readlink -f "$SENTRY_BUILD_PRODUCT_PATH")
+    if [ ! -f "$SENTRY_BUILD_PRODUCT_PATH" ]; then
+        log_error "Sentry build product not found at path: $SENTRY_BUILD_PRODUCT_PATH"
+        exit 1
+    fi
+    log_notice "Resolved Sentry build product path: $SENTRY_BUILD_PRODUCT_PATH"
+fi
 
+# Check if the binary is linked to UIKit.
+log_notice "Checking if Sentry build product is linked to UIKit."
+OTOOL_OUTPUT=$(otool -L "$SENTRY_BUILD_PRODUCT_PATH")
+begin_group "OTool output"
+echo "$OTOOL_OUTPUT"
+end_group
+
+MATCHES=$(echo "$OTOOL_OUTPUT" | grep -c -e "UIKit.framework/UIKit" -e "libswiftUIKit.dylib" ||:)
+log_notice "Matches: $MATCHES"
+
+# Check the linkage.
 case "$LINKAGE_TEST" in
 "linked")
     if [ "$MATCHES" == 0 ]; then
-        echo "UIKit.framework linkage not found."
+        log_error "UIKit.framework linkage not found, but expected linkage."
         exit 1
     fi
-    echo "Success! UIKit.framework linked."
+    log_notice "Success! UIKit.framework linked."
     ;;
 "unlinked")
+    log_notice "Checking if Sentry build product is not linked to UIKit."
     if [ "$MATCHES" != 0 ]; then
-        echo "UIKit.framework linkage found."
+        log_error "UIKit.framework linkage found, but expected no linkage."
         exit 1
     fi
-    echo "Success! UIKit.framework not linked."
+    log_notice "Success! UIKit.framework not linked."
     ;;
 *)
-    echo "Provide an argument for either 'linked' or 'unlinked' UIKit check."
+    log_error "Provide an argument for either 'linked' or 'unlinked' UIKit check."
     exit 1
     ;;
 esac
