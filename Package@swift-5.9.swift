c32b7790bdf8f313024f9632d0bffcaaa4e14bf8
Itay Brenner <itay.brenner@sentry.io>
2025-09-04 09:30:59 -0300

    Remove swift settings

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -39,9 +39,6 @@ var targets: [Target] = [
         dependencies: ["Sentry", "SentryInternal"],
         path: "Sources/SentrySwiftUI",
         exclude: ["SentryInternal/", "module.modulemap"],
-        swiftSettings: [
-            .interoperabilityMode(.Cxx, .when(platforms: [.visionOS]))
-        ],
         linkerSettings: [
             .linkedFramework("Sentry")
         ]),
