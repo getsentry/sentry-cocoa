e33a1ec90d9e3914403f28af7d1a0fcf42581baa
Philipp Hofmann <philipp.hofmann@sentry.io>
2025-09-04 09:48:16 +0200

    ci: Workaround for GH actions to recache runtimes
    List the available simulators as  a workaround for GH actions to recache
    and detect missing runtimes.

--- scripts/ci-select-xcode.sh
+++ scripts/ci-select-xcode.sh
@@ -7,9 +7,24 @@
 
 set -euo pipefail
 
+# Disable SC1091 because it won't work with pre-commit
+# shellcheck source=./scripts/ci-utils.sh disable=SC1091
+source "$(cd "$(dirname "$0")" && pwd)/ci-utils.sh"
+
 XCODE_VERSION="${1}"
 
 # We prefer this over calling `sudo xcode-select` because it will fail if the Xcode version
 # is not installed. Also xcodes is preinstalled on the GH runners.
 xcodes select "$XCODE_VERSION"
 swiftc --version
+
+
+begin_group "List Available Simulators"
+
+# On GH actions this command should cause the GH actions to recache and detect missing runtimes, as pointed out in
+# https://github.com/actions/runner-images/issues/12948#issuecomment-3248563014
+# The command is fast and should avoids problems we had with GH actions not having specific simulators installed 
+# see https://github.com/getsentry/sentry-cocoa/pull/6053.
+
+xcrun simctl list
+end_group
