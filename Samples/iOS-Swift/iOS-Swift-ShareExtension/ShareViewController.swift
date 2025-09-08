17b84ab248b325613157ce316a49b0d3184e3ce6
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-09-08 10:20:05 +0200

    fix setup and add sentry sdk start

--- Samples/iOS-Swift/iOS-Swift-ShareExtension/ShareViewController.swift
+++ Samples/iOS-Swift/iOS-Swift-ShareExtension/ShareViewController.swift
@@ -1,8 +1,23 @@
 import Sentry
+import SentrySampleShared
 import Social
 import UIKit
 
 class ShareViewController: SLComposeServiceViewController {
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupSentry()
+    }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setupSentry()
+    }
+
+    private func setupSentry() {
+        SentrySDKWrapper.shared.startSentry()
+    }
 
     override func isContentValid() -> Bool {
         return true
