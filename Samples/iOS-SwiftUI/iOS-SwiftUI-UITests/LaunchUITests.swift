be6cc0e2a7f9d00e94751d1ca4fc01bf40ff3c58
Noah Martin <noahm444@gmail.com>
2025-09-25 06:08:17 -0700

    Updates

--- Samples/iOS-SwiftUI/iOS-SwiftUI-UITests/LaunchUITests.swift
+++ Samples/iOS-SwiftUI/iOS-SwiftUI-UITests/LaunchUITests.swift
@@ -1,3 +1,4 @@
+import Sentry
 import SentrySampleShared
 import SentrySampleUITestShared
 import XCTest
@@ -49,11 +50,13 @@ class LaunchUITests: XCTestCase {
         XCTAssertEqual(app.staticTexts["TTDInfo"].label, "TTID and TTFD found")
     }
   
-  func testSendsError() {
+  func testCaptureErrorReturnsValidId() {
     let app = newAppSession()
     app.safelyLaunch()
     app.buttons["Capture Error"].tap()
-    XCTAssertFalse(app.staticTexts["errorId"].label.isEmpty)
+    let errorId = app.staticTexts["errorId"].label
+    let sentryErrorId = SentryId(uuidString: errorId)
+    XCTAssertNotEqual(sentryErrorId.sentryIdString, SentryId.empty.sentryIdString, "The id should not be empty, since empty is used to indicate the capture did not work.")
   }
 
     func newAppSession() -> XCUIApplication {
