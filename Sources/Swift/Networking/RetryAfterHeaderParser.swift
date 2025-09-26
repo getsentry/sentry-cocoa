079113dc94515fdea49c705a3254e239033b8a89
Itay Brenner <itay.brenner@sentry.io>
2025-09-26 16:24:30 +0200

    Fallback to NSString.integerValue

--- Sources/Swift/Networking/RetryAfterHeaderParser.swift
+++ Sources/Swift/Networking/RetryAfterHeaderParser.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-/** Parses value of HTTP header "Retry-After" which in most cases is sent in
- combination with HTTP status 429 Too Many Requests. For more details see:
- https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.37
-*/
+/// Parses value of HTTP header "Retry-After" which in most cases is sent in
+/// combination with HTTP status 429 Too Many Requests.
+///
+/// For more details see: https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.37
 @objc(SentryRetryAfterHeaderParser) @_spi(Private)
 public final class RetryAfterHeaderParser: NSObject {
 
@@ -17,21 +17,18 @@ public final class RetryAfterHeaderParser: NSObject {
         super.init()
     }
 
-    /** Parses the HTTP header into a NSDate.
-
-     @param retryAfterHeader The header value.
-
-     @return NSDate representation of Retry-After. If the date can't be parsed nil
-     is returned.
-    */
+    /// Parses the HTTP header into a NSDate.
+    /// - Parameter retryAfterHeader: The header value.
+    /// - Returns: NSDate representation of Retry-After. If the date can't be parsed nil is returned.
     @objc
     public func parse(_ retryAfterHeader: String?) -> Date? {
         guard let retryAfterHeader = retryAfterHeader, !retryAfterHeader.isEmpty else {
             return nil
         }
 
-        // Parse it as Double first, because otherwise it will fail to parse as an integer if it contains a decimal
-        let retryAfterSeconds = Int(Double(retryAfterHeader) ?? 0.0)
+        // Casting to NSString allows us to obtain an integer value even if the string is invalid, contains decimals
+        // or random text
+        let retryAfterSeconds = NSString(string: retryAfterHeader).integerValue
         if retryAfterSeconds != 0 {
             return currentDateProvider.date().addingTimeInterval(TimeInterval(retryAfterSeconds))
         }
