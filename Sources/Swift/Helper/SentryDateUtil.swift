d452da64f81649253e5718f999ca3a718c11be9d
Itay Brenner <itay.brenner@sentry.io>
2025-09-30 15:00:58 -0300

    Go back to use Int since has the same behavior than long on ObjC

--- Sources/Swift/Helper/SentryDateUtil.swift
+++ Sources/Swift/Helper/SentryDateUtil.swift
@@ -22,7 +22,7 @@ import Foundation
     }
 
     @objc(getMaximumDate:andOther:)
-    public static func getMaximumDate(_ first: Date?, andOther second: Date?) -> Date? {
+    public func getMaximumDate(_ first: Date?, andOther second: Date?) -> Date? {
         guard let first = first else {
             return second
         }
@@ -34,7 +34,7 @@ import Foundation
     }
 
     @objc
-    public static func millisecondsSince1970(_ date: Date) -> Int64 {
-        return Int64(date.timeIntervalSince1970 * 1_000)
+    public func millisecondsSince1970(_ date: Date) -> Int {
+        return Int(date.timeIntervalSince1970 * 1_000)
     }
 }
