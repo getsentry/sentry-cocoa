f747c9c3d998c17a3a6d9154d8179c85fff2b268
Noah Martin <noahm444@gmail.com>
2025-10-14 16:21:45 -0400

    ref: Cleanup SentryUIViewControllerPerformanceTracker interface (#6417)

--- Sources/SentrySwiftUI/SentryInternal/SentryInternal.h
+++ Sources/SentrySwiftUI/SentryInternal/SentryInternal.h
@@ -72,10 +72,16 @@ typedef NS_ENUM(NSUInteger, SentrySpanStatus);
 
 - (nullable id<SentrySpan>)getSpan:(SentrySpanId *)spanId;
 
+- (BOOL)hasSpan:(SentrySpanId *)spanId;
+
 - (BOOL)pushActiveSpan:(SentrySpanId *)spanId;
 
 - (void)popActiveSpan;
 
+@end
+
+@protocol SentryInitialDisplayReporting
+- (void)reportInitialDisplay;
 @end
 
 @interface SentryTimeToDisplayTracker : NSObject
@@ -104,13 +110,22 @@ typedef NS_ENUM(NSUInteger, SentrySpanStatus);
 
 @end
 
+@interface SentrySwiftUISpanHelper : NSObject
+
+@property (nonatomic, readonly) BOOL hasSpan;
+
+@property (nonatomic, strong, readonly, nullable) id<SentryInitialDisplayReporting>
+    initialDisplayReporting;
+
+@end
+
 @interface SentryUIViewControllerPerformanceTracker : NSObject
 
 - (void)reportFullyDisplayed;
 
-- (nullable SentryTimeToDisplayTracker *)startTimeToDisplayTrackerForScreen:(NSString *)screenName
-                                                         waitForFullDisplay:(BOOL)waitForFullDisplay
-                                                                     tracer:(SentryTracer *)tracer;
+- (SentrySwiftUISpanHelper *)startTimeToDisplayTrackerForScreen:(NSString *)screenName
+                                             waitForFullDisplay:(BOOL)waitforFullDisplay
+                                                  transactionId:(SentrySpanId *)transactionId;
 
 @end
 
