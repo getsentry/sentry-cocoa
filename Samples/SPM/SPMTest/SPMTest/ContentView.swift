786d2c11af0a6bfc2dd65fa5c12dda13c33214c8
Noah Martin <noahm444@gmail.com>
2025-10-15 21:20:26 -0400

    feat: Add SPM sample

---
+++ Samples/SPM/SPMTest/SPMTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SPMTest
+//
+//  Created by Noah Martin on 10/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
