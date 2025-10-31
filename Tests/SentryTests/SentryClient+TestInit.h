22df6f35d157b44d23a8e55039b5a3794de4eb86
Noah Martin <noahm444@gmail.com>
2025-10-31 16:14:39 -0400

    ref: Refactor SentryClient init

--- Tests/SentryTests/SentryClient+TestInit.h
+++ Tests/SentryTests/SentryClient+TestInit.h
@@ -13,24 +13,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SentryClient ()
 
-- (_Nullable instancetype)initWithOptions:(SentryOptions *)options
-                             dateProvider:(id<SentryCurrentDateProvider>)dateProvider
-                            dispatchQueue:(SentryDispatchQueueWrapper *)dispatchQueue
-                   deleteOldEnvelopeItems:(BOOL)deleteOldEnvelopeItems;
-
-- (_Nullable instancetype)initWithOptions:(SentryOptions *)options
-                              fileManager:(SentryFileManager *)fileManager
-                   deleteOldEnvelopeItems:(BOOL)deleteOldEnvelopeItems;
-
-- (instancetype)initWithOptions:(SentryOptions *)options
-                    fileManager:(SentryFileManager *)fileManager
-         deleteOldEnvelopeItems:(BOOL)deleteOldEnvelopeItems
-               transportAdapter:(SentryTransportAdapter *)transportAdapter;
-
 - (instancetype)initWithOptions:(SentryOptions *)options
                transportAdapter:(SentryTransportAdapter *)transportAdapter
                     fileManager:(SentryFileManager *)fileManager
-         deleteOldEnvelopeItems:(BOOL)deleteOldEnvelopeItems
                 threadInspector:(SentryDefaultThreadInspector *)threadInspector
              debugImageProvider:(SentryDebugImageProvider *)debugImageProvider
                          random:(id<SentryRandomProtocol>)random
