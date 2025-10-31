0280704877cbb9320984b51c8e03f14ac002d1ea
Itay Brenner <itay.brenner@sentry.io>
2025-10-31 14:37:30 -0300

    chore: Fix broken tests on CI (#6621)

--- Tests/SentryTests/SentryHubTests.swift
+++ Tests/SentryTests/SentryHubTests.swift
@@ -1565,7 +1565,6 @@ class SentryHubTests: XCTestCase {
         init() {
             super.init(
                 replayOptions: SentryReplayOptions(sessionSampleRate: 0, onErrorSampleRate: 0),
-                experimentalOptions: SentryExperimentalOptions(),
                 replayFolderPath: FileManager.default.temporaryDirectory,
                 screenshotProvider: MockScreenshotProvider(),
                 replayMaker: MockReplayMaker(),
@@ -1573,8 +1572,7 @@ class SentryHubTests: XCTestCase {
                 touchTracker: nil,
                 dateProvider: TestCurrentDateProvider(),
                 delegate: MockReplayDelegate(),
-                displayLinkWrapper: TestDisplayLinkWrapper(),
-                environmentChecker: TestSessionReplayEnvironmentChecker(mockedIsReliableReturnValue: true)
+                displayLinkWrapper: TestDisplayLinkWrapper()
             )
         }
     }
