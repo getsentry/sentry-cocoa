29c5899d0e629cb66fd4cd773bfeace159845dc3
Noah Martin <noahm444@gmail.com>
2025-11-13 11:22:54 -0800

    Revert "Update package swift file"
    This reverts commit 2181b05abda0faabd6a0c34cb215b1b9fbe8e472.

--- Package.swift
+++ Package.swift
@@ -9,18 +9,17 @@ import MSVCRT
 
 import PackageDescription
 
-let products: [Product] = [
+var products: [Product] = [
     .library(name: "Sentry", targets: ["Sentry", "SentryCppHelper"]),
     .library(name: "Sentry-Dynamic", targets: ["Sentry-Dynamic"]),
     .library(name: "Sentry-Dynamic-WithARM64e", targets: ["Sentry-Dynamic-WithARM64e"]),
     .library(name: "Sentry-WithoutUIKitOrAppKit", targets: ["Sentry-WithoutUIKitOrAppKit", "SentryCppHelper"]),
     .library(name: "Sentry-WithoutUIKitOrAppKit-WithARM64e", targets: ["Sentry-WithoutUIKitOrAppKit-WithARM64e", "SentryCppHelper"]),
     .library(name: "SentrySwiftUI", targets: ["Sentry", "SentrySwiftUI", "SentryCppHelper"]),
-    .library(name: "SentryDistribution", targets: ["SentryDistribution"]),
-    .library(name: "SentrySPM", type: .dynamic, targets: ["SentryObjc"])
+    .library(name: "SentryDistribution", targets: ["SentryDistribution"])
 ]
 
-let targets: [Target] = [
+var targets: [Target] = [
     .binaryTarget(
         name: "Sentry",
         url: "https://github.com/getsentry/sentry-cocoa/releases/download/9.0.0-alpha.0/Sentry.xcframework.zip",
@@ -72,43 +71,47 @@ let targets: [Target] = [
         ]
     ),
     .target(name: "SentryDistribution", path: "Sources/SentryDistribution"),
-    .testTarget(name: "SentryDistributionTests", dependencies: ["SentryDistribution"], path: "Sources/SentryDistributionTests"),
-    // At least one source file is required, therefore we use a dummy class to satisfy the SPM build system
-    .target(
-        name: "SentryHeaders",
-        path: "Sources/Sentry",
-        sources: ["SentryDummyPublicEmptyClass.m"],
-        publicHeadersPath: "Public"
-    ),
-    .target(
-        name: "_SentryPrivate",
-        dependencies: ["SentryHeaders"],
-        path: "Sources/Sentry",
-        sources: ["SentryDummyPrivateEmptyClass.m"],
-        publicHeadersPath: "include",
-        cSettings: [.headerSearchPath("include/HybridPublic")]),
-    .target(
-        name: "SentrySwift",
-        dependencies: ["_SentryPrivate", "SentryHeaders"],
-        path: "Sources/Swift",
-        swiftSettings: [
-            .unsafeFlags(["-enable-library-evolution"])
-        ]),
-    .target(
-        name: "SentryObjc",
-        dependencies: ["SentrySwift"],
-        path: "Sources",
-        exclude: ["Sentry/SentryDummyPublicEmptyClass.m", "Sentry/SentryDummyPrivateEmptyClass.m", "Swift", "SentrySwiftUI", "Resources", "Configuration", "SentryCppHelper", "SentryDistribution", "SentryDistributionTests"],
-        cSettings: [
-            .headerSearchPath("Sentry/include/HybridPublic"),
-            .headerSearchPath("Sentry"),
-            .headerSearchPath("SentryCrash/Recording"),
-            .headerSearchPath("SentryCrash/Recording/Monitors"),
-            .headerSearchPath("SentryCrash/Recording/Tools"),
-            .headerSearchPath("SentryCrash/Installations"),
-            .headerSearchPath("SentryCrash/Reporting/Filters"),
-            .headerSearchPath("SentryCrash/Reporting/Filters/Tools")])
+    .testTarget(name: "SentryDistributionTests", dependencies: ["SentryDistribution"], path: "Sources/SentryDistributionTests")
 ]
+
+    products.append(.library(name: "SentrySPM", type: .dynamic, targets: ["SentryObjc"]))
+    targets.append(contentsOf: [
+        // At least one source file is required, therefore we use a dummy class to satisfy the SPM build system
+        .target(
+            name: "SentryHeaders",
+            path: "Sources/Sentry", 
+            sources: ["SentryDummyPublicEmptyClass.m"],
+            publicHeadersPath: "Public"
+        ),
+        .target(
+            name: "_SentryPrivate",
+            dependencies: ["SentryHeaders"],
+            path: "Sources/Sentry",
+            sources: ["SentryDummyPrivateEmptyClass.m"],
+            publicHeadersPath: "include",
+            cSettings: [.headerSearchPath("include/HybridPublic")]),
+        .target(
+            name: "SentrySwift",
+            dependencies: ["_SentryPrivate", "SentryHeaders"],
+            path: "Sources/Swift",
+            swiftSettings: [
+                .unsafeFlags(["-enable-library-evolution"])
+            ]),
+        .target(
+            name: "SentryObjc",
+            dependencies: ["SentrySwift"],
+            path: "Sources",
+            exclude: ["Sentry/SentryDummyPublicEmptyClass.m", "Sentry/SentryDummyPrivateEmptyClass.m", "Swift", "SentrySwiftUI", "Resources", "Configuration", "SentryCppHelper", "SentryDistribution", "SentryDistributionTests"],
+            cSettings: [
+                .headerSearchPath("Sentry/include/HybridPublic"),
+                .headerSearchPath("Sentry"),
+                .headerSearchPath("SentryCrash/Recording"),
+                .headerSearchPath("SentryCrash/Recording/Monitors"),
+                .headerSearchPath("SentryCrash/Recording/Tools"),
+                .headerSearchPath("SentryCrash/Installations"),
+                .headerSearchPath("SentryCrash/Reporting/Filters"),
+                .headerSearchPath("SentryCrash/Reporting/Filters/Tools")])
+    ])
 
 let package = Package(
     name: "Sentry",
