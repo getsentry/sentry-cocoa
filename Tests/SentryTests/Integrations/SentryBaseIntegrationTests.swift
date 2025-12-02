25d49069f75d69c1181b95d0016b67fb93666ef7
Itay Brenner <itay.brenner@sentry.io>
2025-12-02 17:06:10 -0300

    Use `enableWatchdogTerminationTracking`

--- Tests/SentryTests/Integrations/SentryBaseIntegrationTests.swift
+++ Tests/SentryTests/Integrations/SentryBaseIntegrationTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class MyTestIntegration: SentryBaseIntegration {
     override func integrationOptions() -> SentryIntegrationOption {
-        return .integrationOptionEnableMetricKit
+        return .integrationOptionEnableWatchdogTerminationTracking
     }
 }
 
@@ -43,9 +43,9 @@ class SentryBaseIntegrationTests: XCTestCase {
     func testInstall_FailingIntegrationOption() {
         let sut = MyTestIntegration()
         let options = Options()
-        options.enableMetricKit = false
+        options.enableWatchdogTerminationTracking = false
         let result = sut.install(with: options)
         XCTAssertFalse(result)
-        XCTAssertFalse(logOutput.loggedMessages.filter({ $0.contains("Not going to enable SentryTests.MyTestIntegration because enableMetricKit is disabled.") }).isEmpty)
+        XCTAssertFalse(logOutput.loggedMessages.filter({ $0.contains("Not going to enable SentryTests.MyTestIntegration because enableWatchdogTerminationTracking is disabled.") }).isEmpty)
     }
 }
