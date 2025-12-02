e5b8dc7b9b49b0fa531081739a73d82cc96eaddc
Itay Brenner <itay.brenner@sentry.io>
2025-12-02 16:44:07 -0300

    fix: Disable AutoSessionTracking for System Extensions

--- Sources/Swift/Integrations/Session/SentryAutoSessionTrackingIntegration.swift
+++ Sources/Swift/Integrations/Session/SentryAutoSessionTrackingIntegration.swift
@@ -1,16 +1,22 @@
 @_implementationOnly import _SentryPrivate
 
-protocol SessionTrackerProvider {
+protocol AutoSessionTrackingProvider {
     func getSessionTracker(with options: Options) -> SessionTracker
+    var processInfoWrapper: SentryProcessInfoSource { get }
 }
 
-final class SentryAutoSessionTrackingIntegration<Dependencies: SessionTrackerProvider>: NSObject, SwiftIntegration {
+final class SentryAutoSessionTrackingIntegration<Dependencies: AutoSessionTrackingProvider>: NSObject, SwiftIntegration {
     let tracker: SessionTracker
     
     init?(with options: Options, dependencies: Dependencies) {
         guard options.enableAutoSessionTracking else { 
             SentrySDKLog.debug("Not going to enable \(Self.name) because enableAutoSessionTracking is disabled.")
             return nil 
+        }
+        
+        guard !dependencies.processInfoWrapper.processDirectoryPath.hasSuffix(".systemextension") else {
+            SentrySDKLog.debug("Not going to enable \(Self.name) because it is not supported on system extensions.")
+            return nil
         }
 
         tracker = dependencies.getSessionTracker(with: options)
--- Tests/SentryTests/Integrations/Session/SentryAutoSessionTrackingIntegrationTests.swift
+++ Tests/SentryTests/Integrations/Session/SentryAutoSessionTrackingIntegrationTests.swift
@@ -1,16 +1,49 @@
 @_spi(Private) @testable import Sentry
-import SentryTestUtils
+@_spi(Private) import SentryTestUtils
 import XCTest
 
 class SentryAutoSessionTrackingIntegrationTests: XCTestCase {
+    
+    override func tearDown() {
+        clearTestState()
+    }
 
     func test_AutoSessionTracking_Disabled() {
+        let logOutput = TestLogOutput()
+        SentrySDKLog.setLogOutput(logOutput)
+        SentrySDKLog.configureLog(true, diagnosticLevel: .debug)
+        
         let options = Options()
         options.enableAutoSessionTracking = false
         
         let sut = SentryAutoSessionTrackingIntegration(with: options, dependencies: SentryDependencyContainer.sharedInstance())
         
         XCTAssertNil(sut)
+        let logMessages = logOutput.loggedMessages.filter {
+            $0.contains("Not going to enable SentryAutoSessionTrackingIntegration because enableAutoSessionTracking is disabled.")
+        }
+        XCTAssertEqual(logMessages.count, 1)
+    }
+    
+    func test_AutoSessionTracking_DisabledOnSystemExtension() {
+        let logOutput = TestLogOutput()
+        SentrySDKLog.setLogOutput(logOutput)
+        SentrySDKLog.configureLog(true, diagnosticLevel: .debug)
+        
+        let options = Options()
+        options.enableAutoSessionTracking = true
+        
+        let processInfoWrapper = MockSentryProcessInfo()
+        processInfoWrapper.overrides.processDirectoryPath = "randomPath/myApp.systemextension"
+        SentryDependencyContainer.sharedInstance().processInfoWrapper = processInfoWrapper
+        
+        let sut = SentryAutoSessionTrackingIntegration(with: options, dependencies: SentryDependencyContainer.sharedInstance())
+        
+        XCTAssertNil(sut)
+        let logMessages = logOutput.loggedMessages.filter {
+            $0.contains("Not going to enable SentryAutoSessionTrackingIntegration because it is not supported on system extensions.")
+        }
+        XCTAssertEqual(logMessages.count, 1)
     }
     
     func test_AutoSessionTracking_Enabled() {
