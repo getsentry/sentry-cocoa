70ad24589aacbb92288c1c204b42e214ca1ee8bd
Noah Martin <noahm444@gmail.com>
2025-12-05 09:43:41 +0100

    Testing

--- Tests/SentryTests/Integrations/MetricKit/SentryMXCallStackTreeTests.swift
+++ Tests/SentryTests/Integrations/MetricKit/SentryMXCallStackTreeTests.swift
@@ -15,14 +15,26 @@ final class SentryMXCallStackTreeTests: XCTestCase {
         let contents = try contentsOfResource("MetricKitCallstacks/per-thread")
         let callStackTree = try SentryMXCallStackTree.from(data: contents)
         
-        try assertCallStackTree(callStackTree, callStackCount: 2)
+        XCTAssertEqual(false, callStackTree.callStackPerThread)
+        XCTAssertEqual(2, callStackTree.callStacks.count)
+        try assertCallStackTree(callStackTree)
+        
+        let debugMeta = callStackTree.toDebugMeta()
+        let image = try XCTUnwrap(debugMeta.first { $0.debugID == "9E8D8DE6-EEC1-3199-8720-9ED68EE3F967" })
+        XCTAssertEqual(414_732, image.imageAddress)
     }
     
     func testDecodeCallStackTree_NotPerThread() throws {
         let contents = try contentsOfResource("MetricKitCallstacks/not-per-thread")
         let callStackTree = try SentryMXCallStackTree.from(data: contents)
         
-        try assertCallStackTree(callStackTree, perThread: false, samplesAmount: 14, threadAttributed: nil, subFrameCount: [2, 4, 0])
+        XCTAssertFalse(callStackTree.callStackPerThread)
+        let firstSamples = callStackTree.callStacks[0].callStackRootFrames[0].toSamples()
+        let secondSamples = callStackTree.callStacks[0].callStackRootFrames[1].toSamples()
+        
+        XCTAssertEqual(7, firstSamples.count)
+        XCTAssertEqual(1, firstSamples[0].count)
+        XCTAssertEqual(2, secondSamples.count)
     }
     
     func testDecodeCallStackTree_UnknownFieldsPayload() throws {
@@ -48,43 +60,39 @@ final class SentryMXCallStackTreeTests: XCTestCase {
         XCTAssertThrowsError(try SentryMXCallStackTree.from(data: contents))
     }
     
-    private func assertCallStackTree(_ callStackTree: SentryMXCallStackTree, perThread: Bool = true, callStackCount: Int = 1, samplesAmount: Int = 3, threadAttributed: Bool? = true, subFrameCount: [Int] = [1, 1, 0]) throws {
-        
-        assert(subFrameCount.count == 3, "subFrameCount must contain 3 elements.")
-        
-        XCTAssertNotNil(callStackTree)
-        XCTAssertEqual(perThread, callStackTree.callStackPerThread)
-        
-        XCTAssertEqual(callStackCount, callStackTree.callStacks.count)
-        
+    private func assertCallStackTree(_ callStackTree: SentryMXCallStackTree) throws {
+
         let callStack = try XCTUnwrap(callStackTree.callStacks.first)
-        XCTAssertEqual(threadAttributed, callStack.threadAttributed)
+        XCTAssertEqual(true, callStack.threadAttributed)
         
-        XCTAssertEqual(samplesAmount, callStack.callStackRootFrames[0].toSamples().count)
+        for mxFrame in callStack.callStackRootFrames {
+            XCTAssertEqual(1, mxFrame.toSamples().count)
+            XCTAssertEqual(1, mxFrame.toSamples()[0].count)
+        }
         
-        let firstFrame = try XCTUnwrap(callStack.callStackRootFrames.first)
-        XCTAssertEqual(UUID(uuidString: "9E8D8DE6-EEC1-3199-8720-9ED68EE3F967"), firstFrame.binaryUUID)
-        XCTAssertEqual(414_732, firstFrame.offsetIntoBinaryTextSegment)
-        XCTAssertEqual(1, firstFrame.sampleCount)
-        XCTAssertEqual("Sentry", firstFrame.binaryName)
-        XCTAssertEqual(4_312_798_220, firstFrame.address)
-        XCTAssertEqual(try XCTUnwrap(subFrameCount.first), firstFrame.subFrames?.count)
-        
-        let secondFrame = try XCTUnwrap(try XCTUnwrap(callStack.callStackRootFrames.element(at: 1)))
-        XCTAssertEqual(UUID(uuidString: "CA12CAFA-91BA-3E1C-BE9C-E34DB96FE7DF"), secondFrame.binaryUUID)
-        XCTAssertEqual(46_380, secondFrame.offsetIntoBinaryTextSegment)
-        XCTAssertEqual(1, secondFrame.sampleCount)
-        XCTAssertEqual("iOS-Swift", secondFrame.binaryName)
-        XCTAssertEqual(4_310_988_076, secondFrame.address)
-        XCTAssertEqual(try XCTUnwrap(subFrameCount.element(at: 1)), secondFrame.subFrames?.count)
-        
-        let thirdFrame = try XCTUnwrap(try XCTUnwrap(callStack.callStackRootFrames.element(at: 2)))
-        XCTAssertEqual(UUID(uuidString: "CA12CAFA-91BA-3E1C-BE9C-E34DB96FE7DF"), thirdFrame.binaryUUID)
-        XCTAssertEqual(46_370, thirdFrame.offsetIntoBinaryTextSegment)
-        XCTAssertEqual(1, thirdFrame.sampleCount)
-        XCTAssertEqual("iOS-Swift", thirdFrame.binaryName)
-        XCTAssertEqual(4_310_988_026, thirdFrame.address)
-        XCTAssertEqual(try XCTUnwrap(subFrameCount.element(at: 2)), thirdFrame.subFrames?.count ?? 0)
+//        let firstFrame = try XCTUnwrap(callStack.callStackRootFrames.first)
+//        XCTAssertEqual(UUID(uuidString: "9E8D8DE6-EEC1-3199-8720-9ED68EE3F967"), firstFrame.binaryUUID)
+//        XCTAssertEqual(414_732, firstFrame.offsetIntoBinaryTextSegment)
+//        XCTAssertEqual(1, firstFrame.sampleCount)
+//        XCTAssertEqual("Sentry", firstFrame.binaryName)
+//        XCTAssertEqual(4_312_798_220, firstFrame.address)
+//        XCTAssertEqual(try XCTUnwrap(subFrameCount.first), firstFrame.subFrames?.count)
+//        
+//        let secondFrame = try XCTUnwrap(try XCTUnwrap(callStack.callStackRootFrames.element(at: 1)))
+//        XCTAssertEqual(UUID(uuidString: "CA12CAFA-91BA-3E1C-BE9C-E34DB96FE7DF"), secondFrame.binaryUUID)
+//        XCTAssertEqual(46_380, secondFrame.offsetIntoBinaryTextSegment)
+//        XCTAssertEqual(1, secondFrame.sampleCount)
+//        XCTAssertEqual("iOS-Swift", secondFrame.binaryName)
+//        XCTAssertEqual(4_310_988_076, secondFrame.address)
+//        XCTAssertEqual(try XCTUnwrap(subFrameCount.element(at: 1)), secondFrame.subFrames?.count)
+//        
+//        let thirdFrame = try XCTUnwrap(try XCTUnwrap(callStack.callStackRootFrames.element(at: 2)))
+//        XCTAssertEqual(UUID(uuidString: "CA12CAFA-91BA-3E1C-BE9C-E34DB96FE7DF"), thirdFrame.binaryUUID)
+//        XCTAssertEqual(46_370, thirdFrame.offsetIntoBinaryTextSegment)
+//        XCTAssertEqual(1, thirdFrame.sampleCount)
+//        XCTAssertEqual("iOS-Swift", thirdFrame.binaryName)
+//        XCTAssertEqual(4_310_988_026, thirdFrame.address)
+//        XCTAssertEqual(try XCTUnwrap(subFrameCount.element(at: 2)), thirdFrame.subFrames?.count ?? 0)
     }
 }
 
