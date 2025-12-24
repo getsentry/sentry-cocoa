39337c09a0bf5a01242df0072a72cbff31240481
Itay Brenner <itay.brenner@sentry.io>
2025-12-24 14:58:54 -0300

    Remove header import

--- Sources/Sentry/include/SentryPrivate.h
+++ Sources/Sentry/include/SentryPrivate.h
@@ -33,7 +33,6 @@
 
 // Headers that also import SentryDefines should be at the end of this list
 // otherwise it wont compile
-#import "PrivateSentrySDKOnly.h"
 #import "SentryANRTrackerV1.h"
 #import "SentryANRTrackerV2.h"
 #import "SentryAsyncLog.h"
