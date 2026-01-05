e47ad564f9ead06321eb2b195575cc5eac3675f4
Itay Brenner <itay.brenner@sentry.io>
2026-01-05 15:59:07 -0300

    ref: Migrate SentryWatchdogTerminationTrackingIntegration to Swift
    Migrate the Integration class from Objective-C to Swift, building on top of the
    previously migrated Logic and Tracker Swift classes. The Swift Integration uses
    a protocol-based dependency injection pattern for better testability.
    
    Changes:
    - Add SentryWatchdogTerminationTrackingIntegration.swift with generics
    - Remove old Objective-C Integration implementation
    - Update Integrations.swift to register the Swift integration
    - Update SentrySDKInternal.m to remove ObjC registration
    - Update Integration tests to work with new Swift implementation

---
+++ Sources/Swift/Integrations/WatchdogTerminations/SentryWatchdogTerminationTrackingIntegration.swift
@@ -0,0 +1,119 @@
+@_implementationOnly import _SentryPrivate
+import Foundation
+
+#if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+
+protocol WatchdogTerminationTrackingProvider {
+    var processInfoWrapper: SentryProcessInfoSource { get }
+    var dispatchFactory: SentryDispatchFactory { get }
+    var fileManager: SentryFileManager? { get }
+    var appStateManager: SentryAppStateManager { get }
+    var crashWrapper: SentryCrashWrapper { get }
+    var scopePersistentStore: SentryScopePersistentStore? { get }
+    func getANRTracker(_ interval: TimeInterval) -> SentryANRTracker
+    func getWatchdogTerminationScopeObserverWithOptions(_ options: Options) -> SentryScopeObserver
+}
+
+extension SentryDependencyContainer: WatchdogTerminationTrackingProvider {}
+
+final class SentryWatchdogTerminationTrackingIntegration<Dependencies: WatchdogTerminationTrackingProvider>: NSObject, SwiftIntegration, SentryANRTrackerDelegate {
+
+    private let tracker: SentryWatchdogTerminationTracker
+    private let anrTracker: SentryANRTracker
+    private let appStateManager: SentryAppStateManager
+
+    init?(with options: Options, dependencies: Dependencies) {
+        guard options.enableWatchdogTerminationTracking else {
+            SentrySDKLog.debug("Not going to enable \(Self.name) because enableWatchdogTerminationTracking is disabled.")
+            return nil
+        }
+
+        guard options.enableCrashHandler else {
+            SentrySDKLog.debug("Not going to enable \(Self.name) because enableCrashHandler is disabled.")
+            return nil
+        }
+
+        guard dependencies.processInfoWrapper.environment["XCTestConfigurationFilePath"] == nil else {
+            SentrySDKLog.debug("Not going to enable \(Self.name) because XCTestConfigurationFilePath is set.")
+            return nil
+        }
+
+        let dispatchQueueWrapper = dependencies.dispatchFactory.createUtilityQueue("io.sentry.watchdog-termination-tracker", relativePriority: 0)
+
+        guard let fileManager = dependencies.fileManager else {
+            SentrySDKLog.fatal("File manager is not available")
+            return nil
+        }
+
+        guard let scopeStore = dependencies.scopePersistentStore else {
+            SentrySDKLog.fatal("Scope persistent store is not available")
+            return nil
+        }
+
+        let appStateManager = dependencies.appStateManager
+        let crashWrapper = dependencies.crashWrapper
+
+        let logic = SentryWatchdogTerminationLogic(options: options, crashAdapter: crashWrapper, appStateManager: appStateManager)
+
+        tracker = SentryWatchdogTerminationTracker(
+            options: options,
+            watchdogTerminationLogic: logic,
+            appStateManager: appStateManager,
+            dispatchQueueWrapper: dispatchQueueWrapper,
+            fileManager: fileManager,
+            scopePersistentStore: scopeStore)
+
+        anrTracker = dependencies.getANRTracker(options.appHangTimeoutInterval)
+        self.appStateManager = appStateManager
+
+        super.init()
+
+        tracker.start()
+        anrTracker.add(listener: self)
+
+        let scopeObserver = dependencies.getWatchdogTerminationScopeObserverWithOptions(options)
+
+        SentrySDKInternal.currentHub().configureScope { outerScope in
+            // Add the observer to the scope so that it can be notified when the scope changes.
+            outerScope.add(scopeObserver)
+
+            // Sync the current context to the observer to capture context modifications that happened
+            // before installation.
+            scopeObserver.setContext(outerScope.contextDictionary as? [String: [String: Any]])
+            scopeObserver.setUser(outerScope.userObject)
+            scopeObserver.setEnvironment(outerScope.environmentString)
+            scopeObserver.setDist(outerScope.distString)
+            scopeObserver.setTags(outerScope.tags)
+            scopeObserver.setExtras(outerScope.extraDictionary as? [String: Any])
+            scopeObserver.setFingerprint(outerScope.fingerprintArray as? [String])
+            // We intentionally skip calling `setTraceContext:` since traces are not stored for watchdog
+            // termination events
+            // We intentionally skip calling `setLevel:` since all termination events have fatal level
+        }
+    }
+
+    static var name: String {
+        "SentryWatchdogTerminationTrackingIntegration"
+    }
+
+    func uninstall() {
+        tracker.stop()
+        anrTracker.remove(listener: self)
+    }
+
+    // MARK: - SentryANRTrackerDelegate
+
+    func anrDetected(type: SentryANRType) {
+        appStateManager.updateAppState { appState in
+            appState.isANROngoing = true
+        }
+    }
+
+    func anrStopped(result: SentryANRStoppedResult?) {
+        appStateManager.updateAppState { appState in
+            appState.isANROngoing = false
+        }
+    }
+}
+
+#endif // os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
