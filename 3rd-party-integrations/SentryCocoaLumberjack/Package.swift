56aa1cc60c4165de653ee3d22ece93ebb4b18e85
Denis Andrasec <denrase@gmail.com>
2026-01-19 16:40:24 +0100

    bump to corect current v

--- 3rd-party-integrations/SentryCocoaLumberjack/Package.swift
+++ 3rd-party-integrations/SentryCocoaLumberjack/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack", from: "3.8.0"),
-        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "9.0.0")
+        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "9.1.0")
     ],
     targets: [
         .target(
