17b7e882351f0cd107cba749db4509b7b0623704
Denis Andrašec <denrase@gmail.com>
2026-01-19 16:17:42 +0100

    Log Integrations: Add `SentryPulse` Integration (#7018)

--- Utils/VersionBump/main.swift
+++ Utils/VersionBump/main.swift
@@ -41,7 +41,8 @@ let files = [
     "./Package.swift",
     "./SentrySwiftUI.podspec",
     "./Sources/Sentry/SentryMeta.m",
-    "./3rd-party-integrations/SentrySwiftLog/Package.swift"
+    "./3rd-party-integrations/SentrySwiftLog/Package.swift",
+    "./3rd-party-integrations/SentryPulse/Package.swift"
 ]
 
 // Files that only accept the format x.x.x in order to release an app using the framework.
