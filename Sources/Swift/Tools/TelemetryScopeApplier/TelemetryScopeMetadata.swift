6f230703beddb4e7636f43df4377fb89f81c6f30
Philipp Hofmann <philipp.hofmann@sentry.io>
2026-01-30 09:33:05 +0100

    more moving around

--- Sources/Swift/Tools/TelemetryScopeApplier/TelemetryScopeMetadata.swift
+++ Sources/Swift/Tools/TelemetryScopeApplier/TelemetryScopeMetadata.swift
@@ -34,27 +34,4 @@ struct SentryDefaultScopeApplyingMetadata: TelemetryScopeMetadata {
     }
 }
 
-@_spi(Private)
-@objc
-public protocol SentryLogScopeApplier {
-    func applyScope(_ scope: Scope, toLog log: SentryLog) -> SentryLog
-}
-
-@_spi(Private)
-@objc
-@objcMembers
-public class SentryDefaultLogScopeApplier: NSObject, SentryLogScopeApplier {
-    private let metadata: TelemetryScopeMetadata
-
-    @objc public init(environment: String, releaseName: String?, cacheDirectoryPath: String, sendDefaultPii: Bool) {
-        self.metadata = SentryDefaultScopeApplyingMetadata(environment: environment, releaseName: releaseName, cacheDirectoryPath: cacheDirectoryPath, sendDefaultPii: sendDefaultPii)
-    }
-
-    @objc public func applyScope(_ scope: Scope, toLog log: SentryLog) -> SentryLog {
-        var mutableLog = log
-        scope.addAttributesToItem(&mutableLog, metadata: metadata)
-        return mutableLog
-    }
-}
-
 // swiftlint:enable missing_docs
