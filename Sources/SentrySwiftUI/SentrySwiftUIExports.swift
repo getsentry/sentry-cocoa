34f277023b79ff7e4542ccf6523f527e371d913d
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2026-02-10 12:25:47 +0100

    fixes

---
+++ Sources/SentrySwiftUI/SentrySwiftUIExports.swift
@@ -0,0 +1,7 @@
+#if canImport(SwiftUI) && (os(iOS) || os(tvOS)) && !SENTRY_NO_UI_FRAMEWORK
+
+// Re-export Sentry's SwiftUI types so that `import SentrySwiftUI` provides the same API
+// as `import Sentry`. No duplicate declarations - avoids ambiguity when both are imported.
+@_exported import Sentry
+
+#endif
