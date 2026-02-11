8a7ae8c0290c83fa0253c5bf8eaf88e49150b6d3
itaybre <860036+itaybre@users.noreply.github.com>
2026-02-11 17:13:45 +0000

    release: 9.4.1

--- 3rd-party-integrations/SentryCocoaLumberjack/Package.swift
+++ 3rd-party-integrations/SentryCocoaLumberjack/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack", from: "3.8.0"),
-        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "9.4.0")
+        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "9.4.1")
     ],
     targets: [
         .target(
