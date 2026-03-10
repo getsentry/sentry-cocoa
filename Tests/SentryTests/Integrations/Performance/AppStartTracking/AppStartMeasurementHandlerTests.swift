27995063bf703b592cb4025e2f2fabfc8a981e97
Philipp Hofmann <philipp.hofmann@sentry.io>
2026-03-10 10:53:25 +0100

    test: Add integration tests for standalone app start

--- Tests/SentryTests/Integrations/Performance/AppStartTracking/AppStartMeasurementHandlerTests.swift
+++ Tests/SentryTests/Integrations/Performance/AppStartTracking/AppStartMeasurementHandlerTests.swift
@@ -109,6 +109,81 @@ class AppStartMeasurementHandlerTests: XCTestCase {
         XCTAssertNil(SentrySDKInternal.getAppStartMeasurement())
     }
 
+    // MARK: - SendStandaloneAppStartTransaction Integration Tests
+
+    private func setUpIntegrationHub() -> TestHub {
+        let dateProvider = TestCurrentDateProvider()
+        SentryDependencyContainer.sharedInstance().dateProvider = dateProvider
+
+        let debugImageProvider = TestDebugImageProvider()
+        debugImageProvider.debugImages = [TestData.debugImage]
+        SentryDependencyContainer.sharedInstance().debugImageProvider = debugImageProvider
+
+        let displayLinkWrapper = TestDisplayLinkWrapper(dateProvider: dateProvider)
+        SentryDependencyContainer.sharedInstance().framesTracker.setDisplayLinkWrapper(displayLinkWrapper)
+        SentryDependencyContainer.sharedInstance().framesTracker.start()
+        displayLinkWrapper.call()
+
+        let options = Options()
+        options.dsn = TestConstants.dsnAsString(username: "AppStartMeasurementHandlerTests")
+        options.tracesSampleRate = 1
+        let client = TestClient(options: options)
+        let hub = TestHub(client: client, andScope: Scope())
+        SentrySDKInternal.setCurrentHub(hub)
+        return hub
+    }
+
+    func testSendStandalone_ColdStart_AddsAppStartMeasurement() throws {
+        let hub = setUpIntegrationHub()
+        let measurement = createMeasurement(type: .cold, duration: 0.5)
+
+        SendStandaloneAppStartTransaction().handle(measurement)
+
+        let serialized = try XCTUnwrap(hub.capturedTransactionsWithScope.invocations.first?.transaction)
+        let measurements = try XCTUnwrap(serialized["measurements"] as? [String: Any])
+        let appStartCold = try XCTUnwrap(measurements["app_start_cold"] as? [String: Any])
+        XCTAssertEqual(appStartCold["value"] as? Double, 500)
+    }
+
+    func testSendStandalone_WarmStart_AddsAppStartMeasurement() throws {
+        let hub = setUpIntegrationHub()
+        let measurement = createMeasurement(type: .warm, duration: 0.3)
+
+        SendStandaloneAppStartTransaction().handle(measurement)
+
+        let serialized = try XCTUnwrap(hub.capturedTransactionsWithScope.invocations.first?.transaction)
+        let measurements = try XCTUnwrap(serialized["measurements"] as? [String: Any])
+        let appStartWarm = try XCTUnwrap(measurements["app_start_warm"] as? [String: Any])
+        XCTAssertEqual(appStartWarm["value"] as? Double, 300)
+    }
+
+    func testSendStandalone_AddsDebugMeta() throws {
+        let hub = setUpIntegrationHub()
+        let measurement = createMeasurement(type: .cold)
+
+        SendStandaloneAppStartTransaction().handle(measurement)
+
+        let serialized = try XCTUnwrap(hub.capturedTransactionsWithScope.invocations.first?.transaction)
+        let debugMeta = try XCTUnwrap(serialized["debug_meta"] as? [String: Any])
+        let images = try XCTUnwrap(debugMeta["images"] as? [[String: Any]])
+        XCTAssertFalse(images.isEmpty)
+    }
+
+    func testSendStandalone_SetsStartTimeToAppStartTimestamp() throws {
+        let hub = setUpIntegrationHub()
+        let measurement = createMeasurement(type: .cold)
+
+        SendStandaloneAppStartTransaction().handle(measurement)
+
+        let event = try XCTUnwrap(hub.capturedEventsWithScopes.invocations.first?.event)
+        let eventStartTimestamp = try XCTUnwrap(event.startTimestamp)
+        XCTAssertEqual(
+            eventStartTimestamp.timeIntervalSinceReferenceDate,
+            measurement.appStartTimestamp.timeIntervalSinceReferenceDate,
+            accuracy: 0.001
+        )
+    }
+
     // MARK: - StandaloneAppStartTransactionHelper
 
     func testHelper_ColdStartWithAutoOrigin_ReturnsTrue() {
