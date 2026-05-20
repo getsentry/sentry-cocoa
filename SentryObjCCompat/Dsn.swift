4ca9ce7c67af7eb68aff45a3e7b4105b35a82976
Noah Martin <noahm444@gmail.com>
2026-05-20 09:57:04 -0700

    fix

---
+++ SentryObjCCompat/Dsn.swift
@@ -0,0 +1,25 @@
+@_implementationOnly import Sentry
+import Foundation
+
+/// Parsed representation of a Sentry DSN.
+@objc(SentryCompatDsn)
+public final class Dsn: NSObject {
+    internal let wrapped: Sentry.SentryDsn
+
+    internal init(_ wrapped: Sentry.SentryDsn) {
+        self.wrapped = wrapped
+        super.init()
+    }
+
+    @objc public init?(string dsnString: String?, didFailWithError error: NSErrorPointer) {
+        guard let parsed = Sentry.SentryDsn(string: dsnString, didFailWithError: error) else {
+            return nil
+        }
+        self.wrapped = parsed
+        super.init()
+    }
+
+    @objc public var url: URL { wrapped.url }
+
+    @objc public func getEnvelopeEndpoint() -> URL { wrapped.getEnvelopeEndpoint() }
+}
