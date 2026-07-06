1ecd5de137486b08c132ad4b92baaa435b38fd3d
Denis Andrasec <denrase@gmail.com>
2026-07-06 10:35:10 +0200

    make span addFeatureFlag internal

--- Sources/Swift/Span.swift
+++ Sources/Swift/Span.swift
@@ -1,9 +1,8 @@
 // swiftlint:disable missing_docs
 @_implementationOnly import _SentryPrivate
 
-// Feature flag APIs live in this file so the public Span API has a clear home.
 extension Span {
-    public func addFeatureFlag(name: String, result: Bool) {
+    func addFeatureFlag(name: String, result: Bool) {
         guard let span = self as? SentrySpanInternal else {
             return
         }
