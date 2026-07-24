f6625b5fa766108bccc87182159a4f50318cea10
Itay Brenner <itay.brenner@sentry.io>
2026-07-24 17:17:19 -0300

    ci: add Craft target to release to sentry-apple-binaries
    Configure Craft's commit-on-git-repository target so each
    sentry-cocoa release automatically pushes an updated binary
    Package.swift to getsentry/sentry-apple-binaries and tags it.
    
    Closes #8479

--- Utils/VersionBump/main.swift
+++ Utils/VersionBump/main.swift
@@ -41,6 +41,7 @@ let files = [
     "./Package@swift-6.1.swift",
     "./Package@swift-6.2.swift",
     "./Sources/Sentry/SentryMeta.m",
+    "./distribution/apple-binaries/Package.swift",
     "./3rd-party-integrations/SentrySwiftLog/Package.swift",
     "./3rd-party-integrations/SentrySwiftyBeaver/Package.swift",
     "./3rd-party-integrations/SentryCocoaLumberjack/Package.swift",
@@ -193,7 +194,9 @@ func verifyRestrictedFile(_ file: String, expectedVersion: String) throws {
 }
 
 func getRegexString(for file: String) throws -> String {
-    if file.hasPrefix("./Package") && file.hasSuffix(".swift") {
+    if file.hasPrefix("./distribution/") && file.hasSuffix("/Package.swift") {
+        return "https:\\/\\/github\\.com\\/getsentry\\/sentry-cocoa\\/releases\\/download\\/(?<version>[a-zA-z0-9\\.\\-]+)\\/Sentry"
+    } else if file.hasPrefix("./Package") && file.hasSuffix(".swift") {
         return "https:\\/\\/github\\.com\\/getsentry\\/sentry-cocoa\\/releases\\/download\\/(?<version>[a-zA-z0-9\\.\\-]+)\\/Sentry"
     } else if file.hasPrefix("./3rd-party-integrations/") && file.hasSuffix("/Package.swift") {
         return "\\.package\\(url:\\s\"https:\\/\\/github\\.com\\/getsentry\\/sentry-cocoa\",\\sfrom:\\s\"(?<version>[a-zA-z0-9\\.\\-]+)\""
