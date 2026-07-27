3ba6174d9e2be064fdb3ac0008b605c184fff262
Itay Brenner <itay.brenner@sentry.io>
2026-07-27 11:32:31 -0300

    chore: track missing experimental features in telemetry (#8543)
    * feat: track missing experimental features in telemetry
    
    Add telemetry tracking for watchdogTerminationsV2,
    viewHierarchy, and screenshotFastViewRendering in
    SentryEnabledFeaturesBuilder.
    
    * fix: update feature flags for telemetry tracking
    
    Remove support for visionOS in feature flags for viewHierarchy and screenshotFastViewRendering in SentryEnabledFeaturesBuilder. This change ensures that only iOS and tvOS are considered for these features.

--- Sources/Swift/Helper/SentryEnabledFeaturesBuilder.swift
+++ Sources/Swift/Helper/SentryEnabledFeaturesBuilder.swift
@@ -55,6 +55,18 @@ import Foundation
         if options.experimental.enableStandaloneAppStartTracing {
             features.append("standaloneAppStartTracing")
         }
+        if options.experimental.enableWatchdogTerminationsV2 {
+            features.append("watchdogTerminationsV2")
+        }
+
+#if (os(iOS) || os(tvOS)) && !SENTRY_NO_UI_FRAMEWORK
+        if options.attachViewHierarchy {
+            features.append("viewHierarchy")
+        }
+        if options.screenshot.enableFastViewRendering {
+            features.append("screenshotFastViewRendering")
+        }
+#endif // (os(iOS) || os(tvOS)) && !SENTRY_NO_UI_FRAMEWORK
 
         return features
     }
