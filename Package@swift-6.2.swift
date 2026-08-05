e308c9fc110303414fa5b22ad8d8d1d49339f88d
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2026-08-05 16:40:32 +0200

    chore(kscrash): bump KSCrash to 2.6.0-beta.5

--- Package@swift-6.2.swift
+++ Package@swift-6.2.swift
@@ -203,7 +203,7 @@ targets += [
 ]
 // END:OBJC_WRAPPER
 
-let packageDependencies: [Package.Dependency] = enableKSCrash ? [.package(url: "https://github.com/kstenerud/KSCrash.git", from: "2.6.0-beta.3")] : []
+let packageDependencies: [Package.Dependency] = enableKSCrash ? [.package(url: "https://github.com/kstenerud/KSCrash.git", from: "2.6.0-beta.5")] : []
 
 let package = Package(
     name: "Sentry",
