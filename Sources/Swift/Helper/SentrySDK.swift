dba0b6420dbaf95e332d615c2b976bf7b5defc3a
Itay Brenner <itay.brenner@sentry.io>
2026-08-10 17:19:42 -0300

    Make sure documentation is available for v10 and non v10

--- Sources/Swift/Helper/SentrySDK.swift
+++ Sources/Swift/Helper/SentrySDK.swift
@@ -569,6 +569,7 @@ extension SentrySDK {
     // MARK: - Extended App Launch
 
     #if canImport(UIKit) && !SENTRY_NO_UI_FRAMEWORK && (os(iOS) || os(tvOS) || os(visionOS))
+    #if SDK_V10
     /// Extends the app launch measurement beyond the default end point.
     ///
     /// Call this method after `start(options:)` but before didFinishLaunching notification is posted
@@ -582,9 +583,6 @@ extension SentrySDK {
     /// func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     ///     SentrySDK.start(configureOptions: { options in
     ///         ...
-    #if !SDK_V10
-    ///         options.enableStandaloneAppStartTracing = true
-    #endif
     ///     })
     ///     SentrySDK.extendAppStart()
     ///
@@ -597,14 +595,43 @@ extension SentrySDK {
     ///     return true
     /// }
     /// ```
-    #if !SDK_V10
-    ///
-    /// - Note: This only has an effect when Standalone App Start tracing is enabled.
-    @objc
-    #endif
     public static func extendAppStart() {
         SentryDependencyContainer.sharedInstance().extendedAppLaunchManager.extend()
     }
+    #else
+    /// Extends the app launch measurement beyond the default end point.
+    ///
+    /// Call this method after `start(options:)` but before didFinishLaunching notification is posted
+    /// so the SDK doesn't finish the app start transaction automatically.
+    ///
+    /// Use ``getExtendedAppStartSpan()`` to retrieve the span and add child spans that break
+    /// down the extended launch period. Call `finish()` on that span (or call ``finishExtendedAppStart()``)
+    /// when the app is fully launched.
+    ///
+    /// ```swift
+    /// func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    ///     SentrySDK.start(configureOptions: { options in
+    ///         ...
+    ///         options.enableStandaloneAppStartTracing = true
+    ///     })
+    ///     SentrySDK.extendAppStart()
+    ///
+    ///     let appStartSpan = SentrySDK.getExtendedAppStartSpan()
+    ///     let configSpan = appStartSpan?.startChild(operation: "app.init", description: "fetch remote config")
+    ///     fetchRemoteConfig()
+    ///     configSpan?.finish()
+    ///
+    ///     appStartSpan?.finish()
+    ///     return true
+    /// }
+    /// ```
+    ///
+    /// - Note: This only has an effect when Standalone App Start tracing is enabled.
+    @objc
+    public static func extendAppStart() {
+        SentryDependencyContainer.sharedInstance().extendedAppLaunchManager.extend()
+    }
+    #endif
 
     /// Returns the extended app start span, or `nil` if ``extendAppStart()`` was not called,
     /// the SDK is not started, or the app start transaction was already created.
