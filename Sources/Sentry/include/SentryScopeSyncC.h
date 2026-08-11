0a6844e5b10ae53e009422541f97a1f37086d731
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2026-08-11 11:58:24 +0200

    chore: add nullability and swift exposure to SentryScopeSyncC APIs

--- Sources/Sentry/include/SentryScopeSyncC.h
+++ Sources/Sentry/include/SentryScopeSyncC.h
@@ -2,22 +2,22 @@
 #define SentryScopeSyncC_h
 
 typedef struct {
-    char *user;
-    char *dist;
-    char *context;
-    char *traceContext;
-    char *environment;
-    char *tags;
-    char *extras;
-    char *fingerprint;
-    char *level;
-    char **breadcrumbs; // dynamic array of char arrays
+    char *_Nullable user;
+    char *_Nullable dist;
+    char *_Nullable context;
+    char *_Nullable traceContext;
+    char *_Nullable environment;
+    char *_Nullable tags;
+    char *_Nullable extras;
+    char *_Nullable fingerprint;
+    char *_Nullable level;
+    char *_Nullable *_Nullable breadcrumbs; // dynamic array of char arrays
     long maxCrumbs;
     long currentCrumb;
 
 } SentryCrashScope;
 
-SentryCrashScope *sentrycrash_scopesync_getScope(void);
+SentryCrashScope *_Nonnull sentrycrash_scopesync_getScope(void);
 
 /**
  * Needs to be called before adding or clearing breadcrumbs to initialize the storage of the
@@ -25,25 +25,25 @@ SentryCrashScope *sentrycrash_scopesync_getScope(void);
  */
 void sentrycrash_scopesync_configureBreadcrumbs(long maxBreadcrumbs);
 
-void sentrycrash_scopesync_setUser(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_setUser(const char *_Nullable const jsonEncodedCString);
 
-void sentrycrash_scopesync_setDist(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_setDist(const char *_Nullable const jsonEncodedCString);
 
-void sentrycrash_scopesync_setContext(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_setContext(const char *_Nullable const jsonEncodedCString);
 
-void sentrycrash_scopesync_setTraceContext(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_setTraceContext(const char *_Nullable const jsonEncodedCString);
 
-void sentrycrash_scopesync_setEnvironment(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_setEnvironment(const char *_Nullable const jsonEncodedCString);
 
-void sentrycrash_scopesync_setTags(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_setTags(const char *_Nullable const jsonEncodedCString);
 
-void sentrycrash_scopesync_setExtras(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_setExtras(const char *_Nullable const jsonEncodedCString);
 
-void sentrycrash_scopesync_setFingerprint(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_setFingerprint(const char *_Nullable const jsonEncodedCString);
 
-void sentrycrash_scopesync_setLevel(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_setLevel(const char *_Nullable const jsonEncodedCString);
 
-void sentrycrash_scopesync_addBreadcrumb(const char *const jsonEncodedCString);
+void sentrycrash_scopesync_addBreadcrumb(const char *_Nullable const jsonEncodedCString);
 
 void sentrycrash_scopesync_clearBreadcrumbs(void);
 
