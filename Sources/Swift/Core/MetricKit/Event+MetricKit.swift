08b1d6ad3472f530ac346304bc6a47e9e60adb47
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2026-09-21 10:21:13 +0200

    ref(metrickit): Restore platform guard

--- Sources/Swift/Core/MetricKit/Event+MetricKit.swift
+++ Sources/Swift/Core/MetricKit/Event+MetricKit.swift
@@ -1,3 +1,4 @@
+#if os(iOS) || os(macOS) || os(visionOS)
 extension Event {
     // swiftlint:disable:next missing_docs
     @objc @_spi(Private) public func isMetricKitEvent() -> Bool {
@@ -8,3 +9,4 @@ extension Event {
         return SentryMXManager.Diagnostic.all.contains { $0.mechanism == mechanism.type }
     }
 }
+#endif
